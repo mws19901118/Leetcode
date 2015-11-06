a51745139c8fc5fc6d53434d0c057f7c4e3c771d
mws19901118 <mws19901118@gmail.com>
2015-11-06 03:39:01 -0600

    Create Ugly Number II.py

---
+++ Code/Ugly Number II.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def nthUglyNumber(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        ugly = [1]                              #1 is the 1st ugly number.
+        i2, i3, i5 = 0, 0, 0                    #i2, i3, i5 indicates the index of previous ugly number to generate new ugly number by multiplying 2, 3 and 5 respectively/
+        while len(ugly) < n:
+            next = 0
+            n2 = ugly[i2] * 2                   #Calculate the 3 new possible ugly number.
+            n3 = ugly[i3] * 3
+            n5 = ugly[i5] * 5
+            if n2 <= n3 and n2 <= n5:           #Find the smallest one and move forward the correspoding index by 1.
+                next = n2
+                i2 += 1
+            elif n3 <= n2 and n3 <= n5:
+                next = n3
+                i3 += 1
+            elif n5 <= n2 and n5 <= n3:
+                next = n5
+                i5 += 1
+            if next != ugly[-1]:                #If the new ugly number is not equals to the largest ugly number, append it to the array.
+                ugly.append(next)
+        return ugly[n - 1]                      #Return the ugly number we want.
