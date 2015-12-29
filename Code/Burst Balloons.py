8a6021ed22283fe7e1e119d67fe295fe33bf21ad
Junsheng Wang <mws19901118@gmail.com>
2015-12-29 03:41:56 -0600

    Create Burst Balloons.py

---
+++ Code/Burst Balloons.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def maxCoins(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        l = len(nums)
+        nums.insert(0, 1)                                 #Add the unreal nums[-1] in the beginning of nums.
+        nums.append(1)                                    #Add the unreal nums[n] in the end of nums.
+        l += 2                                            #Calculate the new length.
+        dp = [[0 for j in range(l)] for i in range(l)]    #Record the max sum of coins collected from nums[i] to nums[j](consider nums[i] and nums[j] unreal).
+        for i in range(2, l):                             #Examine every length of intevals from i to j; the minimum length is 2.
+            for j in range(0, l - i):                     #Examine every intevals of current length.
+                for k in range(j + 1, j + i):             #Check every possible balloon as the last one to burst. That means divide the intevals into to subintevals at the last bolloon to burst and calculate which partition will make the most coins.
+                    dp[j][j + i] = max(dp[j][j + i], nums[j]*nums[k]*nums[j + i] + dp[j][k] + dp[k][j + i])
+        return dp[0][l - 1]                               #Return the coins collected from nums[-1] to nums[n].
