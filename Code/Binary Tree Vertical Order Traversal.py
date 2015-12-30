74af74509f9242364c9cd4c976d22d58f34f302c
Junsheng Wang <mws19901118@gmail.com>
2015-12-29 20:26:18 -0600

    Update Binary Tree Vertical Order Traversal.py

--- Code/Binary Tree Vertical Order Traversal.py
+++ Code/Binary Tree Vertical Order Traversal.py
@@ -1,27 +1,14 @@
-# Definition for a binary tree node.
-# class TreeNode(object):
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
-import heapq
 class Solution(object):
-    def traverse(self, root, row, column, height, listdict, heapdict):                  #Traverse through the tree recursively.
-        if column not in listdict:                                                      #If current column hasn't occurred before, add a list of empty lists whose length is the height of the tree corresponding to the column in listdict.
-            listdict[column] = [[] for i in range(height)]                              #Also add a empty heap to heapdict.
-            heapdict[column] = []
-        if listdict[column][row] == []:                                                 #If current row hasn't occurred in current column before, push the row into heaplist[column].
-            heapq.heappush(heapdict[column], row)
-        listdict[column][row].append(root.val)                                          #Append the value of current node to the row-th list of listdict[column].
-        if root.left is not None:
-            self.traverse(root.left, row + 1, column - 1, height, listdict, heapdict)   #Recursively traverse the left child of current node if it is not none. The row of left child is row + 1, column is column - 1.
-        if root.right is not None:
-            self.traverse(root.right, row + 1, column + 1, height, listdict, heapdict)  #Recursively traverse the right child of current node if it is not none. The row of right child is row + 1, column is column + 1.
-            
-    def height(self, root):                                                             #Calculate the height of the tree.
+    def traverse(self, root, row, column, dict):                    #Traverse through the tree recursively.
         if root is None:
-            return 0
-        return max(self.height(root.left), self.height(root.right)) + 1
+            return
+        if column not in dict:                                      #If current column hasn't occurred before, add a new dict corresponding to current column.
+            dict[column] = {}
+        if row not in dict[column]:                                 #If current row hasn't occurred in current column, add a new list corresponding to current column and current row.
+            dict[column][row] = []
+        dict[column][row].append(root.val)                          #Append the value of root according to column and row.
+        self.traverse(root.left, row + 1, column - 1, dict)         #Recursively traverse the left child of current node; its row is row + 1; its column is column - 1.
+        self.traverse(root.right, row + 1, column + 1, dict)        #Recursively traverse the right child of current node; its row is row + 1; its column is column + 1.
         
     def verticalOrder(self, root):
         """
@@ -30,16 +17,16 @@ class Solution(object):
         """
         if root is None:
             return []
-        listdict = {}                                                                   #Store the value correspoding to the column.
-        heapdict = {}                                                                   #Store which rows in current column have value in ascending order.
-        h = self.height(root)                                                           #Precompute the height of the tree.
-        self.traverse(root, 0, 0, h, listdict, heapdict)                                #Traverse: root as column 0, row 0.
+        dict = {}                                                   #Store value according to column and row.
+        self.traverse(root, 0, 0, dict)                             #Traverse starting from root as column 0, row 0.
         result = []
-        start = min(listdict.keys())                                                    #Get the min value of column.
-        end = max(listdict.keys())                                                      #Get the max value of column.
-        for i in range(start, end + 1):                                                 #Check every column from left to right.
+        startc = min(dict.keys())                                   #Get the min value of column
+        endc = max(dict.keys())                                     #Get the max value of column
+        for i in range(startc, endc + 1):                           #Check every column.
             result.append([])
-            while heapdict[i] != []:                                                    #Pop each row that containing values from heap until heap is empty.
-                row = heapq.heappop(heapdict[i])
-                result[i - start].extend(listdict[i][row])                              #Add the list of value of the certain column and row to result of current column.
+            startr = min(dict[i].keys())                            #Get the min value of row in current column.
+            endr = max(dict[i].keys())                              #Get the max value of row in current column.
+            for j in range(startr, endr + 1):
+                if j in dict[i]:
+                    result[i - startc].extend(dict[i][j])           #Add the list of value of the certain column and row to the end of result of current column.
         return result
