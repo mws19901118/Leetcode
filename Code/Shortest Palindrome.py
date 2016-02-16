e9cc9bd6b431d9bf5d4f2c2d525a7bc941d39b40
Junsheng Wang <mws19901118@gmail.com>
2016-02-16 17:32:06 -0600

    Update Shortest Palindrome.py

--- Code/Shortest Palindrome.py
+++ Code/Shortest Palindrome.py
@@ -1,8 +1,24 @@
-class Solution:
-    # @param {string} s
-    # @return {string}
+class Solution(object):
     def shortestPalindrome(self, s):
-        r = s[::-1]                               #Reverse the string.
-        for i in range(len(s) + 1):
-            if s.startswith(r[i:]):               #If s starts with r[i:], there is a palindrome, and it's the longest one inside s starting with the first character of s.
-                return(r[:i] + s)                 #Form a palindrome according to the problem description and return it.
+        """
+        :type s: str
+        :rtype: str
+        """
+        m = '#' + '#'.join(list(s)) + '#'                                       #Construct a new string so that each letter of s is between 2 '#'s in the new string.
+        l = len(m)                                                              #Get the new length.
+        id = 0                                                                  #Record the index of midpoint of current longest palindrome.
+        maxReach = 0                                                            #Record the farest letter could be reached by current longest palindrome.
+        p = [0] * l                                                             #Record the max span length of palindrome at letter m[i] in new string.
+        for i in xrange(l):                                                     #Use Manacher's Algorithm to find the longest palindrome substring in m.
+            if maxReach > i:
+                p[i] = min(maxReach - i, p[id * 2 - i])
+            else:
+                p[i] = 1
+            while i - p[i] >= 0 and i + p[i] < l and m[i - p[i]] == m[i + p[i]]:
+                p[i] += 1
+            if i + p[i] > maxReach:
+                maxReach = i + p[i]
+                id = i
+        for i in xrange(l - 1, -1, -1):                                         #Get the longest palindrome starting from the beginning.
+            if p[i] == i + 1:
+                return s[p[i] - 1:][::-1] + s                                   #Reverse the rest part and add it before then beginning.
