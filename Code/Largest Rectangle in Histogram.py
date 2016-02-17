179b6fa286dac2df6f48b021bd2906aeed5f333f
Junsheng Wang <mws19901118@gmail.com>
2016-02-17 14:18:58 -0600

    Update Largest Rectangle in Histogram.py

--- Code/Largest Rectangle in Histogram.py
+++ Code/Largest Rectangle in Histogram.py
@@ -3,9 +3,9 @@ class Solution:
     # @return an integer
     def largestRectangleArea(self, height):
         height.append(0)                                        #Append 0 in the end to deal with the situation that the sequence of height is a non-descending sequence.
-        stack = [0]                                             #Store the indexes of asending heights; pop stack when encounter a height smaller than the height of index on the top of stack until the heights stored in stack is ascending.
+        stack = []                                              #Store the indexes of asending heights; pop stack when encounter a height smaller than the height of index on the top of stack until the heights stored in stack is ascending.
         r = 0
-        for i in range(1, len(height)):
+        for i in range(len(height)):
             while stack and height[i] < height[stack[-1]]:      #While the height of index on the top of stack is larger than current height, keep pop the stack until the stack is empty.
                 h = height[stack.pop()]                         #All the heights which are stored in the stack(not the top of stack) are smaller than height of index on the top of stack. Thus, let it be not only the left boundry(if stack is not empty) but also the height of current rectangle.
                 w = i if not stack else i - stack[-1] -1        #If stack is empty, width is index of current height; otherwise, width is the distance between current height and the index on the top of stack.
