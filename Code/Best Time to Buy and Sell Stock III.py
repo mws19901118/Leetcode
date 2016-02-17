8620f79ca8b52797e97f5e7d5435481295b2998e
Junsheng Wang <mws19901118@gmail.com>
2016-02-16 20:04:52 -0600

    Update Best Time to Buy and Sell Stock III.py

--- Code/Best Time to Buy and Sell Stock III.py
+++ Code/Best Time to Buy and Sell Stock III.py
@@ -2,25 +2,23 @@ class Solution:
     # @param prices, a list of integer
     # @return an integer
     def maxProfit(self, prices):
-        def max1profit(prices):
-          length=len(prices)
-          if length==0 or length==1:
-              return 0
-          maxprofit=0
-          lowprice=prices[0]
-          for i in range(length):
-              lowprice=min(lowprice,prices[i]) #maintain the lowest price
-              maxprofit=max(prices[i]-lowprice,maxprofit) #if current profit>maxprofit, set maxprofit to current profit
-          return maxprofit
+        maxprofit=0
+        lowprice=0x80000000
+        p1 = [0]*len(prices)
+        for i in range(len(prices)):
+            lowprice=min(lowprice,prices[i])      
+            maxprofit=max(prices[i]-lowprice,maxprofit)                 
+            p1[i] = maxprofit                                   #Calculate the maxprofit for 1 transition from beginning to prices[i].
+            
+        maxprofit=0
+        maxprice=-1
+        p2 = [0]*len(prices)
+        for i in range(len(prices) - 1, -1, -1):
+            maxprice=max(maxprice,prices[i])      
+            maxprofit=max(maxprice-prices[i],maxprofit)                 
+            p2[i] = maxprofit                                   #Calculate the maxprofit for 1 transition from prices[i] to end.
         
-        length=len(prices)
-        if length==0 or length==1:
-            return 0
         maxp=0
-        for i in range(length):
-            if i==length-1 or (i>0 and i<length-1 and prices[i]>=prices[i+1] and prices[i]>prices[i-1]):      #break the list into 2 parts at the peaks or the end
-                temp1=max1profit(prices[:i+1])                                                                #calculate the former part max profit
-                temp2=max1profit(prices[i:])                                                                  #calculate the latter part max profit
-                if temp1+temp2>maxp:                                                                          #dynamic programming for total max profit
-                    maxp=temp1+temp2
+        for i in range(len(prices)):
+            maxp = max(p1[i] + p2[i], maxp)                     #Calculate the maxprofit for 2 transitions, seperated at prices[i].
         return maxp
