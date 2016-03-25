0a92dca9d16337c1d55d942f66837bf3059452a9
Junsheng Wang <mws19901118@gmail.com>
2016-03-24 19:12:50 -0500

    Update Binary Tree Maximum Path Sum.py

--- Code/Binary Tree Maximum Path Sum.py
+++ Code/Binary Tree Maximum Path Sum.py
@@ -1,35 +1,32 @@
-# Definition for a  binary tree node
-# class TreeNode:
+# Definition for a binary tree node.
+# class TreeNode(object):
 #     def __init__(self, x):
 #         self.val = x
 #         self.left = None
 #         self.right = None
 
-class Solution:
-    # @param root, a tree node
-    # @return an integer
+class Solution(object):
+    def find(self, root):                                               #Get the max path sum of current subtree and the max sum along the path from current node to leaf.
+        if root.left is None and root.right is None:                    #If current node is leaf node, both the result is value of current node.
+            return (root.val, root.val)
+        elif root.left is not None and root.right is None:              #If current node only has left child, get the results from its left child.
+            l = self.find(root.left)
+            t = max(root.val, root.val + l[1])                          #The max sum along the path from current node to leaf is the max of current value and the sum of current value and that value of left child.
+            return (max(l[0], t), t)                                    #The max path sum of current node is the max of that of left child and the max sum along the path from current node to leaf.
+        elif root.left is None and root.right is not None:              #If current node only has right child, get the results from its right child.
+            r = self.find(root.right)  
+            t = max(root.val, root.val + r[1])                          #The max sum along the path from current node to leaf is the max of current value and the sum of current value and that value of right child.
+            return (max(r[0], t), t)                                    #The max path sum of current node is the max of that of right child and the max sum along the path from current node to leaf.
+        else:                                                           #If current has both child, get the results from them respectively.                         
+            l = self.find(root.left)
+            r = self.find(root.right)
+            t = max(root.val, root.val + l[1], root.val + r[1])         #The max sum along the path from current node to leaf is the max of current value, the sum of current value and that value of left child and the sum of current value and that value of right child. 
+            return (max(l[0], r[0], t, root.val + l[1] + r[1]), t)      #The max path sum of current node is the max of that of left child, that of right child and the max sum along the path from current node to leaf and the sum of current value and the max sum along the path from left child to leaf and the max sum along the path from right child to leaf.
     def maxPathSum(self, root):
-        dict={}
-        def maxSum(node):                         #return the larger one of max possible sum of left branch and max possible sum of right branch
-            if node==None:
-                return 0
-            if node not in dict:
-                numl=maxSum(node.left)            #numl is the max path sum of the tree whose root is node.left
-                numr=maxSum(node.right)           #numr is the max path sum of the tree whose root is node.right
-                numw=check(node.val,numl+numr,msum)     #check whether numl+numr>0
-                sumLeft=check(node.val,numl,msum)       #the max possible sum of left branch 
-                sumRight=check(node.val,numr,msum)      #the max possible sum of right branch
-                dict[node]=max(sumLeft,sumRight)        
-            return dict[node]
-        def check(value,sum,msum):                #check whether current sum is larger than max sum
-            if sum>0:
-                sum+=value
-            else:
-                sum=value
-            if sum>msum[-1]:
-                msum.append(sum)
-            return sum
-                    
-        msum=[root.val]                           #'msum' is used to store the max path sum, initialed with 'root.val'
-        maxSum(root)                                    
-        return msum[-1]
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        if root is None:
+            return 0
+        return self.find(root)[0]
