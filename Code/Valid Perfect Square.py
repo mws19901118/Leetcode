3f9dc03c7b22032bf84faacb9afb49d51c9c4807
Junsheng Wang <mws19901118@gmail.com>
2018-12-24 22:27:11 -0800

    Update Valid Perfect Square.py

--- Code/Valid Perfect Square.py
+++ Code/Valid Perfect Square.py
@@ -4,17 +4,15 @@ class Solution:
         :type num: int
         :rtype: bool
         """
-        if num == 1:                        #If num is 1, return true.
-            return True
         start = 0
         end = num
-        while start + 1 < end:              #Binary search from 0 to num. If it reaches 2 adjacent integers, stop binary search, because mid will always stuck in the middle.
+        while start <= end:                     #Binary search from 0 to n.
             mid = int((start + end) / 2)
-            square = mid * mid              #Calculate mid and square of mid.
-            if square == num:               #If square equals num, num is perfact square, return true.
+            square = mid * mid
+            if square == num:
                 return True
             elif square < num:
-                start = mid
+                start = mid + 1
             else:
-                end = mid
-        return False                        #Return false if binary search finishes.
+                end = mid - 1
+        return False
