c6cff15bf55c1fdbaddbafad6c9416b4d356c1bb
Junsheng Wang <mws19901118@gmail.com>
2019-01-24 23:33:27 -0800

    Create Maximize Distance to Closest Person.py

---
+++ Code/Maximize Distance to Closest Person.py
@@ -0,0 +1,26 @@
+class Solution(object):
+    def maxDistToClosest(self, seats):
+        """
+        :type seats: List[int]
+        :rtype: int
+        """
+        lastPerson = 0                                        #Index of last person.
+        index = 0                                             #Index pointer.
+        
+        leftEnd = 0
+        while index < len(seats):
+            if seats[index] == 1:
+                leftEnd = index - lastPerson                  #Find the distance between first person and left end.
+                lastPerson = index                            #Update index of last person.
+                break
+            index += 1
+        
+        maxDist = 0
+        while index < len(seats):
+            if seats[index] == 1:
+                maxDist = max(maxDist, index - lastPerson)    #Find the max distance between 2 adjacent people.
+                lastPerson = index                            #Update index of last person.
+            index += 1
+        
+        rightEnd = len(seats) - 1 - lastPerson                #Find the distance between first person and right end.
+        return max(leftEnd, rightEnd, maxDist / 2)            #Return the max of leftEnd, rightEnd and half of maxDist.
