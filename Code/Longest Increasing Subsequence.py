d6ede44bbd742d08e785d2906fa5cec48fc89dca
Junsheng Wang <mws19901118@gmail.com>
2019-03-10 15:14:47 -0700

    Update Longest Increasing Subsequence.py

--- Code/Longest Increasing Subsequence.py
+++ Code/Longest Increasing Subsequence.py
@@ -1,25 +1,18 @@
 class Solution(object):
-    def lengthOfLIS(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
-        if nums == []:
-            return 0
-        l = len(nums)
-        leastendelement = [nums[0]]                                 #The ith element stores the smallest ending element for increasing subsequence whose length is i.
-        for i in range(1, l):
-            if nums[i] > leastendelement[-1]:
-                leastendelement.append(nums[i])                     #If current element is greater than the ending element of current longest increasing subsequence, append current element to leastendelement.
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        leastendelement = []                                        #The ith element stores the smallest ending element for increasing subsequence whose length is i.
+        for x in nums:
+            if leastendelement == [] or x > leastendelement[-1]:
+                leastendelement.append(x)                           #If current element is greater than the ending element of current longest increasing subsequence, append current element to leastendelement.
             else:                                                   #Otherwise, binary search the smallest number in the least that is greater than current element and replace it with current element.
                 start = 0
                 end = len(leastendelement)
                 while start < end:
-                    mid = (start + end) / 2
-                    if leastendelement[mid] >= nums[i] and (mid == 0 or leastendelement[mid - 1] < nums[i]):
-                        leastendelement[mid] = nums[i]
+                    mid = (start + end) // 2
+                    if leastendelement[mid] >= x and (mid == 0 or leastendelement[mid - 1] < x):
+                        leastendelement[mid] = x
                         break
-                    elif leastendelement[mid] < nums[i]:
+                    elif leastendelement[mid] < x:
                         start = mid + 1
                     else:
                         end = mid - 1
