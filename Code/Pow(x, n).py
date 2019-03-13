d8500677b57fa5b35caf8b1dbd7a54301132dc92
Junsheng Wang <mws19901118@gmail.com>
2019-03-12 19:46:11 -0700

    Update Pow(x, n).py

--- Code/Pow(x, n).py
+++ Code/Pow(x, n).py
@@ -1,35 +1,14 @@
 class Solution:
-    # @param {float} x
-    # @param {integer} n
-    # @return {float}
-    def myPow(self, x, n):
-        nIsNegative = False
-        if n < 0:                                               #If n is negative, let n be its absolute value.
-            nIsNegative = True
-            n = -n
-        value = [x]                                             #value[i]=x^(2^power[i])
-        power = [1]
-        while power[-1] < n:
-            value.append(value[-1]*value[-1])
-            power.append(power[-1]*2)
+    def myPow(self, x: float, n: int) -> float:
+        b = format(abs(n), '0b')                                #Convert the abs valie of n to binary.
+        p = [1, x]                                              #Use a list to store the powers of x. Initially, x^0 and x^1.
+        while len(p) <= len(b):                                 #While the length of p is not larger than that of b, keep expanding p so the new power is the square of previous power.
+            p.append(p[-1] * p[-1])
         
-        result = 1
-        length = len(power) - 1                                 #Record the ending position for binary search.
-        while n > 0:
-            start = 0
-            end = length
-            while start < end:                                  #Find the largest power[i] which is smaller than n using binary search.
-                mid = (start + end) / 2
-                if power[mid] > n:
-                    end = mid - 1
-                elif power[mid + 1] <= n:
-                    start = mid + 1
-                else:
-                    start = mid
-                    break
-            n -= power[start]                                   #Update result and n.
-            result *= value[start]
-            length = start - 1
-        if nIsNegative == True:                                 #If n is negative, let result be its reciprocal.
-            result= 1 / result
+        result = 1                                              #Initalize result to be 1.
+        for i in range(len(b) - 1, -1, -1):                     #Traverse through b.
+            if b[i] == '1':                                     #If current bit is '1', multiply result with its corrsponding power(notice the order in p).
+                result *= p[len(b) - i]
+        if n < 0:                                               #If n is negative, let result be its reciprocal.
+            result = 1 / result
         return result
