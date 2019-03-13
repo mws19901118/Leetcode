0a704f73c4bd4f89cf15f1494a656863ca45e445
Junsheng Wang <mws19901118@gmail.com>
2019-03-12 18:31:03 -0700

    Update Search in a Sorted Array of Unknown Size.py

--- Code/Search in a Sorted Array of Unknown Size.py
+++ Code/Search in a Sorted Array of Unknown Size.py
@@ -5,9 +5,10 @@ class Solution:
         :type target: int
         :rtype: int
         """
-        start, end = 0, 1                                                   #Initialize a sliding window [0, 1].
+        end = 1                                                             #Initialize a sliding window [0, 1].
         while reader.get(end) != 2147483647 and reader.get(end) < target:   #If the value at end is not 2147483647 or is smaller than target, target is not in current window.
-            start, end = end, end * 2                                       #Slide window to right and double the size.
+            end = end << 1                                                  #Slide window to right and double the size.
+        start = end >> 1
         while start <= end:                                                 #Binary search target in sliding window.
             mid = (start + end) >> 1
             temp = reader.get(mid)
