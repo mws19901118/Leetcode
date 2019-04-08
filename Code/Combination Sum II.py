68f1296549bc8bca7211d4e13463922e10c7e454
Junsheng Wang <mws19901118@gmail.com>
2019-04-07 21:54:42 -0700

    Update Combination Sum II.py

--- Code/Combination Sum II.py
+++ Code/Combination Sum II.py
@@ -1,28 +1,18 @@
+import copy
 class Solution:
-    def backtrack(self, candidates, list, sum, target, ans):
-        if sum==target:
-            temp=[]
-            for i in list:
-                temp.append(i)                                              #If temporary sum equals target, copy list to temp.
-            ans.append(temp)
-        elif sum<target:
-            l=len(candidates)
-            for i in range(l):
-                if i!=0 and candidates[i]==candidates[i-1]:                 #Eliminate duplication.
+    def backtracking(self, candidates, trace, currentSum, target, result):
+        if currentSum == target:
+            result.append(copy.deepcopy(trace))                                                             #Deepcopy trace to result.
+        elif currentSum < target:
+            for i in range(len(candidates)):
+                if i != 0 and candidates[i] == candidates[i - 1]:                                           #Eliminate duplication.
                     continue
-                list.append(candidates[i])
-                sum+=candidates[i]
-                self.backtrack(candidates[i+1:], list, sum, target, ans)    #Because elements in combination should be non-descending and only appear once, use candidates[i+1:] in the next step.
-                list.pop()
-                sum-=candidates[i]
-    # @param {integer[]} candidates
-    # @param {integer} target
-    # @return {integer[][]}
-    def combinationSum2(self, candidates, target):
-        candidates.sort()                                                   #Sort the candidates.
-        sum=0                                                               #Record temporary sum.
-        list=[]                                                             #Record the possible combination.                                                    
-        ans=[]
-        self.backtrack(candidates, list, sum, target, ans)                  #Backtrack.
-        ans.sort()
-        return ans
+                trace.append(candidates[i])
+                self.backtracking(candidates[i + 1:], trace, currentSum + candidates[i], target, result)    #Because elements in combination should be non-descending and only appear once, use candidates[i + 1:] in the next step.
+                trace.pop()
+            
+    def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
+        candidates.sort()                                                                                   #Sort the candidates.
+        result = []
+        self.backtracking(candidates, [], 0, target, result)                                                #Backtracking
+        return result
