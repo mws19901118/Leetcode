b955677507ebdca17da1248a5cac1ba3fee8e605
Junsheng Wang <mws19901118@gmail.com>
2019-04-21 14:59:27 -0700

    Update Smallest Rectangle Enclosing Black Pixels.py

--- Code/Smallest Rectangle Enclosing Black Pixels.py
+++ Code/Smallest Rectangle Enclosing Black Pixels.py
@@ -1,76 +1,43 @@
-class Solution(object):
-    def minArea(self, image, x, y):
-        """
-        :type image: List[List[str]]
-        :type x: int
-        :type y: int
-        :rtype: int
-        """
-        m = len(image)
-        if m == 0:
+class Solution:
+    def minArea(self, image: List[List[str]], x: int, y: int) -> int:
+        if not image or not image[0]:
             return 0
-        n = len(image[0])
-        if n == 0:
-            return 0
-        
-        start = 0                                   #Binary search in 4 directions for the rectangle edge.
-        end = x
+        m, n = len(image), len(image[0])
+
+        start, end = 0, x                                                       #Binary search in 4 directions for the rectangle edge.
         while start <= end:
-            mid = (start + end) / 2
-            black = False
-            for i in range(n):
-                if image[mid][i] == '1':
-                    black = True
-                    break
-            if black is True:
-                end = mid - 1
-            else:
+            mid = (start + end) // 2
+            if all(image[mid][i] == '0' for i in range(n)):
                 start = mid + 1
+            else:
+                end = mid - 1
         up = start
 
-        start = x
-        end = m - 1
+        start, end = x, m - 1
         while start <= end:
-            mid = (start + end) / 2
-            black = False
-            for i in range(n):
-                if image[mid][i] == '1':
-                    black = True
-                    break
-            if black is True:
-                start = mid + 1
-            else:
+            mid = (start + end) // 2
+            if all(image[mid][i] == '0' for i in range(n)):
                 end = mid - 1
+            else:
+                start = mid + 1
         down = end
 
-        start = 0
-        end = y
+        start, end = 0, y
         while start <= end:
-            mid = (start + end) / 2
-            black = False
-            for i in range(up, down + 1):         #After found the horizontal bound, we can narrow down the scope when finding the vertical bound.
-                if image[i][mid] == '1':
-                    black = True
-                    break
-            if black is True:
-                end = mid - 1
-            else:
+            mid = (start + end) // 2
+            if all(image[i][mid] == '0' for i in range(up, down + 1)):          #After found the horizontal bound, we can narrow down the scope when finding the vertical bound.
                 start = mid + 1
+            else:
+                end = mid - 1
         left = start
 
-        start = y
-        end = n - 1
+        start, end = y, n - 1
         while start <= end:
-            mid = (start + end) / 2
-            black = False
-            for i in range(up, down + 1):
-                if image[i][mid] == '1':
-                    black = True
-                    break
-            if black is True:
-                start = mid + 1
-            else:
+            mid = (start + end) // 2
+            if all(image[i][mid] == '0' for i in range(up, down + 1)):
                 end = mid - 1
+            else:
+                start = mid + 1
         right = end
 
         return (down - up + 1) * (right - left + 1)
