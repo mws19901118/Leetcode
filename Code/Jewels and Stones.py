2a6c58f7892c5bef04fffc8874acd3dcabf6e328
Junsheng Wang <junsheng.wang@uipath.com>
2020-05-02 00:47:09 -0700

    Update Jewels and Stones.py

--- Code/Jewels and Stones.py
+++ Code/Jewels and Stones.py
@@ -1,8 +1,3 @@
 class Solution:
     def numJewelsInStones(self, J: str, S: str) -> int:
-        jewel = set([x for x in J])
-        count = 0
-        for x in S:
-            if x in jewel:
-                count += 1
-        return count
+        return sum(map(lambda s: s in set(J), S))
