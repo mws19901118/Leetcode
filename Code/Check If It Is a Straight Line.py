138fdf2947796599a1b8a74f21fb5d09655ddf95
Junsheng Wang <mws19901118@gmail.com>
2020-05-08 12:04:46 -0700

    Create Check If It Is a Straight Line.py

---
+++ Code/Check If It Is a Straight Line.py
@@ -0,0 +1,4 @@
+class Solution:
+    def checkStraightLine(self, coordinates: List[List[int]]) -> bool:
+        vector = (coordinates[1][0] - coordinates[0][0], coordinates[1][1] - coordinates[0][1])                               #Calculate the vector from first point to second point.
+        return all((c[0] - coordinates[0][0]) * vector[1] == (c[1] - coordinates[0][1]) * vector[0] for c in coordinates)     #Each vector from the first point to any other point should be parallel with the above vector.
