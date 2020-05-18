33e247a3ab333299e7723f2ec70f86311938a78a
Junsheng Wang <mws19901118@gmail.com>
2020-05-18 12:31:20 -0700

    Update Find All Anagrams in a String.py

--- Code/Find All Anagrams in a String.py
+++ Code/Find All Anagrams in a String.py
@@ -10,8 +10,8 @@ class Solution:
             diff[ord(s[i]) - ord('a')] -= 1                           #Update diff and count everytime the running window has a new letter.
             count -= diff[ord(s[i]) - ord('a')] == 0
             if count == 0:                                            #If no not matched letter between running window and p, an anagram is found, so append the current starting index of running window to result list. 
-                result.append(i - len(p) + 1)                         #Pop current starting index out of running window if its length is len(p) and update diff and count if necessary.
-            if i >= len(p) - 1:
+                result.append(i - len(p) + 1)                         
+            if i >= len(p) - 1:                                       #Pop current starting index out of running window if its length is len(p) and update diff and count if necessary.
                 count += diff[ord(s[i - len(p) + 1]) - ord('a')] == 0
                 diff[ord(s[i - len(p) + 1]) - ord('a')] += 1
         return result
