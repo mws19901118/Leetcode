f186a2b975f8b1610d5a6f59d5153177a60801e9
Junsheng Wang <mws19901118@gmail.com>
2020-05-27 22:46:54 -0700

    Create Possible Bipartition.py

---
+++ Code/Possible Bipartition.py
@@ -0,0 +1,15 @@
+from collections import defaultdict
+class Solution:
+    def DFS(self, graph: defaultdict, color: dict, index: int, currentColor: int) -> bool:
+        if index in color:                                                                      #If current person is already colored, check if its color is the same as current color.
+            return color[index] == currentColor
+        color[index] = currentColor                                                             #Color current person.
+        return all(self.DFS(graph, color, i, 1 ^ currentColor) for i in graph[index])           #Color all neighbor with opposite color.
+    
+    def possibleBipartition(self, N: int, dislikes: List[List[int]]) -> bool:
+        graph = defaultdict(list)                                                               #Construct a graph.
+        for x in dislikes:
+            graph[x[0]].append(x[1])
+            graph[x[1]].append(x[0])
+        color = {}
+        return all(self.DFS(graph, color, i, 0) for i in range(1, N + 1) if i not in color)     For each connected component, we can check whether it is bipartite by just trying to coloring it with two colors.
