3e04863710db3f1a2d877e501d0788785e0dfe8a
Junsheng Wang <mws19901118@gmail.com>
2020-06-22 11:26:12 -0700

    Update Single Number II.py

--- Code/Single Number II.py
+++ Code/Single Number II.py
@@ -1,13 +1,6 @@
 class Solution:
-    # @param A, a list of integer
-    # @return an integer
-    def singleNumber(self, A):
-        dict={}
-        for i in xrange(0,len(A)):
-            if dict.has_key(A[i]):
-                dict[A[i]]+=1
-            else:
-                dict[A[i]]=1
-        for num,count in dict.items():
-            if count!=3:
-                return num
+    def singleNumber(self, nums: List[int]) -> int:
+        ones, twos = 0, 0                                                               #ones ^ num performs the same XOR operation you see in the duplicate entries version of Single Number.
+        for num in nums:                                                                #& ~twos removes the newly added number if it's been seen twice.
+            ones, twos = (ones ^ num) & ~twos, (ones & num) | (twos & ~num)             #ones & num takes the number if it's in ones.
+        return ones                                                                     #twos & ~num takes the number only if it's not in twos.
