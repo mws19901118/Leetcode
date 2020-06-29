ebe10b459d3fa112efdf7f1a0e3c83500fc5b2e9
Junsheng Wang <mws19901118@gmail.com>
2020-06-29 09:53:43 -0700

    Update Unique Paths.py

--- Code/Unique Paths.py
+++ Code/Unique Paths.py
@@ -1,15 +1,7 @@
 class Solution:
-    # @return an integer
-    def uniquePaths(self, m, n):                                              #Simple dynamic programming
-        if m==0 or n==0:
-            return 0
-        minSum=[[0 for i in range(n) ] for j in range(m)]
-        minSum[0][0]=1
-        for i in range(1,m):
-            minSum[i][0]=1
-        for i in range(1,n):
-            minSum[0][i]=1
-        for i in range(1,m):
-            for j in range(1,n):
-                minSum[i][j]=minSum[i-1][j]+minSum[i][j-1]
-        return minSum[m-1][n-1]
+    def uniquePaths(self, m: int, n: int) -> int:
+        last = [1 for i in range(n)]                    #Record the unique paths of each grid in last row. It's all 1 for first row.
+        for i in range(1, m):
+            for j in range(1, n):                       #For each row, current paths eqauls the sum of paths from left grid and upper grid.
+                last[j] += last[j - 1]
+        return last[n - 1]
