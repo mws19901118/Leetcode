162253aaf7790ea66e42f9569cb203f038518899
Junsheng Wang <mws19901118@gmail.com>
2020-07-01 10:19:50 -0700

    Update Arranging Coins.py

--- Code/Arranging Coins.py
+++ Code/Arranging Coins.py
@@ -1,4 +1,3 @@
 class Solution:
     def arrangeCoins(self, n: int) -> int:                                  #x * (x + 1) // 2 + k = n, 0 <= k <= x + 1.
-        return (int)(sqrt(2 * n + 0.25) - 0.5)                              #(x + 0.5) ^ 2 - 0.25 + k = n.
-                                                                            #x = int(sqrt(2 * n + 0.25) - 0.5)
+        return (int)(sqrt(2 * n + 0.25) - 0.5)                              #(x + 0.5) ^ 2 - 0.25 + k = n. x = int(sqrt(2 * n + 0.25) - 0.5)
