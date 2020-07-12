8aa8070f680cdc5dbf480aeb04ebd439a01520c3
Junsheng Wang <mws19901118@gmail.com>
2020-07-12 15:09:21 -0700

    Update Reverse Bits.py

--- Code/Reverse Bits.py
+++ Code/Reverse Bits.py
@@ -1,12 +1,3 @@
 class Solution:
-    # @param n, an integer
-    # @return an integer
-    def reverseBits(self, n):
-        b=bin(n)                            #Transfrom decimal number to binary number(as a string).
-        ans=""                              
-        for i in range(32-len(b)+2):        #Eliminate the first 2 characters "0b" and add leading zeros to make the whole number 32 bits.
-            ans=ans+"0"
-        ans=ans+b[2:]
-        ans=ans[::-1]                       #Awesome code to reverse the string.
-        ans=int(ans,2)                      #Transform binary number(as a string) to decimal number.
-        return ans
+    def reverseBits(self, n: int) -> int:
+        return int(format(n, 'b').zfill(32)[::-1], 2)  #Convert int to binary, fill leading 0 to 32 bits, reverse string and convert back to integer.
