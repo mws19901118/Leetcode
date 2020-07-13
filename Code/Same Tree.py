703adaac45d656eaf339e7ef9488a442b09f1e38
Junsheng Wang <mws19901118@gmail.com>
2020-07-13 10:13:28 -0700

    Update Same Tree.py

--- Code/Same Tree.py
+++ Code/Same Tree.py
@@ -1,22 +1,16 @@
-# Definition for a  binary tree node
+# Definition for a binary tree node.
 # class TreeNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
-
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
 class Solution:
-    # @param p, a tree node
-    # @param q, a tree node
-    # @return a boolean
-    def isSameTree(self, p, q):
-        if p==None and q==None:                               #If p and q are both empty, they are same.
+    def isSameTree(self, p: TreeNode, q: TreeNode) -> bool:
+        if p is None and q is None:                                                                                     #If p and q are both empty, they are same.
             return True
-        elif (p==None and q!=None) or (p!=None and q==None):  #If one is empty but the other is not, they are not same.
+        elif (p is not None and q is None) or (p is None and q is not None):                                            #If one is empty but the other is not, they are not same.
             return False
-        else:
-            if p.val!=q.val:                                  #If the value of p is not equal to the value of q, they are not same.
-                return False
-            else:
-                if self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right):       #If the left child of p and the left child of q are same, and the right child of p and right child of q are same, the value of p is equal to the value of q, they are same.
-                    return False
+        elif p.val != q.val:                                                                                            #If the value of p is not equal to the value of q, they are not same.
+            return False
+        else:                                                                                                           #If the left child of p and the left child of q are same, and the right child of p and right child of q are same, the value of p is equal to the value of q, they are same.
+            return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
