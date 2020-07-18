1f68807cbd435047f53aa42eb8c23813365b1584
Junsheng Wang <mws19901118@gmail.com>
2020-07-18 13:03:53 -0700

    Update Course Schedule II.py

--- Code/Course Schedule II.py
+++ Code/Course Schedule II.py
@@ -1,32 +1,17 @@
 class Solution:
-    # @param {integer} numCourses
-    # @param {integer[][]} prerequisites
-    # @return {integer[]}
-    def findOrder(self, numCourses, prerequisites):             #Use the same topological sort used in Course Schedule I.
-        if prerequisites==[]:
-            return [i for i in range(numCourses)]
-        graph=[[] for i in range(numCourses)]
-        indgree=[0 for i in range(numCourses)]
-        order=[]                                                #Record the order of courses.
-        for i in prerequisites:
-            graph[i[1]].append(i[0])
-            indgree[i[0]]+=1
-            
-        stack=[]
-        for i in range(numCourses):
-            if indgree[i]==0:
-                stack.append(i)
-                
-        while stack!=[]:
-            node=stack.pop()
-            order.append(node)                                  #If all the prerequisites of current course are taken, append current course to order.
-            while graph[node]!=[]:
-                temp=graph[node].pop()
-                indgree[temp]-=1
-                if indgree[temp]==0:
-                    stack.append(temp)
-                    
-        if len(order)<numCourses:                               #If the length of order of courses is less than the number of courses, there is a cycle in the direct graph. Thus, return false. 
-            return []
-        else:
-            return order
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        inDegree = [0 for i in range(numCourses)]                                                       #Record the count of unfufilled prerequisites of each course.
+        edges = [[] for i in range(numCourses)]                                                         #Record the courses that current course is their prerequisite.
+        for p in prerequisites:                                                                         #Build graph.
+            inDegree[p[0]] += 1
+            edges[p[1]].append(p[0])
+        stack = [i for i in range(numCourses) if inDegree[i] == 0]                                      #Inialize stack with the courses which have no prerequisite.
+        order = []                                                                                      #Store the course order.
+        while stack:                                                                                    #DFS.
+            x = stack.pop()                                                                             #Take the last course in stack and add it to order.
+            order.append(x)
+            for y in edges[x]:                                                                          #Update the unfufilled prerequisites.
+                inDegree[y] -= 1                                                                        #If no unfufilled prerequisites, add course to stack.
+                if inDegree[y] == 0:
+                    stack.append(y)
+        return [] if len(order) < numCourses else order                                                 #If the length of order is smaller than number of courses, there are cycles, so return empty list. Otherwise return order directly.
