db9f105f3bc957282b0372265b07f6d9155d38f4
Junsheng Wang <mws19901118@gmail.com>
2020-07-25 23:28:28 -0700

    Update Find Minimum in Rotated Sorted Array II.py

--- Code/Find Minimum in Rotated Sorted Array II.py
+++ Code/Find Minimum in Rotated Sorted Array II.py
@@ -2,17 +2,13 @@ class Solution:
     # @param num, a list of integer
     # @return an integer
     def findMin(self, num):
-        if(len(num)==1):
-            return num[0];
-        else:
-            start=0;
-            end=len(num)-1;
-            while(start<end and num[start]>=num[end]): #Binary search
-                middle=int((end+start)/2);
-                if(num[middle]>num[start]):
-                    start=middle+1
-                elif(num[middle]<num[start]):
-                    end=middle
-                else:                                 #Can not discard either branch. Thus, it goes O(n) in such case.
-                    start=start+1
-            return num[start];
+        start, end = 0, len(nums) - 1
+        while start < end and nums[start] >= nums[end]:     #Binary search. If nums[start] < nums[end], it's already asending from start to end, stop binary search.
+            mid = (end + start) // 2
+            if nums[mid] > nums[start]:                     #If nums[mid] > nums[start], min element is on the right of mid.
+                start = mid + 1
+            elif nums[mid] < nums[start]:                   #if nums[mid] < nums[start], min element is either mid or on the left of mid.
+                end = mid
+            else:                                           #Can not discard either branch. Thus, it goes O(n) in such case.
+                start += 1
+        return nums[start]
