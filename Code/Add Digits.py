9b3c6d5f69a7a4dbbd80b08002face83d7e259e1
Junsheng Wang <mws19901118@gmail.com>
2020-07-26 14:32:40 -0700

    Update Add Digits.py

--- Code/Add Digits.py
+++ Code/Add Digits.py
@@ -1,9 +1,3 @@
-class Solution(object):
-    def addDigits(self, num):
-        """
-        :type num: int
-        :rtype: int
-        """
-        if num == 0:                    #If num is 0, return 0.
-            return 0
-        return 1 + (num - 1) % 9        #Otherwise, return the digital root of num(1 + (num - 1) % 9).
+class Solution:
+    def addDigits(self, num: int) -> int:
+        return 1 + (num - 1) % 9 if num else 0
