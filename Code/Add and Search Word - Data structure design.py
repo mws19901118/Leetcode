a70d6279d090583871353aac29e5c7d36e0cff1f
Junsheng Wang <mws19901118@gmail.com>
2020-08-05 14:38:16 -0700

    Update Add and Search Word - Data structure design.py

--- Code/Add and Search Word - Data structure design.py
+++ Code/Add and Search Word - Data structure design.py
@@ -1,27 +1,38 @@
-import re                                                             #Import regular expression.
 class WordDictionary:
-    
-    def __init__(self):
-        self.list=[]                                                  #Use list to store words.
 
-    # @param {string} word
-    # @return {void}
-    # Adds a word into the data structure.
-    def addWord(self, word):
-        if not word in self.list:                                     #If word is not in list, append it to list.
-            self.list.append(word)
+    def __init__(self):                                                                                                             #Essentially, it's a Trie.
+        """
+        Initialize your data structure here.
+        """
+        self.letters = {}
+        self.hasWord = False
+        
+    def addWord(self, word: str) -> None:                                                                                           #Add word to Trie.
+        """
+        Adds a word into the data structure.
+        """
+        if word == "":
+            self.hasWord = True
+            return
+        if word[0] not in self.letters:
+            self.letters[word[0]] = WordDictionary()
+        self.letters[word[0]].addWord(word[1:])
 
-    # @param {string} word
-    # @return {boolean}
-    # Returns if the word is in the data structure. A word could
-    # contain the dot character '.' to represent any one letter.
-    def search(self, word):
-        for i in self.list:
-            if len(i)==len(word) and re.match(word,i):                #If there is an element in list which has the same length with word and match the regular expression of word, return true; otherwise, return false.
-                return True
-        return False
+    def search(self, word: str) -> bool:                                                                                            #Search word in Trie.
+        """
+        Returns if the word is in the data structure. A word could contain the dot character '.' to represent any one letter.
+        """
+        if word == "":
+            return self.hasWord
+        if word[0] != '.':
+            if word[0] not in self.letters:
+                return False
+            return self.letters[word[0]].search(word[1:])
+        else:                                                                                                                       #If the first letter is '.', traverse all its children.
+            return any(self.letters[x].search(word[1:]) for x in self.letters)
+
 
 # Your WordDictionary object will be instantiated and called as such:
-# wordDictionary = WordDictionary()
-# wordDictionary.addWord("word")
-# wordDictionary.search("pattern")
+# obj = WordDictionary()
+# obj.addWord(word)
+# param_2 = obj.search(word)
