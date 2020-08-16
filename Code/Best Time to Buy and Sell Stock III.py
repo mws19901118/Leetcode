a54ce1e00686b7f9cea65bfc5009457feeabce31
Junsheng Wang <mws19901118@gmail.com>
2020-08-16 12:16:30 -0700

    Update Best Time to Buy and Sell Stock III.py

--- Code/Best Time to Buy and Sell Stock III.py
+++ Code/Best Time to Buy and Sell Stock III.py
@@ -1,24 +1,21 @@
 class Solution:
-    # @param prices, a list of integer
-    # @return an integer
-    def maxProfit(self, prices):
-        maxprofit=0
-        lowprice=0x80000000
-        p1 = [0]*len(prices)
-        for i in range(len(prices)):
-            lowprice=min(lowprice,prices[i])      
-            maxprofit=max(prices[i]-lowprice,maxprofit)                 
-            p1[i] = maxprofit                                   #Calculate the maxprofit for 1 transition from beginning to prices[i].
+    def maxProfit(self, prices: List[int]) -> int:
+        maxprofit, lowprice = 0, 0x80000000
+        p1 = [0] * len(prices)
+        for i in range(len(prices)):                                    #Calculate the maxprofit for 1 transition from beginning to prices[i].
+            lowprice = min(lowprice, prices[i])      
+            maxprofit = max(prices[i] - lowprice, maxprofit)                 
+            p1[i] = maxprofit
             
-        maxprofit=0
-        maxprice=-1
-        p2 = [0]*len(prices)
+        maxprofit, maxprice = 0, -1                                     #Calculate the maxprofit for 1 transition from prices[i] to end.
+        p2 = [0] * len(prices)
         for i in range(len(prices) - 1, -1, -1):
-            maxprice=max(maxprice,prices[i])      
-            maxprofit=max(maxprice-prices[i],maxprofit)                 
-            p2[i] = maxprofit                                   #Calculate the maxprofit for 1 transition from prices[i] to end.
+            maxprice = max(maxprice, prices[i])      
+            maxprofit = max(maxprice - prices[i], maxprofit)                 
+            p2[i] = maxprofit
         
-        maxp=0
-        for i in range(len(prices)):
-            maxp = max(p1[i] + p2[i], maxp)                     #Calculate the maxprofit for 2 transitions, seperated at prices[i].
+        maxp = 0
+        for i in range(len(prices)):                                    #Calculate the maxprofit for 2 transitions, seperated at prices[i].
+            maxp = max(p1[i] + p2[i], maxp)
         return maxp
+    
