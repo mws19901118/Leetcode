a37b84102c19d16cbab8ab62ab733ffaf537c25a
Junsheng Wang <mws19901118@gmail.com>
2020-08-23 12:28:19 -0700

    Create Stream of Characters.py

---
+++ Code/Stream of Characters.py
@@ -0,0 +1,36 @@
+class Trie:                                                             #Trie.
+    def __init__(self):
+        self.letters = {}
+        self.hasWord = False
+    
+    def addWord(self, word: str) -> None:
+        if word == "":
+            self.hasWord = True
+            return
+        if word[-1] not in self.letters:                                #Instead of ordering by prefix, ordering by suffix.
+            self.letters[word[-1]] = Trie()
+        self.letters[word[-1]].addWord(word[:-1])
+        
+class StreamChecker:
+
+    def __init__(self, words: List[str]):
+        self.history = []
+        self.trie = Trie()                                              #Initialize trie.
+        for w in words:                                                 #Add words to trie.
+            self.trie.addWord(w)
+        
+    def query(self, letter: str) -> bool:
+        t = self.trie                                                   #Get the root of trie.
+        self.history.append(letter)                                     #Append current letter to history.
+        for x in reversed(self.history):                                #Search for word.
+            if t.hasWord:                                               #If current trie node has word, return true.
+                return True
+            if x not in t.letters:                                      #If current character not in the dictionary of current trie node, return false.
+                return False
+            t = t.letters[x]                                            #Go to next level.
+        return t.hasWord                                                #Return if final trie node has word.
+
+
+# Your StreamChecker object will be instantiated and called as such:
+# obj = StreamChecker(words)
+# param_1 = obj.query(letter)
