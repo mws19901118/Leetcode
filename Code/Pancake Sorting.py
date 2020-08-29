38c230aba1f9c64342af1dc51e58e8038ff05c9a
Junsheng Wang <mws19901118@gmail.com>
2020-08-29 14:06:02 -0700

    Update Pancake Sorting.py

--- Code/Pancake Sorting.py
+++ Code/Pancake Sorting.py
@@ -1,20 +1,10 @@
-class Solution(object):
-    def pancakeSort(self, A):
-        """
-        :type A: List[int]
-        :rtype: List[int]
-        """
+class Solution:
+    def pancakeSort(self, A: List[int]) -> List[int]:
         result = []
-        for i in range(len(A)):
-            index = A.index(i + 1, 0, len(A) - i)                               #Find the index of (i + 1) in A.
-            if index == len(A) - 1 - i:                                         #If it's already the last (i + 1)th element in A, then the subarray starting from (i + 1) is already sorted from largest to smallest.
-                continue                                                        #We don't need to do pancake flip. Continue.
-            result.append(index + 1)                                            #Pancake flip the elements from the beginning to the index of (i + 1).
-            for j in range((index + 1) / 2):                                    #After the flip, (i + 1) is the first element of A.
-                A[j], A[index - j] = A[index - j], A[j]
-            result.append(len(A) - i)                                           #Pancake flip the elements from the beginning to the last (i + 1)th element.
-            for j in range((len(A) - i) / 2):                                   #After the flip, the subarray starting from (i + 1) is already sorted from largest to smallest.
-                A[j], A[len(A) - 1 - i - j] = A[len(A) - 1 - i - j], A[j]
-        
-        result.append(len(A))                                                   #In the end, pancake flip the entire list, so it turns from sorted from largest to smallest to sorted from smallest to largest.
+        for i in range(len(A), 0, -1):                                              #Traverse from len(A) to 1.
+            index = A.index(i)                                                      #Find the index of i in A.
+            result.append(index + 1)                                                #Pancake flip the elements from the beginning to the index.
+            A = A[:index + 1][::-1] + A[index + 1:]
+            result.append(i)                                                        #Pancake flip the elements from the beginning to i.
+            A = A[:i][::-1] + A[i:]
         return result
