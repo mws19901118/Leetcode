77cc54b576cfb9617ef1e904646c4b0e33cbf715
Junsheng Wang <mws19901118@gmail.com>
2020-09-10 00:14:05 -0700

    Update Bulls and Cows.py

--- Code/Bulls and Cows.py
+++ Code/Bulls and Cows.py
@@ -1,28 +1,13 @@
-class Solution(object):
-    def getHint(self, secret, guess):
-        """
-        :type secret: str
-        :type guess: str
-        :rtype: str
-        """
-        l = len(secret)
-        filtered = ""                             #Store guess string after filtered all bulls.
-        dict = {}                                 #Store the number of occurrence of characters of secret except for bulls.
-        bull = 0
-        cow = 0
-        for i in range(l):
-            if secret[i] == guess[i]:             #Calculate bulls.
+class Solution:
+    def getHint(self, secret: str, guess: str) -> str:
+        count = Counter(secret)                                 #Count each number in secret.
+        bull, cow = 0, 0
+        for i, x in enumerate(guess):                           #First pass find all bulls and update count.
+            if x == secret[i]:
                 bull += 1
-            else:
-                if secret[i] not in dict:         #Update dict.
-                    dict[secret[i]] = 1
-                else:
-                    dict[secret[i]] += 1
-                filtered += guess[i]              #Update filtered.
-        for c in filtered:                        #For every characters in filtered, check if it is in dict.
-            if c in dict:
+                count[x] -= 1
+        for i, x in enumerate(guess):                           #Second pass find all cows(does not equal to corresponding number in secret and count not equal to 0) and update count.
+            if x != secret[i] and count[x] != 0:
                 cow += 1
-                dict[c] -= 1
-                if dict[c] == 0:                  #Remove current character from dict if its occurrence is 0.
-                    del dict[c]
-        return str(bull) + 'A' + str(cow) + 'B'
+                count[x] -= 1
+        return str(bull) + "A" + str(cow) + "B"
