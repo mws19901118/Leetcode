ba6b611bacdb2c5d2cdbe1c51fd2293fa4677d88
Junsheng Wang <mws19901118@gmail.com>
2020-09-25 00:34:22 -0700

    Update Largest Number.py

--- Code/Largest Number.py
+++ Code/Largest Number.py
@@ -1,17 +1,7 @@
 class Solution:
-    # @param num, a list of integers
-    # @return a string
-    def largestNumber(self, num):
-        def mycmp(n1,n2):
-            return int(str(n2)+str(n1))-int(str(n1)+str(n2))            #My compare function: if the value of "n1"+"n2" is greater than that of "n2"+"n1", n1 should be in front of n2; vice versa.
-        num.sort(cmp=mycmp)                                             #Sort the array using my compare function.
-        ans=""
-        flag=False                                                      #It indicates that the numbers before current number are all 0 when it is false.
-        for i in num:
-            if flag==False and i!=0:
-                flag=True
-            if flag==True:                                              #If flag is true, convert current number to string and concatenate it with ans.
-                ans+=str(i)
-        if ans=="":                                                     #If all the numbers are 0, ans is an empty string, set it to "0"
-            ans="0"
-        return ans
+    def largestNumber(self, nums: List[int]) -> str:
+        def cmp(a: str, b: str) -> int:                                             #Compare function: if the value of int(a + b)  is greater than that of int(b + a), a should be in front of b; vice versa.
+            return int(b + a) - int(a + b)
+
+        s = "".join(sorted([str(x) for x in nums], key=cmp_to_key(cmp)))            #Covert number to string, sort by compare function, then join together.
+        return str(int(s))                                                          #Remove leading 0 and return.
