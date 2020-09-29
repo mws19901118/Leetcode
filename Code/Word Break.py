b8c1554a74826415b946804e0a7cc4c89d1ff33b
Junsheng Wang <mws19901118@gmail.com>
2020-09-29 00:26:01 -0700

    Update Word Break.py

--- Code/Word Break.py
+++ Code/Word Break.py
@@ -1,16 +1,11 @@
 class Solution:
-    # @param s, a string
-    # @param dict, a set of string
-    # @return a boolean
-    def wordBreak(self, s, dict):
-        cache = {"":True}                                               #buffer strings which have been processed
-        def WB(s):
-            if s in cache:
-                return cache[s]
-            for i in xrange(len(s)):                                    #judge whether the first part of string is in the dict and whether the other part is made up of strings in the dict成
-                if s[:i+1] in dict and WB(s[i+1:])==True:
-                    cache[s]=True                                       #process buffer
-                    return True
-            cache[s]=False                                              #provess buffer
-            return False
-        return WB(s)                                                    #solve the problem in a recursion method
+    def DFS(self, s: str, wordDict: List[str], cache: dict) -> bool:
+        if s == "":                                                                                     #If s is empty, return true.
+            return True
+        if s in cache:                                                                                  #If s in cache, return its value.
+            return cache[s]
+        cache[s] = any(s[:len(w)] == w and self.DFS(s[len(w):], wordDict, cache) for w in wordDict)     #DFS to find if there is any valid word break and update cache.
+        return cache[s]
+    
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        return self.DFS(s, wordDict, {})                                                                #DFS with cache.
