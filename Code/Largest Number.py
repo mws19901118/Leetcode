8d772fea747bdf5892a3d81374766229447fb251
Junsheng Wang <mws19901118@gmail.com>
2020-10-04 00:39:25 -0700

    Update Largest Number.py

--- Code/Largest Number.py
+++ Code/Largest Number.py
@@ -3,5 +3,5 @@ class Solution:
         def cmp(a: str, b: str) -> int:                                             #Compare function: if the value of int(a + b)  is greater than that of int(b + a), a should be in front of b; vice versa.
             return int(b + a) - int(a + b)
 
-        s = "".join(sorted([str(x) for x in nums], key=cmp_to_key(cmp)))            #Covert number to string, sort by compare function, then join together.
+        s = "".join(sorted([str(x) for x in nums], key = cmp_to_key(cmp)))          #Covert number to string, sort by compare function, then join together.
         return str(int(s))                                                          #Remove leading 0 and return.
