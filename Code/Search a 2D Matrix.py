a29f0c541ae8a95b64d1d816dda8a5264f00c6b2
Junsheng Wang <mws19901118@gmail.com>
2020-10-16 21:31:08 -0700

    Update Search a 2D Matrix.py

--- Code/Search a 2D Matrix.py
+++ Code/Search a 2D Matrix.py
@@ -1,23 +1,25 @@
 class Solution:
-    # @param {integer[][]} matrix
-    # @param {integer} target
-    # @return {boolean}
-    def searchMatrix(self, matrix, target):
-        m = len(matrix)
-        if m == 0:
-            return False
-        n = len(matrix[0])
-        if n == 0:
-            return False
-        if target<matrix[0][0] or target>matrix[m - 1][n - 1]:      #If target is smaller than the first element or greater than the last element, return false.
-            return False
-        i = 0
-        j = n - 1                                                   #Begin search from the last element in the first row.
-        while i < m and j >= 0:
-            if matrix[i][j] == target:                              #If find the target, return true.
-                return True
-            elif matrix[i][j] > target:                             #If current element is smaller than target, target must in the left side of current column.
-                j -= 1
-            else:                                                   #If current element is greater than target, target must in the lower side of current row.
-                i += 1
-        return False
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        if not matrix or not matrix[0]:                                                             #Check if matrix is empty or matrix row is empty.
+            return False                                                                            #If so, return false.
+        m, n = len(matrix), len(matrix[0])                                                          #Get the dimensions of matrix.
+        start, end, index = 0, m, 0
+        while start <= end:                                                                         #Binary search in the first column to find the last row that is not greater than target.
+            mid = (start + end) // 2
+            if matrix[mid][0] <= target and (mid >= m - 1 or matrix[mid + 1][0] > target):
+                index = mid                                                                         #Get the row index of the row which target could belong to.
+                break
+            elif matrix[mid][0] > target:
+                end = mid - 1
+            else:
+                start = mid + 1
+        start, end = 0, n - 1
+        while start <= end:                                                                         #Binary search in that row to find target.
+            mid = (start + end) // 2
+            if matrix[index][mid] == target:
+                return True                                                                         #If found, return true.
+            elif matrix[index][mid] > target:
+                end = mid - 1
+            else:
+                start = mid + 1
+        return False                                                                                #Otherwise return false.
