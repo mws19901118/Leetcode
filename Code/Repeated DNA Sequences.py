f257303abfe810bf5646888b45bce692295907e7
Junsheng Wang <mws19901118@gmail.com>
2020-10-17 00:48:51 -0700

    Update Repeated DNA Sequences.py

--- Code/Repeated DNA Sequences.py
+++ Code/Repeated DNA Sequences.py
@@ -1,16 +1,6 @@
 class Solution:
-    # @param s, a string
-    # @return a list of strings
-    def findRepeatedDnaSequences(self, s):
-        l=len(s)
-        dict={}                               #Record the number of occurence of each sequence.
-        result=[]
-        for i in range(l-9):
-            temp=s[i:i+10]
-            if dict.has_key(temp):
-                dict[temp]+=1                 #Count the occurence.
-                if dict[temp]==2:             #If the number of occurence is greanter than 1, append the sequence to result.
-                    result.append(temp)
-            else:
-                dict[temp]=1
-        return result
+    def findRepeatedDnaSequences(self, s: str) -> List[str]:
+        count = defaultdict(int)
+        for i in range(len(s) - 9):                         #Count sequences.
+            count[s[i:i+10]] += 1
+        return [x for x in count if count[x] > 1]           #Return sequences whose count is larger than 1.
