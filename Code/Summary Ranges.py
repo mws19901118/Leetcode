0a4d426d133781967c7bcb2fb51e14b323d8e148
Junsheng Wang <mws19901118@gmail.com>
2020-10-28 10:14:55 -0700

    Update Summary Ranges.py

--- Code/Summary Ranges.py
+++ Code/Summary Ranges.py
@@ -1,19 +1,11 @@
 class Solution:
-    # @param {integer[]} nums
-    # @return {string[]}
-    def summaryRanges(self, nums):
-        l=len(nums)
-        result=[]
-        if l==0:
-            return result
-        i=0
-        while i<l:
-            j=1
-            while i+j<l and nums[i+j]==nums[i+j-1]+1:                     #While nums[j] in the range beginning at nums[i], j+=1.
-                j+=1
-            if j==1:
-                result.append(str(nums[i]))
-            else:
-                result.append(str(nums[i])+"->"+str(nums[i+j-1]))
-            i+=j
+    def summaryRanges(self, nums: List[int]) -> List[str]:
+        result = []
+        i = 0
+        while i < len(nums):
+            j = i + 1
+            while j < len(nums) and nums[j] == nums[j - 1] + 1:                                             #While nums[j] in the range beginning at nums[i], j += 1.
+                j += 1
+            result.append(str(nums[i]) + "->" + str(nums[j - 1]) if j > i + 1 else str(nums[i]))            #Add range to result.
+            i = j
         return result
