a79b26f9fed22ae903c0cc2abf3a7b14329b888c
Junsheng Wang <mws19901118@gmail.com>
2020-10-27 21:31:00 -0700

    Update Linked List Cycle II.py

--- Code/Linked List Cycle II.py
+++ Code/Linked List Cycle II.py
@@ -5,48 +5,34 @@
 #         self.next = None
 
 class Solution:
-    # @param head, a ListNode
-    # @return a list node
-    def detectCycle(self, head):
-        if head==None:
-            return None
-        fast=head
-        slow=head
-        cyclenode=None
-        while fast.next!=None and fast.next.next!=None:
-            fast=fast.next.next
-            slow=slow.next
-            if slow==fast:                                #judge whether the list has cycle
-                cyclenode=slow
+    def detectCycle(self, head: ListNode) -> ListNode:
+        if not head:                                                            #If head is none, return none.
+            return head
+        fast, slow = head, head
+        hasCycle = False
+        count = 0
+        while fast.next is not None and fast.next.next is not None:             #Use fast and slow pointers to determine if there is cycle. Also count the steps from head to where fast and slow pointers meet.
+            fast = fast.next.next
+            slow = slow.next
+            count += 1
+            if fast == slow:
+                hasCycle = True
                 break
-        if cyclenode==None:
+        if not hasCycle:                                                        #If no cycle, return none.
             return None
+        length = 0                                                              #Count the length of cycle.
+        while True:
+            fast = fast.next
+            length += 1
+            if fast == slow:
+                break
+        if count >= length:                                                     #Imagine the loop is cut at the meet point. The linked list then becomes Y shape. One start is head, the other is the next of meeting point and end at meeting point.
+            for i in range(count - length):                                     #Next, it becomes how to find the intersection of Y shape linked list with the lengths of 2 linked list are known.
+                head = head.next
         else:
-            newhead=cyclenode.next                        #cut the list into a 'Y' shape one and the 'Y' node is the node where the cycle begins
-            cyclenode.next=None
-            len1=0                                        #count the distance between 'Y' node and bifurcation 1
-            temp1=head
-            while temp1!=cyclenode:
-                len1+=1
-                temp1=temp1.next
-            len2=0
-            temp2=newhead                                 #count the distance between 'Y' node and bifurcation 2
-            while temp2!=cyclenode:
-                len2+=1
-                temp2=temp2.next
-            temp1=head
-            temp2=newhead
-            if len1>=len2:                                #the longer bifurcation move forword |len1-len2| times
-                t=len1-len2
-                while t!=0:
-                    temp1=temp1.next
-                    t-=1
-            else:
-                t=len2-len1
-                while t!=0:
-                    temp2=temp2.next
-                    t-=1
-            while temp1!=temp2:                           #the 'Y' node is the first node where temp1=temp2
-                temp1=temp1.next
-                temp2=temp2.next
-            return temp1
+            for i in range(length - count):
+                fast = fast.next
+        while fast != head:
+            fast = fast.next
+            head = head.next
+        return head
