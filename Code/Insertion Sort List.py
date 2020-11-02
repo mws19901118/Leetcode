e698a545b97d6638c37f611b5c52c481b6434c8e
Junsheng Wang <mws19901118@gmail.com>
2020-11-02 11:28:27 -0800

    Update Insertion Sort List.py

--- Code/Insertion Sort List.py
+++ Code/Insertion Sort List.py
@@ -1,37 +1,29 @@
 # Definition for singly-linked list.
 # class ListNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.next = None
-
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
 class Solution:
-    # @param head, a ListNode
-    # @return a ListNode
-    def insertionSortList(self, head):
-        if head==None:
+    def insertionSortList(self, head: ListNode) -> ListNode:
+        if not head:
             return head
-        result=ListNode(head.val)
-        tail=result                           #result is the head of new list
-        current=head.next
-        while current!=None:
-            temp1=ListNode(current.val)
-            if temp1.val<=result.val:         #current value is smaller than or equal to head value, temp1 becomes the new head.
-                temp1.next=result
-                result=temp1
+        result = ListNode(head.val)
+        tail = result                                                   #Result is the head of new list
+        head = head.next
+        while head:
+            node = ListNode(head.val)
+            if node.val <= result.val:                                  #If current value is smaller than or equal to result value, node becomes the new head.
+                node.next = result
+                result = node
+            elif tail.val < node.val:                                   #If current value is larger than tail value, node becomes the new tail.
+                tail.next = node
+                node.next = None
+                tail = node
             else:
-                if tail.val<temp1.val:        #current value is larger than tail value, temp1 becomes the new tail.
-                    tail.next=temp1
-                    temp1.next=None
-                    tail=temp1
-                else:                 
-                    temp2=result
-                    while temp2.next!=None:   #find the correct place of current value
-                        temp3=temp2.next
-                        if temp3.val<temp1.val:
-                            temp2=temp3
-                        else:                 #insert
-                            temp2.next=temp1
-                            temp1.next=temp3
-                            break
-            current=current.next
+                curr = result
+                while curr.next and curr.next.val < node.val:           #Find the correct place of current value
+                    curr = curr.next
+                node.next = curr.next                                   #Insert.
+                curr.next = node
+            head = head.next
         return result
