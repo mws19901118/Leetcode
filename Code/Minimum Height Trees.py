55567f8c189671b693275aec0a8ff846aaab1d25
Junsheng Wang <mws19901118@gmail.com>
2020-11-04 10:50:44 -0800

    Update Minimum Height Trees.py

--- Code/Minimum Height Trees.py
+++ Code/Minimum Height Trees.py
@@ -1,31 +1,21 @@
-class Solution(object):
-    def findMinHeightTrees(self, n, edges):
-        """
-        :type n: int
-        :type edges: List[List[int]]
-        :rtype: List[int]
-        """
-        neighbor = [[] for i in range(n)]
-        degree = [0] * n
-        remain = set()
-        for i in range(n):                  #Store the remaining nodes.
-            remain.add(i)
-        for e in edges:                     #Construct the adjacency list and count degrees.
-            neighbor[e[0]].append(e[1])
-            neighbor[e[1]].append(e[0])
-            degree[e[0]] += 1
-            degree[e[1]] += 1
-        q = []
-        for i in range(n):                  #Find all the nodes whose degree is 1.
-            if degree[i] == 1:
-                q.append(i)
-        while len(remain) > 2:              #BFS until no more than 2 nodes left.
-            newq = []
-            for start in q:                 #Store the nodes in next level.
-                remain.remove(start)        #Remove current node from the remaining set.
-                for end in neighbor[start]: #Update degree of nodes which connect current node.
-                    degree[end] -= 1
-                    if degree[end] == 1:    #If the degree is 1, append it to newq.
-                        newq.append(end)
-            q = newq
-        return list(remain)                 #Return the list of remaining set.
+class Solution:
+    def findMinHeightTrees(self, n: int, edges: List[List[int]]) -> List[int]:
+        degree = [0 for i in range(n)]                                                              #Store the degree of each node.
+        adjacentList = [[] for i in range(n)]                                                       #Store the adjacent list of each node.
+        for s, e in edges:                                                                          #Construct degree and adjacent list.
+            degree[s] += 1
+            degree[e] += 1
+            adjacentList[s].append(e)
+            adjacentList[e].append(s)
+        q = set([i for i in range(n) if degree[i] <= 1])                                            #Find all the nodes whose degree is not larger than 1. 
+        result = set()
+        while q:                                                                                    #Start BFS.
+            newq = set()
+            for x in q:                                                                             #For each node in queue, traverse all its neighbor in adjacent list.
+                for y in adjacentList[x]:
+                    degree[y] -= 1                                                                  #Minus 1 to the neighbor's degree.
+                    if degree[y] == 1:                                                              #If neighbor's degree is 1, add it to the new queue.
+                        newq.add(y)
+            result = q                                                                              #Always store current queue in result.
+            q = newq                                                                                #Replace current queue with new queue.
+        return list(result)                                                                         #Return result after converting it to list.
