78652882ce52ae71eaaf821c4ce4a416e09779f9
Junsheng Wang <mws19901118@gmail.com>
2020-11-13 00:25:10 -0800

    Update Populating Next Right Pointers in Each Node.py

--- Code/Populating Next Right Pointers in Each Node.py
+++ Code/Populating Next Right Pointers in Each Node.py
@@ -1,19 +1,20 @@
-# Definition for a  binary tree node
-# class TreeNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
-#         self.next = None
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val: int = 0, left: 'Node' = None, right: 'Node' = None, next: 'Node' = None):
+        self.val = val
+        self.left = left
+        self.right = right
+        self.next = next
+"""
 
 class Solution:
-    # @param root, a tree node
-    # @return nothing
-    def connect(self, root):
-        if root==None or root.left==None:              #If reach the bottom, return.
-            return
-        root.left.next=root.right                      #Next pointer of left child of current node points to right child of current node.
-        if root.next!=None:                            #Because it's a perfect binary tree, if current node isn't the rightmost node of current level,
-            root.right.next=root.next.left             #current node must have right child and next node must have left child and the former node's next node is the latter node.
-        self.connect(root.left)                        #recursion
-        self.connect(root.right)
+    def connect(self, root: 'Node') -> 'Node':
+        if root is None or root.left is None:                   #If reach the bottom, return root.
+            return root
+        root.left.next = root.right                             #Next pointer of left child of current node points to right child of current node.
+        if root.next is not None:                               #Because it's a perfect binary tree, if current node isn't the rightmost node of current level,
+            root.right.next = root.next.left                    #Current node must have right child and next node must have left child and the former node's next node is the latter node.
+        root.left = self.connect(root.left)                     #Recursively connect left subtree.
+        root.right = self.connect(root.right)                   #Recursively connect right subtree.
+        return root
