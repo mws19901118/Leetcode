d1fa8f621031103d47d2913521a88e082820d837
Junsheng Wang <mws19901118@gmail.com>
2020-11-12 20:56:50 -0800

    Update Permutations II.py

--- Code/Permutations II.py
+++ Code/Permutations II.py
@@ -1,14 +1,11 @@
 class Solution:
-    def perm(self, nums, start):                        #Return a set of tuples, representing the unique permutations of the numbers which appear after (including) the index "start".
-        if start == len(nums)-1:                        #If start is the last element of nums, return it in tuple.
-            return {(nums[start],)}
+    def backtracking(self, nums: List[int], start: int) -> set:         #Return a set of tuples, representing the unique permutations of the numbers which appear after (including) the index "start".
+        if start == len(nums) - 1:                                      #If start is the last element of nums, return it in tuple.
+            return {(nums[start], )}
         s = set()
-        for r in self.perm(nums, start+1):
-            for i in range(len(nums)-start):
-                s.add(r[:i]+(nums[start],)+r[i:])       #Generate new permutations by inserting the current element into different positions of r.
+        for r in self.backtracking(nums, start + 1):                    #Generate new permutations by inserting the current element into different positions of r.
+            for i in range(len(r) + 1):
+                s.add(r[:i] + (nums[start], ) + r[i:])                  
         return s
-    # @param {integer[]} nums
-    # @return {integer[][]}
-    def permuteUnique(self, nums):
-        if not nums: return []
-        return [list(t) for t in self.perm(nums, 0)]    #Convert each tuple in set into a list and put them in a whole list.
+    def permuteUnique(self, nums: List[int]) -> List[List[int]]:
+        return [list(t) for t in self.backtracking(nums, 0)]            #Convert each tuple in set into a list and put them in a whole list.
