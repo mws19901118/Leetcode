9cc3afc43e56dd318d08c3d0d24577cbfffae0ab
Junsheng Wang <mws19901118@gmail.com>
2020-11-20 20:04:59 -0800

    Update Search in Rotated Sorted Array.py

--- Code/Search in Rotated Sorted Array.py
+++ Code/Search in Rotated Sorted Array.py
@@ -1,31 +1,23 @@
 class Solution:
-    # @param A, a list of integers
-    # @param target, an integer to be searched
-    # @return an integer
-    def search(self, A, target):
-        n=len(A)
-        if n==0:
-            return -1
-        start=0
-        end=n-1
-        while start<=end:
-            mid=(start+end)/2
-            if A[mid]==target:
+    def search(self, nums: List[int], target: int) -> int:
+        start, end = 0, len(nums) - 1
+        while start <= end:
+            mid = (start + end) // 2
+            if nums[mid] == target:
                 return mid
-            else:
-                if A[mid]>=A[start] and A[mid]<=A[end]:           #If A[start]<=A[mid]<=A[end], it's a normal binary search.
-                    if A[mid]>target:
-                        end=mid-1
-                    else:
-                        start=mid+1
-                elif A[mid]>=A[start] and A[mid]>A[end]:          #If A[mid]>=A[start] and A[mid]>A[end], the rotate pivot is the right side of mid.
-                    if target>=A[start] and target<A[mid]:        #If A[start]<=target<A[mid], target is in the left side of mid; otherwise, target is in the right side of mid.
-                        end=mid-1
-                    else:
-                        start=mid+1
-                elif A[mid]<A[start] and A[mid]<=A[end]:          #If A[mid]<A[start] and A[mid]<=A[end], the rotate pivot is the left side of mid.
-                    if target<=A[end] and target>A[mid]:          #If A[mid]<target<=A[end], target is in the right side of mid; otherwise, target is in the left side of mid.
-                        start=mid+1
-                    else:
-                        end=mid-1
-        return -1                                                 #If can not find target, return -1.
+            if nums[mid] >= nums[start] and nums[mid] > nums[end]:          #If nums[mid] >= nums[start] and nums[mid] > nums[end], the rotate pivot is the right side of mid.
+                if nums[mid] > target and target >= nums[start]:            #If nums[start] <= target < nums[mid], target is in the left side of mid; otherwise, target is in the right side of mid.
+                    end = mid - 1
+                else:
+                    start = mid + 1
+            elif nums[mid] < nums[start] and nums[mid] <= nums[end]:        #If nums[mid] < nums[start] and nums[mid] <= nums[end], the rotate pivot is the left side of mid.
+                if nums[mid] < target and target <= nums[end]:              #If nums[mid] < target <= nums[end], target is in the right side of mid; otherwise, target is in the left side of mid.
+                    start = mid + 1
+                else:
+                    end = mid - 1
+            elif nums[mid] >= nums[start] and nums[mid] <= nums[end]:       #If nums[start] <= nums[mid] <= nums[end], it's a normal binary search.
+                if nums[mid] < target:
+                    start = mid + 1
+                else:
+                    end = mid - 1
+        return -1                                                           #If can not find target, return -1.
