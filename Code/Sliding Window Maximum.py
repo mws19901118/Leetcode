0279552e6a6f11abd731b8d6431f73b0b17635e4
Junsheng Wang <mws19901118@gmail.com>
2020-11-28 23:43:07 -0800

    Update Sliding Window Maximum.py

--- Code/Sliding Window Maximum.py
+++ Code/Sliding Window Maximum.py
@@ -1,17 +1,13 @@
 class Solution:
-    # @param {integer[]} nums
-    # @param {integer} k
-    # @return {integer[]}
-    def maxSlidingWindow(self, nums, k):
-        n=len(nums)
-        result=[]
-        dq=collections.deque()                  #Suppose the index of current max value in the window is m. The deque stores in dq[0]. It also stores all the indexes i such that i>m and nums[i]<nums[m]. 
-        for i in range(n):
-            while dq and nums[dq[-1]]<nums[i]:  #Delete all the indexes in the window from behind whose corrsponding element is smaller than nums[i].
-                dq.pop()
-            dq.append(i)                        #Append i to the deque.
-            if dq[0]==i-k:                      #If the previous max value in the window falls out of window now, popleft its index.
+    def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+        result = []
+        dq = deque()                                            #Initalize a deque, which contains all possible numbers to be sliding window max and is sorted in non-ascending order.
+        for i, x in enumerate(nums):                            #Traverse through nums.
+            if i >= k and nums[i - k] == dq[0]:                 #If i >= k and nums[i - k] eqauls the current max, pop it from left.
                 dq.popleft()
-            if i>=k-1:                          #If the window size equals k, append the index of current max value in the window to result.
-                result.append(nums[dq[0]])
+            while dq and dq[-1] < x:                            #While dq is not empty and the tail of dq is smaller than current new number, pop tail of dq.
+                dq.pop()
+            dq.append(x)                                        #Append current new number to dq.
+            if i >= k - 1:                                      #If i >= k - 1, append current max to result.
+                result.append(dq[0])
         return result
