35b605f6ad8ad770de57c6247a4005687759a475
Junsheng Wang <mws19901118@gmail.com>
2020-12-07 12:35:50 -0800

    Update Spiral Matrix II.py

--- Code/Spiral Matrix II.py
+++ Code/Spiral Matrix II.py
@@ -1,21 +1,16 @@
 class Solution:
-    # @return a list of lists of integer
-    def generateMatrix(self, n):
-        matrix=[[0 for i in range(n)] for j in range(n)]
-        count=1                                         #Record the current number to be added to the matrix.
-        for i in range(n/2):
-            for j in range(n-2*i):                      #Deal with the top line, the ith row, with index starting at i, ending at n-i-1.
-                matrix[i][j+i]=count
-                count+=1
-            for j in range(n-2*i-1):                    #Deal with the right line, the (n-1-i)th column, with index starting at i+1, ending at n-i-1.
-                matrix[j+i+1][n-1-i]=count
-                count+=1
-            for j in range(n-2*i-1):                    #Deal with the bottom line, the (n-1-i)th row, with index starting at n-2-i, ending at i.
-                matrix[n-1-i][n-2-i-j]=count
-                count+=1
-            for j in range(n-2*(i+1)):                  #Deal with the left line, the ith column, with index starting at n-2-i, ending at i+1.
-                matrix[n-2-i-j][i]=count
-                count+=1
-        if n%2==1:                                      #If n%2==1, add the point in the center of the matrix.
-                matrix[n/2][n/2]=count
-        return matrix
+    def generateMatrix(self, n: int) -> List[List[int]]:
+        matrix = [[0 for i in range(n)] for j in range(n)]              #Initialize the matrix.
+        directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]                 #Initialize directions in spiral order.
+        length, x, y, number = n - 1, 0, 0, 1                           #Initial spiral length is n - 1; initial position is (0, 0); initial number is 1.
+        while length > 0:                                               #Traverse while spiral length is greater than 0.
+            for dx, dy in directions:                                   #Traverse each direction in spiral order.
+                for i in range(length):                                 #Fill the matrix for each spiral arm.
+                    matrix[x + i * dx][y + i * dy] = number + i
+                x += length * dx                                        #Update x position.
+                y += length * dy                                        #Update y position.
+                number += length                                        #Update number.
+            x, y, length = x + 1, y + 1, length - 2                     #Increase x and y by 1 and decrease length by 2 to enter next inner spiral matrix.
+        if length == 0:                                                 #If length is 0, there is one last number to fill in the center of matrix. If cannot be filled in traverse.
+            matrix[x][y] = number
+        return matrix                                                   #Return matrix.
