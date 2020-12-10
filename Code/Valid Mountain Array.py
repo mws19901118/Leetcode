0869641f241808f0435433b593396af3bcddcfeb
Junsheng Wang <mws19901118@gmail.com>
2020-12-10 14:36:36 -0800

    Update Valid Mountain Array.py

--- Code/Valid Mountain Array.py
+++ Code/Valid Mountain Array.py
@@ -1,16 +1,9 @@
 class Solution:
-    def validMountainArray(self, A):
-        """
-        :type A: List[int]
-        :rtype: bool
-        """
-        l = len(A)
-        if l < 3:                                               #If length is smaller than 3, A is not mountain array.
-            return False
-        front = 0                                               #Find the index of peak element from the front.
-        while front + 1 < l and A[front + 1] > A[front]:
+    def validMountainArray(self, arr: List[int]) -> bool:
+        front = 0
+        while front + 1 < len(arr) and arr[front + 1] > arr[front]:     #Find the index of peak element from the front.
             front += 1
-        back = l - 1                                            #Find the index of peak element from the back.
-        while back - 1 >= 0 and A[back - 1] > A[back]:
+        back = len(arr) - 1
+        while back - 1 >= 0 and arr[back - 1] > arr[back]:              #Find the index of peak element from the back.
             back -= 1
-        return front == back and front != l - 1 and back != 0   #Front and back should be equal and also can't be on the boundary of either side of A.
+        return front == back and front != len(arr) - 1 and back != 0    #Front and back should be equal and also can't be on the boundary of either side of arr.
