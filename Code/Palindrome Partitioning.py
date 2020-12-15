4449b54fac3e89ccb6c3552ad03d0dd672164756
Junsheng Wang <mws19901118@gmail.com>
2020-12-14 22:25:37 -0800

    Update Palindrome Partitioning.py

--- Code/Palindrome Partitioning.py
+++ Code/Palindrome Partitioning.py
@@ -1,47 +1,16 @@
 class Solution:
-    # @param s, a string
-    # @return a list of lists of string
-    def partition(self, s):                                       #The same method as Word Break II
-        def Palindrome(s,dict):
-            if s in dict:
-                return True
-            l=len(s)
-            i=l/2-1
-            while i>=0:
-                if s[i]!=s[l-1-i]:
-                    break
-                i-=1
-            if i==-1:
-                dict.add(s)
-                return True
-            else:
-                return False
-
-        dict=set(['a'])  
-        n=len(s)
-        A=[None]*n
-        i=n-1
-    
-        while i>=0:
-            if Palindrome(s[i:n],dict):
-                A[i]=[n]
-            else:
-                A[i]=[]
-            for j in xrange(i+1,n):
-                if A[j] and Palindrome(s[i:j],dict):
-                    A[i].append(j)
-            i-=1
-            
-        result=[]
-        path_list=[[0]]
-        while path_list:
-            new_list=[]
-            for path in path_list:
-                if path[-1]==n:
-                    temp=[ s[path[i]:path[i+1]] for i in xrange(len(path)-1) ]
-                    result.append(temp)
-                else:
-                    for j in A[path[-1]]:
-                        new_list.append(path+[j])
-            path_list=new_list
+    def dfs(self, s: str, start: int, stack: List[str], dp: List[List[bool]], result: List[List[str]]) -> None:
+        if start == len(s):                                                             #If DFS reaches the end of string, append deep copy of current stack to result. 
+            result.append(copy.deepcopy(stack))
+            return
+        for i in range(start, len(s)):                                                  #Traverse the string from the start index. 
+            if s[i] == s[start] and (i <= start + 1 or dp[start + 1][i - 1]):           #If the current letter is the same of the letter in start index and s[start + 1:i] is palindrome or i <= start + 1, s[start:i + 1] is also a palindrome.
+                dp[start][i] = True                                                     #Update dp.
+                stack.append(s[start:i + 1])                                            #Add it to stack.
+                self.dfs(s, i + 1, stack, dp, result)                                   #DFS from i + 1.
+                stack.pop()                                                             #Pop stack.
+    def partition(self, s: str) -> List[List[str]]:
+        result = []
+        dp = [[False for i in range(len(s))] for j in range(len(s))]                    #Initialize dp; dp[i][j] stores if s[i][j + 1] is palindrome. 
+        self.dfs(s, 0, [], dp, result)                                                  #DFS from the beginning.
         return result
