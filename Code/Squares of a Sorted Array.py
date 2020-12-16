67a2a539b907d6f386b060e45b3c80b84f8ee655
Junsheng Wang <mws19901118@gmail.com>
2020-12-15 16:09:37 -0800

    Update Squares of a Sorted Array.py

--- Code/Squares of a Sorted Array.py
+++ Code/Squares of a Sorted Array.py
@@ -1,23 +1,18 @@
-class Solution(object):
-    def sortedSquares(self, A):
-        """
-        :type A: List[int]
-        :rtype: List[int]
-        """
-        minAbs = max(abs(A[0]), abs(A[-1])) + 1
-        index = -1
-        for i, a in enumerate(A):                                 #Find the element in A with min abs value and its index.
-            if abs(a) < minAbs:
-                minAbs = abs(a)
-                index = i
-        squares = [minAbs ** 2]                                   #Add the square of minAbs to the squares list.
-        left = index - 1                                          #Left pointer, traverse to the left of index.
-        right = index + 1                                         #Right pointer, traverse to the right of index.
-        while len(squares) < len(A):
-            if right >= len(A) or abs(A[left]) <= abs(A[right]):  #If right pointer reaches the right end or the abs value of right pointer is not smaller than that of left pointer, add the square of value of left pointer to the squares list. 
-                squares.append(A[left] ** 2)
+class Solution:
+    def sortedSquares(self, nums: List[int]) -> List[int]:
+        minAbs = 10001
+        minAbsIndex = -1
+        for i, x in enumerate(nums):                                                            #Find the element in A with min abs value and its index.
+            if abs(x) < minAbs:
+                minAbs = abs(x)
+                minAbsIndex = i
+        left, right = minAbsIndex - 1, minAbsIndex + 1                                          #Left pointer, traverse to the left of index and right pointer, traverse to the right of index.
+        squares = [minAbs * minAbs]                                                             #Add the square of minAbs to the squares list.
+        while left >= 0 or right < len(nums):
+            if left >= 0 and (right >= len(nums) or abs(nums[left]) <= abs(nums[right])):       #If right pointer reaches the right end or the abs value of right pointer is not smaller than that of left pointer, add the square of value of left pointer to the squares list.
+                squares.append(nums[left] * nums[left])
                 left -= 1
-            else:                                                 #Otherwise, add the square of value of right pointer to the squares list. 
-                squares.append(A[right] ** 2)
+            else:                                                                               #Otherwise, add the square of value of right pointer to the squares list.
+                squares.append(nums[right] * nums[right])
                 right += 1
         return squares
