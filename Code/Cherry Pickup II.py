54c337dddcf76e00bce14a905fc7c755f2528c0f
Junsheng Wang <mws19901118@gmail.com>
2020-12-19 17:17:03 -0800

    Create Cherry Pickup II.py

---
+++ Code/Cherry Pickup II.py
@@ -0,0 +1,15 @@
+class Solution:
+    def cherryPickup(self, grid: List[List[int]]) -> int:
+        m,n = len(grid), len(grid[0])                                                   #Get the dimensions of grid.
+        cherryDict = {(0, n-1): grid[0][0] + grid[0][-1]}                               #Store cherries picked by the pair of columns of robots in first row.
+        for i in range(1, m):                                                           #Traverse top down.
+            newCherryDict = defaultdict(int)                                            #Store the cherries picked so far by the pair of columns of robots in current row.
+            for (x, y), val in cherryDict.items():
+                robot1 = [i for i in [x - 1, x, x + 1] if i >= 0 and i < n]             #Generate all available columns for robot 1 in this row.
+                robot2 = [i for i in [y - 1, y, y + 1] if i >= 0 and i < n]             #Generate all available columns for robot 2 in this row.
+                for a in robot1:
+                    for b in robot2:
+                        newVal = val + grid[i][a] + grid[i][b] * (a != b)               #For each pair of columns of robot, calculate new value of cherries picked so far. If robots are in same column, only add it once.
+                        newCherryDict[(a, b)] = max(newCherryDict[a, b], newVal)        #Add or update the value by pair of columns of robots.
+            cherryDict = newCherryDict                                                  #Replace cherryDict with newCherryDict.
+        return max(cherryDict.values())                                                 #Return the max value of cherries in last row.
