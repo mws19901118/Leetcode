10d0284e069ec3ddcfee4ffb8be60a103e330d28
Junsheng Wang <mws19901118@gmail.com>
2020-12-22 14:32:50 -0800

    Update Balanced Binary Tree.py

--- Code/Balanced Binary Tree.py
+++ Code/Balanced Binary Tree.py
@@ -1,24 +1,14 @@
-# Definition for a  binary tree node
+# Definition for a binary tree node.
 # class TreeNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
-def depth(A):
-    if A==None:
-        return 0
-    else:
-        a=depth(A.left)
-        b=depth(A.right)
-        if a==-1 or b==-1 or abs(a-b)>1:              #If the binary tree whose root is A, A's left child or A's right child, return -1.
-            return -1
-        return max(a,b)+1                             #If not, return the depth of A 
-
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
 class Solution:
-    # @param root, a tree node
-    # @return a boolean
-    def isBalanced(self, root):
-        if depth(root)!=-1:
-            return True
-        else:
-            return False
+    def height(self, root: TreeNode) -> int:
+        if root is None:                                                                                                                            #If root is none, return 0.
+            return 0
+        leftHeight, rightHeight = self.height(root.left), self.height(root.right)                                                                   #Get the height of left subtree and right subtree.
+        return max(leftHeight, rightHeight) + 1 if leftHeight != -1 and rightHeight != -1 and abs(leftHeight - rightHeight) <= 1 else -1            #If both left subtree and right subtree is balanced and the height difference is no more than 1, return the depth of current tree; otherwise return -1.
+    def isBalanced(self, root: TreeNode) -> bool:
+        return self.height(root) != -1
