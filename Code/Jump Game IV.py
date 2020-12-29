423b66e6d1ca75eae71881e13bde074223ea3ac2
Junsheng Wang <mws19901118@gmail.com>
2020-12-28 20:55:55 -0800

    Update Jump Game IV.py

--- Code/Jump Game IV.py
+++ Code/Jump Game IV.py
@@ -1,40 +1,32 @@
 class Solution:
     def minJumps(self, arr: List[int]) -> int:
-        newArr = []                                       #Preprocess. For a number that appears at least 2 consecutive times in arr, we only need 2 of them.
+        indexes = defaultdict(list)
         i = 0
-        while i < len(arr):
+        while i < len(arr):                                             #Find the indexes in arr of each number.
             j = i + 1
             while j < len(arr) and arr[j] == arr[i]:
                 j += 1
-            newArr.append(arr[i])
-            if j - i >= 2:
-                newArr.append(arr[i])
+            indexes[arr[i]].append(i)
+            if j - i >= 2:                                              #For a number that appears at least 2 consecutive times in arr, we only need the start and end index.
+                indexes[arr[i]].append(j - 1)
             i = j
-        
-        indexes = defaultdict(list)                       #Store the indexes of each number
-        for i, x in enumerate(newArr):
-            indexes[x].append(i)
-        graph = defaultdict(set)                          #Build graph, node is index and there is an edge between 2 nodes if we can jump from one to the other.
-        for i, x in enumerate(newArr):
-            if i > 0:
-                graph[i].add(i - 1)
-            if i < len(newArr) - 1:
-                graph[i].add(i + 1)
-            for j in indexes[x]:
-                if j != i:
-                    graph[i].add(j)
-        
-        steps = 0
-        q = set([0])
-        visited = set()
-        while q:                                          #BFS and count steps.
-            newq = set()
+
+        steps = 0                                                       #Count steps.
+        q = [0]
+        visited = {0}
+        while q:                                                        #BFS.
+            newq = []
             for x in q:
-                if x == len(newArr) - 1:                  #Return steps when we reach the end.
+                if x == len(arr) - 1:                                   #If reach the end, return steps.
                     return steps
-                for y in graph[x]:
-                    if y not in q and y not in visited:
-                        newq.add(y)
-                visited.add(x)
-            steps += 1
-            q = newq
+                for y in indexes[arr[x]]:                               #Iterate over indexes with same value of arr[x].
+                    if y not in visited:                                #If it's not visited, add it to newq and visited.
+                        newq.append(y)
+                        visited.add(y)
+                indexes[arr[x]].clear()                                 #All indexes in indexes[arr[x]] are visited. Clear indexes[arr[x]] to avoid duplicate visit.
+                for y in [x - 1, x + 1]:                                #Visit the adjacent indexes if they are valid and not visited. 
+                    if 0 <= y < len(arr) and y not in visited:
+                        newq.append(y)
+                        visited.add(y)
+            steps += 1                                                  #Increase steps.
+            q = newq                                                    #Replace q with newq.
