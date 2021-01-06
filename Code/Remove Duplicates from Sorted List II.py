005b9cb560903dd859c6b86626c9b84d0bdf0ad6
Junsheng Wang <mws19901118@gmail.com>
2021-01-05 17:36:11 -0800

    Update Remove Duplicates from Sorted List II.py

--- Code/Remove Duplicates from Sorted List II.py
+++ Code/Remove Duplicates from Sorted List II.py
@@ -1,24 +1,20 @@
 # Definition for singly-linked list.
 # class ListNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.next = None
-
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
 class Solution:
-    # @param head, a ListNode
-    # @return a ListNode
-    def deleteDuplicates(self, head):
-        fakehead=ListNode(-1)                                                   #Because head may be deleted, add a fakehead before the head node.
-        fakehead.next=head
-        current=fakehead                                                        #Record the current node whose link is to be determined.
-        while current.next!=None:
-            nextnode=current.next
-            count=1
-            while nextnode.next!=None and nextnode.next.val==current.next.val:
-                count+=1                                                        #Count the number of appereance.
-                nextnode=nextnode.next
-            if count>1:
-                current.next=nextnode.next                                      #Delete the duplicate nodes.
-            else:
-                current=current.next
-        return fakehead.next
+    def deleteDuplicates(self, head: ListNode) -> ListNode:
+        dummyHead = ListNode(0, head)                                           #Because head may be deleted, add a dummy head before the head node.
+        tail = dummyHead
+        while tail.next:                                                        #Traverse list until the next of tail is none.
+            count = 0                                                           #Count current value.
+            curr = tail.next
+            while curr and curr.val == tail.next.val:
+                count += 1
+                curr = curr.next
+            if count > 1:                                                       #If current value has multiple appereances, delete them.
+                tail.next = curr
+            else:                                                               #Otherwise, move tail to next node.
+                tail = tail.next
+        return dummyHead.next                                                   #Return the next of dummy head.
