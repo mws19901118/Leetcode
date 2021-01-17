a9cada80b275c2cd531b6d2826172e0b3281084e
Junsheng Wang <mws19901118@gmail.com>
2021-01-17 14:02:32 -0800

    Update Count Sorted Vowel Strings.py

--- Code/Count Sorted Vowel Strings.py
+++ Code/Count Sorted Vowel Strings.py
@@ -1,3 +1,3 @@
 class Solution:
     def countVowelStrings(self, n: int) -> int:
-        return (n + 1) * (n + 2) * (n + 3) * (n + 4) // 24  #Return the n + 1 of Pentatope number, https://en.wikipedia.org/wiki/Pentatope_number.
+        return (n + 1) * (n + 2) * (n + 3) * (n + 4) // 24  #Choose n digits from the pool of 5 digits with repetitions, a.k.a the n + 1 of Pentatope number, https://en.wikipedia.org/wiki/Pentatope_number.
