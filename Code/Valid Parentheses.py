cd128373c0790000f66a9a46f14f568c58e4a061
Junsheng Wang <mws19901118@gmail.com>
2021-01-20 14:29:48 -0800

    Update Valid Parentheses.py

--- Code/Valid Parentheses.py
+++ Code/Valid Parentheses.py
@@ -1,15 +1,12 @@
 class Solution:
-    # @param {string} s
-    # @return {boolean}
-    def isValid(self, s):
-        dict={'(':')','[':']','{':'}'}                        #Store the valid matches.
-        left=['(','[','{']                                    #Store the left bracket.
-        stack=[]
-        for i in s:
-            if stack==[]:                                     #If stack is empty, append i into stack.
-                stack.append(i)
-            elif stack[-1] in left and i==dict[stack[-1]]:    #If the last element in stack is left bracket and i is right bracket, there is a valid match, then pop stack.
+    def isValid(self, s: str) -> bool:
+        parentheseDict = {')': '(', ']': '[', '}': '{'}         #Store the valid matches.
+        stack = []
+        for x in s:
+            if x in parentheseDict.values():                    #If x is left parenthesis, append x into stack.
+                stack.append(x)
+            elif stack and stack[-1] == parentheseDict[x]:      #If the top of stack matches x, pop stack.
                 stack.pop()
-            else:                                             #Otherwise, append i into stack.
-                stack.append(i)
-        return stack==[]                                      #If stack is empty, the string is valid.
+            else:                                               #Otherwise s is not valid, return false directly.
+                return False
+        return not stack                                        #If stack is empty, the s is valid.
