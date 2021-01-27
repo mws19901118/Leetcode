52e2e8ea520d654ba089ee0ceee312126558132b
Junsheng Wang <mws19901118@gmail.com>
2021-01-27 10:57:48 -0800

    Create Concatenation of Consecutive Binary Numbers.py

---
+++ Code/Concatenation of Consecutive Binary Numbers.py
@@ -0,0 +1,12 @@
+class Solution:
+    def concatenatedBinary(self, n: int) -> int:
+        length = 1                                                  #Record current length of binary.
+        exponentOf2 = 1                                             #Record the largest exponent of 2 which is not greater than current binary
+        result = 0
+        modulo = 10 ** 9 + 7                                        #Modulo is 10 ** 9 + 7.
+        for i in range(1, n + 1):                                   #Traverse from 1 to n.
+            if i == exponentOf2 << 1:                               #If i reaches a new exponent of 2, update exponentOf2 and length.
+                exponentOf2 <<= 1
+                length += 1
+            result = ((result << length) + i) % modulo              #Shift result to left for length bit and add i then calculate the remain.
+        return result
