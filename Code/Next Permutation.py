7c4630d8c77ad7a62c2d4a2410028447bc80d909
Junsheng Wang <mws19901118@gmail.com>
2021-01-31 19:19:38 -0800

    Update Next Permutation.py

--- Code/Next Permutation.py
+++ Code/Next Permutation.py
@@ -1,20 +1,15 @@
 class Solution:
-    # @param {integer[]} nums
-    # @return {void} Do not return anything, modify nums in-place instead.
-    def nextPermutation(self, nums):
-        n=len(nums)
-        i=n-2
-        while i>=0 and nums[i]>=nums[i+1]:    #Find the first element i which violate the descending order from i to the end, i.e. nums[i]<nums[i+1].
-            i-=1
-        if i==-1:                             #If can't find such element, the array is totally descending. Thus, we can't get the next permutation. So we reverse the array to sort it in ascending order.
-            nums.reverse()
-        else:
-            j=n-1
-            while nums[j]<=nums[i]:           #Search from the end to i to find the first element j which is greater than i.
-                j-=1
-            a=nums[i]                         #Swap the value of nums[i] and nums[j]. That's the beginning of difference between current permutation and next permutation.
-            nums[i]=nums[j]
-            nums[j]=a
-            t=nums[i+1:]                      #Reverse the array behind i to sort it in ascending order
-            t.reverse()
-            nums[i+1:]=t
+    def nextPermutation(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        i = len(nums) - 1
+        while i >= 1 and nums[i] <= nums[i - 1]:                                #Search from end to find the first element i which violate the non-ascending order from i to the end, i.e. nums[i] > nums[i - 1].
+            i -= 1
+        if i > 0:                                                               #If found, find the first element nums[j] which is greater than nums[i - 1] from behind.
+            j = len(nums) - 1
+            while j >= 0 and nums[j] <= nums[i - 1]:
+                j -= 1
+            nums[i - 1], nums[j] = nums[j], nums[i - 1]                         #Swap nums[i - 1] and nums[j], now nums[i:] is still in non-ascending order.
+        for j in range((len(nums) - i) // 2):                                   #Reverse nums[i:]
+.            nums[i + j], nums[-(j + 1)] = nums[-(j + 1)], nums[i + j]
