7640baae250edb460c2f452cdd7bae7298b41397
Junsheng Wang <mws19901118@gmail.com>
2021-02-02 00:43:53 -0800

    Create Trim a Binary Search Tree.py

---
+++ Code/Trim a Binary Search Tree.py
@@ -0,0 +1,18 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def trimBST(self, root: TreeNode, low: int, high: int) -> TreeNode:
+        if root is None:                                            #If root is none, return none.
+            return None
+        if root.val < low:                                          #If root is smaller than low, return the trimmed right subtree.
+            return self.trimBST(root.right, low, high)
+        elif root.val > high:                                       #If root is larger than high, return the trimmed left subtree.
+            return self.trimBST(root.left, low, high)
+        else:                                                       #If root is between low and high, trim left subtree and right subtree respectively and then return root.
+            root.left = self.trimBST(root.left, low, high)
+            root.right = self.trimBST(root.right, low, high)
+            return root
