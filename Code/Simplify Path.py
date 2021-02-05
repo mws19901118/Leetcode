a7637ccf655abb96c350e77918f61e5bdd5d0529
Junsheng Wang <mws19901118@gmail.com>
2021-02-05 00:50:00 -0800

    Update Simplify Path.py

--- Code/Simplify Path.py
+++ Code/Simplify Path.py
@@ -1,20 +1,13 @@
 class Solution:
-    # @param path, a string
-    # @return a string
-    def simplifyPath(self, path):
-        p=path.split('/')                                   #Split the string.
-        i=0
-        while i<len(p):
-            c=p[i]                                          #Record current part of path.
-            if c=='' or c=='.':                             #If current part is '' or '.', delete it.
-                del p[i]
-                i-=1;
-            elif c=='..':                                   #If current part is '..', delete it and the part before it(if have).
-                del p[i]
-                if(i>0):
-                    del p[i-1]
-                    i-=2;
-                else:
-                    i-=1;
-            i+=1;
-        return '/'+ '/'.join(p)                             #Join the remain parts.
+    def simplifyPath(self, path: str) -> str:
+        splits = path.split("/")                            #Split the string.
+        stack = []                                          #Use stack to store valid folder or file name.
+        for s in splits:                                    #Traverse splits.
+            if not s or s == '.':                           #If s is "" or ".", continue.
+                continue
+            elif s == "..":                                 #Else if s is ".." and stack is not empty, pop stack.
+                if stack:
+                    stack.pop()
+            else:                                           #Otherwise, append s to stack.
+                stack.append(s)
+        return "/" + "/".join(stack)                        #Join the stack and return.
