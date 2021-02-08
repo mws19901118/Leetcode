e0c746a9a052ff597b2cdcdcce9e112994efeb3c
Junsheng Wang <mws19901118@gmail.com>
2021-02-08 00:12:25 -0800

    Update Peeking Iterator.py

--- Code/Peeking Iterator.py
+++ Code/Peeking Iterator.py
@@ -26,33 +26,30 @@ class PeekingIterator(object):
         :type iterator: Iterator
         """
         self.it = iterator
-        self.buffur = None                                    #Use a buffer int to store the peeking element. If there is no peeking elements, it's none.
+        self.buffur = None                                                      #Use a buffer int to store the peeking element. If there is no peeking elements, it's none.
         if self.it.hasNext():
-            self.buffer = self.it.next()                      #Get tbe peeking element and store it in buffer.
+            self.buffer = self.it.next()                                        #Get tbe peeking element and store it in buffer.
 
     def peek(self):
         """
         Returns the next element in the iteration without advancing the iterator.
         :rtype: int
         """
-        return self.buffer                                    #Return buffer.
+        return self.buffer                                                      #Return buffer.
 
     def next(self):
         """
         :rtype: int
         """
         temp = self.buffer
-        if self.it.hasNext():                                 #Get the next peeking element.
-            self.buffer = self.it.next()
-        else:
-            self.buffer = None
-        return temp                                           #Return old buffer.
+        self.buffer = self.itr.next() if self.itr.hasNext() else None
+        return temp                                                             #Return old buffer.
 
     def hasNext(self):
         """
         :rtype: bool
         """
-        return self.buffer != None                            #If buffer is none, there is no next element.
+        return self.buffer != None                                              #If buffer is none, there is no next element.
 
 # Your PeekingIterator object will be instantiated and called as such:
 # iter = PeekingIterator(Iterator(nums))
