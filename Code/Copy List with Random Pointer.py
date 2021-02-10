cb9840f86714c4fc3bd57a6bb45379efe9cb600e
Junsheng Wang <mws19901118@gmail.com>
2021-02-10 00:45:57 -0800

    Update Copy List with Random Pointer.py

--- Code/Copy List with Random Pointer.py
+++ Code/Copy List with Random Pointer.py
@@ -1,38 +1,26 @@
 """
 # Definition for a Node.
 class Node:
-    def __init__(self, val, next, random):
-        self.val = val
+    def __init__(self, x: int, next: 'Node' = None, random: 'Node' = None):
+        self.val = int(x)
         self.next = next
         self.random = random
 """
+
 class Solution:
     def copyRandomList(self, head: 'Node') -> 'Node':
-        if head is None:
-            return None
-        
-        pointer = head
-        while pointer is not None:                                      #Duplicate every node and insert them after the old ones.
-            copy = Node(pointer.val, pointer.next, pointer.random)
-            pointer.next = copy
-            pointer = pointer.next.next
-            
-        pointer1 = head.next
-        pointer2 = head
-        while pointer2 is not None:                                     #Set the new node's random pointer to the next node of old node's random pointer.
-            if pointer2.random is not None:
-                pointer1.random = pointer2.random.next
-            pointer2 = pointer2.next.next
-            if pointer1.next is not None:
-                pointer1 = pointer1.next.next
-
-        newHead = head.next
-        pointer1 = head.next
-        pointer2 = head
-        while pointer1 is not None:                                     #Separate old linked list and new linked list.
-            pointer2.next = pointer1.next
-            if pointer1.next is not None:
-                pointer1.next = pointer1.next.next
-            pointer1 = pointer1.next
-            pointer2 = pointer2.next
-        return newHead
+        curr = head
+        while curr:                                                                 #Duplicate every node and insert them after the old ones.
+            curr.next = Node(curr.val, curr.next, None)
+            curr = curr.next.next
+        curr = head
+        while curr:                                                                 #Set the new node's random pointer to the next node of old node's random pointer if old node's random pointer is not none.
+            if curr.random:
+                curr.next.random = curr.random.next
+            curr = curr.next.next
+        dummyHead = Node(0, head, None)
+        curr = dummyHead
+        while curr.next:                                                            #Separate old linked list and new linked list.
+            curr.next = curr.next.next
+            curr = curr.next
+        return dummyHead.next
