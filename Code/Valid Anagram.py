4065432ebd1afab728abc58492f6d69bacf963eb
Junsheng Wang <mws19901118@gmail.com>
2021-02-11 00:04:28 -0800

    Update Valid Anagram.py

--- Code/Valid Anagram.py
+++ Code/Valid Anagram.py
@@ -1,20 +1,3 @@
 class Solution:
-    # @param {string} s
-    # @param {string} t
-    # @return {boolean}
-    def isAnagram(self, s, t):      #Count word frequencies.
-        if len(s)!=len(t):
-            return False
-        ds={}
-        dt={}
-        for i in range(len(s)):
-            if s[i] not in ds:
-                ds[s[i]]=1
-            else:
-                ds[s[i]]+=1
-        for i in range(len(t)):
-            if t[i] not in dt:
-                dt[t[i]]=1
-            else:
-                dt[t[i]]+=1
-        return ds==dt
+    def isAnagram(self, s: str, t: str) -> bool:
+        return Counter(s) == Counter(t)                 #Return if the word frequencies of s and t are equal.
