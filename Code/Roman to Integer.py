861d0e4a25ec61c002fdb4ec8fe295b8702b7e5a
Junsheng Wang <mws19901118@gmail.com>
2021-02-20 01:06:00 -0800

    Update Roman to Integer.py

--- Code/Roman to Integer.py
+++ Code/Roman to Integer.py
@@ -1,14 +1,11 @@
 class Solution:
     def romanToInt(self, s: str) -> int:
-        d = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-        numeral=['M', 'D', 'C', 'L', 'X', 'V', 'I']
-        if not s:
-            return 0
-        for c in numeral:
-            try:
-                i = s.index(c)                                                          #Search for M, D, C, L, X, V and I in order. Get the index of first appearance of a certain character in s.
-            except ValueError:
-                i = -1                                                                  #If not found, catch the ValueError and set i to be -1.
-            finally:
-                if i != -1:
-                    return -self.romanToInt(s[:i]) + d[c] + self.romanToInt(s[i+1:])    #Return its value minus the value of Romen numeral between that character plus the value of Romen numeral after that character.
+        romanNegative = {"IV": -1, "XL": -10, "CD": -100, "IX": -1, "XC": -10, "CM": -100}      #Map Roman 4, 40, 400 and 9, 90, 900 to its negative value, -1, -10 or -100.
+        romanPositive = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}       #Map other Roman to its value.
+        result = 0
+        for i in range(len(s)):                                                                 #Scan s.
+            if i + 1 < len(s) and s[i:i + 2] in romanNegative:                                  #If s[i:i + 2] is 4, 40, 400 or 9, 90, 900, add the negative value to result.
+                result += romanNegative[s[i:i + 2]]
+            else:                                                                               #Otherwise, add its value to result.
+                result += romanPositive[s[i]]
+        return result                                                                           #Return result.
