96d56f761d5ac706f9d6998158f2408d31982e13
Junsheng Wang <mws19901118@gmail.com>
2021-02-23 02:09:03 -0800

    Update Search a 2D Matrix II.py

--- Code/Search a 2D Matrix II.py
+++ Code/Search a 2D Matrix II.py
@@ -1,27 +1,12 @@
 class Solution:
-    # @param {integer[][]} matrix
-    # @param {integer} target
-    # @return {boolean}
-    def searchMatrix(self, matrix, target):
-        row=len(matrix)
-        if row==0:
-            return False
-        column=len(matrix[0])
-        if column==0:
-            return False
-        if target<matrix[0][0] or target>matrix[row-1][column-1]:
-            return False
-        if row>1:                               #If there are more than 1 rows, split them into 2 parts, search the target in the 2 parts respectively then return the or operation of the results.
-            return self.searchMatrix(matrix[:row/2], target) or self.searchMatrix(matrix[row/2:], target)
-        else:                                   #If there is only 1 row, binary search the target in that row.
-            start=0
-            end=column-1
-            while start<=end:
-                mid=(start+end)/2
-                if matrix[row-1][mid]==target:
-                    return True
-                elif matrix[row-1][mid]>target:
-                    end=mid-1
-                else:
-                    start=mid+1
-            return False
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        m, n = len(matrix), len(matrix[0])              #Get the dimensions.
+        i, j = 0, n - 1                                 #Starting from the top right corner.
+        while i < m and j >= 0:
+            if matrix[i][j] == target:                  #If found target, return true.
+                return True
+            elif matrix[i][j] > target:                 #If current number is greater than target, move left.
+                j -= 1
+            else:                                       #Otherwise, move down.
+                i += 1
+        return False                                    #If not found, return false.
