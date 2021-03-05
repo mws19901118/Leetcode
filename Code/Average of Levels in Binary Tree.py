7d01a68e41c6b744b3aa117b66cd2f4051c3e035
Junsheng Wang <mws19901118@gmail.com>
2021-03-05 00:15:21 -0800

    Create Average of Levels in Binary Tree.py

---
+++ Code/Average of Levels in Binary Tree.py
@@ -0,0 +1,22 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def averageOfLevels(self, root: TreeNode) -> List[float]:
+        average = []                              #Initialize average for each level.
+        q = [root]                                #Add root to queue.
+        while q:                                  #BFS.
+            newq = []                             #Initialize new queue.
+            levelSum = 0                          #Initialize level sum.
+            for x in q:                           #For each node in queue, add its value to level sum.
+                levelSum += x.val
+                if x.left is not None:            #If left child of current node is not none, append it to new queue.
+                    newq.append(x.left)
+                if x.right is not None:           #If right child of current node is not none, append it to new queue.
+                    newq.append(x.right)
+            average.append(levelSum / len(q))     #Calculate level average and append it to average list.
+            q = newq                              #Replace queue with new queue.
+        return average
