5bc8d749af544d47f200515f172335202c131ee6
Junsheng Wang <mws19901118@gmail.com>
2021-03-13 00:51:03 -0800

    Update Create Sorted Array through Instructions.py

--- Code/Create Sorted Array through Instructions.py
+++ Code/Create Sorted Array through Instructions.py
@@ -3,9 +3,9 @@ class Solution:
     def createSortedArray(self, instructions: List[int]) -> int:
         sortedList = SortedList()
         cost = 0
-        modulo = 10 ** 9 + 7
+        division = 10 ** 9 + 7
         for x in instructions:                                                                        #Traverse through instructions.
             cost += min(sortedList.bisect_left(x), len(sortedList) - sortedList.bisect_right(x))      #Find the first and last place to insert x in sorted list, let's say A and B. There are A numbers smaller than x and len(sortedList) - B numbers greater than x, so cost to insert x is min(A, len(sortedList) - B).
-            cost %= modulo                                                                            #Modulo 10 ** 9 + 7.
+            cost %= division                                                                          #Calculate modulo.
             sortedList.add(x)                                                                         #Insert x to sortedList.
         return cost
