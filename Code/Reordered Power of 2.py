9bd92b15da6a89cd1a070678bf5d3c3795600cef
Junsheng Wang <mws19901118@gmail.com>
2021-03-21 00:48:03 -0700

    Create Reordered Power of 2.py

---
+++ Code/Reordered Power of 2.py
@@ -0,0 +1,11 @@
+class Solution:
+    def reorderedPowerOf2(self, N: int) -> bool:
+        s = str(N)                                                                                                          #Covert N to string.
+        lower, upper = math.ceil(math.log2(10 ** (len(s) - 1))), math.floor(math.log2(10 ** len(s) - 1))                    #Find the lower and upper bound of power so that the power of 2 will have the same length with N.
+        count = Counter(s)                                                                                                  #Count each digit in N.
+        power = 1 << lower
+        for i in range(lower, upper + 1):                                                                                   #Traverse all the power of 2 from 2 ** lower to 2 ** upper.
+            if Counter(str(power)) == count:                                                                                #If current power of 2 has same digit distribution with N, then it can be form by reordering N, so return True.
+                return True
+            power <<= 1
+        return False                                                                                                        #Return false if such power of 2 not found.
