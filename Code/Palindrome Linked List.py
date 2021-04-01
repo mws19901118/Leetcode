c4ddb2e340bee491d2f6977bf8dd22724d6d07de
Junsheng Wang <mws19901118@gmail.com>
2021-04-01 16:29:23 -0700

    Update Palindrome Linked List.py

--- Code/Palindrome Linked List.py
+++ Code/Palindrome Linked List.py
@@ -1,37 +1,22 @@
 # Definition for singly-linked list.
 # class ListNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.next = None
-
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
 class Solution:
-    # @param {ListNode} head
-    # @return {boolean}
-    def isPalindrome(self, head):
-        if head==None or head.next==None:
-            return True
-            
-        fast=head
-        slow=head
-        while fast.next!=None and fast.next.next!=None:   #Use fast pointer and slow pointer to find the mid node of linked list.
-            fast=fast.next.next
-            slow=slow.next
-        if fast.next!=None:
-            fast=fast.next
-        slow=slow.next
-        
-        current=slow.next                                 #Reverse the second half of the linked list.
-        slow.next=None
-        while current!=None:
-            temp=current
-            current=current.next
-            temp.next=slow
-            slow=temp
-            
-        while slow!=None:                                 #Compare the value of each node of first half and reversed second half.
-            if slow.val!=head.val:                        #If there are an unmatched pair of nodes, the linked list in not palindrome.
-                return False
-            else:
-                slow=slow.next
-                head=head.next
-        return True
+    def isPalindrome(self, head: ListNode) -> bool:
+        fast, slow = head, head
+        while fast.next and fast.next.next:                             #Use fast pointer and slow pointer to find the mid node of linked list.
+            fast = fast.next.next
+            slow = slow.next
+        dummy = ListNode()
+        curr = slow.next
+        while curr:                                                     #Reverse the second half of the linked list.
+            temp = curr
+            curr = curr.next
+            temp.next = dummy.next
+            dummy.next = temp
+        a, b = head, dummy.next
+        while a and b and a.val == b.val:                               #Compare the value of each node of first half and reversed second half.
+            a, b = a.next, b.next
+        return b is None                                                #If there are an unmatched pair of nodes, the linked list in not palindrome.
