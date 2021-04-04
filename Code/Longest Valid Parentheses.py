3d4fc1a7f480828726dfc6fc542881eb83f8c82f
Junsheng Wang <mws19901118@gmail.com>
2021-04-03 22:32:10 -0700

    Update Longest Valid Parentheses.py

--- Code/Longest Valid Parentheses.py
+++ Code/Longest Valid Parentheses.py
@@ -1,19 +1,14 @@
 class Solution:
-    # @param {string} s
-    # @return {integer}
-    def longestValidParentheses(self, s):
-        l=len(s)
-        if l==0:
-            return 0
-        record=[0]*l                                              #Record the length of longest valid parentheses ending at each character.
-        left=[]                                                   #It's a stack recording every index of '('.
-        for i in range(l):                                        #Traverse through the string.
-            if s[i]=='(':                                         #If current character is '(', append the index to left.
+    def longestValidParentheses(self, s: str) -> int:
+        record = [0] * len(s)                                               #Record the length of longest valid parentheses ending at each character.
+        left = []                                                           #It's a stack recording every index of '('.
+        for i, x in enumerate(s):                                           #Traverse through the string.
+            if x == '(':                                                    #If current character is '(', append the index to left.
                 left.append(i)
-            elif left!=[]:                                        #If current character is ')' and left is not empty, pop stack, which is the nearest '(' before current ')'(note as leftindex).
-                leftindex=left.pop()
-                if i - leftindex == record[i-1]+1:                #If the distance between i and leftindex equals the length of longest valid parentheses ending at i-1 plus 1, the string from leftindex to i is a valid parentheses.
-                    record[i] = record[i-1] + 2
-                    if leftindex > 0:                             #Combine the longest valid parentheses ending at i and the longest valid parentheses ending at leftindex-1.
-                        record[i] += record[leftindex-1]
-        return max(record)                                        #Return the max of record.
+            elif left != []:                                                #If current character is ')' and left is not empty, pop stack, which is the nearest '(' before current ')'(note as leftindex).
+                leftindex = left.pop()
+                if i - leftindex == record[i - 1] + 1:                      #If the distance between i and leftindex equals the length of longest valid parentheses ending at i - 1 plus 1, the string from leftindex to i is a valid parentheses.
+                    record[i] = record[i - 1] + 2
+                    if leftindex > 0:                                       #Combine the longest valid parentheses ending at i and the longest valid parentheses ending at leftindex - 1.
+                        record[i] += record[leftindex - 1]
+        return max(record) if record else 0                                 #Return the max of record or 0 if record is empty.
