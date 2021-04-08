0460a066d39e928861416110d0d0b80fd6ecf6d2
Junsheng Wang <mws19901118@gmail.com>
2021-04-08 00:17:38 -0700

    Update Letter Combinations of a Phone Number.py

--- Code/Letter Combinations of a Phone Number.py
+++ Code/Letter Combinations of a Phone Number.py
@@ -1,18 +1,16 @@
 class Solution:
-    def backtrack(self, result, current, digits, index, dict):
-        if index==len(digits):                                                                        #If index equals the length of digits, the backtracking comes to an end.
-            if index!=0:                                                                              #If digits is a empty string, we don't have to append anything.
-                result.append("".join(current))                                                       #Otherwise, join the characters and append it to result.
-        else:
-            t=dict[digits[index]]                                                                     #Get the corresponding characters of current digit.
-            for i in range(len(t)):
-                current.append(t[i])                                                                  #Append a character to the temporary list of characters.
-                self.backtrack(result, current, digits, index+1, dict)                                #Backtrack next digit.
-                current.pop()                                                                         #Remove the character just appended.
-    # @param {string} digits
-    # @return {string[]}
-    def letterCombinations(self, digits):
-        dict={'2':'abc','3':'def','4':'ghi','5':'jkl','6':'mno','7':'pqrs','8':'tuv','9':'wxyz'}      #Map digits to characters.
+    def dfs(self, digitToLetter: dict, digits: str, result: List[str], stack: List[str]) -> None:
+        if not digits:                                                                                                          #If reaches the end of digits, join stack and append to result if stack is not empty, then return.
+            if stack:
+                result.append("".join(stack))
+            return
+        for x in digitToLetter[digits[0]]:                                                                                      #Traverse the corresponding letters of current digit.
+            stack.append(x)                                                                                                     #Append the letter to the stack.
+            self.dfs(digitToLetter, digits[1:], result, stack)                                                                  #Keep DFS next digit.
+            stack.pop()                                                                                                         #Pop stack.
+            
+    def letterCombinations(self, digits: str) -> List[str]:
+        digitToLetter = {'2': 'abc', '3': 'def', '4': 'ghi', '5': 'jkl', '6': 'mno', '7': 'pqrs', '8': 'tuv', '9': 'wxyz'}      #Map digits to characters.
         result=[]
-        self.backtrack(result, [], digits, 0, dict)                                                   #Begin backtracking with index=h0.
-        return result
+        self.dfs(digitToLetter, digits, result, [])                                                                             #Begin backtracking with index=h0.
+        return result                                                                                                           #Return result.
