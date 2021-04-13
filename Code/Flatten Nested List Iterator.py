bdb5287ffb67c73c88e0c24f58b029ed01755d5b
Junsheng Wang <mws19901118@gmail.com>
2021-04-13 01:46:09 -0700

    Update Flatten Nested List Iterator.py

--- Code/Flatten Nested List Iterator.py
+++ Code/Flatten Nested List Iterator.py
@@ -2,52 +2,34 @@
 # This is the interface that allows for creating nested lists.
 # You should not implement it, or speculate about its implementation
 # """
-#class NestedInteger(object):
-#    def isInteger(self):
+#class NestedInteger:
+#    def isInteger(self) -> bool:
 #        """
 #        @return True if this NestedInteger holds a single integer, rather than a nested list.
-#        :rtype bool
 #        """
 #
-#    def getInteger(self):
+#    def getInteger(self) -> int:
 #        """
 #        @return the single integer that this NestedInteger holds, if it holds a single integer
 #        Return None if this NestedInteger holds a nested list
-#        :rtype int
 #        """
 #
-#    def getList(self):
+#    def getList(self) -> [NestedInteger]:
 #        """
 #        @return the nested list that this NestedInteger holds, if it holds a nested list
 #        Return None if this NestedInteger holds a single integer
-#        :rtype List[NestedInteger]
 #        """
 
-class NestedIterator(object):
-
-    def __init__(self, nestedList):
-        """
-        Initialize your data structure here.
-        :type nestedList: List[NestedInteger]
-        """
-        self.stack = nestedList[::-1]                                               #Use stack to store nested integers. #Push all the initial nested integers to stack in reverse order.
-
-    def next(self):
-        """
-        :rtype: int
-        """
-       return self.stack.pop().getInteger()                                         #Pop stack and return its value.
-
-    def hasNext(self):
-        """
-        :rtype: bool
-        """
+class NestedIterator:
+    def __init__(self, nestedList: [NestedInteger]):
+        self.stack = nestedList[::-1]                                               #Use stack to store nested integers. Push all the initial nested integers to stack in reverse order.
+    
+    def next(self) -> int:
+        return self.stack.pop().getInteger()                                        #Pop stack and return its value.
+    
+    def hasNext(self) -> bool:
         while self.stack and not self.stack[-1].isInteger():                        #While the stack is not empty and the top of stack is list.
             top = self.stack.pop().getList()                                        #Pop stack and get its list.
             self.stack.extend(top[::-1])                                            #Push nested integers in list to stack in reverse order.
 
-        return self.stack                                                           #Return if the stack is empty
-
-# Your NestedIterator object will be instantiated and called as such:
-# i, v = NestedIterator(nestedList), []
-# while i.hasNext(): v.append(i.next())
+        return self.stack
