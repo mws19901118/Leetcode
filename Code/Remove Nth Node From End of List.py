6f2ee57c133c6ce3109c57fa4a4da52a5e38090c
Junsheng Wang <mws19901118@gmail.com>
2021-04-18 00:54:19 -0700

    Update Remove Nth Node From End of List.py

--- Code/Remove Nth Node From End of List.py
+++ Code/Remove Nth Node From End of List.py
@@ -1,22 +1,17 @@
 # Definition for singly-linked list.
 # class ListNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.next = None
-
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
 class Solution:
-    # @param {ListNode} head
-    # @param {integer} n
-    # @return {ListNode}
-    def removeNthFromEnd(self, head, n):
-        fast=head
-        slow=head
-        for i in range(n):
-            fast=fast.next              #Let fast pointer be exactly n nodes after slow pointer.
-        if fast==None:                  #If fast is none, i.e. we have to remove the first node, return the next node of head.
+    def removeNthFromEnd(self, head: ListNode, n: int) -> ListNode:
+        fast, slow = head, head
+        for _ in range(n):              #Let fast pointer be exactly n nodes after slow pointer.
+            fast = fast.next
+        if not fast:                    #If fast is none, i.e. we have to remove the first node, return the next node of head.
             return head.next
-        while fast.next!=None:          #Move fast pointer and slow pointer afterward synchronously, until fast is the tail of the linked list.
-            fast=fast.next
-            slow=slow.next
-        slow.next=slow.next.next        #Then the next node of slow is the n-th node form end, so remove it.
-        return head
+        while fast.next:                #Move fast pointer and slow pointer afterward synchronously, until fast is the tail of the linked list.
+            fast = fast.next
+            slow = slow.next
+        slow.next = slow.next.next      #Then the next node of slow is the n-th node form end, so remove it.
+        return head                     #Return head.
