773a6287473b7f1f57c376381974b4e3deef97f8
Junsheng Wang <mws19901118@gmail.com>
2021-04-19 22:09:39 -0700

    Update Combination Sum IV.py

--- Code/Combination Sum IV.py
+++ Code/Combination Sum IV.py
@@ -1,17 +1,15 @@
 class Solution:
-    def backtracking(self, nums, cache, target):                            #Return the count of possible combinations for a target.
-        if target == 0:                                                     #If target is 0, return 1.
-            return 1
-        elif target > 0:                                                    #If target is larger than 0 and target is in cache, return target[cache]
-            if target in cache:
-                return cache[target]
-            count = 0                                                       #Initialize count.
-            for x in nums:                                                  #Traverse through nums and substract x from target, then backtracking.
-                count += self.backtracking(nums, cache, target - x)         #Add up each result of backtracking.
-            cache[target] = count                                           #Add count to cache for current target.
-            return count                                                    #Return count.
-        else:                                                               #If target is smaller than 0, return 0.
-            return 0
-            
+    def dp(self, nums: List[int], cache: dict, target: int) -> int:
+        if target in cache:                                                   #If target is already in cache, return cache[target].
+            return cache[target]
+        count = 0                                                             #Count the combination sum of target.
+        for x in nums:                                                        #Traverse nums.
+            if x > target:                                                    #If x is already greater than 0, stop.
+                break
+            else:                                                             #Otherwise, add the combination sum of target - x to count.
+                count += self.dp(nums, cache, target - x)
+        cache[target] = count                                                 #Add count to cache for target.
+        return count                                                          #Return count.
     def combinationSum4(self, nums: List[int], target: int) -> int:
-        return self.backtracking(nums, {}, target)                          #Backtracking.
+        nums.sort()                                                           #Sort nums.
+        return self.dp(nums, {0: 1}, target)                                  #Start dp with memorization and the combination sum of 0 is 1.
