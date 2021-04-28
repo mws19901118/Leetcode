6e4b601558fe819177d0ba3579cc5e71fe439885
Junsheng Wang <mws19901118@gmail.com>
2021-04-28 12:06:27 -0700

    Update Unique Paths II.py

--- Code/Unique Paths II.py
+++ Code/Unique Paths II.py
@@ -1,28 +1,8 @@
 class Solution:
-    # @param obstacleGrid, a list of lists of integers
-    # @return an integer
-    def uniquePathsWithObstacles(self, obstacleGrid):               #Simple dynamic programming. If encounter a obstacle, set the number of paths in current coordinate to 0.
-        m=len(obstacleGrid)
-        if m==0:
-            return 0
-        n=len(obstacleGrid[0])
-        if n==0:
-            return 0
-        minSum=[[0 for i in range(n) ] for j in range(m)]
-        minSum[0][0]=1
-        if obstacleGrid[0][0]==1:
-                minSum[0][0]=0
-        for i in range(1,m):
-            minSum[i][0]=minSum[i-1][0]
-            if obstacleGrid[i][0]==1:
-                minSum[i][0]=0
-        for i in range(1,n):
-            minSum[0][i]=minSum[0][i-1]
-            if obstacleGrid[0][i]==1:
-                minSum[0][i]=0
-        for i in range(1,m):
-            for j in range(1,n):
-                minSum[i][j]=minSum[i-1][j]+minSum[i][j-1]
-                if obstacleGrid[i][j]==1:
-                    minSum[i][j]=0
-        return minSum[m-1][n-1]
+    def uniquePathsWithObstacles(self, obstacleGrid: List[List[int]]) -> int:
+        dp = [0] * (len(obstacleGrid[0]) + 1)                       #Initialize dp array with dp[1] = 1 and others 0.
+        dp[1] = 1
+        for row in obstacleGrid:                                    #Traverse rows in obstacleGrid.
+            for i, x in enumerate(row):                             #Traverse each row.
+                dp[i + 1] = 0 if x else dp[i + 1] + dp[i]           #If current cell is 0, set dp[i + 1] to 0; otherwise, set it to dp[i + 1] + dp[i] as the sum of paths from up and left.
+        return dp[-1]                                               #Return the last element in dp.
