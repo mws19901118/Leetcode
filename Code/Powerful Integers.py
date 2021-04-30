2e8ba3d2958c255e5277c13aa308250e8b5c1ff3
Junsheng Wang <mws19901118@gmail.com>
2021-04-30 00:56:24 -0700

    Update Powerful Integers.py

--- Code/Powerful Integers.py
+++ Code/Powerful Integers.py
@@ -1,26 +1,20 @@
-class Solution(object):
-    def powerfulIntegers(self, x, y, bound):
-        """
-        :type x: int
-        :type y: int
-        :type bound: int
-        :rtype: List[int]
-        """
-        expx = [1]                            #Find all the power of x which is smaller than bound.
-        p = x
-        while p < bound and p > expx[-1]:     #Current power has to be larger than the last one to avoid dead loop when x is 1.
-            expx.append(p)
-            p *= x
-        expy = [1]                            #Find all the power of y which is smaller than bound.
-        p = y
-        while p < bound and p > expy[-1]:     #Current power has to be larger than the last one to avoid dead loop when y is 1.
-            expy.append(p)
-            p *= y
-        powerful = set()                      #Use a set to store all possible powerful integers.
+class Solution:
+    def findPowerUnderBound(self, num: int, bound: int) -> List[int]:
+        powers = [1]                                                                            #Initialize powers with 1.
+        if num > 1:                                                                             #If num > 1, find all pwers of num under bound.
+            p = num
+            while p < bound:
+                powers.append(p)
+                p *= num
+        return powers
+    
+    def powerfulIntegers(self, x: int, y: int, bound: int) -> List[int]:
+        expx, expy = self.findPowerUnderBound(x, bound), self.findPowerUnderBound(y, bound)     #Find all the powers of x and y which is not larger than bound respectively.
+        powerful = set()                                                                        #Use a set to store all possible powerful integers.
         for i in expx:
             for j in expy:
                 if i + j <= bound:
-                    powerful.add(i + j)       #Traverse each pair of power of x and power y, if the sum of them is not larger than bound, add to the set.
+                    powerful.add(i + j)                                                         #Traverse each pair of power of x and power y, if the sum of them is not larger than bound, add to the set.
                 else:
                     break
-        return list(powerful)                 #Return the set as list.
+        return list(powerful)                                                                   #Return the set as list.
