9bfc5e41d152977efb5187059718a1f257116db0
Junsheng Wang <mws19901118@gmail.com>
2021-05-05 01:42:51 -0700

    Update Jump Game II.py

--- Code/Jump Game II.py
+++ Code/Jump Game II.py
@@ -1,18 +1,10 @@
 class Solution:
-    # @param {integer[]} nums
-    # @return {integer}
-    def jump(self, nums):
-        lastReach=0                                         #Record the max index last step can reach.
-        currentReach=0                                      #Record the max index current step can reach.
-        steps=0
-        i=0                                                 #Record current index.
-        while currentReach<len(nums)-1:
-            while i<=lastReach:                             #Only consider the incresement between current step and last step.
-                currentReach=max(currentReach,i+nums[i])    #Update currentReach.
-                i+=1                                        
-            if currentReach==lastReach:                     #If current step can't reach further than last step, return -1.
-                return -1
-            else:
-                steps+=1
-                lastReach=currentReach                      #Set lastReach as currentReach.
-        return steps
+    def jump(self, nums: List[int]) -> int:
+        lastReach, reach, count = -1, 0, 0                      #Initialize the max index previous step can reach, current step can reach and step count.
+        while lastReach < reach and reach < len(nums) - 1:      #Traverse while can reach further.
+            farReach = 0
+            for i in range(lastReach + 1, reach + 1):           #Calculate the farest reach.
+                farReach = max(farReach, i + nums[i])
+            count += 1                                          #Increase count.
+            lastReach, reach = reach, farReach                  #Set lastReach to be reach and reach to be farReach.
+        return count                                            #Return count.
