f377d48b791474abc198a8af52865c12cff100e0
Junsheng Wang <mws19901118@gmail.com>
2021-05-12 00:31:48 -0700

    Update Range Sum Query 2D - Immutable.py

--- Code/Range Sum Query 2D - Immutable.py
+++ Code/Range Sum Query 2D - Immutable.py
@@ -1,33 +1,16 @@
-class NumMatrix(object):
-    def __init__(self, matrix):
-        """
-        initialize your data structure here.
-        :type matrix: List[List[int]]
-        """
-        self.row = len(matrix)
-        if self.row == 0:
-            return
-        self.column = len(matrix[0])
-        if self.column == 0:
-            return
-        self.sumfromstart = [[0 for j in range(self.column + 1)] for i in range(self.row + 1)]          #Store the sum of matrix whose top left corner is the top left corner of origin matrix and bottom right corner is every element of original matrix.
-        for i in range(self.row):
-            for j in range(self.column):
-                self.sumfromstart[i + 1][j + 1] = matrix[i][j] + self.sumfromstart[i][j + 1] + self.sumfromstart[i + 1][j] - self.sumfromstart[i][j]
+class NumMatrix:
 
-    def sumRegion(self, row1, col1, row2, col2):
-        """
-        sum of elements matrix[(row1,col1)..(row2,col2)], inclusive.
-        :type row1: int
-        :type col1: int
-        :type row2: int
-        :type col2: int
-        :rtype: int
-        """
-        return self.sumfromstart[row2 + 1][col2 + 1] - self.sumfromstart[row1][col2  + 1] - self.sumfromstart[row2 + 1][col1] + self.sumfromstart[row1][col1]
+    def __init__(self, matrix: List[List[int]]):
+        m, n = len(matrix), len(matrix[0])                                                                          #Get dimemsions.
+        self.s = [[0 for j in range(n + 1)] for i in range(m + 1)]
+        for i in range(m):                                                                                          #Store the sum of matrix whose top left corner is the top left corner of origin matrix and bottom right corner is every element of original matrix.
+            for j in range(n):
+                self.s[i + 1][j + 1] = matrix[i][j] + self.s[i][j + 1] + self.s[i + 1][j] - self.s[i][j]
+        
+    def sumRegion(self, row1: int, col1: int, row2: int, col2: int) -> int:
+        return self.s[row2 + 1][col2 + 1] - self.s[row2 + 1][col1] - self.s[row1][col2 + 1] + self.s[row1][col1]    #Calculate sum and return.
 
 
 # Your NumMatrix object will be instantiated and called as such:
-# numMatrix = NumMatrix(matrix)
-# numMatrix.sumRegion(0, 1, 2, 3)
-# numMatrix.sumRegion(1, 2, 3, 4)
+# obj = NumMatrix(matrix)
+# param_1 = obj.sumRegion(row1,col1,row2,col2)
