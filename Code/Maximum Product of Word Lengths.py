6d53c09aa6bd60bbda1fd6f0645140a40348b6e0
Junsheng Wang <mws19901118@gmail.com>
2021-05-27 01:02:43 -0700

    Update Maximum Product of Word Lengths.py

--- Code/Maximum Product of Word Lengths.py
+++ Code/Maximum Product of Word Lengths.py
@@ -1,21 +1,14 @@
-class Solution(object):
-    def maxProduct(self, words):
-        """
-        :type words: List[str]
-        :rtype: int
-        """
-        dict = {}
+class Solution:
+    def maxProduct(self, words: List[str]) -> int:
+        lengths = defaultdict(int)
         for s in words:
-            integer = 0
+            binary = 0
             for c in s:
-                integer |= 1 << (ord(c) - ord('a'))             #Map the letter set of a string to a 26-bit integer. Each bit indicates if the string has correspoding letter.
-            if integer not in dict:
-                dict[integer] = len(s)                          #Store the length of string in a dict and the key is the correspoding integer.
-            else:
-                dict[integer] = max(dict[integer], len(s))      #For every integer, only store the length of the longest string.
+                binary |= 1 << (ord(c) - ord('a'))                  #Map the letter set of a string to a 26-bit binary. Each bit indicates if the string has correspoding letter.
+            lengths[binary] = max(lengths[binary], len(s))          #For every binary, only store the length of the longest string.
         result = 0
-        for i in dict.keys():                                   #Traverse through every 2 integer pair.
-            for j in dict.keys():
-                if i & j == 0:                                  #If the and value of 2 integer is 0, the 2 strings don't share common letter.
-                    result = max(result, dict[i] * dict[j])     #Update the max product.
+        for x, lx in lengths.items():                               #Traverse through every binary pair.
+            for y, ly in lengths.items():
+                if x & y == 0:                                      #If x & y == 0, the 2 strings don't share common letter.
+                    result = max(result, lx * ly)                   #Update the max product.
         return result
