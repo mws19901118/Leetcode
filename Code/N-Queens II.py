20302c6d0fcb3c14cfdb6dfbda7e8a7097139576
Junsheng Wang <mws19901118@gmail.com>
2021-05-29 00:25:50 -0700

    Update N-Queens II.py

--- Code/N-Queens II.py
+++ Code/N-Queens II.py
@@ -1,48 +1,22 @@
 class Solution:
-    def lock(self, board, x, y):                              #Most code is the same with N-Queen.
-        n=len(board)
-        board[x][y]=-1
-        for i in range(x+1,n):
-            board[i][y]+=1
-        for i in range(0,y):
-            board[x][i]+=1
-        for i in range(y+1,n):
-            board[x][i]+=1
-        for i in range(min(n-1-x,y)):
-            board[x+1+i][y-1-i]+=1
-        for i in range(min(n-1-x,n-1-y)):
-            board[x+1+i][y+1+i]+=1
-        
-    def unlock(self, board, x, y):
-        n=len(board)
-        board[x][y]=0
-        for i in range(x+1,n):
-            board[i][y]-=1
-        for i in range(0,y):
-            board[x][i]-=1
-        for i in range(y+1,n):
-            board[x][i]-=1
-        for i in range(min(n-1-x,y)):
-            board[x+1+i][y-1-i]-=1
-        for i in range(min(n-1-x,n-1-y)):
-            board[x+1+i][y+1+i]-=1
-
-
-    def setQueen(self, ans, board, x, y):
-        n=len(board)
-        self.lock(board, x, y)
-        if x==n-1:
-            ans[0]+=1
-        else:
-            for i in range(n):
-                if board[x+1][i]==0:
-                    self.setQueen(ans, board, x+1, i)
-        self.unlock(board, x, y)
-    
-    # @return a list of lists of string
-    def totalNQueens(self, n):
-        ans=[0]                                             #Because I can not modify a number in method, use a list which has only one element counting the total number.
-        board=[[0 for i in range(n)] for j in range(n)]
-        for i in range(n):
-            self.setQueen(ans, board,0,i)
-        return ans[0]
+    def dfs(self, x: int, n: int, board: List[List[str]], column: List[int], diagonal1: List[int], diagonal2: List[int]) -> int:
+        if x == n:                                                                                                                      #If DFS has finished last row, we found a solution, so return 1.
+            return 1
+        result = 0
+        for y in range(n):                                                                                                              #Traverse each cell in current row.
+            if not column[y] and not diagonal1[x + y] and not diagonal2[n - 1 - x + y]:                                                 #If there is no queens in this column and 2 diagonals, we can place a queen here.
+                board[x][y] = 'Q'
+                column[y] += 1                                                                                                          #Update column, diagonal1 and diagonal2.
+                diagonal1[x + y] += 1
+                diagonal2[n - 1 - x + y] += 1
+                result += self.dfs(x + 1, n, board, column, diagonal1, diagonal2)                                                       #DFS next row.
+                board[x][y] = '.'                                                                                                       #Restore the board.
+                column[y] -= 1
+                diagonal1[x + y] -= 1
+                diagonal2[n - 1 - x + y] -= 1
+        return result
+                
+    def solveNQueens(self, n: int) -> int:
+        board = [['.' for j in range(n)] for i in range(n)]                                                                             #Initialize empty board.
+        column, diagonal1, diagonal2 = [0] * n, [0] * (2 * n - 1), [0] * (2 * n - 1)                                                    #Initialize the number of queens in each column and diagonal(2 directions).
+        return self.dfs(0, n, board, column, diagonal1, diagonal2)                                                                      #DFS from first row.
