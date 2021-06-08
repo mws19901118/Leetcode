1eb03a5019bba75c05b20144f99d252e46a8c5e8
Junsheng Wang <mws19901118@gmail.com>
2021-06-08 11:43:35 -0700

    Update Construct Binary Tree from Preorder and Inorder Traversal.py

--- Code/Construct Binary Tree from Preorder and Inorder Traversal.py
+++ Code/Construct Binary Tree from Preorder and Inorder Traversal.py
@@ -1,21 +1,19 @@
-# Definition for a  binary tree node
+# Definition for a binary tree node.
 # class TreeNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
-
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
 class Solution:
-    # @param preorder, a list of integers
-    # @param inorder, a list of integers
-    # @return a tree node
-    def buildTree(self, preorder, inorder):
-        length=len(preorder)
-        if length==0:
+    def build(self, preorder: List[int], s1: int, e1: int, inorder: List[int], s2: int, e2: int, indexes: dict) -> TreeNode:        #Helper to build tree with preorder[s1:e1 + 1], inorder[s2:e2 + 1] and cached indexes of integers in inorder.
+        if s1 > e1 or s2 > e2:                                                                                                      #If the slice of preorder or inorder is empty, return none.
             return None
-        root=TreeNode(preorder[0])                                            #Root is always the first element of preorder.
-        if length>1:
-            mid=inorder.index(preorder[0])                                    #Find the index of root in inorder.
-            root.left=self.buildTree(preorder[1:mid+1], inorder[0:mid])       #The left child of root is from 1 to mid+1 in preorder while from 0 to mid in inder.
-            root.right=self.buildTree(preorder[mid+1:], inorder[mid+1:])      #The right child of root is from mid+1 to the end in both preorder and inorder.
-        return root
+        node = TreeNode(preorder[s1])                                                                                               #Create a node with the first integer in the preorder slice.
+        index = indexes[preorder[s1]]                                                                                               #Find the corresponding index in inorder.
+        node.left = self.build(preorder, s1 + 1, s1 + (index - s2), inorder, s2, index - 1, indexes)                                #Construct the left child. For preorder, the offset of left child is (index - s2).
+        node.right = self.build(preorder, s1 + (index - s2) + 1, e1, inorder, index + 1, e2, indexes)                               #Construct the right child.
+        return node                                                                                                                 #Return node.
+    
+    def buildTree(self, preorder: List[int], inorder: List[int]) -> TreeNode:
+        indexes = {x: i for i, x in enumerate(inorder)}                                                                             #Cache indexes of integers in inorder.
+        return self.build(preorder, 0, len(preorder) - 1, inorder, 0, len(inorder) - 1, indexes)                                    #Return constructed binary tree.
