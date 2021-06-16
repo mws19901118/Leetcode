103aed0ab02912c4be4145b6df923de25bfb33a4
Junsheng Wang <mws19901118@gmail.com>
2021-06-16 00:38:05 -0700

    Update Generate Parentheses.py

--- Code/Generate Parentheses.py
+++ Code/Generate Parentheses.py
@@ -1,27 +1,34 @@
 class Solution:
-    def backtrack(self, n, p, count, result):
-        if len(p)==2*n:                           #If current length is 2*n, the backtracking comes to an end.
-            if count==0:                          #If there is no unmatched left parentheses, we find a combination of well-formes parentheses.
-                result.append("".join(p))         #Append current answer to result.
-        else:
-            if count<n:                           #Only when count<n can we add left parenthesis at current position.
-                p.append('(')
-                count+=1                          #Adding a left parenthesis means here comes a new unmatched left parenthesis.
-                self.backtrack(n,p,count,result)
-                p.pop()
-                count-=1
-            if count>0:                           #Only when count>0 can we add right parenthesis at current position.
-                p.append(')')
-                count-=1                          #Adding a right parenthesis means there is an unmatched left parenthesis being matched.
-                self.backtrack(n,p,count,result)
-                p.pop()
-                count+=1
+    def backtrack(self, n: int, p: List[str], count: int, result: List[str]):
+        if len(p) == 2 * n:                                                     #If current length is 2 * n, the backtracking comes to an end.
+            if count == 0:                                                      #If there is no unmatched left parentheses, we find a combination of valid parentheses.
+                result.append("".join(p))                                       #Append current answer to result.
+            return
+        if count < n:                                                           #Only when count < n can we add left parenthesis at current position.
+            p.append('(')
+            self.backtrack(n, p, count + 1, result)                             #Add a left parenthesis and keep backtracking.
+            p.pop()
+        if count > 0:                                                           #Only when count > 0 can we add right parenthesis at current position.
+            p.append(')')
+            self.backtrack(n, p, count - 1, result)                             #Add a right parenthesis and keep backtracking.
+            p.pop()
             
-    # @param {integer} n
-    # @return {string[]}
-    def generateParenthesis(self, n):
-        p=[]                                      #Record current sequence of parentheses.
-        count=0                                   #Record the number of unmatched left parentheses.
-        result=[]
-        self.backtrack(n,p,count,result)          #Backtrack.
+    def generateParenthesis(self, n: int) -> List[str]:
+        result = []
+        self.backtrack(n, [], 0, result)                                        #Backtracking.
         return result
+
+class Solution:
+    @cache                                                                      #Cache result.
+    def generateParenthesis(self, n: int) -> List[str]:
+        if n == 1:                                                              #If n == 1, return one pair of parenthesis.
+            return ["()"]
+        parenthesis = set()                                                     #Use a set to store valid parenthesis of length n.
+        for i in range(1, (n + 1) // 2 + 1):                                    #Traverse from 1 to n / 2.
+            for p1 in self.generateParenthesis(i):                              #Get all valid parenthesis for i.
+                for p2 in self.generateParenthesis(n - i):                      #Get all valid parenthesis for n - i.
+                    parenthesis.add(p1 + p2)                                    #Add p1 + p2 to set.
+                    parenthesis.add(p2 + p1)                                    #Add p2 + p1 to set.
+        for p in self.generateParenthesis(n - 1):                               #Get all valid parenthesis for n - 1.
+            parenthesis.add("(" + p + ")")                                      #Add (p) to set.
+        return parenthesis                                                      #Return set.
