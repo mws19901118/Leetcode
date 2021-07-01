e4bbf9ba13cbcbae36c254c2abe184d23c045c4e
Junsheng Wang <mws19901118@gmail.com>
2021-07-01 11:44:39 -0700

    Update Integer to Roman.py

--- Code/Integer to Roman.py
+++ Code/Integer to Roman.py
@@ -1,11 +1,9 @@
 class Solution:
-    # @param {integer} num
-    # @return {string}
-    def intToRoman(self, num):                      #Use a table to store Roman numeral corresponding to each digit.
+    def intToRoman(self, num: int) -> str:              #Use a table to store Roman numeral corresponding to each digit.
         table = [['', 'I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX'], ['', 'X', 'XX', 'XXX', 'XL', 'L', 'LX', 'LXX', 'LXXX', 'XC'], ['', 'C', 'CC', 'CCC', 'CD', 'D', 'DC', 'DCC', 'DCCC', 'CM'], ['', 'M', 'MM', 'MMM']]
         roman = ''
-        for i in range(3, -1, -1):
+        for i in reversed(range(4)):
             l = 10 ** i
-            roman += table[i][num // l]            #Convert the integer to Roman numeral digit by digit.
+            roman += table[i][num // l]                 #Convert the integer to Roman numeral digit by digit.
             num %= l
         return roman
