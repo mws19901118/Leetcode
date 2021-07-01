0e3b676adb4e44be4512c2f6d663f8e5d0e2262a
Junsheng Wang <mws19901118@gmail.com>
2021-07-01 14:09:36 -0700

    Update Gray Code.py

--- Code/Gray Code.py
+++ Code/Gray Code.py
@@ -1,11 +1,9 @@
 class Solution:
-    # @return a list of integers
-    def grayCode(self, n):
-        if n==0:                                              #If n equals 0, return [0].
-            return [0]
-        else:
-            result=self.grayCode(n-1)                         #The first half of gray code sequence for n is the gray code sequence for n-1.
-            length=2**(n-1)
-            for i in range(length):
-                result.append(length|result[length-1-i])      #The second half of gray code sequence for n is the reverse gray code sequence for n-1 bitwise ORed with 2^(n-1).
-            return result
+    def grayCode(self, n: int) -> List[int]:
+        code = [0, 1]                           #Initialize gray code for n = 1.
+        mask = 1                                #Initialize mask to be 2 ** (n - 1).
+        for i in range(2, n + 1):               #Generate gray codes from 2 to n.
+            mask <<= 1                          #Left shift mask by 1 bit to make it 2 ** (i - 1). 
+            for j in reversed(range(mask)):     #Traverse backwards the gray code from i - 1, whose length equals mask.
+                code.append(code[j] | mask)     #Append code[j] | mask to code, to form the gray code of i. The second half of gray code is the reverse of first half with the most significant bit is 1.
+        return code
