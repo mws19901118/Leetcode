9745f50ac8feb8d668566b70030a0de8a5e43c01
Junsheng Wang <mws19901118@gmail.com>
2021-07-09 00:26:21 -0700

    Update Longest Increasing Subsequence.py

--- Code/Longest Increasing Subsequence.py
+++ Code/Longest Increasing Subsequence.py
@@ -1,19 +1,10 @@
-class Solution(object):
+class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
-        leastendelement = []                                        #The ith element stores the smallest ending element for increasing subsequence whose length is i.
-        for x in nums:
-            if leastendelement == [] or x > leastendelement[-1]:
-                leastendelement.append(x)                           #If current element is greater than the ending element of current longest increasing subsequence, append current element to leastendelement.
-            else:                                                   #Otherwise, binary search the smallest number in the least that is greater than current element and replace it with current element.
-                start = 0
-                end = len(leastendelement)
-                while start < end:
-                    mid = (start + end) // 2
-                    if leastendelement[mid] >= x and (mid == 0 or leastendelement[mid - 1] < x):
-                        leastendelement[mid] = x
-                        break
-                    elif leastendelement[mid] < x:
-                        start = mid + 1
-                    else:
-                        end = mid - 1
-        return len(leastendelement)                                 #Return the length of leastendelement.
+        subsequence = []                                    #The ith element stores the smallest ending element for increasing subsequence whose length is i.
+        for x in nums:                                      #Traverse nums.
+            if not subsequence or subsequence[-1] < x:      #If current longest increasing subsequence is empty or its last element is smaller than x, append x to subsequence.
+                subsequence.append(x)
+            else:                                           #Otherwise, Binary search the first index to insert x in subsequence and still keep it sorted and replace the element in that index with x.
+                index = bisect_left(subsequence, x) 
+                subsequence[index] = x
+        return len(subsequence)                             #Return the length of subsequence.
