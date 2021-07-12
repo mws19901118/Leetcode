b5612a2f67fa3a875fe80e387bf82d447cac2034
Junsheng Wang <mws19901118@gmail.com>
2021-07-12 00:16:47 -0700

    Update Isomorphic Strings.py

--- Code/Isomorphic Strings.py
+++ Code/Isomorphic Strings.py
@@ -1,29 +1,9 @@
 class Solution:
-    # @param {string} s
-    # @param {string} t
-    # @return {boolean}
-    def isIsomorphic(self, s, t):
-        ss=[]                               #Store the index of first apperence of each character of s.
-        tt=[]                               #Store the index of first apperence of each character of t.
-        ls=len(s)
-        lt=len(t)
-        if ls!=lt:
-            return False
-        for i in range(ls):
-            j=0
-            while j<i:
-                if s[ss[j]]==s[i]:
-                    break
-                j+=1
-            ss.append(j)
-        for i in range(lt):
-            j=0
-            while j<i:
-                if t[tt[j]]==t[i]:
-                    break
-                j+=1
-            tt.append(j)
-        if ss==tt:
-            return True
-        else:
-            return False
+    def isIsomorphic(self, s: str, t: str) -> bool:
+        map1, map2 = {}, {}                                                                                                 #Map characters in s to t and characters in t to s.
+        for x, y in zip(s, t):                                                                                              #Traverse s and t simultaneously.
+            if x not in map1 and y not in map2:                                                                             #If both x and y is not mapped, map x to y and y to x.
+                map1[x], map2[y] = y, x
+            elif (x not in map1 and y in map2) or (x in map1 and y not in map2) or y != map1[x] or x != map2[y]:            #If one is mapped but the other is not or x and y are not mapped to each other, s and t are not isomorphic strings, return false.
+                return False
+        return True                                                                                                         #Return true.
