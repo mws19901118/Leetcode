b8def7081ed45852cb096fdf3cc4db2132b7eea5
Junsheng Wang <mws19901118@gmail.com>
2021-07-13 19:20:12 -0700

    Update Find Peak Element.py

--- Code/Find Peak Element.py
+++ Code/Find Peak Element.py
@@ -1,19 +1,10 @@
-class Solution(object):
-    def findPeakElement(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
-        l = len(nums)
-        nums.insert(0, -0x8fffffff)                                         #Add a negative max in the start.
-        nums.append(-0x8fffffff)                                            #Add a negative max in the end.
-        start = 1
-        end = l
-        while start <= end:                                                 #Do binary search from 1 to l.
-            mid = (start + end) / 2
-            if nums[mid] > nums[mid - 1] and nums[mid] > nums[mid + 1]:     #If nums[mid] is a peak element, return mid - 1(becase we added a nagative max in the start).
-                return mid - 1
-            elif nums[mid] > nums[mid - 1] and nums[mid] < nums[mid + 1]:   #If it's in a rising subarray, there must be at least 1 peak element in the second half.
+class Solution:
+    def findPeakElement(self, nums: List[int]) -> int:
+        start, end = 0, len(nums) - 1
+        while start < end:                                                   #Do binary search from 0 to len(nums) - 1.
+            mid = (start + end) // 2
+            if nums[mid] > nums[mid + 1]:                                    #If nums[mid] > nums[mid + 1], potential peak is in nums[:mid + 1].
+                end = mid
+            else:                                                            #Otherwise potential peak is in nums[mid + 1:].
                 start = mid + 1
-            else:                                                           #Otherwise, there must be at least 1 peak element in the first half.
-                end = mid - 1
+        return start                                                         #Return start after binary search.
