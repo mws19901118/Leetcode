294423e80afa0180fd850f204a60ca1dd26cf2b5
Junsheng Wang <mws19901118@gmail.com>
2021-07-18 15:23:32 -0700

    Update Reverse Nodes in k-Group.py

--- Code/Reverse Nodes in k-Group.py
+++ Code/Reverse Nodes in k-Group.py
@@ -1,29 +1,26 @@
 # Definition for singly-linked list.
 # class ListNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.next = None
-
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
 class Solution:
-    # @param {ListNode} head
-    # @param {integer} k
-    # @return {ListNode}
-    def reverseKGroup(self, head, k):
-        if  head==None:                               #If the linked list is empty, return none.
-            return None
-        count=0
-        nextgroup=head
-        while nextgroup!=None and count<k:            #Find the (k+1)-th element in the linked list.
-            nextgroup=nextgroup.next
-            count+=1
-        if count<k:                                   #If the number of elements in the linked list is smaller than k, return the head node.
-            return head
-        tail=head
-        cursor=head.next
-        for i in range(k-1):                          #Otherwise, reverse the first k elements.
-            currenthead=cursor
-            cursor=cursor.next
-            currenthead.next=head
-            head=currenthead
-        tail.next=self.reverseKGroup(nextgroup, k)    #Recursively reverse the remain linked list and append the result to the tail of reversed first part of linked list.
-        return head
+    def reverseKGroup(self, head: ListNode, k: int) -> ListNode:
+        dummyHead = ListNode()                      #Create a dummy head.
+        tail = dummyHead                            #Store the tail pointer of new linked list.
+        while head:                                 #Traverse linked list.
+            count, curr = 0, head                   #Initialize count and curr for the traverse of next group.
+            while count < k and curr:               #Traverse next group and count nodes.
+                count += 1
+                curr = curr.next
+            if count < k:                           #If count < k, we reach the last group, so no need to revrese.
+                tail.next = head                    #Directly point the next of tail to head and move head to curr, which is none.
+                head = curr
+            else:                                   #Otherwise, we have to reverse current group.
+                for _ in range(k):                  #Traverse nodes in current group.
+                    temp = head                     #Point a temp pointer to current node.
+                    head = head.next                #Move head to next.
+                    temp.next = tail.next           #Insert temp to the next of tail.
+                    tail.next = temp
+                for _ in range(k):                  #Move tail to the actual tail after insert the reversed group.
+                    tail = tail.next
+        return dummyHead.next                       #Return the next of dummy head.
