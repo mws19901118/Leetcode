c74bac53657abfb66cada8901e07674d2f2bd286
Junsheng Wang <mws19901118@gmail.com>
2021-07-19 00:11:27 -0700

    Update Lowest Common Ancestor of a Binary Search Tree.py

--- Code/Lowest Common Ancestor of a Binary Search Tree.py
+++ Code/Lowest Common Ancestor of a Binary Search Tree.py
@@ -11,10 +11,9 @@ class Solution:
     # @param {TreeNode} q
     # @return {TreeNode}
     def lowestCommonAncestor(self, root, p, q):
-        while root!=None:
-            if (root.val<=p.val and root.val>=q.val) or (root.val>=p.val and root.val<=q.val): #If root.val is between p.val and q.val, root is the LCA.
-                return root
-            elif root.val>p.val and root.val>q.val:                                            #If root.val is greater than both p.val and q.val, the LCA is in the left child of root.
-                root=root.left
-            else:                                                                              #If root.val is smaller than both p.val and q.val, the LCA is in the right child of root.
-                root=root.right
+        if (root.val <= p.val and root.val >= q.val) or (root.val <= q.val and root.val >= p.val):      #If root.val is between p.val and q.val, root is the LCA.
+            return root
+        elif root.val > p.val and root.val > q.val:                                                     #If root.val is greater than both p.val and q.val, the LCA is in the left child of root.
+            return self.lowestCommonAncestor(root.left, p, q)
+        else:                                                                                           #If root.val is smaller than both p.val and q.val, the LCA is in the right child of root.
+            return self.lowestCommonAncestor(root.right, p, q)
