4beb880d54502594f147be17b7d5840eca618656
Junsheng Wang <mws19901118@gmail.com>
2021-07-26 00:30:52 -0700

    Update Convert Sorted Array to Binary Search Tree.py

--- Code/Convert Sorted Array to Binary Search Tree.py
+++ Code/Convert Sorted Array to Binary Search Tree.py
@@ -1,28 +1,12 @@
-# Definition for a  binary tree node
+# Definition for a binary tree node.
 # class TreeNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
-
-def transform(num,start,end,node):
-    mid=(start+end)/2                                 #Because num is in ascending order, the index of value of node should be the mean of start and end.
-    node.val=num[mid]
-    if start<mid:                                     #Mid is larger than start means that node has left child.
-        node.left=TreeNode(-1)                        #Initialize the left child of node.
-        transform(num, start, mid-1, node.left)       #Construct the BST whose root is the left child of node. 
-    if end>mid:                                       #Mid is smaller than end means that node has right child.
-        node.right=TreeNode(-1)                       #Initialize the right child of node.
-        transform(num, mid+1, end, node.right)        #Construct the BST whose root is the right child of node. 
-
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
 class Solution:
-    # @param num, a list of integers
-    # @return a tree node
-    def sortedArrayToBST(self, num):
-        length=len(num)
-        root=TreeNode(-1)                             #Initialize root.
-        if length==0:
+    def sortedArrayToBST(self, nums: List[int]) -> TreeNode:
+        if not nums:                                                                                                    #If nums is empty, return none.
             return None
-        transform(num, 0, length-1, root)
-        return root
-        
+        mid = len(nums) // 2                                                                                            #Find the index of element in the middle in nums.
+        return TreeNode(nums[mid], self.sortedArrayToBST(nums[:mid]), self.sortedArrayToBST(nums[mid + 1:]))            #Create TreeNode with nums[mid] as value and left child and right child created recursively.
