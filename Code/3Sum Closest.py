892bbf4645808d28c4d835b7ec46409c466d8803
Junsheng Wang <mws19901118@gmail.com>
2021-07-27 11:12:22 -0700

    Update 3Sum Closest.py

--- Code/3Sum Closest.py
+++ Code/3Sum Closest.py
@@ -1,21 +1,17 @@
 class Solution:
-    # @param {integer[]} nums
-    # @param {integer} target
-    # @return {integer}
-    def threeSumClosest(self, nums, target):
-        nums.sort()                                   #Sort the list.
-        result=0x7fffffff                             #Set the initial value of result to be a very large value.
-        for i in range(len(nums)-2):                  #Enumerate the first smallest integer i of the 3 integers.
-            j=i+1                                     #Set the second integer j to be the integer behind i.
-            k=len(nums)-1                             #Set the third integer k to be the last element in the list.
-            while j<k:                                #Ensure j is smaller than or equal to k.
-                t=nums[i]+nums[j]+nums[k]             #This is the temporary sum t=i+j+k.
-                if t==target:                         #If t equals target, it's the closest sum, return it.
-                    return t
-                if abs(t-target)<abs(result-target):  #If the distance between t and target is smaller than the distance between result and target, update result with t.
-                    result=t
-                if t<target:                          #If t is smaller than target, replace j with the next integer to increase temporary sum.
-                    j+=1
-                else:                                 #If t is greater than target, replace k with the previous integer to decrease temporary sum.
-                    k-=1
+    def threeSumClosest(self, nums: List[int], target: int) -> int:
+        nums.sort()                                             #Sort the list.
+        result = 100000                                         #Set the initial value of result to be a very large value.
+        for i in range(len(nums)):                              #Traverse nums as the smallest of 3 integers. 
+            start, end = i + 1, len(nums) - 1                   #Set the second integer to be the integer at i + 1 and thrid integer to be the integer at len(nums) - 1.
+            while start < end:                                  #Iterate while start is smaller than end.
+                sum3 = nums[i] + nums[start] + nums[end]        #Calculate the 3 sum.
+                if abs(sum3 - target) < abs(result - target):   #If it's closer to target than current result, update result.
+                    result = sum3
+                if sum3 < target:                               #If it's smaller than target, move start forward because list is sorted.
+                    start += 1
+                elif sum3 > target:                             #If it's larger than target, move end backward because list is sorted.
+                    end -= 1
+                else:                                           #If it's target, directly return target.
+                    return target
         return result
