65cfd8799b6dccdb5781c27d17787e76d7c28db1
Junsheng Wang <mws19901118@gmail.com>
2021-07-31 23:59:58 -0700

    Update Trapping Rain Water.py

--- Code/Trapping Rain Water.py
+++ Code/Trapping Rain Water.py
@@ -1,30 +1,13 @@
 class Solution:
-    # @param {integer[]} height
-    # @return {integer}
-    def trap(self, height):
-        l=len(height)
-        if l<=2:                                                  #To trap water, length of integers should be at least 3.
-            return 0
-        
-        sum=0                                                     #Record the total water trapped volume.
-        i=0                                                       #This is the first pointer.
-        while i<l:
-            j=1                                                   #This is the second pointer.
-            temp=0                                                #Record current trapped water volume.
-            maxh=0                                                #Record the max height of integer behind i.
-            maxgap=j                                              #Record the distance betweem maxh and i.
-            maxcurrenttrap=0                                      #Record the current trapped water when traverse reaches maxh(excluded).
-            while i+j<l and height[i+j]<height[i]:
-                if height[i+j]>=maxh:                             #Find the maxh(as rear as possible), and then update maxh, maxg and maxcurrenttrap.
-                    maxh=height[i+j]
-                    maxg=j
-                    maxcurrenttrap=temp
-                temp+=height[i]-height[i+j]                       #Calculate current trapped water volume.
-                j+=1
-            if i+j==l:                                            #If reaches the end, the delta sum is maxcurrenttrap minus the area of void rectangle above current valid trapped water whose height is height[i]-maxh and width is maxg-1.
-                sum+=maxcurrenttrap-(maxg-1)*(height[i]-maxh)
-                i+=maxg                                           #Continue traverse at i+maxg.
-            else:                                                 #If encounters a higher integer, add current trapped water to sum and continue traverse at 1+j.
-                i+=j
-                sum+=temp
-        return sum
+    def trap(self, height: List[int]) -> int:
+        count, left, right, leftMax, rightMax = 0, 0, len(height) - 1, 0, 0         #Initalize count, left point, right pointer, left max height and right max height.
+        while left < right:                                                         #Traverse while left is smaller than right, the taller of left height and right height will always be the current max on corresponding side.
+            if height[left] < height[right]:                                        #If left height is shorter than right height, the water trapped depends on the left height. 
+                leftMax = max(leftMax, height[left])                                #Update left max height.
+                count += leftMax - height[left]                                     #Add leftMax - height[left] to count.
+                left += 1                                                           #Increase left pointer.
+            else:                                                                   #Otherwise, the water trapped depends on the right height.
+                rightMax = max(rightMax, height[right])                             #Update right max height.
+                count += rightMax - height[right]                                   #Add rightMax - height[right] to count.
+                right -= 1                                                          #Decrease right pointer.
+        return count                                                                #Return count.
