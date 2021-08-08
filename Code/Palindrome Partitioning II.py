7ee1de0cab79afa266e1b8517cf5fa92f2c50476
Junsheng Wang <mws19901118@gmail.com>
2021-08-07 22:57:16 -0700

    Update Palindrome Partitioning II.py

--- Code/Palindrome Partitioning II.py
+++ Code/Palindrome Partitioning II.py
@@ -1,19 +1,10 @@
 class Solution:
-    # @param s, a string
-    # @return an integer
-    def minCut(self, s):                                              #This is a dynamic programming problem.
-        n=len(s)
-        A=[[False for a in range(n)] for b in range(n)]               #A[i][j] means whether s[i..j] forms a palindrome. Note:Have to initialize a 2-D list in this way.If use 'A=[[False]*n]*n', every row will change as one row changes.
-        cut=[n]*n                                                     #cut[i] means the minCut for s[i..n-1]
-        i=n-1
-        while i>=0:
-            cut[i]=n-1-i
-            for j in xrange(i,n):
-                if s[i]==s[j] and (j-i<2 or A[i+1][j-1]):             #if s[i]==s[j] and s[i+1..j-1] is a palindrome or the length of s[i..j] less than 2, s[i..j] forms a palindrome, thus A[i][j] is True.
-                    A[i][j]=True
-                    if j==n-1:                                        #if j==n-1, the string s[i..n-1] is a palindrome, minCut is 0, cut[i]=0; 
-                        cut[i]=0
-                    elif cut[j+1]+1<cut[i]:                           #if the current cut num (first cut s[i..j] and then cut the rest s[j+1...n-1]) is 1+cut[j+1], compare it to the exisiting minCut num cut[i], repalce if smaller.
-                        cut[i]=cut[j+1]+1
-            i-=1
-        return cut[0]                                                 #return minCut for s[0..n-1]
+    def minCut(self, s: str) -> int:
+        isPalindrome = [[False for _ in range(len(s))] for _ in range(len(s))]                  #Initialize 2D array where isPalindrome[i][j] indicating whether s[i:j + 1] forms a palindrome. 
+        cut = [len(s)] * len(s)                                                                 #Cut[i] means the min cut for s[i:]
+        for i in reversed(range(len(s))):                                                       #Traverse backward.
+            for j in range(i, len(s)):                                                          #Traverse from s[i] to the end of s.
+                if s[i] == s[j] and (j - i < 2 or isPalindrome[i + 1][j - 1]):                  #If s[i] == s[j] and s[i + 1:j] is a palindrome or the length of s[i:j + 1] less than 2, s[i:j + 1] forms a palindrome.
+                    isPalindrome[i][j] = True
+                    cut[i] = 0 if j == len(s) - 1 else min(cut[i], cut[j + 1] + 1)              #If j == len(s) - 1, the string s[i:] is a palindrome, cut[i] = 0; otherwise update cut[i] to be cut[j] + 1 if the latter one is smaller.
+        return cut[0]                                                                           #Return min cut for s.
