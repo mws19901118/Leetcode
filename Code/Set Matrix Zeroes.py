3802576c219efa357d180974651c69b6828a8b53
Junsheng Wang <mws19901118@gmail.com>
2021-08-13 14:51:23 -0700

    Update Set Matrix Zeroes.py

--- Code/Set Matrix Zeroes.py
+++ Code/Set Matrix Zeroes.py
@@ -1,27 +1,30 @@
 class Solution:
-    # @param matrix, a list of lists of integers
-    # RETURN NOTHING, MODIFY matrix IN PLACE.
-    def setZeroes(self, matrix):
-        row=len(matrix)
-        if row==0:
-            return False
-        column=len(matrix[0])
-        if column==0:
-            return False
-        sign=0xFFFFFFFF                                           #A rare value of integer to indicate the integer below a 0.
-        for i in range(row):
-            flag=False                                            #Initially, the current row should not be set to 0.
-            for j in range(column):
-                if matrix[i][j]==0:                               #If current integer is 0, set flag to True, set all the integers above it to 0 and set the integer(If have) below it to sign.
-                    if i+1<row and matrix[i+1][j]!=0:
-                        matrix[i+1][j]=sign
-                    flag=True
-                    for k in range(i):
-                        matrix[k][j]=0
-                if matrix[i][j]==sign:                            #If current integer is below a 0, set it to 0 and set the integer(If have) below it to sign.
-                    if i+1<row and matrix[i+1][j]!=0:
-                        matrix[i+1][j]=sign
-                    matrix[i][j]=0
-            if flag==True:                                        #If find a 0 in current row, set the whole row to 0.
-                for j in range(column):
-                    matrix[i][j]=0
+    def setZeroes(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        m, n = len(matrix), len(matrix[0])                  #Get dimensions.
+        firstRow, firstColumn = False, False                #Indicate if first row has 0 and first column has 0.
+        for i, j in product(range(m), range(n)):            #Traverse matrix.
+            if matrix[i][j] != 0:                           #If current cell is not 0, continue.
+                continue
+            if i == 0:                                      #If i == 0, update firstRow.
+                firstRow = True
+            if j == 0:                                      #If j == 0, update firstColumn.
+                firstColumn = True
+            matrix[i][0], matrix[0][j] = 0, 0               #Set the first cell in current row and column to 0 to indicate that this row or column should be set to 0.
+
+        for i in range(1, m):                               #Traverse rows except first row.
+            if matrix[i][0] == 0:                           #If the first cell is 0, set the entire row to 0.
+                for j in range(n):
+                    matrix[i][j] = 0
+        for j in range(1, n):                               #Traverse columns except first column.
+            if matrix[0][j] == 0:                           #If the first cell is 0, set the entire column to 0.
+                for i in range(m):
+                    matrix[i][j] = 0
+        if firstRow:                                        #If first row should be set to 0, set first row to 0.
+            for j in range(n):
+                matrix[0][j] = 0
+        if firstColumn:                                     #If first column should be set to 0, set first column to 0.
+            for i in range(m):
+                matrix[i][0] = 0
