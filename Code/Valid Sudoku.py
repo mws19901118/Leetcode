a5e7022386630123461dec1fd342f00fa008b677
Junsheng Wang <mws19901118@gmail.com>
2021-08-20 00:20:13 -0700

    Update Valid Sudoku.py

--- Code/Valid Sudoku.py
+++ Code/Valid Sudoku.py
@@ -1,28 +1,11 @@
 class Solution:
-    # @param {character[][]} board
-    # @return {boolean}
-    def isValidSudoku(self, board):
-        row=[{} for i in range(9)]                          #Record the dicts for every row.
-        column=[{} for i in range(9)]                       #Record the dicts for every column.
-        square=[{} for i in range(9)]                       #Record the dicts for every little square.
-        for i in range(9):
-            for j in range(9):
-                c=board[i][j]
-                if c=='.':                                  #If c is not digit, continue.
-                    continue
-                    
-                if c in row[i]:                             #If c is already in a row, return false; otherwise, store c in correspoding row dict.
-                    return False
-                else:
-                    row[i][c]=True
-                    
-                if c in column[j]:                          #If c is already in a column, return false; otherwise, store c in correspoding column dict.
-                    return False
-                else:
-                    column[j][c]=True
-                
-                if c in square[i/3*3+j/3]:                  #If c is already in a little square, return false; otherwise, store c in correspoding little square dict.
-                    return False
-                else:
-                    square[i/3*3+j/3][c]=True
-        return True                                         #If there is no conflicts, return true.
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        row, column, box = [{} for i in range(9)], [{} for i in range(9)], [{} for i in range(9)]           #Record the dicts for every row, column or box.
+        for i, j in product(range(9), range(9)):                                                            #Traverse board.
+            if board[i][j] == '.':                                                                          #If c is not digit, continue.
+                continue
+            c = board[i][j]
+            if c in row[i] or c in column[j] or c in box[i // 3 * 3 + j // 3]:                              #If c is already in a row, a column or a box, return false.
+                return False
+            row[i][c] = column[j][c] = box[i // 3 * 3 + j // 3][c] = True                                   #Store c in corresponding row, column and box.
+        return True                                                                                         #Return true at the end.
