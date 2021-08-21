659d9e140cd9c2fc887515b7db7adafde93f86f2
Junsheng Wang <mws19901118@gmail.com>
2021-08-21 00:55:21 -0700

    Update Valid Sudoku.py

--- Code/Valid Sudoku.py
+++ Code/Valid Sudoku.py
@@ -2,10 +2,10 @@ class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
         row, column, box = [{} for i in range(9)], [{} for i in range(9)], [{} for i in range(9)]           #Record the dicts for every row, column or box.
         for i, j in product(range(9), range(9)):                                                            #Traverse board.
-            if board[i][j] == '.':                                                                          #If c is not digit, continue.
+            if board[i][j] == '.':                                                                          #If x is not digit, continue.
                 continue
-            c = board[i][j]
-            if c in row[i] or c in column[j] or c in box[i // 3 * 3 + j // 3]:                              #If c is already in a row, a column or a box, return false.
+            x = board[i][j]
+            if x in row[i] or x in column[j] or x in box[i // 3 * 3 + j // 3]:                              #If x is already in a row, a column or a box, return false.
                 return False
-            row[i][c] = column[j][c] = box[i // 3 * 3 + j // 3][c] = True                                   #Store c in corresponding row, column and box.
+            row[i][x] = column[j][x] = box[i // 3 * 3 + j // 3][x] = True                                   #Store x in corresponding row, column and box.
         return True                                                                                         #Return true at the end.
