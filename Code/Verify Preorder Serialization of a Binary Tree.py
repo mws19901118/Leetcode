c88cd4e2cb19fddf22999f01ee286c47abbcff26
Junsheng Wang <mws19901118@gmail.com>
2021-08-26 09:06:23 -0700

    Update Verify Preorder Serialization of a Binary Tree.py

--- Code/Verify Preorder Serialization of a Binary Tree.py
+++ Code/Verify Preorder Serialization of a Binary Tree.py
@@ -1,24 +1,14 @@
-class Solution(object):
-    def isValidSerialization(self, preorder):
-        """
-        :type preorder: str
-        :rtype: bool
-        """
-        if preorder == '':                                      #If preoder is empty, return true.
-            return True
-        p = preorder.split(',')                                 #Split preorder by ','.
-        if p[0] == '#':                                         #If the root is '#', it's valid only if the length of p is 1. 
-            return len(p) == 1
-        stack = [(p[0], 0)]                                     #Append the root to stack.
-        i = 1                                                   #Record the current node.
-        while stack != [] and i < len(p):                       #While stack is not empty and current node is valid, do the followings:
-            if p[i] != '#':                                     #If current node is not '#', append a tuple to stack.
-                stack.append((p[i], 0))                         #The first value of the tuple is value of current node, the second value is an int. 0 indicates we havn't finished traversing its left child.
-                i += 1                                          #Go to next node.
-            else:                                               #Otherwise, if stack is not empty, pop the stack.
-                if stack != []:
-                    node = stack.pop()
-                    if node[1] == 0:                            #If the second value of the popped tuple is 0, set it to 1 and append the modified tuple to stack.
-                        stack.append((node[0], 1))              #1 indicates we have finished traversing the left child but haven't finished traversing the right child.
-                        i += 1
-        return stack == [] and i == len(p) - 1                  #If stack is empty and we exactly traversed every node, the preorder is valid; otherwise, it's invalid.
+class Solution:
+    def isValidSerialization(self, preorder: str) -> bool:
+        nodes = preorder.split(",")                       #Split preorder string by ",".
+        stack = []                                        #Use stack to store node.
+        for i, x in enumerate(nodes):                     #Traverse nodes.
+            if x != '#':                                  #If x is number, append false to stack, meaning that hasn't visited the right child of current node.
+                stack.append(False)
+            else:                                         #Otherwise, while stack top is true, pop stack.
+                while stack and stack[-1]:
+                    stack.pop()
+                if not stack:                             #If stack is empty, x must be the last element in nodes to make it a valid preorder serialization of binary tree.
+                    return i == len(nodes) - 1
+                stack[-1] = True                          #Set top of stack to true, meaning that has visited the right child of current node.
+        return not stack                                  #Return if the stack is empty.
