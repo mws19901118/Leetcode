0d26896e472772eeac98dffd0d56fa8f31d6fdc5
Junsheng Wang <mws19901118@gmail.com>
2021-08-29 15:29:58 -0700

    Update Patching Array.py

--- Code/Patching Array.py
+++ Code/Patching Array.py
@@ -1,18 +1,11 @@
-class Solution(object):
-    def minPatches(self, nums, n):
-        """
-        :type nums: List[int]
-        :type n: int
-        :rtype: int
-        """
-        miss = 1                                      #Indicate the smallest sum in [1, n] that is missing.
-        count = 0                                     #Count the total number of patches.
-        i = 0                                         #Traverse from start to end.
-        while miss <= n:                              #miss should not be greater than n.
-            if i < len(nums) and nums[i] <= miss:     #If nums[i] is smaller than or equal to miss, we can build all the sum in [1, miss + nums[i]).
+class Solution:
+    def minPatches(self, nums: List[int], n: int) -> int:
+        miss, count, i = 1, 0, 0                        #Initialize the smallest missing number, total count of patches and the pointer which traverses in nums. 
+        while miss <= n:                                #Iterate while miss is not be greater than n.
+            if i < len(nums) and nums[i] <= miss:       #If nums[i] is smaller than or equal to miss, we can build all the sum in [1, miss + nums[i]).
                 miss += nums[i]
                 i += 1
-            else:                                     #Otherwise, we have to add a number at miss. Then we can build all the sum in [1, 2 * miss).
-                miss += miss
+            else:                                       #Otherwise, we have to add a number at miss. Then we can build all the sum in [1, 2 * miss).
+                miss *= 2
                 count += 1
         return count
