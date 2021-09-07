946f2ba97f79209e6816e881f0fd5ab88ff557fb
Junsheng Wang <mws19901118@gmail.com>
2021-09-07 00:17:19 -0700

    Update Reverse Linked List.py

--- Code/Reverse Linked List.py
+++ Code/Reverse Linked List.py
@@ -1,20 +1,14 @@
 # Definition for singly-linked list.
 # class ListNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.next = None
-
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
 class Solution:
-    # @param {ListNode} head
-    # @return {ListNode}
-    def reverseList(self, head):
-        if head==None:
-            return head
-        current=head.next                       #Record current node.
-        head.next=None                          #After reverse, the original head node will become tail, so set the next node of head to none.
-        while current!=None:
-            nextnode=current.next               #Record the next node of current node.
-            current.next=head                   #Insert current node in front of head node.
-            head=current                        #Update head node.
-            current=nextnode                    #Update current node.
-        return head
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        dummyHead = ListNode(None)              #Create a dummy node.
+        while head:                             #Travese linked list.
+            curr = head                         #Get current node.
+            head = head.next                    #Move head to next.
+            curr.next = dummyHead.next          #Point next of current node to next of dummyHead.
+            dummyHead.next = curr               #Insert curr after dummyHead.
+        return dummyHead.next                   #Return the next of dummyHead.
