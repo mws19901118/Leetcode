ef2aff164e489b9582224fc19799f169d578a779
Junsheng Wang <mws19901118@gmail.com>
2021-09-11 17:53:31 -0700

    Update Basic Calculator.py

--- Code/Basic Calculator.py
+++ Code/Basic Calculator.py
@@ -1,30 +1,25 @@
-class Solution(object):
-    def calculate(self, s):
-        """
-        :type s: str
-        :rtype: int
-        """
+class Solution:
+    def calculate(self, s: str) -> int:
+        stack = [0]                                                             #Initialize a stack to store number and operator.
+        sign, currentNumber = '+', 0                                            #Initialize current sign and number.
+        operation = {'+': lambda x, y: x + y, '-': lambda x, y: x - y}          #Use lambda function to represent plus and minus.
         s = '(' + s + ')'                                                       #Add parentheses outside s.
-        def cal_right_parentheses(s, i):                                        #Calculate the value inside current parentheses.
-            num = 0                                                             #Store current number, which hasn't been calculated.
-            res = 0                                                             #Store current result.
-            op = '+'                                                            #Default operator is plus.
-            calculation = {'+': lambda x, y: x + y, '-': lambda x, y: x - y}    #Use lambda function to represent plus and minus.
-            
-            while i < len(s):                                                   #Traverse through s.
-                if s[i] in '+-':                                                #If encounters '+' or '-', calculate res and num with previous operator.
-                    res = calculation[op](res, num)
-                    num = 0                                                     #Reset num to 0.
-                    op = s[i]                                                   #Set operator to current one.
-                elif s[i] in '0123456789':                                      #If encounters digits, update num.
-                    num = num * 10 + int(s[i])
-                elif s[i] == '(':                                               #If encounters left parenthesis, recursively calculate the value inside current parentheses.
-                    num,i = cal_right_parentheses(s, i + 1)
-                elif s[i] == ')':                                               #If encounters right parenthesis, calculate res and num with previous operator.
-                    res = calculation[op](res, num)
-                    num = 0                                                     #Reset num to 0.
-                    return res, i                                               #Return res and current index.
-                i += 1
-            res = calculation[op](res, num)                                     #Calculate res and num with previous operator.
-            return res, i                                                       #Return res and current index.
-        return cal_right_parentheses(s, 0)[0]
+        for x in s:                                                             #Traverse s.
+            if x == ' ':                                                        #If x is space, skip it.
+                continue
+            elif x.isdigit():                                                   #If x is dight, update current number.
+                currentNumber = currentNumber * 10 + int(x)
+            elif x == '+' or x == '-':                                          #If x is '+' or '-', calculate the result with stack[-1] and update stack[-1].
+                stack[-1] = operation[sign](stack[-1], currentNumber)
+                sign, currentNumber = x, 0                                      #Set sign to x and clear current number.
+            elif x == '(':                                                      #If x is '(', append current sign to stack and append 0 as starting a new calculation inside parentheses.
+                stack.append(sign)
+                stack.append(0)
+                sign, currentNumber = '+', 0                                    #Set sign to '+' and clear current number.
+            elif x == ')':                                                      #If x is ')', calculate the result with stack[-1] and update stack[-1].
+                stack[-1] = operation[sign](stack[-1], currentNumber)
+                currentNumber = stack.pop()                                     #Pop the result inside parentheses.
+                perviousSign = stack.pop()                                      #Pop the operator before parentheses.
+                stack[-1] = operation[perviousSign](stack[-1], currentNumber)   #Merge the result inside parenteses with previous result.
+                sign, currentNumber = '+', 0                                    #Set sign to '+' and clear current number.
+        return stack[0]                                                         #Return stack[0].
