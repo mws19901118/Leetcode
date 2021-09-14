cbfe11e5b093887c579a723267b70b8e76226ffb
Junsheng Wang <mws19901118@gmail.com>
2021-09-14 07:43:05 -0700

    Update Reverse Only Letters.py

--- Code/Reverse Only Letters.py
+++ Code/Reverse Only Letters.py
@@ -1,14 +1,10 @@
 class Solution:
-    def reverseOnlyLetters(self, S):
-        """
-        :type S: str
-        :rtype: str
-        """
-        letters = [c for c in S if c.isalpha()]     #Find all letters in S and put them in a list in order.
+    def reverseOnlyLetters(self, s: str) -> str:
+        letters = [c for c in s if c.isalpha()]     #Find all letters in s and put them in a list in order.
         ans = []
-        for c in S:
-            if c.isalpha():                         #For each letter in S, append the reverse letter to ans.
+        for c in s:
+            if c.isalpha():                         #For each letter in s, append the reverse letter to ans.
                 ans.append(letters.pop())
-            else:                                   #For each non-letter in S, append it to ans.
+            else:                                   #For each non-letter in s, append it to ans.
                 ans.append(c)
         return "".join(ans)                         #Join ans and return.
