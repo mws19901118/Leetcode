83d8d7774a1a2f5b24a887d9f57d5d0d6c8e69d1
Junsheng Wang <mws19901118@gmail.com>
2021-09-16 10:28:34 -0700

    Update Spiral Matrix.py

--- Code/Spiral Matrix.py
+++ Code/Spiral Matrix.py
@@ -1,29 +1,24 @@
 class Solution:
-    # @param matrix, a list of lists of integers
-    # @return a list of integers
-    def spiralOrder(self, matrix):
-        result=[]
-        m=len(matrix)
-        if m==0:
-            return result
-        n=len(matrix[0])
-        if n==0:
-            return result
-        mine=min(m,n)                                               #Find the min length of edges
-        for i in range(mine/2):                                     #Loop for mine/2 times to deal with the outer level.
-            for j in range(n-2*i):                                  #Deal with the top line, the ith row, with index starting at i, ending at n-i-1.
-                result.append(matrix[i][j+i])
-            for j in range(m-2*i-1):                                #Deal with the right line, the (n-1-i)th column, with index starting at i+1, ending at m-i-1.
-                result.append(matrix[j+i+1][n-1-i])
-            for j in range(n-2*i-1):                                #Deal with the bottom line, the (m-1-i)th row, with index starting at n-2-i, ending at i.
-                result.append(matrix[m-1-i][n-2-i-j])
-            for j in range(m-2*(i+1)):                              #Deal with the left line, the ith column, with index starting at m-2-i, ending at i+1.
-                result.append(matrix[m-2-i-j][i])
-        if mine%2==1:                                               #If mine%2 equals 1, there is still a column(m>=n) or a row(m<n) hasn't bee de
-            if m>=n:
-                for i in range(m-(mine/2)*2):                       #Calculate the number of remaining elements.
-                    result.append(matrix[mine/2+i][mine/2])         #Traverse downward starting at matrix[mine/2][mine/2]
-            else:
-                for i in range(n-(mine/2)*2):                       #Calculate the number of remaining elements.
-                    result.append(matrix[mine/2][mine/2+i])         #Traverse rightward starting at matrix[mine/2][mine/2]
-        return result
+    def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
+        m, n = len(matrix), len(matrix[0])                              #Get the dimensions.
+        result = []                                                     #Initialize result.
+        rowLength, columnLength = n - 1, m - 1                          #Initialize the spiral row length and spiral column length.
+        for i in range(min(m // 2, n // 2)):                            #Traverse from outer spiral to inner spiral; the number of full spiral is min(m // 2, n // 2).
+            for j in range(i, i + rowLength):                           #Append top spiral row.
+                result.append(matrix[i][j])
+            for j in range(i, i + columnLength):                        #Append right spiral column.
+                result.append(matrix[j][-(i + 1)])
+            for j in reversed(range(i + 1, i + rowLength + 1)):         #Append bottom spiral row.
+                result.append(matrix[-(i + 1)][j])
+            for j in reversed(range(i + 1, i + columnLength + 1)):      #Append left spiral column.
+                result.append(matrix[j][i])
+            rowLength -= 2                                              #Decrease spiral row length by 2 for next spiral.
+            columnLength -= 2                                           #Decrease spiral column length by 2 for next spiral.
+        if rowLength > -1 and columnLength > -1:                        #If both spiral row length and spiral column length is greater than -1, there is still one innermost partial spiral unvisited.
+            if rowLength >= columnLength:                               #If spiral row length is not smaller than spiral column length, append final row to result.
+                for i in range(rowLength + 1):
+                    result.append(matrix[m // 2][m // 2 + i])
+            else:                                                       #Otherwise, append final column to result.
+                for i in range(columnLength + 1):
+                    result.append(matrix[n // 2 + i][n // 2])
+        return result                                                   #Return result.
