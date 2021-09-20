45c598d9f15e95cc92d0f485859cc14b50196116
Junsheng Wang <mws19901118@gmail.com>
2021-09-19 23:04:36 -0700

    Update Distinct Subsequences.py

--- Code/Distinct Subsequences.py
+++ Code/Distinct Subsequences.py
@@ -1,18 +1,8 @@
 class Solution:
-    # @return an integer
-    def numDistinct(self, S, T):
-        if len(S)==0 or len(T)==0:
-            return 0
-        dp=[0]*len(T)
-        for i in range(len(S)):
-            for j in range(len(T)):
-                if S[len(S)-1-i]==T[j]:
-                    if j==len(T)-1:
-                        dp[j]+=1
-                    else:
-                        dp[j]+=dp[j+1]
-        return dp[0]
-        
-  #Basically the idea is iterate from the end of S, and if SS[len(S)-1-i]==T[j], 
-  #then the distinct sub sequences of T.substring(j) in S.substring(i) can be calculated as dp[j]=dp[j]+dp[j+1]. 
-  #The final result is occu[0]. 
+    def numDistinct(self, s: str, t: str) -> int:
+        dp = [0 for _ in range(len(t))]                                                         #Initialize dp array; dp[j] means the number of distinct subsequences equals t[:j + 1] in for s[i + 1] while i is traversing.
+        for i in range(len(s)):                                                                 #Traverse s.
+            prev = 0                                                                            #Initialize the dp[j - 1] for s[i] to be 0 because j can be 0 and that means no subsequence in s[:i + 1] equals empty string.
+            for j in range(len(t)):                                                             #Traverse t.
+                dp[j], prev = dp[j] + int(s[i] == t[j]) * (prev if j > 0 else 1), dp[j]         #Update dp[j] to be new value and prev to dp[j]. If s[i] != t[j], dp[j] remains same. Otherwise, there are more new subsequnces. Particularly, 1 more if j == 0. If j > 0, all subsequences eqauls t[:j] in s[:i] can form t[:j + 1] with s[i], so add prev to dp[j].
+        return dp[-1]                                                                           #Return dp[-1].
