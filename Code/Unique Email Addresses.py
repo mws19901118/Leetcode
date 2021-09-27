aa2aefe2dc02d503706dd9e23b0b8f6121824e19
Junsheng Wang <mws19901118@gmail.com>
2021-09-27 00:20:03 -0700

    Update Unique Email Addresses.py

--- Code/Unique Email Addresses.py
+++ Code/Unique Email Addresses.py
@@ -1,18 +1,8 @@
 class Solution:
-    def numUniqueEmails(self, emails):
-        """
-        :type emails: List[str]
-        :rtype: int
-        """
-        count = set()                         #Use a set to store the tuple of local name and domain name.
-        for e in emails:
-            splits = e.split("@")             #Split email by '@' to get the local name and domain name.
-            local = splits[0]
-            domain = splits[1]
-            index = local.find("+")           #Find the index of '+' in local name.
-            if index != -1:
-                local = local[:index]         #Ignore '+' and everything after it in local name.
-            local = local.replace(".", "")    #Remove all '.' in local name.
-            count.add((local, domain))        #Add the tuple of local name and domain name to set.
-        return len(count)                     #Return the unique count of tuples.
-            
+    def numUniqueEmails(self, emails: List[str]) -> int:
+        s = set()                                                                                                   #Store local name and domain name tuple in a set.
+        for e in emails:                                                                                            #Traverse emails.
+            atIndex = e.index('@')                                                                                  #Find the index of '@'.
+            localName, domainName = e[:atIndex].replace('.', '').split('+')[0], e[atIndex + 1:]                     #Local name is the substring before '@' then remove all '.' and ignore substring after '+'; domain name is the substring after '@'.
+            s.add((localName, domainName))                                                                          #Add tuple to s.
+        return len(s)                                                                                               #Return length of s.
