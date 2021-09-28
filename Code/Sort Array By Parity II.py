639168cedd5f0236eb4f8ba47a39efea206b8be0
Junsheng Wang <mws19901118@gmail.com>
2021-09-28 00:15:30 -0700

    Update Sort Array By Parity II.py

--- Code/Sort Array By Parity II.py
+++ Code/Sort Array By Parity II.py
@@ -1,16 +1,13 @@
 class Solution:
-    def sortArrayByParityII(self, A):
-        """
-        :type A: List[int]
-        :rtype: List[int]
-        """
-        result = [None] * len(A)    #Create a list with the same length of A.
-        even, odd = 0, 1            #Even number starts with inde 0 while odd number starts with index 1.
-        for a in A:
-            if a % 2:               #For each number in A, if it's odd, put it in the current odd index of result and increase odd by 2.
-                result[odd] = a
-                odd += 2
-            else:                   #For each number in A, if it's even, put it in the current even index of result and increase odd by 2.
-                result[even] = a
+    def sortArrayByParityII(self, nums: List[int]) -> List[int]:
+        even, odd = 0, 1                                          #Initialize pointers of even index and odd index.
+        while True:                                               #Traverse nums.
+            while even < len(nums) and nums[even] & 1 == 0:       #While current number on even index is even, move forward even index.
                 even += 2
-        return result
+            while odd < len(nums) and nums[odd] & 1 == 1:         #While current number on odd index is odd, move forward odd index.
+                odd += 2
+            if even < len(nums) and odd < len(nums):              #If both even and odd index is valid, swap the numbers on them.
+                nums[even], nums[odd] = nums[odd], nums[even]
+            else:                                                 #Otherwise, jump out of loop.
+                break
+        return nums                                               #Return nums.
