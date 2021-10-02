44f93a433368b589e7c08c842f39afd871fa0e8e
Junsheng Wang <mws19901118@gmail.com>
2021-10-01 22:13:55 -0700

    Update Dungeon Game.py

--- Code/Dungeon Game.py
+++ Code/Dungeon Game.py
@@ -1,18 +1,9 @@
 class Solution:
-    # @param dungeon, a list of lists of integers
-    # @return a integer
-    def calculateMinimumHP(self, dungeon):
-        row=len(dungeon)
-        column=len(dungeon[0])
-        HP=[[1 for i in range(column)] for i in range(row)]
-        for i in range(row):
-            for j in range(column):
-                if i==0 and j==0:
-                    HP[row-1-i][column-1-j]=max(HP[row-1-i][column-1-j]-dungeon[row-1-i][column-1-j],1)       #For the bottom-right corner, HP[i][j]=max(1-dungeon[i][j],1)
-                elif i==0 and j!=0:
-                    HP[row-1-i][column-1-j]=max(HP[row-1-i][column-j]-dungeon[row-1-i][column-1-j],1)         #For the bottom line, HP[i][j]=max(HP[i][j+1]-dungeon[i][j],1)
-                elif i!=0 and j==0:
-                    HP[row-1-i][column-1-j]=max(HP[row-i][column-1-j]-dungeon[row-1-i][column-1-j],1)         #For the right line, HP[i][j]=max(HP[i+1][j]-dungeon[i][j],1)
-                else:
-                    HP[row-1-i][column-1-j]=max(min(HP[row-1-i][column-j],HP[row-i][column-1-j])-dungeon[row-1-i][column-1-j],1)  #For any cell else, HP[i][j]=max(min(HP[i][j+1],HP[i+1][j])-dungeon[i][j],1)
-        return HP[0][0]         #Return the min HP of starting cell.
+    def calculateMinimumHP(self, dungeon: List[List[int]]) -> int:
+        m, n = len(dungeon), len(dungeon[0])                                                                    #Get the dimensions.
+        dp = [200001] * n                                                                                       #Initialize dp list with size n. In current iteration of text1[:i + 1], dp[j] meams the min hp for dungeon[i][j] alive.
+        dp[-1] = 1                                                                                              #Set dp[-1] to 1 while other other indexes to a really large number(larger than the max sum of a row).
+        for i in reversed(range(m)):                                                                            #Traverse dungeon from bottom right corner to top left corner.
+            for j in reversed(range(n)):
+                dp[j] = max(1, (dp[j] if j == n - 1 else min(dp[j + 1], dp[j])) - dungeon[i][j])                #Since knight can only move rightward or downward, min hp for dungeon[i][j] will first take the smaller min hp dungeon[i + 1][j] and dungeon[i][j + 1](only dungeon[i + 1][j] if j is at the end and intialize dp[j] at a really lartge number will cover the case where i == m - 1). Then substract dungeon[i][j] from previous value. Also, dp[j] should be at least 1.
+        return dp[0]                                                                                            #Return dp[0].
