ab63296b3d43d9000f3fe671bebec28ad0fe16f6
Junsheng Wang <mws19901118@gmail.com>
2021-10-03 21:40:51 -0700

    Update Island Perimeter.py

--- Code/Island Perimeter.py
+++ Code/Island Perimeter.py
@@ -1,15 +1,12 @@
 class Solution:
     def islandPerimeter(self, grid: List[List[int]]) -> int:
-        if not grid:
-            return 0
-        m, n = len(grid), len(grid[0])
+        m, n = len(grid), len(grid[0])                      #Get dimensions.
         perimeter = 0
-        for i in range(m):                                      #Traverse through grid.
-            for j in range(n):
-                if grid[i][j] == 1:                             #If current cell is island, add 4 to perimeter.
-                    perimeter += 4
-                    if i - 1 >= 0 and grid[i - 1][j] == 1:      #If upper cell is island too, minus 2 from perimeter.
-                        perimeter -= 2
-                    if j - 1 >= 0 and grid[i][j - 1] == 1:      #If left cell is island too, minus 2 from perimeter.
-                        perimeter -= 2
+        for i, j in product(range(m), range(n)):            #Traverse through grid.
+            if grid[i][j] == 1:                             #If current cell is island, add 4 to perimeter.
+                perimeter += 4
+                if i - 1 >= 0 and grid[i - 1][j] == 1:      #If upper cell is island too, minus 2 from perimeter.
+                    perimeter -= 2
+                if j - 1 >= 0 and grid[i][j - 1] == 1:      #If left cell is island too, minus 2 from perimeter.
+                    perimeter -= 2
         return perimeter
