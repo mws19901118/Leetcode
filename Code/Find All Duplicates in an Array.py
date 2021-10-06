941575cfe9a975f86854089cc4a7cc7de98de3fb
Junsheng Wang <mws19901118@gmail.com>
2021-10-05 23:40:16 -0700

    Update Find All Duplicates in an Array.py

--- Code/Find All Duplicates in an Array.py
+++ Code/Find All Duplicates in an Array.py
@@ -1,9 +1,5 @@
-class Solution(object):
-    def findDuplicates(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: List[int]
-        """
+class Solution:
+    def findDuplicates(self, nums: List[int]) -> List[int]:
         result = []
         for x in nums:                      #Because the 1 ≤ a[i] ≤ n (n = size of array), use the a[abs(a[i]) - 1] to record if a[i] has been visited during traverse.
             if nums[abs(x) - 1] < 0:        #If a[abs(a[i]) - 1] is negative, a[i] is already visited, add a[i] to result.
