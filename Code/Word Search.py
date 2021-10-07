7d89bf01c11d16068d73af73a483ab8e0e734f8b
Junsheng Wang <mws19901118@gmail.com>
2021-10-06 22:03:22 -0700

    Update Word Search.py

--- Code/Word Search.py
+++ Code/Word Search.py
@@ -1,34 +1,20 @@
 class Solution:
-    # @param board, a list of lists of 1 length string
-    # @param word, a string
-    # @return a boolean
-    def exist(self, board, word):
-        def backtrack(i,j,k):               #Backtracking search; i,j is the coordinate of current character in board and k is the index of current character in word.
-            if (i,j) in stack or (i<0 or i>=row or j<0 or j>=column) or board[i][j]!=word[k]:     #If coordinate is out of bound or coordinate has aleardy been in the path or the character in board doesn't match the character in word, return false.
-                return False
-            else:
-                stack.append((i,j))         #Append current coordinate to the stack.
-                if k==n-1:                  #If find the word, return true.
-                    return True
-                else:
-                    if backtrack(i+1, j, k+1) or backtrack(i-1, j, k+1) or backtrack(i, j+1, k+1) or backtrack(i, j-1, k+1):    #Search the adjcent characters.  
-                        return True
-                    else:
-                        stack.pop()         #If can not find the word, pop stack and return false.
-                        return False
-        
-        row=len(board)
-        if row==0:
+    def find(self, board: List[List[str]], x: int, y: int, visited: set, word: str) -> bool:
+        if board[x][y] != word[0]:                                                                                                                  #If board[x][y] != word[0], return false.
             return False
-        column=len(board[0])
-        if column==0:
-            return False
-        n=len(word)
-        if n==0:
-            return False
-        for i in range(row):
-            for j in range(column):
-                stack=[]                            #Record the path of backtracking search.
-                if backtrack(i,j,0):                #If find word, return true.
-                    return True
-        return False                                #If can not find word, return false.
+        if len(word) == 1:                                                                                                                          #If len(word) == 1, we have found the word, return true.
+            return True
+        m, n = len(board), len(board[0])                                                                                                            #Get dimensions.
+        result = False                                                                                                                              #Initialize result.
+        visited.add((x, y))                                                                                                                         #Add (x, y) to visited.
+        for nx, ny in [(x - 1, y), (x, y + 1), (x + 1, y), (x, y - 1)]:                                                                             #Traverse 4 neighbors.
+            result |= (nx >= 0 and nx < m and ny >= 0 and ny < n and (nx, ny) not in visited and self.find(board, nx, ny, visited, word[1:]))       #If neightbor is valid and unvisited, search word[1:] starting from neighbor and update result.
+        visited.remove((x, y))                                                                                                                      #Remove (x, y) from visited.
+        return result                                                                                                                               #Return result.
+
+    def exist(self, board: List[List[str]], word: str) -> bool:
+        m, n = len(board), len(board[0])                                                                                                            #Get dimensions.
+        result = False                                                                                                                              #Initialize result.
+        for i, j in product(range(m), range(n)):                                                                                                    #Traverse board.
+            result |= self.find(board, i, j, set(), word)                                                                                           #Searching word starting from current cell and update result.
+        return result                                                                                                                               #Return result.
