3a411e845c54bc91051c5128e13d34b656b1d8f8
Junsheng Wang <mws19901118@gmail.com>
2021-10-12 17:21:18 -0700

    Update Construct Binary Search Tree from Preorder Traversal.py

--- Code/Construct Binary Search Tree from Preorder Traversal.py
+++ Code/Construct Binary Search Tree from Preorder Traversal.py
@@ -7,12 +7,10 @@
 
 class Solution:
     def bstFromPreorder(self, preorder: List[int]) -> TreeNode:
-        if preorder == []:                                      #If preorder is empty, return none.
+        if not preorder:                                                #If preorder is empty, return none.
             return None
-        root = TreeNode(preorder[0])                            #Create root node for the first element in preorder.
-        i = 1
-        while i < len(preorder) and preorder[i] < preorder[0]:  #Find the subarray in preorder that all the elements are smaller than the value in root.
-            i += 1
-        root.left = self.bstFromPreorder(preorder[1:i])         #Construct left child from the subarray from last step.
-        root.right = self.bstFromPreorder(preorder[i:])         #Construct right child from the remaining array.
+        root = TreeNode(preorder[0])
+        index = bisect.bisect(preorder, root.val, 1, len(preorder))     #Binary search for the index of preorder[0] in the rest of array.
+        root.left = self.bstFromPreorder(preorder[1:index])             #Construct left child from the subarray from last step.
+        root.right = self.bstFromPreorder(preorder[index:])             #Construct right child from the remaining array.
         return root
