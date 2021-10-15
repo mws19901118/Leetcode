bcc146eac687cab2d592882a581b55dbeb8b4ea6
Junsheng Wang <mws19901118@gmail.com>
2021-10-14 21:50:28 -0700

    Update Best Time to Buy and Sell Stock with Cooldown.py

--- Code/Best Time to Buy and Sell Stock with Cooldown.py
+++ Code/Best Time to Buy and Sell Stock with Cooldown.py
@@ -1,20 +1,11 @@
-class Solution(object):
-    def maxProfit(self, prices):
-        """
-        :type prices: List[int]
-        :rtype: int
-        """
-        l = len(prices)
-        if l == 0:
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        if not prices:                                  #Return 0 if prices is empty.
             return 0
-        sell = [0, 0]                                 #Store the max profit when sell the stock on the ith day.
-        hold = [0, -prices[0]]                        #Store the max profit when hold the stock on the ith day.
-        wait = [0, 0]                                 #Store the max profit when cooldown on the ith day.
-        for i in range(1, l):
-            s = hold[-1] + prices[i]                  #Sell on current day.
-            h = max(wait[-1] - prices[i], hold[-1])   #Hold on current day, either buy on current day or hold the stock bought before.
-            w = max(sell[-1], wait[-1])               #Cooldown on current day.
-            sell.append(s)
-            hold.append(h)
-            wait.append(w)
-        return max(sell[-1], hold[-1], wait[-1])      #Return the max profit after the last day.
+        sell, wait, hold = [0], [0], [-prices[0]]       #Initialize the list to store max profit to sell, wait or hold on the ith day.
+        for p in prices[1:]:                            #Traverse prices.
+            s, w, h = sell[-1], wait[-1], hold[-1]      #Get the max profit from selling, waiting or holding on previous day.
+            sell.append(h + p)                          #If sell today, the max profit now is h + p.
+            wait.append(max(s, w))                      #If wait today, the max profit now is max(s, w).
+            hold.append(max(h, w - p))                  #If hold today, the max profit now is max(h, w - p).
+        return max(sell[-1], wait[-1], hold[-1])        #Return the max profit after the last day.
