4ca910f7f84c129b8f9ef24cc5b7e1b5898f01cc
Junsheng Wang <mws19901118@gmail.com>
2021-10-17 20:52:44 -0700

    Update Cousins in Binary Tree.py

--- Code/Cousins in Binary Tree.py
+++ Code/Cousins in Binary Tree.py
@@ -6,20 +6,22 @@
 #         self.right = right
 class Solution:
     def isCousins(self, root: TreeNode, x: int, y: int) -> bool:
-        level = set([root])
-        while level:                                                                                #BFS each level of binary tree.
-            foundX, foundY = False, False
-            nextLevel = set()
-            for node in level:
-                if node.left is not None:
-                    nextLevel.add(node.left)
-                    foundX |= node.left.val == x and (node.right is None or node.right.val != y)    #If left child is x, foundX is true only if right child is none or right child is not y.
-                    foundY |= node.left.val == y and (node.right is None or node.right.val != x)    #If left child is y, foundX is true only if right child is none or right child is not z.
-                if node.right is not None:
-                    nextLevel.add(node.right)
-                    foundX |= node.right.val == x and (node.left is None or node.left.val != y)     #If right child is x, foundX is true only if left child is none or left child is not y.
-                    foundY |= node.right.val == y and (node.left is None or node.left.val != x)     #If right child is y, foundX is true only if left child is none or left child is not x.
-            if foundX and foundY:                                                                   #If found both x and y, return true.
-                return True
-            level = nextLevel
-        return False
+        level = [root]                                              #Initialize the q.
+        while level:                                                #BFS each level of binary tree.
+            nextLevel = []                                          #Initialize the list for next level.
+            parent = None                                           #Initialize the parent of x or y for current level.
+            for node in level:                                      #Traverse all nodes in level.
+                for child in [node.left, node.right]:               #Traverse node.left and node.right.
+                    if not child:                                   #If child is none, skip it.
+                        continue
+                    nextLevel.append(child)                         #Append child to nextLevel.
+                    if child.val != x and child.val != y:           #If the value of child is neither x nor y, skip it.
+                        continue
+                    if not parent:                                  #Now we found a x or y. If parent is none, set parent to node.
+                        parent = node
+                    elif parent == node:                            #If parent is current node it self, x and y are under one parent node, so they are not cousins, then return false.
+                        return False
+                    else:                                           #Otherwise, x and y are cousions, then return true.
+                        return True
+            level = nextLevel                                       #Replace level with nextLevel.
+        return False                                                #Return false at last.
