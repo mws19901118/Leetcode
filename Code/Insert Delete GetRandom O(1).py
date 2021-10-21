c5b1449ef36f9ccf4a4081f10d661d1593a67d22
Junsheng Wang <mws19901118@gmail.com>
2021-10-20 17:21:27 -0700

    Update Insert Delete GetRandom O(1).py

--- Code/Insert Delete GetRandom O(1).py
+++ Code/Insert Delete GetRandom O(1).py
@@ -1,17 +1,10 @@
-import random
 class RandomizedSet:
 
     def __init__(self):
-        """
-        Initialize your data structure here.
-        """
         self.array = []                                           #Use an array to store the values.
         self.map = {}                                             #Use a map to store the index in array of each element.
 
     def insert(self, val: int) -> bool:
-        """
-        Inserts a value to the set. Returns true if the set did not already contain the specified element.
-        """
         if val in self.map:                                       #If val is already in map, return false.
             return False
         self.array.append(val)                                    #Append val to array.
@@ -19,9 +12,6 @@ class RandomizedSet:
         return True
 
     def remove(self, val: int) -> bool:
-        """
-        Removes a value from the set. Returns true if the set contained the specified element.
-        """
         if val not in self.map:                                   #If val is not in map, return false.
             return False
         self.map[self.array[-1]] = self.map[val]                  #Update the index of the last element to be index of val.
@@ -31,9 +21,6 @@ class RandomizedSet:
         return True
 
     def getRandom(self) -> int:
-        """
-        Get a random element from the set.
-        """
         return random.choice(self.array)                          #Return a random element in array.
 
 # Your RandomizedSet object will be instantiated and called as such:
