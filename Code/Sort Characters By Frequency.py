021e69f3a73052f597251a8a7c77c77634445ed4
Junsheng Wang <mws19901118@gmail.com>
2021-10-21 19:08:28 -0700

    Update Sort Characters By Frequency.py

--- Code/Sort Characters By Frequency.py
+++ Code/Sort Characters By Frequency.py
@@ -1,10 +1,5 @@
-from collections import Counter
 class Solution:
     def frequencySort(self, s: str) -> str:
-        counts = Counter(s)                                         #Count each character.
-        segments = [(counts[k], k * counts[k]) for k in counts]     #Duplicate each character for the times of its count and put it together with its count in a tuple to form a list.
-        segments.sort(reverse = True)                               #Sort the tuple list in desending order.
-        result = ""                                                 #Add each segment in order to result.
-        for s in segments:
-            result += s[1]
-        return result
+        counts = Counter(s)                                                     #Count each character.
+        characters = sorted(list(count.keys()), key = lambda x: -count[x])      #Sort characters in desending order by its count.
+        return "".join([x * count[x] for x in characters])                      #Join the segments(each character duplicated by its count) together and return.
