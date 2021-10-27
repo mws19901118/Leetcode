648dece192ef658890715b847d56efd34c7b207f
Junsheng Wang <mws19901118@gmail.com>
2021-10-26 20:23:24 -0700

    Update Sort Colors.py

--- Code/Sort Colors.py
+++ Code/Sort Colors.py
@@ -3,12 +3,12 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        i=0
+        i = 0
         start, end = 0, len(nums) - 1
         while i < len(nums) and i <= end:
             if nums[i] == 1:                                            #If current value is 1, simply increase i by 1.
                 i += 1
-            elif nums[i]==0:                                            #If current value is 0, to move it to front, swap nums[i] with nums[start] and increase start by 1.
+            elif nums[i] == 0:                                          #If current value is 0, to move it to front, swap nums[i] with nums[start] and increase start by 1.
                 nums[i], nums[start] = nums[start], nums[i]
                 start += 1
                 i += 1                                                  #We have to increase i by 1 here , because all the value in front of i is no greater than nums[i], if we don't do this, it may result in incorrect growth of start and index out of bound.
