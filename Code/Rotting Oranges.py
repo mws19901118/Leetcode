9e7554011586319f82f79da06ae5ecf851f26736
Junsheng Wang <mws19901118@gmail.com>
2021-10-28 23:49:50 -0700

    Update Rotting Oranges.py

--- Code/Rotting Oranges.py
+++ Code/Rotting Oranges.py
@@ -1,26 +1,22 @@
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
-        fresh, rotten = set(), set()                                                                              #Use 2 sets to store all coordinates of initial fresh and rotten oranges respectively.
-        for i in range(len(grid)):
-            for j in range(len(grid[i])):
-                if grid[i][j] == 2:
-                    rotten.add((i, j))
-                elif grid[i][j] == 1:
-                    fresh.add((i, j))
-        time = 0
-        while rotten:                                                                                             #While there are new rotten oranges, do BFS.
-            if not fresh:                                                                                         #If no more fresh oranges, stop BFS.
-                break
-            q = set()                                                                                             #Store all coordinates of new rotten oranges in this time.
-            for x, y in rotten:
-                for i, j in [(-1, 0), (0, 1), (1, 0), (0, -1)]:
-                    nx, ny = x + i, y + j
-                    if nx >= 0 and nx < len(grid) and ny >= 0 and ny < len(grid[x]) and grid[nx][ny] == 1:        #Find all fresh oranges next to rotten oranges, they will become rotten.
-                        grid[nx][ny] = 2                                                                          #Update status in grid.
-                        q.add((nx, ny))                                                                           #Add coordinate to q.
-                        fresh.remove((nx, ny))                                                                    #Remove from fresh.
-            rotten = q
-            time += 1                                                                                             #Increase time by 1.
-        if fresh:
-            time = -1
-        return time                                                                                               #Return time.
+        m, n = len(grid), len(grid[0])                                                          #Get dimensions.
+        count, time = 0, 0                                                                      #Intialize fresh count and total time.
+        q = []                                                                                  #Intialize queue.
+        for i, j in product(range(m), range(n)):                                                #Traverse grid.
+            if grid[i][j] == 1:                                                                 #If current orange is fresh, increase count.
+                count += 1
+            elif grid[i][j] == 2:                                                               #If current orange is rotten, add coordinate to q.
+                q.append((i, j))
+
+        while q and count:                                                                      #While q is not emptu and count is larger than 0, do BFS.
+            newq = []                                                                           #Initialize new queue.
+            for x, y in q:                                                                      #Traverse each coordinate in q.
+                for nx, ny in [(x - 1, y), (x, y + 1), (x + 1, y), (x, y - 1)]:                 #Traverse its neighbors.
+                    if nx >= 0 and nx < m and ny >= 0 and ny < n and grid[nx][ny] == 1:         #If neighbor is valid and is fresh, append neighbor's coordinate to newq, decrease count and set neightbor to rotten.
+                        newq.append((nx, ny))
+                        count -= 1
+                        grid[nx][ny] = 2
+            q = newq                                                                            #Replace q with newq.
+            time += 1                                                                           #Increase time.
+        return time if not count else -1                                                        #If count is not 0, it's impossible to rotten all oranges so return -1; otherwise, return time.
