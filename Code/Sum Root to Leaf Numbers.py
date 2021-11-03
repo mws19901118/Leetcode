dd8455d1971031aa98c55a13184165246e936f8a
Junsheng Wang <mws19901118@gmail.com>
2021-11-02 18:43:58 -0700

    Update Sum Root to Leaf Numbers.py

--- Code/Sum Root to Leaf Numbers.py
+++ Code/Sum Root to Leaf Numbers.py
@@ -1,54 +1,17 @@
-# Definition for a  binary tree node
+# Definition for a binary tree node.
 # class TreeNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
-
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
 class Solution:
-    # @param root, a tree node
-    # @return an integer
-    def sumNumbers(self, root):
-        if root==None:
+    def traverse(self, root: Optional[TreeNode], number: int) -> int:                       #Traverse tree with the number from root to the parent of current node.
+        if not root:                                                                        #If current node is none, return 0.
             return 0
-            
-        sum=0
-
-        end=TreeNode(-1)
-        stack=[]                                              #The same as Binary Tree Preorder Traversal, as long as come to a leaf node, calculate the root number and add it to sum
-        stack.append(end)
-        
-        flag=[]
-        isleaf={}                                             #dictionary to indicate whether a treenode is a leaf node
-        write=False
-        flag.append(True)
-        while root!=end:
-            if write==False:
-                write=True
-            if root.left!=None:
-                isleaf[root]=False                            #have left child, not leaf, add to dictionary
-                temp=root.left
-                root.left=None
-                stack.append(root)
-                flag.append(write)
-                root=temp
-                write=False
-            else:
-                if root.right!=None:
-                    isleaf[root]=False                        #have right child, not leaf, add to dictionary
-                    temp=root.right
-                    root.right=None
-                    stack.append(root)
-                    flag.append(write)
-                    root=temp
-                    write=False
-                else:
-                    if root not in isleaf:                    #not exist in dictionary, must be leaf
-                        length=len(stack)                     
-                        temp=0                                
-                        for i in range(0,length-1):           #calculate root number, note:stack[0] is end node(TreeNode(-1)), also the stack does not contain current leaf node
-                            temp+=pow(10,i)*stack[length-1-i].val
-                        sum+=temp*10+root.val                 #add to sum
-                    root=stack.pop()
-                    write=flag.pop()
-        return sum
+        number = number * 10 + root.val                                                     #Update number to include current node.
+        if not root.left and not root.right:                                                #If current node is leaf, return number.
+            return number
+        return self.traverse(root.left, number) + self.traverse(root.right, number)         #Return the sum of result of traversing left child and right child.
+    
+    def sumNumbers(self, root: Optional[TreeNode]) -> int:
+        return self.traverse(root, 0)                                                       #Start traversing from root with 0.
