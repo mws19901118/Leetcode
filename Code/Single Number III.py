5213d063acbf386d2062d87834ff4d11fef87d61
Junsheng Wang <mws19901118@gmail.com>
2021-11-05 17:35:12 -0700

    Update Single Number III.py

--- Code/Single Number III.py
+++ Code/Single Number III.py
@@ -1,22 +1,15 @@
 class Solution(object):
-    def singleNumber(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: List[int]
-        """
-        l = len(nums)
-        if l < 2:
-            return []
+   def singleNumber(self, nums: List[int]) -> List[int]:
         t = 0
-        for x in nums:
-            t ^= x                                #Find the xor of the 2 single numbers.
+        for x in nums:                              #Find the xor of the 2 single numbers.
+            t ^= x
         bits = 0
-        while (t >> bits) % 2 == 0:
-            bits += 1                             #Find the lowest bit where they are different.
-        t0 = 0
-        t1 = 0
+        while t & 1 == 0:                           #Find the lowest bit where they are different.
+            t >>= 1
+            bits += 1
+        t0, t1 = 0, 0                               #According to the value at this bit, divide the original numbers into 2 groups, each of which has only 1 single number. Find the single number using xor operation respectively.
         for x in nums:
-            if (x >> bits) % 2 == 0:              #According to the value at this bit, divide the original numbers into 2 groups, each of which has only 1 single number. Find the single number using xor operation respectively.
+            if (x >> bits) & 1:
                 t0 ^= x
             else:
                 t1 ^= x
