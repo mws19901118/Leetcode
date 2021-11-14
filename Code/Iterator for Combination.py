2e5bc5084fd4343ca56a2d31c6548b0c8ce96f8e
Junsheng Wang <mws19901118@gmail.com>
2021-11-13 21:34:15 -0800

    Update Iterator for Combination.py

--- Code/Iterator for Combination.py
+++ Code/Iterator for Combination.py
@@ -1,26 +1,27 @@
 class CombinationIterator:
-
+    
     def __init__(self, characters: str, combinationLength: int):
-        self.combinationsInBinary = []                                                      #Store all possible combinations in binary(binary whose count of '1' equals combinationLength).
-        self.index = 0                                                                      #Index of the iterator.
-        self.characters = characters
-        self.generateCombinationsInBinary(len(characters), combinationLength)               #Generate all combinations in binary.
+        self.combinations = []                                                                  #Initialize combinations.
+        self.index = 0                                                                          #Initialize pointer in combinations.
+        self.DFS([], list(characters), combinationLength)                                       #Generate all combinations.
         
     def next(self) -> str:
-        binary = self.combinationsInBinary[self.index]                                      #Get the combination in binary string.
-        self.index += 1                                                                     #Move to next index.
-        s = "".join([self.characters[i] for i in range(len(binary)) if binary[i] == '1'])   #Convert from binary string to combination by joining all character in characters whose corresponding bit in binary is '1'.
-        return s
+        result = self.combinations[self.index]                                                  #Get result.
+        self.index += 1                                                                         #Move pointer.
+        return result                                                                           #Return result.
 
     def hasNext(self) -> bool:
-        return self.index < len(self.combinationsInBinary)                                  #Return if index has reached the end.
+        return self.index < len(self.combinations)                                              #Return if pointer has reached the end.
 
-    def generateCombinationsInBinary(self, length: int, combinationLength: int):
-        upperBound = (1 << length) - 1                                                      #The upper bound of binary is 2 ^ length - 1, length is the length of characters.
-        for i in range(upperBound, 0, -1):                                                  #From upper bound to 0, find possible combinations in binary.
-            b = "{0:b}".format(i).zfill(length)                                             #Convert to binary string.
-            if b.count("1") == combinationLength:                                           #If the count of '1' equals combinationLength, add it to combinationsInBinary list.
-                self.combinationsInBinary.append(b)
+    def DFS(self, stack: List[str], characters: List[str], combinationLength: int) -> None:     #DFS.
+        if len(stack) == combinationLength:                                                     #If stack length equals combinationLength, join the characters in stack and append to self.combinations.
+            self.combinations.append("".join(stack))
+            return
+        remainLength = combinationLength - len(stack)                                           #Calculate remain length.
+        for i in range(len(characters) - remainLength + 1):                                     #Traverse the rest of characters until cannot make valid combination.
+            stack.append(characters[i])                                                         #Append current character to stack.
+            self.DFS(stack, characters[i + 1:], combinationLength)                              #Keep DFS.
+            stack.pop()                                                                         #Pop stack.
                 
 # Your CombinationIterator object will be instantiated and called as such:
 # obj = CombinationIterator(characters, combinationLength)
