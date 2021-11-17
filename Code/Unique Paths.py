9ffe2cff461480c094dd8eacc855168d0076cc54
Junsheng Wang <mws19901118@gmail.com>
2021-11-16 17:14:00 -0800

    Update Unique Paths.py

--- Code/Unique Paths.py
+++ Code/Unique Paths.py
@@ -1,7 +1,6 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        last = [1 for i in range(n)]                    #Record the unique paths of each grid in last row. It's all 1 for first row.
-        for i in range(1, m):
-            for j in range(1, n):                       #For each row, current paths eqauls the sum of paths from left grid and upper grid.
-                last[j] += last[j - 1]
+        last = [1] * n                                      #Record the unique paths of each grid in last row. It's all 1 for first row.
+        for i, j in product(range(m - 1), range(n - 1)):    #For each row, current paths eqauls the sum of paths from left grid and upper grid.
+            last[j + 1] += last[j]
         return last[n - 1]
