2c15b5ef6203e871c969ff1691fc715237fa8928
Junsheng Wang <mws19901118@gmail.com>
2021-11-18 17:30:39 -0800

    Update Hamming Distance.py

--- Code/Hamming Distance.py
+++ Code/Hamming Distance.py
@@ -1,7 +1,3 @@
 class Solution:
     def hammingDistance(self, x: int, y: int) -> int:
-        d, count = x ^ y, 0       # Calculate x^y and count 1s in its binary.
-        while d > 0:
-            count += d & 1
-            d >>= 1
-        return count
+        return "{0:b}".format(x ^ y).count("1")       #Return the count of 1 in x ^ y.
