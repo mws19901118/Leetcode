6da8e06f7ac034e1d6ef2b1c28deb85d05974a72
Junsheng Wang <mws19901118@gmail.com>
2021-11-26 19:13:44 -0800

    Update Product of Array Except Self.py

--- Code/Product of Array Except Self.py
+++ Code/Product of Array Except Self.py
@@ -1,14 +1,10 @@
-import math
 class Solution:
-    # @param {integer[]} nums
-    # @return {integer[]}
-    def productExceptSelf(self, nums):
-        l=len(nums)
-        p=[1]                                 #Record the product of array except self.
-        for i in range(1,l):
-            p.append(p[i-1]*nums[i-1])        #Calculate the product before each element.
-        t=1                                   #Record the product after current element.
-        for i in range(l):
-            p[l-1-i]*=t                       #The answer equals the product of product before current element and the product after current element.
-            t*=nums[l-1-i]                    #Update t.
-        return p
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        product = [1]                                       #Initialize products with a dummy product at the beginning.
+        for x in nums:                                      #Traverse nums and calculate the product from the beginning to current number.
+            product.append(product[-1] * x)
+        rightProduct = 1                                    #Initialzie the product from right.
+        for i in reversed(range(len(nums))):                #Traverse nums backward.
+            product[i + 1] = product[i] * rightProduct      #The product except self at each index i equals to the product from beginning to index i - 1 multiply the product from right to index i + 1. 
+            rightProduct *= nums[i]                         #Update rightProduct.
+        return product[1:]                                  #Return product[1:] as the first product is dummy.
