da07549f69370dceb969c0f4f050605e68879e7f
Junsheng Wang <mws19901118@gmail.com>
2021-11-30 17:11:13 -0800

    Update House Robber.py

--- Code/House Robber.py
+++ Code/House Robber.py
@@ -1,7 +1,6 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        rob, notrob = [0] * (len(nums) + 1), [0] * (len(nums) + 1)      #Initialize the rob or not rob list to record the current max money robbed.
-        for i, x in enumerate(nums):
-            rob[i + 1] = notrob[i] + x                                  #If rob, new max money is previous not rob max money plus the money robbed from this house.
-            notrob[i + 1] = max(rob[i], notrob[i])                      #If not rob, new max money is the greater value of previous not rob max money and rob max money.
-        return max(rob[-1], notrob[-1])                                 #Return the greater value of rob[-1] and notrob[-1].
+        rob, norob = 0, 0                                       #Initialize the max monmey to rob or not rob.
+        for x in nums:                                          #Traverse nums.
+            rob, norob = norob + x, max(rob, norob)             #If rob, new max money is previous not rob max money plus the money robbed from this house; if not rob, new max money is the greater value of previous not rob max money and rob max money.
+        return max(rob, norob)                                  #Return the greater value of rob and norob.
