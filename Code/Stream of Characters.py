0ac4453928841a384cd09b49133a2b41db39d02c
Junsheng Wang <mws19901118@gmail.com>
2021-12-03 21:38:17 -0800

    Update Stream of Characters.py

--- Code/Stream of Characters.py
+++ Code/Stream of Characters.py
@@ -4,7 +4,7 @@ class Trie:                                                             #Trie.
         self.hasWord = False
     
     def addWord(self, word: str) -> None:
-        if word == "":
+        if not word:
             self.hasWord = True
             return
         if word[-1] not in self.letters:                                #Instead of ordering by prefix, ordering by suffix.
@@ -25,7 +25,7 @@ class StreamChecker:
         for x in reversed(self.history):                                #Search for word.
             if t.hasWord:                                               #If current trie node has word, return true.
                 return True
-            if x not in t.letters:                                      #If current character not in the dictionary of current trie node, return false.
+            elif x not in t.letters:                                    #If current character not in the dictionary of current trie node, return false.
                 return False
             t = t.letters[x]                                            #Go to next level.
         return t.hasWord                                                #Return if final trie node has word.
