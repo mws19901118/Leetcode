7591906f4565228eab135f7744e64068987f9d87
Junsheng Wang <mws19901118@gmail.com>
2021-12-14 23:30:00 -0800

    Update Insertion Sort List.py

--- Code/Insertion Sort List.py
+++ Code/Insertion Sort List.py
@@ -4,26 +4,18 @@
 #         self.val = val
 #         self.next = next
 class Solution:
-    def insertionSortList(self, head: ListNode) -> ListNode:
-        if not head:
-            return head
-        result = ListNode(head.val)
-        tail = result                                                   #Result is the head of new list
-        head = head.next
-        while head:
-            node = ListNode(head.val)
-            if node.val <= result.val:                                  #If current value is smaller than or equal to result value, node becomes the new head.
-                node.next = result
-                result = node
-            elif tail.val < node.val:                                   #If current value is larger than tail value, node becomes the new tail.
-                tail.next = node
-                node.next = None
-                tail = node
-            else:
-                curr = result
-                while curr.next and curr.next.val < node.val:           #Find the correct place of current value
-                    curr = curr.next
-                node.next = curr.next                                   #Insert.
-                curr.next = node
-            head = head.next
-        return result
+    def insertionSortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        dummyHead = ListNode(-5001, head)                               #Create a dummy head.
+        tail = dummyHead.next                                           #Initialize tail to be the actual head.
+        while tail.next:                                                #Traverse list while tail.next is not none.
+            if tail.next.val > tail.val:                                #If the tail.next.val is greater than tail.val, traversed list is sorted so far; so move tail to next and continue. 
+                tail = tail.next
+                continue
+            curr = dummyHead                                            #Find the place to insert tail.next in the traversed list.
+            while curr != tail and curr.next.val < tail.next.val:
+                curr = curr.next
+            temp = tail.next                                            #Detach tail.next from tail and insert it after curr.
+            tail.next = temp.next
+            temp.next = curr.next
+            curr.next = temp
+        return dummyHead.next                                           #Return dummyHead.next.
