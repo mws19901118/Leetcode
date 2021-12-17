27b87cfe56491cdfbe88ca167b6ee04a73a17501
Junsheng Wang <mws19901118@gmail.com>
2021-12-16 22:53:44 -0800

    Update Maximal Square.py

--- Code/Maximal Square.py
+++ Code/Maximal Square.py
@@ -1,10 +1,10 @@
 class Solution:
     def maximalSquare(self, matrix: List[List[str]]) -> int:
-        dp = [[int(matrix[i][j]) for j in range(len(matrix[i]))] for i in range(len(matrix))]               #Record the max side length of squares.
-        maxLength = 0                                                                                       #Record current side length of square whose bottom right corner is matrix[i-1][j-1].
-        for i in range(len(dp)):
-            for j in range(len(dp[i])):
-                if dp[i][j] == 1 and i > 0 and j > 0:                                                       #If dp[i][j] is '1', current side length equals 1 plus the min side length of squares whose bottom right corner is dp[i-1][j-1], dp[i][j-1], dp[i-1][j] respectively.
-                    dp[i][j] = min(dp[i - 1][j - 1], dp[i][j - 1], dp[i - 1][j]) + 1
-                maxLength = max(maxLength, dp[i][j])                                                        #Update the max side length.
-        return maxLength * maxLength                                                                        #Return the square of max side length.
+        m, n = len(matrix), len(matrix[0])                                          #Get dimensions.
+        dp = [[0 for j in range(n + 1)] for i in range(m + 1)]                      #Record the max side length of squares.
+        maxLength = 0                                                               #Record current side length of square whose bottom right corner is matrix[i-1][j-1].
+        for i, j in product(range(m), range(n)):                                    #Traverse matrix.
+            if matrix[i][j] == '1':                                                 #If matrix[i][j] is '1', current side length equals 1 plus the min side length of squares whose bottom right corner is dp[i][j], dp[i + 1][j], dp[i][j + 1] respectively.
+                dp[i + 1][j + 1] = min(dp[i][j], dp[i + 1][j], dp[i][j + 1]) + 1
+                maxLength = max(maxLength, dp[i + 1][j + 1])                        #Update the max side length.
+        return maxLength * maxLength                                                #Return the square of max side length.
