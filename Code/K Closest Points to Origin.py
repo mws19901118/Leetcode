c27e5cf1ec261cd0f382929174e5dbef8de3a572
Junsheng Wang <mws19901118@gmail.com>
2021-12-25 21:19:45 -0800

    Update K Closest Points to Origin.py

--- Code/K Closest Points to Origin.py
+++ Code/K Closest Points to Origin.py
@@ -1,24 +1,15 @@
-class Solution(object):
-    def findKth(self, dist, K):                                             #Find the K-th distance in dist.
-        if len(dist) == 1:
-            return dist[0][0]
-        randomD = dist[0][0]
-        lower = filter(lambda x: x[0] < randomD, dist[1:])
-        upper = filter(lambda x: x[0] > randomD, dist[1:])
-        if len(lower) == K - 1:
+class Solution:
+    def findK(self, distances: List[tuple], k: int) -> int:                                                                                     #Find the k-th smallest distance. 
+        randomD = distances[0][2]
+        lower, upper = list(filter(lambda x: x[2] <= randomD, distances[1:])), list(filter(lambda x: x[2] > randomD, distances[1:]))
+        if len(lower) == k - 1:
             return randomD
-        elif len(lower) < K - 1:
-            return self.findKth(upper, K - 1 - len(lower))
+        elif len(lower) > k - 1:
+            return self.findK(lower, k)
         else:
-            return self.findKth(lower, K)
-    def kClosest(self, points, K):
-        """
-        :type points: List[List[int]]
-        :type K: int
-        :rtype: List[List[int]]
-        """
-        dist = map(lambda p: (p[0] * p[0] + p[1] * p[1], p), points)      #Convert the list of point to a list of tuple where the first element is distance to origin and second element is point.
+            return self.findK(upper, k - len(lower) - 1)
         
-        kthD = self.findKth(dist, K)                                      #Find the K-th closet distance.
-        
-        return map(lambda x: x[1], filter(lambda x: x[0] <= kthD, dist))  #Covert all tuples whose distance is smaller than or equal to kthD back to point.
+    def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
+        distances = [(p[0], p[1], p[0] * p[0] + p[1] * p[1]) for p in points]                                                                   #Convert the list of point to a list of tuple where the first 2 elements are point and third element is distance to origin.
+        kth = self.findK(distances, k)                                                                                                          #Find the k-th smallest distance.
+        return [[d[0], d[1]] for d in distances if d[2] <= kth]                                                                                 #Return the points whose distance is no larger than k-th smallest distance.
