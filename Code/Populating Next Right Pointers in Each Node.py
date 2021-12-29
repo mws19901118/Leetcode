bc8a8c670a283dc73e69d52c0bf0d45697caf58c
Junsheng Wang <mws19901118@gmail.com>
2021-12-28 19:53:28 -0800

    Update Populating Next Right Pointers in Each Node.py

--- Code/Populating Next Right Pointers in Each Node.py
+++ Code/Populating Next Right Pointers in Each Node.py
@@ -9,12 +9,12 @@ class Node:
 """
 
 class Solution:
-    def connect(self, root: 'Node') -> 'Node':
-        if root is None or root.left is None:                   #If reach the bottom, return root.
+    def connect(self, root: 'Optional[Node]') -> 'Optional[Node]':
+        if not root or not root.left:                   #If reach the bottom, return.
             return root
-        root.left.next = root.right                             #Next pointer of left child of current node points to right child of current node.
-        if root.next is not None:                               #Because it's a perfect binary tree, if current node isn't the rightmost node of current level,
-            root.right.next = root.next.left                    #Current node must have right child and next node must have left child and the former node's next node is the latter node.
-        root.left = self.connect(root.left)                     #Recursively connect left subtree.
-        root.right = self.connect(root.right)                   #Recursively connect right subtree.
+        root.left.next = root.right                     #Next pointer of left child of current node points to right child of current node.
+        if root.next:                                   #Because it's a perfect binary tree, if current node isn't the rightmost node of current level,
+            root.right.next = root.next.left            #Current node must have right child and next node must have left child and the former node's next node is the latter node.
+        root.left = self.connect(root.left)             #Recursively connect left subtree.
+        root.right = self.connect(root.right)           #Recursively connect right subtree.
         return root
