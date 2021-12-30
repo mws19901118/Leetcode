ccc4384307889a070415bce44f63dbd8c4a17e48
Junsheng Wang <mws19901118@gmail.com>
2021-12-29 17:28:57 -0800

    Update Smallest Integer Divisible by K.py

--- Code/Smallest Integer Divisible by K.py
+++ Code/Smallest Integer Divisible by K.py
@@ -1,9 +1,9 @@
 class Solution:
-    def smallestRepunitDivByK(self, K: int) -> int:
-        length, r = 1, 1 % K          #Initialize length and the remainder of N % K.
-        for i in range(K):            #Since its remainder, there are at most K different value of r.
-            if r == 0:                #If the remainder is 0, N is divisible by K, return length.
+    def smallestRepunitDivByK(self, k: int) -> int:
+        length, r = 1, 1 % k          #Initialize length and the remainder of N % k.
+        for i in range(k):            #Since its remainder, there are at most k different value of r.
+            if r == 0:                #If the remainder is 0, N is divisible by k, return length.
                 return length
-            r = (r * 10 + 1) % K      #Calculate the remainder of next N, (N * 10 + 1) % K.
+            r = (r * 10 + 1) % k      #Calculate the remainder of next N, (N * 10 + 1) % k.
             length += 1               #Increase length by 1.
-        return -1                     #Return -1 if no N is divisible by K.
+        return -1                     #Return -1 if no N is divisible by k.
