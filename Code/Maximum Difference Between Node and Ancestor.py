8caecae1009b93b86db09be27410104f7ce5900c
Junsheng Wang <mws19901118@gmail.com>
2021-12-30 17:45:37 -0800

    Update Maximum Difference Between Node and Ancestor.py

--- Code/Maximum Difference Between Node and Ancestor.py
+++ Code/Maximum Difference Between Node and Ancestor.py
@@ -5,18 +5,19 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def traverse(self, root: TreeNode) -> tuple:                                                                                                                  #Traverse binary tree from node, return min value, max value and max difference between node and ancestor in this binary tree.
-        if root.left is None and root.right is None:                                                                                                              #If root is leaf, return current root node value as min value and max value and 0 as max difference between node and ancestor
+    def traverse(self, root: Optional[TreeNode]) -> (int, int, int):                                                                                            #Traverse binary tree from node, return min value, max value and max difference between node and ancestor in this binary tree.
+        if not root.left and not root.right:                                                                                                                    #If root is leaf, return current root node value as min value and max value and 0 as max difference between node and ancestor.
             return root.val, root.val, 0
-        elif root.left is None and root.right is not None:                                                                                                        #If left child is none, traverse right subtree and calculate min value, max value and max difference between node and ancestor then return.
+        elif not root.left and root.right:                                                                                                                      #If left child is none, traverse right subtree and calculate min value, max value and max difference between node and ancestor then return.
             r = self.traverse(root.right)
             return min(r[0], root.val), max(r[1], root.val), max(r[2], abs(root.val - r[0]), abs(root.val - r[1]))
-        elif root.left is not None and root.right is None:                                                                                                        #If right child is none, traverse left subtree and calculate min value, max value and max difference between node and ancestor then return.
+        elif root.left and not root.right:                                                                                                                      #If right child is none, traverse left subtree and calculate min value, max value and max difference between node and ancestor then return.
             l = self.traverse(root.left)
             return min(l[0], root.val), max(l[1], root.val), max(l[2], abs(root.val - l[0]), abs(root.val - l[1]))
-        else:                                                                                                                                                     #Otherwise traverse both left and right subtree and calculate min value, max value and max difference between node and ancestor then return.
+        else:                                                                                                                                                   #Otherwise traverse both left and right subtree and calculate min value, max value and max difference between node and ancestor then return.
             l, r = self.traverse(root.left), self.traverse(root.right)
             return min(l[0], r[0], root.val), max(l[1], r[1], root.val), max(l[2], r[2], abs(root.val - min(l[0], r[0])), abs(root.val - max(l[1], r[1])))
-    def maxAncestorDiff(self, root: TreeNode) -> int:
+        
+    def maxAncestorDiff(self, root: Optional[TreeNode]) -> int:
         result = self.traverse(root)
-        return result[2]                                                                                                                                          #Return max difference between node and ancestor in this binary tree.
+        return result[2]                                                                                                                                        #Return max difference between node and ancestor in this binary tree.                                                                                                                     
