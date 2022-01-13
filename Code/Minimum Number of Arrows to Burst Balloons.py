c9d699ab1a1090ee4510fe863a74f5bfd4d72a7c
Junsheng Wang <mws19901118@gmail.com>
2022-01-12 20:02:42 -0800

    Update Minimum Number of Arrows to Burst Balloons.py

--- Code/Minimum Number of Arrows to Burst Balloons.py
+++ Code/Minimum Number of Arrows to Burst Balloons.py
@@ -1,12 +1,11 @@
 class Solution:
     def findMinArrowShots(self, points: List[List[int]]) -> int:
-        points.sort()                                             #Sort points by starting point ascending ly.
-        count, i = 0, 0                                           #Initialize count to be 0 and pointer i to start from 0.
-        while i < len(points):
-            j, end = i + 1, points[i][1]                          #Initialize pointer j to start from i + 1, and common end to be the end of points[i].
-            while j < len(points) and points[j][0] <= end:        #If points[j]'s start is not greater than common end, a single arrow can burst from points[i] to points[j].
-                end = min(end, points[j][1])                      #Update common end to be the smallest end from points[i] to points[j].
-                j += 1                                            #Move j to next.
-            count += 1                                            #Add 1 to count.
-            i = j                                                 #Move i to j.
-        return count
+        points.sort()                                               #Sort points by starting point in ascending order.
+        end, arrows = float('inf'), 1                               #Initialize the earilist end of ballons bursted by current arrow and arrows count.
+        for i, p in enumerate(points):                              #Traverse points.
+            if p[0] > end:                                          #If the start of current ballon is greater than end, a new arrow is needed and reset end.
+                arrows += 1
+                end = p[1]
+            else:                                                   #Otherwise, just update end.
+                end = min(end, p[1])
+        return arrows                                               #Return arrows.
