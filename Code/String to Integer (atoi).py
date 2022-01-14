5f2562442e8cdec3125018677c6bf1c4d1d3577f
Junsheng Wang <mws19901118@gmail.com>
2022-01-13 18:16:52 -0800

    Update String to Integer (atoi).py

--- Code/String to Integer (atoi).py
+++ Code/String to Integer (atoi).py
@@ -1,30 +1,11 @@
 class Solution:
-    # @param {string} str
-    # @return {integer}
-    def myAtoi(self, str):
-        l=len(str)
-        i=0
-        flag=False
-        while i<l and str[i]==' ':                              #Deal with whitespaces in the beginning.
-            i+=1
-        if i<l and str[i]=='-':                                 #Deal with minus sign.
-            flag=True
-            i+=1
-        elif i<l and str[i]=='+':                               #Deal with plus sign
-            i+=1
-        if i==l or ord(str[i])<48 or ord(str[i])>57:            #Deal with unconvertable situations.
-            return 0
-        else:
-            j=i
-            while j<l and ord(str[j])>=48 and ord(str[j])<=57:  #Calculate the length of integer.
-                j+=1
-            answer=0
-            for k in range(i,j):
-                answer+=(ord(str[k])-48)*10**(j-1-k)            #Convert.
-            if flag:                                            #Deal with negative integer.
-                answer=-answer                      
-            if answer>2147483647:                               #Deal with out of range situations.
-                return 2147483647
-            if answer<-2147483648:
-                return -2147483648
-            return answer
+    def myAtoi(self, s: str) -> int:
+        s = s.lstrip()                                                                              #Remove leading whitespace.
+        sign = 1
+        if s and s[0] in "+-":                                                                      #Handle the case if the first character is sign.
+            sign = -1 if s[0] == '-' else 1
+            s = s[1:]
+        i = 0
+        while i < len(s) and s[i].isdigit():                                                        #Find the index of first non digit character. 
+            i += 1
+        return min(2 ** 31 - 1, max(- 2 ** 31, 0 if not s[:i] else sign * int(s[:i])))              #Convert s[:i] to int and make sure it not exceeds upper and lower bounds.
