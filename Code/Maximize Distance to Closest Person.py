4c35bf2761e4b33b6f451ffb7371837691a49956
Junsheng Wang <mws19901118@gmail.com>
2022-01-15 21:17:05 -0800

    Update Maximize Distance to Closest Person.py

--- Code/Maximize Distance to Closest Person.py
+++ Code/Maximize Distance to Closest Person.py
@@ -1,21 +1,13 @@
 class Solution:
     def maxDistToClosest(self, seats: List[int]) -> int:
-        lastPerson = 0                                        #Index of last person.
-        index = 0                                             #Index pointer.
-        
-        leftEnd = 0
-        while index < len(seats):
-            if seats[index] == 1:
-                leftEnd = index - lastPerson                  #Find the distance between first person and left end.
-                lastPerson = index                            #Update index of last person.
-                break
-            index += 1
-        maxDist = 0
-        while index < len(seats):
-            if seats[index] == 1:
-                maxDist = max(maxDist, index - lastPerson)    #Find the max distance between 2 adjacent people.
-                lastPerson = index                            #Update index of last person.
-            index += 1
-        
-        rightEnd = len(seats) - 1 - lastPerson                #Find the distance between last person and right end.
-        return max(leftEnd, rightEnd, maxDist // 2)           #Return the max of leftEnd, rightEnd and half of maxDist.
+        lastPerson = -1                                                             #Initialize the index of last person.
+        distanceToLeft, maxDistance = 0, 0                                          #Initialize the distance from first person to left and the max distance to closest person if sitting between 2 people.
+        for i, x in enumerate(seats):                                               #Traverse seat.
+            if not x:                                                               #If current seat has no person, continue.
+                continue
+            if lastPerson == -1:                                                    #If this is the first person, update distanceToLeft.
+                distanceToLeft = i
+            else:                                                                   #Otherwise, update maxDistance.
+                maxDistance = max(maxDistance, (i - lastPerson) // 2)
+            lastPerson = i                                                          #Update the index of last person.
+        return max(maxDistance, distanceToLeft, len(seats) - 1 - lastPerson)        #Return the max distance of sitting between 2 peoples, on the left end or on the right end.
