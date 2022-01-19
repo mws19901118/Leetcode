6be2b7502dea884d1a6194296df305b76a25a361
Junsheng Wang <mws19901118@gmail.com>
2022-01-18 22:18:08 -0800

    Update Linked List Cycle II.py

--- Code/Linked List Cycle II.py
+++ Code/Linked List Cycle II.py
@@ -6,19 +6,15 @@
 
 class Solution:
     def detectCycle(self, head: ListNode) -> ListNode:
-        if not head:                                                            #If head is none, return none.
-            return head
-        fast, slow = head, head
-        hasCycle = False
-        count = 0
-        while fast.next is not None and fast.next.next is not None:             #Use fast and slow pointers to determine if there is cycle. Also count the steps from head to where fast and slow pointers meet.
+        fast, slow = head, head                                                 #Initialize fast & slow pointers.
+        count = 0                                                               #Count the length from head to where fast catches slow.
+        while fast and fast.next:                                               #Move forward fast and slow in different pace.
             fast = fast.next.next
             slow = slow.next
             count += 1
-            if fast == slow:
-                hasCycle = True
+            if fast == slow:                                                    #If fast catches slow, stop.
                 break
-        if not hasCycle:                                                        #If no cycle, return none.
+        if slow != fast or not count:                                           #If fast does not catch slow or they didn't move at all, there is no cycle.
             return None
         length = 0                                                              #Count the length of cycle.
         while True:
@@ -26,7 +22,7 @@ class Solution:
             length += 1
             if fast == slow:
                 break
-        if count >= length:                                                     #Imagine the loop is cut at the meet point. The linked list then becomes Y shape. One start is head, the other is the next of meeting point and end at meeting point.
+        if count >= length:                                                     #Imagine the loop is cut at the meet point. The linked list then becomes Y shape. One start is head, the other is the next of catching point and end at catching point.
             for i in range(count - length):                                     #Next, it becomes how to find the intersection of Y shape linked list with the lengths of 2 linked list are known.
                 head = head.next
         else:
