d420084e5914f4f2c77a926e7ef8a4f3ace05b72
Junsheng Wang <mws19901118@gmail.com>
2022-01-27 20:09:11 -0800

    Update Add and Search Word - Data structure design.py

--- Code/Add and Search Word - Data structure design.py
+++ Code/Add and Search Word - Data structure design.py
@@ -1,34 +1,23 @@
 class WordDictionary:
 
-    def __init__(self):                                                                                                             #Essentially, it's a Trie.
-        """
-        Initialize your data structure here.
-        """
+    def __init__(self):                                                                         #Initialize trie node.
         self.letters = {}
         self.hasWord = False
-        
-    def addWord(self, word: str) -> None:                                                                                           #Add word to Trie.
-        """
-        Adds a word into the data structure.
-        """
-        if word == "":
+
+    def addWord(self, word: str) -> None:                                                       #Add word to trie.
+        if not word:
             self.hasWord = True
             return
         if word[0] not in self.letters:
             self.letters[word[0]] = WordDictionary()
         self.letters[word[0]].addWord(word[1:])
 
-    def search(self, word: str) -> bool:                                                                                            #Search word in Trie.
-        """
-        Returns if the word is in the data structure. A word could contain the dot character '.' to represent any one letter.
-        """
-        if word == "":
+    def search(self, word: str) -> bool:                                                        #Search word in trie.
+        if not word:
             return self.hasWord
         if word[0] != '.':
-            if word[0] not in self.letters:
-                return False
-            return self.letters[word[0]].search(word[1:])
-        else:                                                                                                                       #If the first letter is '.', traverse all its children.
+            return word[0] in self.letters and self.letters[word[0]].search(word[1:])
+        else:
             return any(self.letters[x].search(word[1:]) for x in self.letters)
 
 
