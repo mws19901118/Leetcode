9a3d56c4203b9699db1670363f4a347170cabeda
Junsheng Wang <mws19901118@gmail.com>
2022-02-01 17:31:10 -0800

    Update Find All Anagrams in a String.py

--- Code/Find All Anagrams in a String.py
+++ Code/Find All Anagrams in a String.py
@@ -1,10 +1,9 @@
 class Solution:
     def findAnagrams(self, s: str, p: str) -> List[int]:
         diff = [0] * 26                                               #Maintain the diff of letter count between running window in s and p.
-        count = 0                                                     #Count the number of letters not match.
         for x in p:                                                   #Initialize the diff(letter count of p) and count.
-            count += diff[ord(x) - ord('a')] == 0
             diff[ord(x) - ord('a')] += 1
+        count = sum(x != 0 for x in diff)                             #Count the number of letters not match.
         result = []
         for i in range(len(s)):
             diff[ord(s[i]) - ord('a')] -= 1                           #Update diff and count everytime the running window has a new letter.
