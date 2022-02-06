9f719aaf3b4103e0e10f33bb3b36d1076f58e01d
Junsheng Wang <mws19901118@gmail.com>
2022-02-05 18:25:27 -0800

    Update Remove Duplicates from Sorted Array II.py

--- Code/Remove Duplicates from Sorted Array II.py
+++ Code/Remove Duplicates from Sorted Array II.py
@@ -1,11 +1,8 @@
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
-        if len(nums) < 2:                           #If nums length is smaller than 2, there is no duplicates.
-            return len(nums)
-        slow, fast = 2, 2                           #Start 2 pointers from the third number.
-        while fast < len(nums):                     #Traverse fast pointer through nums.
+        slow = 2                                    #Initialize slow pointer at index 2.
+        for fast in range(2, len(nums)):            #Traverse fast pointer from inedx 2 to end of nums.
             if nums[slow - 2] != nums[fast]:        #If nums[fast] is a number without duplicates more than 2, replace nums[slow] with nums[fast] and move forward slow pointer.
                 nums[slow] = nums[fast]
                 slow += 1
-            fast += 1                               #Move forward fast pointer.
         return slow                                 #Return where slow pointer stops.
