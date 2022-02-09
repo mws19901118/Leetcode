97f6bf9df8b9d9427de84b492c71599ec1b75100
Junsheng Wang <mws19901118@gmail.com>
2022-02-08 17:16:54 -0800

    Update K-diff Pairs in an Array.py

--- Code/K-diff Pairs in an Array.py
+++ Code/K-diff Pairs in an Array.py
@@ -1,6 +1,4 @@
 class Solution:
     def findPairs(self, nums: List[int], k: int) -> int:
-        count, pairs = Counter(nums), 0                                             #Count nums.
-        for x in count:                                                             #Iterate over each value.
-            pairs += (k > 0 and x + k in count) or (k == 0 and count[x] > 1)        #If k > 0 and x + k in count or k == 0 and count[x] > 1, there is a pair.
-        return pairs
+        count = Counter(nums)                                                                   #Count nums.
+        return sum((k > 0 and x + k in count) or (k == 0 and count[x] > 1) for x in count)      #If k > 0 and x + k in count or k == 0 and count[x] > 1, there is a pair.
