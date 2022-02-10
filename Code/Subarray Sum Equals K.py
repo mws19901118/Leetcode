c44470efcf24b1497ddce2ef571b603b1818df17
Junsheng Wang <mws19901118@gmail.com>
2022-02-09 17:11:26 -0800

    Update Subarray Sum Equals K.py

--- Code/Subarray Sum Equals K.py
+++ Code/Subarray Sum Equals K.py
@@ -1,10 +1,8 @@
-from collections import defaultdict
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
         prefixSums = defaultdict(int)               #Count the appearances of prefix sum value.
         prefixSums[0] = 1                           #Initially, it's 0.
-        s, count = 0, 0
-        for i, x in enumerate(nums):                #Traverse the list.
+        for x in nums:                              #Traverse the list.
             s += x                                  #Calculate current prefix sum value.
             count += prefixSums[s - k]              #Add the count of prefix sum whose value is s - k to result. The subarray starting after prefix sum s - k ending at current element has a sum equals k.
             prefixSums[s] += 1                      #Update the count of current prefix sum value.
