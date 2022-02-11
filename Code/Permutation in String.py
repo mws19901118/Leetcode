bb4d2adf874e10b11d0c936e73e858e799584218
Junsheng Wang <mws19901118@gmail.com>
2022-02-10 18:47:53 -0800

    Update Permutation in String.py

--- Code/Permutation in String.py
+++ Code/Permutation in String.py
@@ -1,16 +1,9 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
-        diff = [0] * 26                                                       #Maintain the diff of letter count between running window in s2 and s1.
-        count = 0                                                             #Count the number of letters not match.
-        for x in s1:                                                          #Initialize the diff(letter count of s1) and count.
-            count += diff[ord(x) - ord('a')] == 0
-            diff[ord(x) - ord('a')] += 1
-        for i in range(len(s2)):
-            diff[ord(s2[i]) - ord('a')] -= 1                                  #Update diff and count everytime the running window has a new letter.
-            count -= diff[ord(s2[i]) - ord('a')] == 0
-            if count == 0:                                                    #If no not matched letter between running window and s1, an anagram is found, return true. 
+        s1Counter, s2Counter = Counter(s1), Counter(s2[:len(s1)])           #Count each letter in s1 and s2[:len(s1)].
+        for i in range(len(s1), len(s2) + 1):                               #Traverse from the len(si)-th letter in s2 to the end of s2.
+            if all(s1Counter[x] == s2Counter[x] for x in s1Counter):        #If all letters in s1Counter has same count in s2Counter, s2[i - len(s1):i] is a permutation of s1, so return true.
                 return True
-            if i >= len(s1) - 1:                                              #Pop current starting index out of running window if its length is len(s1) and update diff and count if necessary.
-                count += diff[ord(s2[i - len(s1) + 1]) - ord('a')] == 0
-                diff[ord(s2[i - len(s1) + 1]) - ord('a')] += 1
-        return False
+            s2Counter[s2[i]] += 1                                           #Update s2Counter to count each letter in s2[i - len(s1):i].
+            s2Counter[s2[i - len(s1)]] -= 1
+        return all(s1Counter[x] == s2Counter[x] for x in s1Counter)         #Check if the s2[len(s2) - len(s1):] is a permutation of s1.
