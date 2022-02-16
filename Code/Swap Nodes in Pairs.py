21777a2b21a36ff676cab271aa55cbb26a29d2c2
Junsheng Wang <mws19901118@gmail.com>
2022-02-15 21:27:06 -0800

    Update Swap Nodes in Pairs.py

--- Code/Swap Nodes in Pairs.py
+++ Code/Swap Nodes in Pairs.py
@@ -4,13 +4,10 @@
 #         self.val = val
 #         self.next = next
 class Solution:
-    def swapPairs(self, head: ListNode) -> ListNode:
-        if  head is None:
-            return None
-        if head.next is None: 
+    def swapPairs(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if not head or not head.next:                       #If less than 2 nodes in linked list, return head directly.
             return head
-        head.next.next = self.swapPairs(head.next.next)     #Reverse the linked list after first pair. 
-        tail = head.next
-        head.next = tail.next                               #Set the next of head to be next of tail.
-        tail.next = head                                    #Reverse first pair.
-        return tail                                         #Return the new head.
+        tail = head.next                                    #Get the tail of first pair.
+        head.next = self.swapPairs(tail.next)               #Reverse the remaining of linked list and set next of head to it.
+        tail.next = head                                    #Set next ot tail to head
+        return tail                                         #Return tail after the reverse.
