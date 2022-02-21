8d8be8d093179e8773b55b56369a045d34d1e9d6
Junsheng Wang <mws19901118@gmail.com>
2022-02-20 21:02:54 -0800

    Update Majority Element.py

--- Code/Majority Element.py
+++ Code/Majority Element.py
@@ -1,12 +1,10 @@
 class Solution:
-    # @param num, a list of integers
-    # @return an integer
-    def majorityElement(self, num):
-        count={}
-        for i in num:
-            if count.has_key(i):
-                count[i]=count[i]+1
-            else:
-                count[i]=1
-            if count[i]>len(num)/2:
-                return i
+    def majorityElement(self, nums: List[int]) -> int:
+        majority, count = None, 0                           #Initialize majority and count.
+        for x in nums:                                      #Traverse nums.
+            if count == 0 or x == majority:                 #If current count is 0 or x equals majority, set majority to x and increase count.
+                majority = x
+                count += 1
+            else:                                           #Otherwise decrease count.
+                count -= 1
+        return majority                                     #Return majority.
