60b4a9721479acb6a7952945362292c125c1f016
Junsheng Wang <mws19901118@gmail.com>
2022-02-23 22:22:25 -0800

    Update Sort List.py

--- Code/Sort List.py
+++ Code/Sort List.py
@@ -4,32 +4,25 @@
 #         self.val = val
 #         self.next = next
 class Solution:
-    def merge(self, left: ListNode, right: ListNode) -> ListNode:                         #Merge 2 sorted linked list.
-        dummyHead = ListNode(0)
-        tail = dummyHead
-        while left is not None and right is not None:  
-            if left.val <= right.val:
-                tail.next = left
-                left = left.next
-            else:  
-                tail.next = right
-                right = right.next
-            tail = tail.next
-        if left is None:
-            tail.next = right
-        if right is None:
-            tail.next = left
-        return dummyHead.next  
-    def sortList(self, head: ListNode) -> ListNode:
-        if head == None or head.next == None:
+    def sortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if not head or not head.next:                               #If head is none or only has one node, directly return head.
             return head
-        slow, fast = head, head                                                         #fast pointer and slow pointer, in order to cut off the list.
+        fast, slow = head, head                                     #Use fast & slow pointers to find the mid point of list.
         while fast.next and fast.next.next:
-            slow = slow.next
             fast = fast.next.next
-        head1, head2 = head, slow.next
-        slow.next = None                                                                #head1 and head2 are the head of two new list, respectively.
-        head1 = self.sortList(head1)                                                    #Sort head1.
-        head2 = self.sortList(head2)                                                    #Sort head2.
-        head = self.merge(head1, head2)                                                 #Merge.
-        return head        
+            slow = slow.next
+        h2 = slow.next
+        slow.next = None                                            #Cut list into 2 halves.
+        h1, h2 = self.sortList(head), self.sortList(h2)             #Sort first half and second half respectively.
+        dummyHead = ListNode()                                      #Initialize a dummy head.
+        tail = dummyHead
+        while h1 and h2:                                            #Merge first half and second half.
+            if h1.val <= h2.val:
+                tail.next = h1
+                h1 = h1.next
+            else:
+                tail.next = h2
+                h2 = h2.next
+            tail = tail.next
+        tail.next = h1 if h1 else h2
+        return dummyHead.next                                       #Return the next of dummy head.
