4fa7f9b81bdf78a34d1eb6da56c2213bafc865e5
Junsheng Wang <mws19901118@gmail.com>
2022-03-02 19:51:20 -0800

    Update Arithmetic Slices.py

--- Code/Arithmetic Slices.py
+++ Code/Arithmetic Slices.py
@@ -1,12 +1,7 @@
 class Solution:
-    def numberOfArithmeticSlices(self, A: List[int]) -> int:
-        count = 0
-        i = 0
-        while i < len(A) - 2:                                           #Traverse the possible start of arithmetic sequence. Because minimal length is 3, so the max index of start is len(A) - 3.
-            j = i + 1                                                   
-            while j < len(A) and A[j] - A[j - 1] == A[i + 1] - A[i]:    #Find the end of current arithmetic sequence.
-                j += 1
-            length = j - i                                              #Calculate the length of arithmetic sequence.
-            count += (length - 2) * (length - 1) // 2                   #Calculate total slices in current sequence.
-            i = j - 1                                                   #Move start to next possible start, which is the end of current arithmetic sequence.
-        return count                                                    #Return count.
+    def numberOfArithmeticSlices(self, nums: List[int]) -> int:
+        result, count = 0, 0                                                                        #Initialize result and count of arithmetic subarrays ending at current index.
+        for i in range(2, len(nums)):                                                               #Traverse nums from index 2.
+            count = count + 1 if nums[i] - nums[i - 1] == nums[i - 1] - nums[i - 2] else 0          #Increase count by 1 if nums[i - 2:i + 1] is a arithmetic subarray; otherwise, set count to 0.
+            result += count                                                                         #Add count to result
+        return result
