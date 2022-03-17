795dcd07f7d62faf640fc8e377127d4ca96840f8
Junsheng Wang <mws19901118@gmail.com>
2022-03-16 20:41:16 -0700

    Update Score of Parentheses.py

--- Code/Score of Parentheses.py
+++ Code/Score of Parentheses.py
@@ -1,10 +1,10 @@
 class Solution:
-    def scoreOfParentheses(self, S: str) -> int:
+    def scoreOfParentheses(self, s: str) -> int:
         stack = [0]                                 #Use a stack to store the score of each level of left parenthesis so far, initally it's 0.
-        for x in S:                                 #Traverse S.
+        for x in s:                                 #Traverse s.
             if x == '(':                            #If x is '(', append 0 to stack.
                 stack.append(0)
             else:
-                y = stack.pop()                     #Otherwise, pop stack to get score of cureent level.
-                stack[-1] += y * 2 if y else 1      #If it's 0, nothing is in this '()', so add 1 to upper level; otherwise, add y * 2 to upper level, '(A)' situation.
-        return stack[0]                             #Return the only value in stack.
+                y = stack.pop()                     #Otherwise, pop stack.
+                stack[-1] += y * 2 if y else 1
+        return stack[0]
