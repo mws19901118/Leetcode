b56de9ba3feb91f8967ccc4c9e7cce05315cf6de
Junsheng Wang <mws19901118@gmail.com>
2022-03-22 17:55:25 -0700

    Update Broken Calculator.py

--- Code/Broken Calculator.py
+++ Code/Broken Calculator.py
@@ -1,13 +1,13 @@
 class Solution:
-    def brokenCalc(self, X: 'int', Y: 'int') -> 'int':
-        result = 0                        #Work backward, using "divide by 2" and "add 1" to reach X from Y. Take "divide by 2" as many as possible.
-        while X < Y:                      #While Y is larger than X, add 1 if it is odd, else divide by 2. 
-            while X < Y and Y % 2 == 0:
-                Y /= 2
+    def brokenCalc(self, startValue: int, target: int) -> int:
+        result = 0                                              #Work backward, using "divide by 2" and "add 1" to reach startValue from target. Take "divide by 2" as many as possible.
+        while startValue < target:                              #While target is larger than startValue, add 1 if it is odd, else divide by 2. 
+            while startValue < target and target % 2 == 0:
+                target >>= 1
                 result += 1
-            if X < Y:
-                Y += 1
+            if startValue < target:
+                target += 1
                 result += 1
 
-        result += int(X - Y)              #Then, we need to do X - Y additions to reach X
+        result += int(startValue - target)                      #Then, we need to do (startValue - target) additions to reach startValue.
         return result
