260a1eb3c3f24ef8a086a5d8d074cff455e8330c
Junsheng Wang <mws19901118@gmail.com>
2022-03-29 21:00:53 -0700

    Update Search a 2D Matrix.py

--- Code/Search a 2D Matrix.py
+++ Code/Search a 2D Matrix.py
@@ -1,25 +1,13 @@
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-        if not matrix or not matrix[0]:                                                             #Check if matrix is empty or matrix row is empty.
-            return False                                                                            #If so, return false.
-        m, n = len(matrix), len(matrix[0])                                                          #Get the dimensions of matrix.
-        start, end, index = 0, m, 0
-        while start <= end:                                                                         #Binary search in the first column to find the last row that is not greater than target.
+        start, end = 0, len(matrix) - 1
+        while start < end:                                                                      #Binary search for the row where target should locate.
             mid = (start + end) // 2
-            if matrix[mid][0] <= target and (mid >= m - 1 or matrix[mid + 1][0] > target):
-                index = mid                                                                         #Get the row index of the row which target could belong to.
-                break
-            elif matrix[mid][0] > target:
+            if matrix[mid][0] > target:                                                         #If matrix[mid][0] > target, target can only be in matrix[:mid].
                 end = mid - 1
-            else:
+            elif matrix[mid][-1] < target:                                                      #If matrix[mid][-1] < target, target can only be in matrix[mid + 1:].
                 start = mid + 1
-        start, end = 0, n - 1
-        while start <= end:                                                                         #Binary search in that row to find target.
-            mid = (start + end) // 2
-            if matrix[index][mid] == target:
-                return True                                                                         #If found, return true.
-            elif matrix[index][mid] > target:
-                end = mid - 1
-            else:
-                start = mid + 1
-        return False                                                                                #Otherwise return false.
+            else:                                                                               #Otherwise, target can only be in matrix[mid].
+                start, end = mid, mid
+        index = bisect_left(matrix[start], target)                                              #Binary search for the left-most index to insert target in matrix[start].
+        return index != len(matrix[start]) and matrix[start][index] == target                   #If index is valid and target is at the index, return true; otherwise, return false.
