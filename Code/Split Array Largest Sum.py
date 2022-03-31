436d7c25f5d481e5cdfc225d6aa88c231a2a990a
Junsheng Wang <mws19901118@gmail.com>
2022-03-30 22:30:10 -0700

    Update Split Array Largest Sum.py

--- Code/Split Array Largest Sum.py
+++ Code/Split Array Largest Sum.py
@@ -4,14 +4,12 @@ class Solution:
         result = end                          #Since the least number of subarray is 1, so start from the max number in array.
         while start <= end:                   #Also, if m = 1, then the least sum is the sum of array, so end with the sum of array.
             mid = (start + end) // 2          #Find the mid value between start and end.
-            s = 0                             #Store the temp sum.
-            count = 1                         #Count the number of subarray whose sum is not larger than mid.
+            s, count = 0, 1                   #Store the temp sum and count of subarray whose sum is not larger than mid.
             for x in nums:                    #Traverse the array to split the array as less as possible so the sum of each subarray is no larger than mid.
                 if s + x > mid:
                     count += 1
-                    s = x
-                else:
-                    s += x
+                    s = 0
+                s += x
             if count > m:                     #If the total satisfied subarrays are more than m, continue binary search from mid + 1 to end.
                 start = mid + 1
             else:                             #Otherwise, update result to find the least satisfied result and keep binary search from start to mid - 1.
