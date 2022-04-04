0dbe7d5ddff61a9a397039a4d750616ae4910b27
Junsheng Wang <mws19901118@gmail.com>
2022-04-03 17:25:17 -0700

    Update Swapping Nodes in a Linked List.py

--- Code/Swapping Nodes in a Linked List.py
+++ Code/Swapping Nodes in a Linked List.py
@@ -5,20 +5,12 @@
 #         self.next = next
 class Solution:
     def swapNodes(self, head: ListNode, k: int) -> ListNode:
-        count = 0
         curr = head
-        while curr:                             #Count linked list length.
-            count += 1
+        for _ in range(k - 1):                  #Find the k-th node from beginning.
             curr = curr.next
-        a, b = None, None                       #Initialize the 2 nodes to swap with None.
-        curr = head
-        step = 0
-        while not a or not b:                   #Traverse linked list until both a and b are set.
-            step += 1
-            if step == k:                       #If step equals k, point a to current node.
-                a = curr
-            if step == count - k + 1:           #If step equals count - k + 1, point b to current node.
-                b = curr
+        a, b = curr, head                       #Store it in a and then create a new pointer from beginning.
+        while curr.next:                        #Move curr and b both towards end; when curr is the end, b is the k-th node from the end.
             curr = curr.next
+            b = b.next
         a.val, b.val = b.val, a.val             #Swap a.val and b.val.
         return head                             #Return head.
