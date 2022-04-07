5159faf7df747395714ea9331f4e978850f44134
Junsheng Wang <mws19901118@gmail.com>
2022-04-06 17:05:02 -0700

    Update Last Stone Weight.py

--- Code/Last Stone Weight.py
+++ Code/Last Stone Weight.py
@@ -1,4 +1,3 @@
-import heapq
 class Solution:
     def lastStoneWeight(self, stones: List[int]) -> int:
         maxHeap = [-stone for stone in stones]            #Use the negative value of each stone to form a max heap.
@@ -8,7 +7,4 @@ class Solution:
             x = -heapq.heappop(maxHeap)                   #Find the second heaviest stone x.
             if x < y:                                     #If x < y, push x - y(weight y - x) to max heap.
                 heapq.heappush(maxHeap, x - y)
-        if len(maxHeap) == 1:                             #If only 1 stone left, return its weight.
-            return -maxHeap[0]
-        else:                                             #Otherwise return 0.
-            return 0
+        return -maxHeap[0] if len(maxHeap) else 0         #If only 1 stone left, return its weight; otherwise return 0.
