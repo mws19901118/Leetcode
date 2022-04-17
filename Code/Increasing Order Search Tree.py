684c2d326b27352ad4253abf83520b39de79443f
Junsheng Wang <mws19901118@gmail.com>
2022-04-16 20:45:39 -0700

    Update Increasing Order Search Tree.py

--- Code/Increasing Order Search Tree.py
+++ Code/Increasing Order Search Tree.py
@@ -1,28 +1,21 @@
 # Definition for a binary tree node.
 # class TreeNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
-
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
 class Solution:
-    def increasingBST(self, root):
-        """
-        :type root: TreeNode
-        :rtype: TreeNode
-        """
-        order = []
-        self.traverse(root, order)
-        newRoot = TreeNode(order[0])            #Create new tree.
-        itr = newRoot
-        for i in range(1, len(order)):
-            itr.right = TreeNode(order[i])
-            itr = itr.right
-        return newRoot
+    def convert(self, root: TreeNode) -> (TreeNode, TreeNode):          #Convert BST and return root and leaf.
+        newRoot, newLeaf = None, None                                   #Initialize new root and new leaf.
+        if root.left:                                                   #If root has left child, convert it's left subtree and update newRoot and newLeaf.
+            newRoot, newLeaf = self.convert(root.left)
+            root.left = None                                            #Set root.left to none.
+            newLeaf.right, newLeaf = root, root                         #Point newLeaf.right to root and then point newLeaf to root.
+        else:                                                           #Otherwise, newRoot and newLeaf is root.
+            newRoot, newLeaf = root, root
+        if root.right:                                                  #If root has right child, convert it's right subtree and update newLeaf.
+            newLeaf.right, newLeaf = self.convert(root.right)
+        return newRoot, newLeaf                                         #Return newRoot and newLeaf.
         
-    def traverse(self, root, order):            #Traverse the tree and record the values in order.
-        if root is None:
-            return
-        self.traverse(root.left, order)
-        order.append(root.val)
-        self.traverse(root.right, order)
+    def increasingBST(self, root: TreeNode) -> TreeNode:
+        return self.convert(root)[0]                                    #Return the root after convertion.
