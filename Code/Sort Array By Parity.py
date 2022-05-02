332d44b5b89218fcf059ee26ae7fabce16ac7c27
Junsheng Wang <mws19901118@gmail.com>
2022-05-01 17:26:28 -0700

    Update Sort Array By Parity.py

--- Code/Sort Array By Parity.py
+++ Code/Sort Array By Parity.py
@@ -1,14 +1,3 @@
 class Solution:
-    def sortArrayByParity(self, A):
-        """
-        :type A: List[int]
-        :rtype: List[int]
-        """
-        odd = []
-        even = []
-        for a in A:
-            if a % 2:             #Find all odd values.
-                odd.append(a)
-            else:                 #Find all even values.
-                even.append(a)
-        return even + odd         #Add odd at the end of even and return.
+    def sortArrayByParity(self, A: List[int]) -> List[int]:
+        return [x for x in A if not x & 1] + [x for x in A if x & 1]        #Put even integers before odd integers.
