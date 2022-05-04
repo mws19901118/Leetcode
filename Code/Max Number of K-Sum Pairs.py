89543375f46ed2f1d53337405847a1557b2d2963
Junsheng Wang <mws19901118@gmail.com>
2022-05-03 21:10:28 -0700

    Update Max Number of K-Sum Pairs.py

--- Code/Max Number of K-Sum Pairs.py
+++ Code/Max Number of K-Sum Pairs.py
@@ -1,12 +1,4 @@
 class Solution:
     def maxOperations(self, nums: List[int], k: int) -> int:
-        c = Counter(nums)                             #Count each number in nums.
-        result = 0
-        for x in c:                                   #Traverse the keys in counter.
-            if k - x == x:                            #If 2 * x == k, and c[x] // 2 to result.
-                result += c[x] // 2
-            elif k - x in c:                          #Otherwise if k - x in c, add min(c[x], c[k - x]) to result.
-                result += min(c[x], c[k - x])
-                c[x] = 0                              #Set c[x] and c[k - x] to 0 because they have already been visited.
-                c[k - x] = 0
-        return result
+        c = Counter(nums)                                                                                                   #Count each number in nums.
+        return sum([(c[x] // 2) if k - x == x else min(c[x], c[k - x]) for x in c if x <= k - x and k - x in c])            #Traverse the keys in counter. If x < k - x, add min(c[x], c[k - x]) to result; if x == k - x, add c[x] // 2 to result.
