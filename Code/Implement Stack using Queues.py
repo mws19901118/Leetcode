2a7247e7ed072e8c61ae7966e97127aec2ec0e0e
Junsheng Wang <mws19901118@gmail.com>
2022-05-04 18:37:17 -0700

    Update Implement Stack using Queues.py

--- Code/Implement Stack using Queues.py
+++ Code/Implement Stack using Queues.py
@@ -1,40 +1,29 @@
-class Stack:
-    # initialize your data structure here.
-    def __init__(self):                                   #Use 2 queues to simulate stack.
-        self.q1=[]
-        self.q2=[]
+class MyStack:
 
-    # @param x, an integer
-    # @return nothing
-    def push(self, x):
-        if self.q1==[]:                                   #If q1 is empty, append x into q2; otherwise append x into q1.
-            self.q2.append(x)
-        else:
-            self.q1.append(x)
+    def __init__(self):
+        self.q = deque()                                            #Use a queue to simulate stack.
 
-    # @return nothing
-    def pop(self):
-        if self.q1==[]:                                 #If q1 is empty, deque all the elements of q2 and append them into q1 except the last element.
-            while len(self.q2)>1:
-                self.q1.append(self.q2[0])
-                self.q2.remove(self.q2[0])
-            self.q2.remove(self.q2[0])
-        else:                                           #If q2 is empty, deque all the elements of q1 and append them into q2 except the last element.
-            while len(self.q1)>1:
-                self.q2.append(self.q1[0])
-                self.q1.remove(self.q1[0])
-            self.q1.remove(self.q1[0])
+    def push(self, x: int) -> None:
+        self.q.append(x)                                            #Append x to queue.
 
-    # @return an integer
-    def top(self):
-        if self.q1==[] and self.q2==[]:                 #If q1 and q2 are both empty, return None.   
-            return None
-        elif self.q1==[]:                               #If q1 is empty, return the last element of q2.
-            return self.q2[-1]
-        else:                                           #If q2 is empty, return the last element of q1.
-            return self.q1[-1]
-
-    # @return an boolean
-    def empty(self):
-        return self.q1==[] and self.q2==[]              #If q1 and q2 are both empty, return true; otherwise return false.
+    def pop(self) -> int:
+        n = len(self.q)                                             #Get current queue length.
+        for _ in range(n - 1):                                      #For the first n - 1 elements, popleft then append to queue.
+            self.q.append(self.q.popleft())
+        return self.q.popleft()                                     #Popleft the last element and return.
         
+    def top(self) -> int:
+        result = self.pop()                                         #Pop the element at "stack" top.
+        self.q.append(result)                                       #Append it to queue again.
+        return result                                               #Return result.
+
+    def empty(self) -> bool:
+        return not self.q                                           #Return if queue is empty.
+
+
+# Your MyStack object will be instantiated and called as such:
+# obj = MyStack()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.empty()
