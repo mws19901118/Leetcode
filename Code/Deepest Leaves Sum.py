da019dab813310781e29a1cb472b1d7e470bfe90
Junsheng Wang <mws19901118@gmail.com>
2022-05-14 17:11:36 -0700

    Update Deepest Leaves Sum.py

--- Code/Deepest Leaves Sum.py
+++ Code/Deepest Leaves Sum.py
@@ -11,12 +11,11 @@ class Solution:
             newq = []
             leavesSum = 0
             for x in q:
-                if x.left is None and x.right is None:        #Calculate the sum of leaves in current level.
+                if not x.left and not x.right:                #Calculate the sum of leaves in current level.
                     leavesSum += x.val
-                if x.left is not None:
-                    newq.append(x.left)
-                if x.right is not None:
-                    newq.append(x.right)
+                for y in [x.left, x.right]:
+                    if y:
+                        newq.append(y)
             if not newq:                                      #If reaches the last level, return leavesSum. 
                 return leavesSum
             q = newq                                          #Go tot next level.
