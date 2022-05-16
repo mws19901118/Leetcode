37fe935a1f71308e90a547735bafe3d6aacbaae5
Junsheng Wang <mws19901118@gmail.com>
2022-05-15 21:43:03 -0700

    Update Shortest Path in Binary Matrix.py

--- Code/Shortest Path in Binary Matrix.py
+++ Code/Shortest Path in Binary Matrix.py
@@ -1,6 +1,5 @@
 class Solution:
     def shortestPathBinaryMatrix(self, grid: List[List[int]]) -> int:
-        m, n = len(grid), len(grid[0])                                                  #Get dimensions of grid.
         if grid[0][0]:                                                                  #Check if the start cell is empty; if not, return -1.
             return -1
         length = 1                                                                      #Initialize length.
@@ -9,13 +8,12 @@ class Solution:
         while q:                                                                        #BFS.
             newq = []
             for x, y in q:
-                if x == m - 1 and y == n - 1:                                           #If reaches the end cell, return length.
+                if x == len(grid) - 1 and y == len(grid) - 1:                           #If reaches the end cell, return length.
                     return length
-                for nx in [x - 1, x, x + 1]:                                            #Traverse surronding cells in all 8 directions.
-                    for ny in [y - 1, y, y + 1]:
-                        if 0 <= nx < m and 0 <= ny < n and grid[nx][ny] == 0:           #If its valid and empty, append it to newq and set it to 1.
-                            newq.append((nx, ny))
-                            grid[nx][ny] = 1
+                for nx, ny in product([x - 1, x, x + 1], [y - 1, y, y + 1]):            #Traverse surronding cells in all 8 directions.
+                    if 0 <= nx < m and 0 <= ny < n and grid[nx][ny] == 0:               #If its valid and empty, append it to newq and set it to 1.
+                        newq.append((nx, ny))
+                        grid[nx][ny] = 1
             length += 1
             q = newq
         return -1                                                                       #Return -1 if cannot reach the end cell.
