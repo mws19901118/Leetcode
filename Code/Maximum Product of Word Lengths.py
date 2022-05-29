ab2ba59f9e17341a9622670163be0a31c2230442
Junsheng Wang <mws19901118@gmail.com>
2022-05-28 20:57:00 -0700

    Update Maximum Product of Word Lengths.py

--- Code/Maximum Product of Word Lengths.py
+++ Code/Maximum Product of Word Lengths.py
@@ -1,14 +1,10 @@
 class Solution:
     def maxProduct(self, words: List[str]) -> int:
-        lengths = defaultdict(int)
-        for s in words:
-            binary = 0
-            for c in s:
-                binary |= 1 << (ord(c) - ord('a'))                  #Map the letter set of a string to a 26-bit binary. Each bit indicates if the string has correspoding letter.
-            lengths[binary] = max(lengths[binary], len(s))          #For every binary, only store the length of the longest string.
-        result = 0
-        for x, lx in lengths.items():                               #Traverse through every binary pair.
-            for y, ly in lengths.items():
-                if x & y == 0:                                      #If x & y == 0, the 2 strings don't share common letter.
-                    result = max(result, lx * ly)                   #Update the max product.
-        return result
+        masks = {x: 1 << (ord(x) - ord('a')) for x in 'abcdefghijklmnopqrstuvwxyz'}                                                             #Initialize the bit masks for each letter.
+        lengths = defaultdict(int)                                                                                                              #Store the max word length of each word bit mask.
+        for w in words:                                                                                                                         #Traverse words.
+            bitMask = 0
+            for x in w:                                                                                                                         #Calculate the bit mask for each word.
+                bitMask |= masks[x]
+            lengths[bitMask] = max(len(w), lengths[bitMask])                                                                                    #Update the max word length of each bit mask.
+        return max([lengths[x] * lengths[y] for x, y in product(lengths.keys(), lengths.keys()) if not x & y], default = 0)                     #Return the max product of word length of any 2 words not sharing common letter; if not found such pair, return 0.
