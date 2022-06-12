d329b8935c89555b1690880b2180f7cfbd486dbb
Junsheng Wang <mws19901118@gmail.com>
2022-06-11 18:01:06 -0700

    Update Maximum Erasure Value.py

--- Code/Maximum Erasure Value.py
+++ Code/Maximum Erasure Value.py
@@ -1,15 +1,11 @@
 class Solution:
-    def maximumUniqueSubarray(self, nums: List[int]) -> int:
-        indexes = {}                                        #Store the index of each integer.
-        leftEnd, currentSum, maxScore = 0, 0, 0             #Initialize the left end of subarray, current subarray sum and max score.
-        for i, x in enumerate(nums):                        #Traverse nums.
-            if x in indexes:                                #If x is already seen, remove all integers from left end to last index of x from current sum and indexes.
-                lastIndex = indexes[x]
-                for y in nums[leftEnd:lastIndex + 1]:
-                    currentSum -= y
-                    del indexes[y]
-                leftEnd = lastIndex + 1                     #Update new left end to be the next of last index of x.
-            currentSum += x                                 #Add x to current sum.
-            maxScore = max(maxScore, currentSum)            #Keep updating max score so far.
-            indexes[x] = i                                  #Add current index to map.
-        return maxScore
+    def maximumUniqueSubarray(self, nums: List[int]) -> int:    #Basically, this question is asking the largest sum subarray without duplicate value.
+        leftSum, prefixSum, result = 0, 0, 0                    #Intialize the prefix sum on the left, the prefix sum so far and result.
+        visited = {-1:0}                                        #Store the prefix sum to where current value is last visited.
+        for i, x in enumerate(nums):                            #Traverse nums.
+            prefixSum += x                                      #Update prefix sum.
+            if x in visited:                                    #If x is visited, update leftSum to visited[x] if it's larger, because all value is positive.
+                leftSum = max(leftSum, visited[x])
+            visited[x] = prefixSum                              #Update visited[x].
+            result = max(result, prefixSum - leftSum)           #Update result to prefixSum - leftSum if it's larger.
+        return result                                           #Return result.
