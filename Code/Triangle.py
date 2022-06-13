eb23e04b79c8ceb7a007cfcf35d36d4d8b7e1b3b
Junsheng Wang <mws19901118@gmail.com>
2022-06-12 23:14:02 -0700

    Update Triangle.py

--- Code/Triangle.py
+++ Code/Triangle.py
@@ -1,10 +1,7 @@
 class Solution:
     def minimumTotal(self, triangle: List[List[int]]) -> int:
-        level = triangle[0]                                                         #Record the minimum path sum at current row, initially it's the first row of triangle.
-        for i in range(1, len(triangle)):                                           #Traverse triangle by level.
-            newLevel = [level[0] + triangle[i][0]]                                  #Initialize the minimum path sum at this level with leftmost path sum.
-            for j in range(1, len(triangle[i]) - 1):                                #Calculate the minimim path sum at this level for number not at either side and append.
-                newLevel.append(triangle[i][j] + min(level[j - 1], level[j]))
-            newLevel.append(level[-1] + triangle[i][-1])                            #Append the rightmost minimum path sum.
-            level = newLevel                                                        #Replace level with newLevel.
-        return min(level)                                                           #Return minumum number in level.
+        level = triangle[0]                                                                                                                                                             #Record the minimum path sum at current row, initially it's the first row of triangle.
+        for i in range(1, len(triangle)):                                                                                                                                               #Traverse triangle by level.
+            nextLevel = [level[0] + triangle[i][0]] + [triangle[i][j] + min(level[j - 1], level[j]) for j in range(1, len(triangle[i]) - 1)] + [level[-1] + triangle[i][-1]]            #Updatet the minimal path sum at next level.
+            level = newLevel                                                                                                                                                            #Replace level with nextLevel.
+        return min(level)                                                                                                                                                               #Return minumum number in level.
