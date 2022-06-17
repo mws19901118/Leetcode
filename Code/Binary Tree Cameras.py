fae82741f4c369f16c08631a793d677f1992a774
Junsheng Wang <mws19901118@gmail.com>
2022-06-16 22:53:31 -0700

    Update Binary Tree Cameras.py

--- Code/Binary Tree Cameras.py
+++ Code/Binary Tree Cameras.py
@@ -5,7 +5,7 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def dfs(self, node: TreeNode) -> Tuple:                 #DFS, return a tuple of min cameras needed in 3 situations(all nodes covered except node, all nodes covered covered and no camera placed at node, all nodes covered and camera placed at node).
+    def dfs(self, node: TreeNode) -> Tuple:                 #DFS, return a tuple of min cameras needed in 3 situations(all nodes covered except node, all nodes covered and no camera placed at node, all nodes covered and camera placed at node).
         if not node:                                        #If node is none, return (0, 0, inf).
             return 0, 0, float('inf')
         l, r = self.dfs(node.left), self.dfs(node.right)    #DFS left subtree and right subtree.
@@ -14,5 +14,5 @@ class Solution:
         dp2 = 1 + min(l) + min(r)                           #To cover all nodes and place camera at node, the left child and right child are automatically covered. 
         return dp0, dp1, dp2
     
-    def minCameraCover(self, root: TreeNode) -> int:
+    def minCameraCover(self, root: Optional[TreeNode]) -> int:
         return min(self.dfs(root)[1:])                      #DFS from root and return the min value of cameras needed to cover all nodes, either place camera at root or not.
