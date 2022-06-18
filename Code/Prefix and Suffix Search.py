8cad160805a25fc852cab31a197a855fb21e94de
Junsheng Wang <mws19901118@gmail.com>
2022-06-18 00:21:34 -0700

    Update Prefix and Suffix Search.py

--- Code/Prefix and Suffix Search.py
+++ Code/Prefix and Suffix Search.py
@@ -1,41 +1,42 @@
-class Trie:                                                                                                     #Use trie to store the indexes of words which has certain prefix.
+class Trie:                                                                                                                                                     #Use trie to store max index for the pairs of (prefix, suffix) letter at same distance from end.
     def __init__(self):
-        self.map = {}                                                                                           #Initialize map for current node.
+        self.map = {}
+        self.letters = 'abcdefghijklmnopqrstuvwxyz'                                                                                                             #Store letters.
     
-    def insert(self, word: str, index: int) -> None:                                                            #Insert word into trie.
-        if not word:                                                                                            #If word is empty, return.
+    def insert(self, pairs: List[tuple], index: int) -> None:                                                                                                   #Insert pairs into trie.
+        if not pairs:                                                                                                                                           #If pairs is empty, return.
             return
-        if word[0] not in self.map:                                                                             #If the first letter not in map, create an entry(a list for indexes and a Trie for next node) in map for it.
-            self.map[word[0]] = ([], Trie())
-        self.map[word[0]][0].append(index)                                                                      #Append index to the list.
-        self.map[word[0]][1].insert(word[1:], index)                                                            #Insert word[1:] in next node recursively.
+        if pairs[0] not in self.map:                                                                                                                            #If the first pair not in map, insteniate the max index and node to next level pair.  
+            self.map[pairs[0]] = [-1, Trie()]
+        self.map[pairs[0]][0] = index                                                                                                                           #Since we always insert pairs of word from small index to large index, keep update index to the latest.
+        self.map[pairs[0]][1].insert(pairs[1:], index)                                                                                                          #Insert remaining pairs.
     
-    def find(self, prefix: str) -> List[int]:                                                                   #Find the list of indexes for given prefix.
-        if not prefix or prefix[0] not in self.map:                                                             #If prefix is empty or the first letter not in map, return empty list.
-            return []
-        return self.map[prefix[0]][0] if len(prefix) == 1 else self.map[prefix[0]][1].find(prefix[1:])          #If prefix has only one letter, return the list in map; otherwise, return the result of prefix[1:] recursively.
-    
+    def find(self, pairs: str) -> int:
+        if not pairs:                                                                                                                                           #If no more pairs, return -1.
+            return -1
+        expandedPairs = [pairs[0]]                                                                                                                              #Expand pairs[0] in case prefix or suffix in pair is none; otherwise it only has pairs[0].
+        if not pairs[0][0]:                                                                                                                                     #If prefix is none, match any pair in current node with this suffix.
+            expandedPairs = [(x, pairs[0][1]) for x in self.letters]
+        elif not pairs[0][1]:                                                                                                                                   #If suffix is none, match any pair in current node with this prefix.
+            expandedPairs = [(pairs[0][0], x) for x in self.letters]
+
+        result = -1                                                                                                                                             #Initialize result.
+        for p in expandedPairs:                                                                                                                                 #Traverse expandedPairs.
+            if p in self.map:                                                                                                                                   #If p is in self.map, there are words with given prefix and suffix so far.
+                result = max(result, self.map[p][0] if len(pairs) == 1 else self.map[p][1].find(pairs[1:]))                                                     #If pairs has only pair, result is the index in map; otherwise, keep finding pairs[1:] recursively.
+        return result                                                                                                                                           #Return the max result from traverse.
+
 class WordFilter:
 
     def __init__(self, words: List[str]):
-        self.prefixTree = Trie()                                                                                #Initalize trie for prefix.
-        self.suffixTree = Trie()                                                                                #Initalize trie for suffix.
-        for i, w in enumerate(words):                                                                           #Traverse words.
-            self.prefixTree.insert(w, i)                                                                        #Insert word to prefix tree.
-            self.suffixTree.insert(w[::-1], i)                                                                  #Insert the reverse of word to suffix tree.
+        self.trie = Trie()                                                                                                                                      #Instantiate trie. 
+        for i, w in enumerate(words):                                                                                                                           #Traverse words.
+            pairs = [(w[j], w[-(j + 1)]) for j in range(len(w))]                                                                                                #For each word, generate paris of (prefix, suffix) letter at same distance from end.
+            self.trie.insert(pairs, i)                                                                                                                          #Insert pairs.
 
     def f(self, prefix: str, suffix: str) -> int:
-        prefixIndexes = self.prefixTree.find(prefix)                                                            #Find the indexes from prefix tree.
-        suffixIndexes = self.suffixTree.find(suffix[::-1])                                                      #Find the indexes from suffix tree.
-        i, j = len(prefixIndexes) - 1, len(suffixIndexes) - 1
-        while i >= 0 and j >= 0:                                                                                #Traverse 2 lists to find the largest common index and return.
-            if prefixIndexes[i] > suffixIndexes[j]:
-                i -= 1
-            elif prefixIndexes[i] < suffixIndexes[j]:
-                j -= 1
-            else:
-                return prefixIndexes[i]
-        return -1                                                                                               #If not found, return -1.
+        pairs = [(prefix[i] if i < len(prefix) else None, suffix[-(i + 1)] if i < len(suffix) else None) for i in range(max(len(prefix), len(suffix)))]         #Generate paris of (prefix, suffix) letter at same distance from end; if any prefix letter has no corrsponding suffix letter or vice versa, pad the pair with none.
+        return self.trie.find(pairs)                                                                                                                            #Return the result of find pairs from trie.
 
 # Your WordFilter object will be instantiated and called as such:
 # obj = WordFilter(words)
