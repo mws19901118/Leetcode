6fe88ee980cd95b869f3a395ea7a4cd287044e06
Junsheng Wang <mws19901118@gmail.com>
2022-06-18 23:08:18 -0700

    Update Search Suggestions System.py

--- Code/Search Suggestions System.py
+++ Code/Search Suggestions System.py
@@ -1,22 +1,22 @@
 class Trie:
-    def __init__(self):                                                                       #Initialize trie node.
-        self.words = []                                                                       #Store words in current node.
-        self.map = defaultdict(Trie)                                                          #Store trie nodes in next level.
+    def __init__(self):                                                                         #Initialize trie node.
+        self.indexes = []                                                                       #Store indexes of words in current node.
+        self.map = defaultdict(Trie)                                                            #Store trie nodes in next level.
     
-    def addWord(self, index: int, word: str) -> None:
-        self.words.append(word)                                                               #Append word to words list of current node.
-        if index == len(word):                                                                #If reaches the end of word, return.
+    def insert(self, word: str, index: int) -> None:
+        self.indexes.append(index)                                                              #Append word index to indexes list.
+        if not word:                                                                            #If this is the end of word, return
             return
-        self.map[word[index]].addWord(index + 1, word)                                        #Add word to next level trie node.
+        self.map[word[0]].insert(word[1:], index)                                               #Add word to next level trie node.
         
 class Solution:
     def suggestedProducts(self, products: List[str], searchWord: str) -> List[List[str]]:
-        trie = Trie()                                                                         #Initialize trie.
-        products.sort()                                                                       #Sort products lexicographically.
-        for p in products:                                                                    #Add each word to trie.
-            trie.addWord(0, p)
-        result = []                                                                           #Initialize result.
-        for x in searchWord:
-            result.append(trie.map[x].words[:3])                                              #Append top 3 search results of current type to result.
-            trie = trie.map[x]                                                                #Go to next level trie node.
+        trie = Trie()                                                                           #Initialize trie.
+        products.sort()                                                                         #Sort products lexicographically.
+        for i, p in enumerate(products):                                                        #Add each word and its index to trie.
+            trie.insert(p, i)
+        result = []                                                                             #Initialize result.
+        for x in searchWord:                                                                    #Traverse searchWord.
+            trie = trie.map[x]                                                                  #Go to next level trie node.
+            result.append([products[i] for i in trie.indexes[:3]])                              #Append top 3 search results of current type to result.
         return result
