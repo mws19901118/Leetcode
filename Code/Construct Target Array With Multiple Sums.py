b4e05169b56192cfeea1b2733c842b7ada78836b
Junsheng Wang <mws19901118@gmail.com>
2022-06-23 21:52:13 -0700

    Update Construct Target Array With Multiple Sums.py

--- Code/Construct Target Array With Multiple Sums.py
+++ Code/Construct Target Array With Multiple Sums.py
@@ -1,14 +1,15 @@
 class Solution:
     def isPossible(self, target: List[int]) -> bool:
-        total = sum(target)                       #Sum up the total number.
-        heap = [-x for x in target]               #Use max heap to store elements.
+        total = sum(target)                         #Sum up the total number.
+        heap = [-x for x in target]                 #Use max heap to store elements.
         heapq.heapify(heap)
-        while heap[0] != -1:                      #While the heap top is not 1.
-            x = -heapq.heappop(heap)              #Pop heap.
-            total -= x                            #Subtract x from total, which is the previous sum.
-            if x <= total or total < 1:           #If x <= total or total < 1, x cannot be set, return false.
+        while heap[0] != -1:                        #While the heap top is not 1.
+            x = -heapq.heappop(heap)                #Pop heap.
+            total -= x                              #Subtract x from total, which is the previous sum.
+            if x <= total or total < 1:             #If x <= total or total < 1, x cannot be set, return false.
                 return False
-            x %= total                            #Subtract total from x until x is smaller than total, which equals to x %= total.
-            heapq.heappush(heap, -x)              #Push updated x back to heap.
-            total += x                            #Update total after pushing x back to heap.
+            n = max((x - 1) // total, 1)
+            x -= n * total                          #Subtract total from x until x is smaller than total but not zero, which equals to x - max((x - 1) // total, 1) * total.
+            heapq.heappush(heap, -x)                #Push updated x back to heap.
+            total += x                              #Update total after pushing x back to heap.
         return True
