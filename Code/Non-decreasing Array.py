e84945ce48026298cbdabf58758899d778b1b5e5
Junsheng Wang <mws19901118@gmail.com>
2022-06-24 19:28:04 -0700

    Update Non-decreasing Array.py

--- Code/Non-decreasing Array.py
+++ Code/Non-decreasing Array.py
@@ -8,8 +8,4 @@ class Solution:
         j = i + 1                                                                                                                 #Make sure the array from c to end is also non-decreasing.
         while j < len(nums) and nums[j] >= nums[j - 1]:
             j += 1
-        return j == len(nums) and (i == 1 or i + 1 == len(nums) or nums[i] >= nums[i - 2] or nums[i + 1] >= nums[i - 1])          #Also, if b is start, we can set b = c;
-                                                                                                                                  #if c is end, we can set c = b;
-                                                                                                                                  #if c >= a. we can set b = c;
-                                                                                                                                  #if d >= b, we can set c = b.
-                                                                                                                                  #Only under these 4 conditions can we make the whole array non-decreasing.
+        return j == len(nums) and (i == 1 or i + 1 == len(nums) or nums[i] >= nums[i - 2] or nums[i + 1] >= nums[i - 1])          #Also, if b is start or c is end or c >= a or d >= b, we can set b = c; only under these 4 conditions can we make the whole array non-decreasing.
