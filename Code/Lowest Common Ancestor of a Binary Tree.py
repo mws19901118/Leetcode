fcb0f5270443e391a0772b81450a7afd60724946
Junsheng Wang <mws19901118@gmail.com>
2022-07-25 17:21:21 -0700

    Update Lowest Common Ancestor of a Binary Tree.py

--- Code/Lowest Common Ancestor of a Binary Tree.py
+++ Code/Lowest Common Ancestor of a Binary Tree.py
@@ -7,13 +7,11 @@
 
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-        if root is None or root is p or root is q:                  #If root is none or root is p or q, return itself.
+        if not root or root is p or root is q:                      #If root is none or root is p or q, return itself.
             return root
         left = self.lowestCommonAncestor(root.left, p, q)           #Get the LCA of p and q in the tree whose root is the left child of current root.
         right = self.lowestCommonAncestor(root.right, p, q)         #Get the LCA of p and q in the tree whose root is the right child of current root.
-        if left is not None and right is not None:                  #If root is not the LCA of p and q, either p or q must be none.
+        if left and right:                                          #If root is not the LCA of p and q, either p or q must be none.
             return root
-        elif left is not None:
-            return left
         else:
-            return right
+            return left if left else right
