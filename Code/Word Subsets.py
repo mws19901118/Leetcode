889ac1b9ce6284f0973f842e70051a999bc5c793
Junsheng Wang <mws19901118@gmail.com>
2022-07-29 18:21:22 -0700

    Update Word Subsets.py

--- Code/Word Subsets.py
+++ Code/Word Subsets.py
@@ -1,12 +1,12 @@
 class Solution:
-    def isUniversal(self, a: str, counterBUnion: Counter) -> bool:                            #Determine if a is universal.
-        counterA = Counter(a)                                                                 #Get the counter of a.
-        return all(counterA[x] >= counterBUnion[x] for x in counterBUnion)                    #Return if all characters in counterBUnion has at least the same count in counterA as in counterBUnion.
+    def isUniversal(self, w: str, counterUnion: Counter) -> bool:                                   #Determine if a is universal.
+        counter = Counter(w)                                                                        #Get the counter of a.
+        return all(counter[x] >= counterUnion[x] for x in counterUnion)                             #Return if all characters in counterBUnion has at least the same count in counterA as in counterB.
     
-    def wordSubsets(self, A: List[str], B: List[str]) -> List[str]:
-        counterBUnion = Counter()
-        for w in B:                                                                           #Union the character count of each word in B.
-            counterB = Counter(w)
-            for x in counterB:
-                counterBUnion[x] = max(counterBUnion[x], counterB[x])
-        return [word for word in A if self.isUniversal(word, counterBUnion)]                  #Return all the universal word in A.
+    def wordSubsets(self, words1: List[str], words2: List[str]) -> List[str]:
+        counterUnion = Counter()
+        for w in words2:                                                                            #Union the character count of each word in words2.
+            counter = Counter(w)
+            for x in counter:
+                counterUnion[x] = max(counterUnion[x], counter[x])
+        return filter(lambda x: self.isUniversal(x, counterUnion), words1)                          #Return all the universal word in words1.
