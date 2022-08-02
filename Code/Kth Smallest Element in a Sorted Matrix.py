35d0529c9b41e03682cec50ef9cda07a0a64806d
Junsheng Wang <mws19901118@gmail.com>
2022-08-01 22:02:14 -0700

    Update Kth Smallest Element in a Sorted Matrix.py

--- Code/Kth Smallest Element in a Sorted Matrix.py
+++ Code/Kth Smallest Element in a Sorted Matrix.py
@@ -3,8 +3,8 @@ class Solution:
         minV, maxV = matrix[0][0], matrix[-1][-1]       #Get the min value and max value in matrix.
         while minV < maxV:                              #Bianry search kth smallest element from minV to maxV.
             mid = (minV + maxV) // 2                    #Get mid of minV and maxV.
-            count, x, y = 0, 0, len(matrix) - 1         #Count how many elements are no larger than mid. And initialize a pointer to the upper right corner.   
-            while x < len(matrix) and y >= 0:           #While the pointer is valid.
+            count, x, y = 0, 0, len(matrix) - 1         #Count how many elements are no larger than mid and initialize a pointer to the upper right corner.   
+            while x < len(matrix) and y >= 0:           #While the pointer is valid, traverse from upper right corner to lower left corner.
                 if matrix[x][y] <= mid:                 #If current value is no larger than mid, all elements before current element and in the same row are no larger than mid.
                     count += y + 1                      #Update count.
                     x += 1                              #Move to next row.
