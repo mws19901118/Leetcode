10106e4220e16b2ec962e0b9b30d75da371a53f0
Junsheng Wang <mws19901118@gmail.com>
2022-08-14 19:20:04 -0700

    Update Word Ladder II.py

--- Code/Word Ladder II.py
+++ Code/Word Ladder II.py
@@ -1,44 +1,40 @@
 class Solution:
-    def DFS(self, stack: List[int], wordList: List[str], target: int, wordIndex: defaultdict, prev: defaultdict(list), result: List[List[str]]):
-        if stack[-1] == target:                                                                                 #If the top of stack is target, convert each index to its word and append the converted stack to result, then return.
-            result.append([wordList[i] for i in stack])
+    def backtracking(self, endWord: str, stack: List[str], previousWord: defaultdict(list), result: List[List[str]]) -> None:
+        if not stack:                                                                                                       #If stack is empty, return.      
             return
-        for x in prev[stack[-1]]:                                                                               #Traverse each index in prev[stack[-1]].
-            stack.append(x)                                                                                     #Append x to stack.
-            self.DFS(stack, wordList, target, wordIndex, prev, result)                                          #Keep DFS.
-            stack.pop()                                                                                         #Pop stack.
+        if stack[-1] == endWord:                                                                                            #If the top of stack is target, add a deepcopy of stack to result and then return.
+            result.append(deepcopy(stack))
+            return
+        for x in previousWord[stack[-1]]:                                                                                   #Traverse each word in previousWord[stack[-1]].
+            stack.append(x)                                                                                                 #Append x to stack.
+            self.backtracking(endWord, stack, previousWord, result)                                                         #Keep backtracking.
+            stack.pop()                                                                                                     #Pop stack.
         
     def findLadders(self, beginWord: str, endWord: str, wordList: List[str]) -> List[List[str]]:
-        if endWord not in wordList:                                                                             #If endWord not in wordList, return [] directly.
+        if endWord not in wordList:                                                                                         #If endWord not in wordList, return [] directly.
             return []
-        wordList.append(beginWord)                                                                              #Append beginWord to wordList.
-        wordIndex = {w: i for i, w in enumerate(wordList)}                                                      #Get the word to index mapping.
-        adjacentList = defaultdict(list)                                                                        #Intialize adjacent list.
-        for i, w in enumerate(wordList):                                                                        #Traverse wordList.
-            for j in range(len(w)):                                                                             #Try replace each letter in w with other letters.
-                for c in 'abcdefghijklmnopqrstuvwxyz':
-                    if c == w[j]:
+        extendedWordSet = set(wordList + [beginWord])                                                                       #Add beginWord to workList and de-dupe.
+        adjacentList = {w:[] for w in extendedWordSet}                                                                      #Initialize the adjacent list of each word.
+        for w in extendedWordSet:                                                                                           #Traverse the extendedWordSet and build the adjacent list.
+            for i in range(len(w)):                                                                                         #Replace each letter with another possible letter.
+                for x in 'abcdefghijklmnopqrstuvwxyz':
+                    if x == w[i]:                                                                                           #If new letter is same as old one, skip.
                         continue
-                    newWord =w[:j] + c + w[j + 1:]                                                              #Get the word after replacing one letter.
-                    if newWord in wordIndex:                                                                    #If it's in wordIndex, build an edge from i to wordIndex[newWord].
-                        adjacentList[i].append(wordIndex[newWord])
-        
-        q, visited = set([wordIndex[endWord]]), t([wordIndex[endWord]])                                         #Initalize queue set and visited set for BFS, starting with endWord.
-        prev = defaultdict(list)                                                                                #Initalize the previous words in BFS for each word.
-        while q:                                                                                                #BFS.
-            newq = set()                                                                                        #Initalize new queue.
-            for w in q:                                                                                         #Traverse each index in q.
-                if w == wordIndex[beginWord]:                                                                   #If current index is the index of beginWord, break.
-                    break
-                for x in adjacentList[w]:                                                                       #Traverse the indexes in adjacent list of w.
-                    if x not in visited:                                                                        #If x is not visited, add it to newq and append w to prev[x].
-                        newq.add(x)
-                        prev[x].append(w)
-            q = newq                                                                                            #Replace q with newq.
-            visited |= newq                                                                                     #Update visited so all indexes in newq are visited.
-        
-        if not prev[wordIndex[beginWord]]:                                                                      #If beginWord index has no previous word index, we can not reach from beginWord to endWord, return []. 
-            return []
-        result = []                                                                                             #Initialize result.
-        self.DFS([wordIndex[beginWord]], wordList, wordIndex[endWord], wordIndex, prev, result)                 #DFS to build all sequences.
-        return result                                                                                           #Return result.
+                    newWord = w[:i] + x + w[i + 1:]                                                                         #Construct new word.
+                    if newWord in adjacentList:                                                                             #If newWord is in adjacentList, append newWord to adjacentList[w].
+                        adjacentList[w].append(newWord)
+        q, visited = set([endWord]),  set([endWord])                                                                        #Start BFS from endWord.
+        previousWord = defaultdict(list)                                                                                    #Store the previous words of each word.
+        while q and beginWord not in q:                                                                                     #BFS while q is not empty and beginWord not in q.
+            newq = set()                                                                                                    #Initialize newq.
+            for x in q:                                                                                                     #Traverse each word in q.
+                for y in adjacentList[x]:                                                                                   #Append the word in adjacentList[x] to newq if it's not visited.
+                    if y not in visited:
+                        newq.add(y)
+                        previousWord[y].append(x)                                                                           #Also append x to previousWord[y].
+            q = newq                                                                                                        #Replace q with newq.
+            visited |= newq                                                                                                 #Update visited.
+        result = []
+        self.backtracking(endWord, [beginWord], previousWord, result)                                                       #Construct all paths from beginWord to endWord.
+        return result                                                                                                       #Return result.
+    
