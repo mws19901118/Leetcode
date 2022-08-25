bb0b02ef9544da156b7905844f6ae2330ffb6ac8
Junsheng Wang <mws19901118@gmail.com>
2022-08-24 17:21:24 -0700

    Update Ransom Note.py

--- Code/Ransom Note.py
+++ Code/Ransom Note.py
@@ -1,6 +1,4 @@
-from collections import Counter
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
-        ransomNoteDict = Counter(ransomNote)                                                                #Count each letter in ransom note.
-        magazineDict = Counter(magazine)                                                                    #Count each letter in magazine.
+        ransomNoteDict, magazineDict = Counter(ransomNote), Counter(magazine)                               #Count each letter in ransom note and magazine.
         return all(x in magazineDict and magazineDict[x] >= ransomNoteDict[x] for x in ransomNoteDict)      #Count of letter in ransom note should be smaller than or equal to its count in magazine.
