12100da9bcfca1e1c51a30d9fdafc921c823d324
Junsheng Wang <mws19901118@gmail.com>
2022-08-27 18:04:25 -0700

    Update Sort the Matrix Diagonally.py

--- Code/Sort the Matrix Diagonally.py
+++ Code/Sort the Matrix Diagonally.py
@@ -1,15 +1,14 @@
 class Solution:
     def diagonalSort(self, mat: List[List[int]]) -> List[List[int]]:
-        startingPositions = [(i, 0) for i in range(len(mat))]                     #Enumerate the starting positions of each diagonal.
-        startingPositions.extend([(0, i) for i in range(1, len(mat[0]))])
-        for x, y in startingPositions:                                            #Traverse starting positions.
-            diagonal = []
-            i, j = x, y
-            while i < len(mat) and j < len(mat[i]):                               #Traverse the diagonal and store numbers in a list.
-                diagonal.append(mat[i][j])
+        m, n = len(mat), len(mat[0])                                                #Get the dimensions of mat.
+        diagonals = [(0, x) for x in range(n)] + [(x, 0) for x in range(1, m)]      #Enumerate starting positions.
+        for x, y in diagonals:                                                      #Traverse starting posisionts.
+            values, i = [], 0                                                       #Store the values in this diagonal and length of diagonal.
+            while x + i < m and y + i < n:                                          #Traverse the diagonal until reaches the bound.
+                values.append(mat[x + i][y + i])                                    #Add current number to values.
                 i += 1
-                j += 1
-            diagonal.sort()                                                       #Sort list.
-            for i in range(len(diagonal)):                                        #Update the values in diagonal.
-                mat[x + i][y + i] = diagonal[i]
-        return mat                                                                #Return matrix.
+            values.sort()                                                           #Sort values.
+            for i, v in enumerate(values):                                          #Update the values in diagonal.
+                mat[x + i][y + i] = v
+        return mat                                                                  #Return matrix.
+    
