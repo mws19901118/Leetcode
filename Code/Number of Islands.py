e7ddddab459ae9a7a92024c5874e48220ad942ed
Junsheng Wang <mws19901118@gmail.com>
2022-08-28 17:12:30 -0700

    Update Number of Islands.py

--- Code/Number of Islands.py
+++ Code/Number of Islands.py
@@ -1,30 +1,21 @@
 class Solution:
-    # @param grid, a list of list of characters
-    # @return an integer
-    def numIslands(self, grid):
-        m=len(grid)
-        if m==0:
-            return 0
-        n=len(grid[0])
-        if n==0:
-            return 0
-        count=0
-        for i in range(m):
-            for j in range(n):
-                if grid[i][j]=='1':
-                    queue=[(i,j)]                                                     #If current character is '1', append the coordinate to queue.
-                    while queue!=[]:
-                        c= queue.pop(0)                                               #Deque the first element, and set the corresponding character to '0', then do breadth-first search.
-                        x=c[0]
-                        y=c[1]
-                        grid[x][y]='0'
-                        if x-1>=0 and (x-1,y) not in queue and grid[x-1][y]=='1':
-                            queue.append((x-1,y))
-                        if x+1<m and (x+1,y) not in queue and grid[x+1][y]=='1':
-                            queue.append((x+1,y))
-                        if y-1>=0 and (x,y-1) not in queue and grid[x][y-1]=='1':
-                            queue.append((x,y-1))
-                        if y+1<n and (x,y+1) not in queue and grid[x][y+1]=='1':
-                            queue.append((x,y+1))
-                    count+=1                                                          #Count the number of islands.
+    def BFS(self, grid: List[List[str]], i: int, j:int):
+        grid[i][j] == '2'                                                                                               #Set current coordinate as visited.
+        q = [(i, j)]                                                                                                    #Initialize queue.
+        while q:                                                                                                        #BFS while q is not empty.
+            nextq = []                                                                                                  #Initialize nextq.
+            for x, y in q:                                                                                              #Traverse each coordinate in queue.
+                for nx, ny in [(x - 1, y), (x, y + 1), (x + 1, y), (x, y - 1)]:                                         #Traverse each neighbor.
+                    if nx >= 0 and nx < len(grid) and ny >= 0 and ny < len(grid[0]) and grid[nx][ny] == '1':            #If the neighbor is valid and unvisited land.
+                        grid[nx][ny] = '2'                                                                              #Mark neighbor as visited.
+                        nextq.append((nx, ny))                                                                          #Append the coordinate of neighbor to nextq.
+            q = nextq                                                                                                   #Replace q with nextq.
+            
+    def numIslands(self, grid: List[List[str]]) -> int:
+        count = 0                                                                                                       #Initialize count.
+        m, n = len(grid), len(grid[0])                                                                                  #Get the dimensions.
+        for i, j in product(range(m), range(n)):                                                                        #Traverse grid.
+            if grid[i][j] == '1':                                                                                       #If grid is '1', increase count and do BFS to mark the island as visited.
+                count += 1
+                self.BFS(grid, i, j)
         return count
