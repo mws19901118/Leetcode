c0a8596e0dba02c6f9f4f526c4dec628a90bb49e
Junsheng Wang <mws19901118@gmail.com>
2022-09-02 18:33:42 -0700

    Update Numbers With Same Consecutive Differences.py

--- Code/Numbers With Same Consecutive Differences.py
+++ Code/Numbers With Same Consecutive Differences.py
@@ -1,26 +1,13 @@
-class Solution(object):
-    def numsSameConsecDiff(self, N, K):
-        """
-        :type N: int
-        :type K: int
-        :rtype: List[int]
-        """
-        def dp(n, valid):      
-            if n == N:                                    #If current digit length is N, we have found all numbers. Add them to result. 
-                result.extend(valid)
-            else:                                         #Otherwise, we need to enumerate all possible number in next digit.
-                nextD = []                                #Initialize a list for valid numbers in next digit.
-                for t in valid:                           #For each number passed in valid number list.
-                    d = t % 10                            #Calculate the last digit d.
-                    if d + K <= 9:                        #Judge if d + K is valid and append it to nextD.
-                        nextD.append(t * 10 + d + K)
-                    if K != 0 and d - K >= 0:             #Judge if d - K is valid and append it to nextD. Don't need to do this if K is 0 to avoid duplicate.
-                        nextD.append(t * 10 + d - K)
-                dp(n + 1, nextD)                          #DP next digit.
-        
-        if N == 1:                                        #If N is 1, directly return 0 to 9.
-            return [0,1,2,3,4,5,6,7,8,9]
-        result = []                                       #Initialize result list.
-        for i in range(1, 10):                            #DP from the first digit, 1 to 9.
-            dp(1, [i])
-        return result
+class Solution:
+    def numsSameConsecDiff(self, n: int, k: int) -> List[int]:
+        q = set([1, 2, 3, 4, 5, 6, 7, 8, 9])            #Initialize possible first digits.
+        for i in range(n - 1):                          #Iterate n - 1 times.
+            newq = set()                                #Initialize possible numbers of with i + 2 digits.
+            for x in q:                                 #Traverse result from last iteration.
+                d = x % 10                              #Get the last digit.
+                if d + k < 10:                          #If d + k is a digit, add x * 10 + d + k to newq.
+                    newq.add(x * 10 + d + k)
+                if d - k >= 0:                          #If d - k is a digit, add x * 10 + d - k to newq.
+                    newq.add(x * 10 + d - k)
+            q = newq                                    #Replace q with newq.
+        return q                                        #Return q.
