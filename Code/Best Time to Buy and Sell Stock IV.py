c28d5c8bb69098b7b614e7c7a702b20591deb135
Junsheng Wang <mws19901118@gmail.com>
2022-09-09 23:47:17 -0700

    Update Best Time to Buy and Sell Stock IV.py

--- Code/Best Time to Buy and Sell Stock IV.py
+++ Code/Best Time to Buy and Sell Stock IV.py
@@ -1,28 +1,10 @@
 class Solution:
-    # @return an integer as the maximum profit 
-    def maxProfit(self, k, prices):
-        res=0
-        n=len(prices)
-        if n==0:
+    def maxProfit(self, k: int, prices: List[int]) -> int:
+        if not k or not len(prices):                                                        #If k == 0 or prices is empty, directly return 0.
             return 0
-        peak=[]                                                                     #Record peak and nadir elements, including the start and the end.
-        for i in range(1,n):
-            if prices[i]>prices[i-1]:
-                res+=prices[i]-prices[i-1]                                          #Calculate the total profit without restriction.
-        peak.append(prices[0])
-        for i in range(1,n-1):
-            if (prices[i-1]<=prices[i] and prices[i+1]<prices[i]) or (prices[i-1]>=prices[i] and prices[i+1]>prices[i]):
-                peak.append(prices[i])
-        if n>1:
-            peak.append(prices[-1])
-            
-        l=len(peak)
-        if k>=l/2:                                                                  #If k is greater than the half length of peak, then we can do transaction whenever we want, return the total profit without restriction.
-            return res
-        dp=[[0 for i in range(l+1)] for j in range(k)]                              #Use dp[k][i+1] represents the max profit of using [0,i] data(only consider peak elements) and k transactions.
-        for i in range(1,k+1):                                                      #dp[k][i+1]=max(dp[k-1][i+1], dp[k][i], peak[i] + max( dp[k-1][j] - peak[j] )) { 0 <= j < i }
-            currentmax=-0xFFFFFFFF
-            for j in range(l):
-                dp[i%2][j+1]=max(dp[(i-1)%2][j+1],dp[i%2][j],peak[j]+currentmax)    
-                currentmax=max(currentmax,dp[(i-1)%2][j]-peak[j])
-        return dp[k%2][l]
+        cost, profit = [float('inf')] * k, [float('-inf')] * k                              #Initialize the min cost(buy stock but not sell yet) and max profit of performing total i + 1 transactions, 0 <= i < k. 
+        for price in prices:                                                                #Traverse prices.
+            for i in range(k):
+                cost[i] = min(cost[i], price - (profit[i - 1] if i > 0 else 0))             #If buy at current price will decrease cost for transaction i + 1, then update cost[i].
+                profit[i] = max(profit[i], price - cost[i])                                 #If sell at current price will increase profit for transaction i + 1, then update profit[i].
+        return profit[-1]
