368a66db6977064fe69ea2c3eb878cfd64d107e4
Junsheng Wang <mws19901118@gmail.com>
2022-09-18 19:16:44 -0700

    Update Find Duplicate File in System.py

--- Code/Find Duplicate File in System.py
+++ Code/Find Duplicate File in System.py
@@ -3,10 +3,8 @@ class Solution:
         pathByContent = defaultdict(list)                                       #Store file path by content.
         for p in paths:                                                         #Traverse paths.
             segments = p.split(" ")                                             #Split p by " ".
-            directory = segments[0]                                             #The first segment is directory.
-            for s in segments[1:]:                                              #Traverse the rest segments.
-                splits = s.split("(")
-                fileName = splits[0]                                            #File name is before bracket.
-                content = splits[1][:-1]                                        #Content is inside bracket.
-                pathByContent[content].append(directory + "/" + fileName)       #Concatenate directory and filename to get file path then hash it by file content.
+            for x in segments[1:]:                                              #Traverse the rest segments.
+                leftP = x.index('(')                                            #Find the index of left parentheses.
+                fileName, content = x[:leftP], x[leftP + 1:-1]                  #Get the file name(before parenteses) and content(in parentheses).
+                pathByContent[content].append(segments[0] + "/" + fileName)     #Concatenate directory and filename to get file path then hash it by file content.
         return [p for p in pathByContent.values() if len(p) > 1]                #Return all file paths list whose length is greater than 1.
