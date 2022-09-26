424ae0e662f659ccd163414fcc96697e09d09000
Junsheng Wang <mws19901118@gmail.com>
2022-09-25 18:41:59 -0700

    Update Satisfiability of Equality Equations.py

--- Code/Satisfiability of Equality Equations.py
+++ Code/Satisfiability of Equality Equations.py
@@ -1,32 +1,20 @@
-class UnionFind:                                                      #Union Find.
-    def __init__(self, value):
+class UnionFind:                                                                                                      #Union find class.
+    def __init__(self):
         self.parent = self
-        self.value = value
-    
-    def union(self, a):
-        self.find().parent = a.find()
     
     def find(self):
-        p = self.parent
-        while p != p.parent:
-            p = p.parent
-        return p
-    
+        if self.parent == self:
+            return self
+        self.parent = self.parent.find()
+        return self.parent
+
+    def union(self, x) -> None:
+        self.find().parent = x.find()
+
 class Solution:
-    def equationsPossible(self, equations: 'List[str]') -> 'bool':
-        nonEqualPairs = []
-        d = {}                                                        #Use dict to store union find.
-        for e in equations:
-            if e[0] not in d:                                         #Initialize union find.
-                d[e[0]] = UnionFind(e[0])
-            if e[3] not in d:
-                d[e[3]] = UnionFind(e[3])
-            if e[1:3] == "==":                                        #If the equaltion is "equal", union the 2 variables.
-                d[e[0]].union(d[e[3]])
-            else:                                                     #If the equaltion is "not equal", store the pair of variables in a list.
-                nonEqualPairs.append((e[0], e[3]))
-        
-        for p in nonEqualPairs:                                       #For each pair of vairables that are not equal, if their union find have the same parent, then the equaltion list can't be satisfied.
-            if d[p[0]].find() == d[p[1]].find():
-                return False
-        return True
+    def equationsPossible(self, equations: List[str]) -> bool:
+        unionFinds = defaultdict(lambda: UnionFind())                                                                 #Use a defaultdict to store union finds for each letter.
+        for e in equations:                                                                                           #Traverse each equal equations.
+            if e[1:3] == "==":
+                unionFinds[e[0]].union(unionFinds[e[-1]])                                                             #Build the relations between union finds.
+        return all(unionFinds[e[0]].find() != unionFinds[e[-1]].find() for e in equations if e[1:3] == "!=")          #For each unequal union finds, their parent shouldn't be same; otherwise, return false.
