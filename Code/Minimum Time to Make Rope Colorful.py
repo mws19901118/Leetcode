c5c5f3ec5fcff4a15bbec7a89112300f5e7451b0
Junsheng Wang <mws19901118@gmail.com>
2022-10-02 22:50:44 -0700

    Update Minimum Time to Make Rope Colorful.py

--- Code/Minimum Time to Make Rope Colorful.py
+++ Code/Minimum Time to Make Rope Colorful.py
@@ -5,7 +5,6 @@ class Solution:
             j = i + 1
             while j < len(colors) and colors[i] == colors[j]:                     #Find all consecutive same colors.
                 j += 1
-            if j - i > 1:                                                         #If it's more than 1, add the sum of needed time minus the max needed time.
-                count += sum(neededTime[i:j]) - max(neededTime[i:j])
+            count += sum(neededTime[i:j]) - max(neededTime[i:j])                  #add the sum of needed time minus the max needed time for each consecutive same color.
             i = j
         return count                                                              #Return count.
