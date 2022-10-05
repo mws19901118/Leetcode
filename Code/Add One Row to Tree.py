e37aa9b275245bf97b37a0d42b626bc0a79d2f3f
Junsheng Wang <mws19901118@gmail.com>
2022-10-04 18:28:26 -0700

    Update Add One Row to Tree.py

--- Code/Add One Row to Tree.py
+++ Code/Add One Row to Tree.py
@@ -5,20 +5,16 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def addOneRow(self, root: TreeNode, v: int, d: int) -> TreeNode:
-        if d == 1:                                                  #Handle the case where d == 1: add a new root and set its left child to original root.
-            newRoot = TreeNode(v, root, None)
-            return newRoot
-        q = [root]
-        l = 1
-        while l < d - 1:                                            #Traverse binary tree in level order until current level is d - 1.
-            newq = []
-            for x in q:
-                if x.left is not None: newq.append(x.left)
-                if x.right is not None: newq.append(x.right)
-            q = newq
-            l += 1
-        for x in q:                                                 #Insert new level under current level.
-            x.left = TreeNode(v, x.left, None)                      #Left child of current node becomes the left child of new left child of current node.
-            x.right = TreeNode(v, None, x.right)                    #Right child of current node becomes the right child of new right child of current node.
-        return root                                                 #Return root.
+    def traverse(self, root: Optional[TreeNode], val: int, depth: int) -> Optional[TreeNode]:
+        if not root:                                                                                        #If root is none, return none.
+            return None
+        if depth == 1:                                                                                      #If depth is 1, insert node as its left and right children then return root.
+            root.left = TreeNode(val, root.left, None)
+            root.right = TreeNode(val, None, root.right)
+            return root
+        self.traverse(root.left, val, depth - 1)                                                            #Recursively traverse left subtree with depth - 1.
+        self.traverse(root.right, val, depth - 1)                                                           #Recursively traverse right subtree with depth - 1.
+        return root                                                                                         #Return root.
+
+    def addOneRow(self, root: Optional[TreeNode], val: int, depth: int) -> Optional[TreeNode]:
+        return TreeNode(val, root, None) if depth == 1 else self.traverse(root, val, depth - 1)             #If depth is 1, return the new root with current root as its left; otherwise, return the result from traverse.
