895f0c5486526483e7d3ef660f1a779d631ef954
Junsheng Wang <mws19901118@gmail.com>
2022-10-11 17:08:59 -0700

    Update Largest Perimeter Triangle.py

--- Code/Largest Perimeter Triangle.py
+++ Code/Largest Perimeter Triangle.py
@@ -1,11 +1,7 @@
-class Solution(object):
-    def largestPerimeter(self, A):
-        """
-        :type A: List[int]
-        :rtype: int
-        """
-        A.sort()                                    #Sort A.
-        for i in xrange(len(A) - 3, -1, -1):        #Find the largest consecutive elements which can form a triangle, then return the sum of those 3.
-            if A[i] + A[i+1] > A[i+2]:              #Let's say they are a < b < c. If a + b <= c, then any a' and b' before a and b will have a' + b' <= c. Thus no valid triangle existed given the longest edge is c.
-                return A[i] + A[i+1] + A[i+2]       #Otherwise a + b > c, it's a valid triangle and any a' abd b' before a and b will have a' + b' <= a + b. Thus, a + b + c has the longest perimeter for the triagnle whose longest edge is c.
-        return 0                                    #If no valid triangle found, return 0.
+class Solution:
+    def largestPerimeter(self, nums: List[int]) -> int:
+        nums.sort(reverse = True)                               #Sort nums in desending order.
+        for i in range(2, len(nums)):                           #Find the largest consecutive elements which can form a triangle, then return the sum of those 3.
+            if nums[i - 2] < nums[i - 1] + nums[i]:             #Let's say they are a < b < c. If a + b <= c, then any a' and b' before a and b will have a' + b' <= c. Thus no valid triangle existed given the longest edge is c.
+                return nums[i - 2] + nums[i - 1] + nums[i]      #Otherwise a + b > c, it's a valid triangle and any a' abd b' before a and b will have a' + b' <= a + b. Thus, a + b + c has the longest perimeter for the triagnle whose longest edge is c.
+        return 0                                                #If no valid triangle found, return 0.
