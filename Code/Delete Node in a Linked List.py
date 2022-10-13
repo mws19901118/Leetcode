fc1993743ea5fa1575010b956e87db50a5f1193f
Junsheng Wang <mws19901118@gmail.com>
2022-10-12 18:21:59 -0700

    Update Delete Node in a Linked List.py

--- Code/Delete Node in a Linked List.py
+++ Code/Delete Node in a Linked List.py
@@ -5,9 +5,11 @@
 #         self.next = None
 
 class Solution:
-    # @param {ListNode} node
-    # @return {void} Do not return anything, modify node in-place instead.
     def deleteNode(self, node):
-        node.val=node.next.val    #We can't find the node before current node, so we can't delete current node directly.
-        node.next=node.next.next  #But we can let the value of current node be the value of next node and delete next node.
-                                  #It's like swap current node and next node.
+        """
+        :type node: ListNode
+        :rtype: void Do not return anything, modify node in-place instead.
+        """
+        node.val = node.next.val    #We can't find the node before current node, so we can't delete current node directly.
+        node.next = node.next.next  #But we can let the value of current node be the value of next node and delete next node.
+                                    #It's like swap current node and next node.
