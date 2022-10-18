bd68f004212301894bc875291562fb3891d64212
Junsheng Wang <mws19901118@gmail.com>
2022-10-17 20:52:06 -0700

    Update Count and Say.py

--- Code/Count and Say.py
+++ Code/Count and Say.py
@@ -1,24 +1,13 @@
 class Solution:
-    def generate(self, s):
-        if s=="":
+    def countAndSay(self, n: int) -> str:
+        if n == 1:                                              #If n == 1, directly return "1".
             return "1"
-        l=len(s)
-        i=0
-        ans=""
-        while i<l:
-            j=1
-            while i+j<l and s[i+j]==s[i]:       #Count the number of consecutive same digits.
-                j+=1
-            ans+=str(j)+s[i]                    #Append to result.
-            i+=j
-        return ans
-    # @param {integer} n
-    # @return {string}
-    def countAndSay(self, n):
-        if n==0:                                #If n==0, return "".
-            return ""
-        
-        ans=""
-        for i in xrange(n):                     #Generate sequence basing on the last sequence n times.
-            ans=self.generate(ans)
-        return ans
+        p = self.countAndSay(n - 1)                             #Get the result of n - 1.
+        result, i = "", 0                                       #Initialize result and pointer.
+        while i < len(p):                                       #Traverse p.
+            j = i + 1
+            while j < len(p) and p[j] == p[i]:                  #Count current digit.
+                j += 1
+            result += str(j - i) + p[i]                         #Add count and the digit to result
+            i = j
+        return result                                           #Return result.
