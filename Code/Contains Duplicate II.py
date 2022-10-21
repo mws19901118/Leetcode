a3df3b1a7ce815bf8e3b8aeb991a16c2dcb07e4a
Junsheng Wang <mws19901118@gmail.com>
2022-10-20 18:25:15 -0700

    Update Contains Duplicate II.py

--- Code/Contains Duplicate II.py
+++ Code/Contains Duplicate II.py
@@ -1,15 +1,8 @@
 class Solution:
-    # @param {integer[]} nums
-    # @param {integer} k
-    # @return {boolean}
-    def containsNearbyDuplicate(self, nums, k):
-        dict={}                               #The keys of dict are values of elements in nums; the values of dict are a list storing the indexes of elements which have the same value.
-        for i in range(len(nums)):
-            if nums[i] not in dict:
-                dict[nums[i]]=[i]             #Initialize the list.
-            else:
-                if i-dict[nums[i]][-1]<=k:    #If there the distance between current index and last index is not larger than k, return true.
-                    return True
-                else:
-                    dict[nums[i]].append(i)
-        return False
+    def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
+        indexes = defaultdict(lambda: -1)                           #Use default dict to store last indexes of each value.
+        for i, x in enumerate(nums):                                #Traverse nums.
+            if indexes[x] != -1 and i - indexes[x] <= k:            #If indexes[x] != -1 and i - indexes[x] <= k, return true.
+                return True
+            indexes[x] = i                                          #Set indexes[x] to i.
+        return False                                                #Return false.
