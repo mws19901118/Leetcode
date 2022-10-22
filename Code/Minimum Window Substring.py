54a4b44ba00f4b7a48c8299f8abb3c975bf86b57
Junsheng Wang <mws19901118@gmail.com>
2022-10-21 20:41:43 -0700

    Update Minimum Window Substring.py

--- Code/Minimum Window Substring.py
+++ Code/Minimum Window Substring.py
@@ -7,17 +7,15 @@ class Solution:
         while end < len(s):                                                     #While the sliding window haven't reached the end of s, find the rightmost end for current start.
             while end < len(s) and match < len(countT):                         #While not all charcters of t has been satisfied in sliding window.
                 countS[s[end]] += 1                                             #Update count of s[end] in sliding window.
-                if s[end] in countT and countS[s[end]] == countT[s[end]]:       #If s[end] is in t and the count of s[end] is same in both sliding window and t, one more character of t is satisfied.
-                    match += 1
+                match += (countS[s[end]] == countT[s[end]])                     #If s[end] is in t and the count of s[end] is same in both sliding window and t, one more character of t is satisfied.
                 end += 1                                                        #Move end to next character.
             
             if match < len(countT):                                             #If sliding window reaches end of s but not all characters in t has been satisfied, break.
                 break
             
             while start < end and match == len(countT):                         #While all charcters of t has been satisfied in sliding window, find the rightmost start for current end.
+                match -= (countS[s[start]] == countT[s[start]])                 #If s[end] is in t and the count of s[end] in sliding window is smaller than that in t, one less character of t is satisfied.
                 countS[s[start]] -= 1                                           #Update count of s[end] in sliding window.
-                if s[start] in countT and countS[s[start]] < countT[s[start]]:  #If s[end] is in t and the count of s[end] in sliding window is smaller than that in t, one less character of t is satisfied.
-                    match -= 1
                 start += 1                                                      #Move start to next character.
                 
             if not result or len(result) > len(s[start - 1:end]):               #Update result if needed.
