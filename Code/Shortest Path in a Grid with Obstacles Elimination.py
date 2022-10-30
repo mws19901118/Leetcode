fd734c058763f085baba5b01d67fcc920372b22c
Junsheng Wang <mws19901118@gmail.com>
2022-10-29 22:05:45 -0700

    Update Shortest Path in a Grid with Obstacles Elimination.py

--- Code/Shortest Path in a Grid with Obstacles Elimination.py
+++ Code/Shortest Path in a Grid with Obstacles Elimination.py
@@ -1,22 +1,19 @@
 class Solution:
     def shortestPath(self, grid: List[List[int]], k: int) -> int:
-        m, n = len(grid), len(grid[0])                                                                                            #Get dimensions.
-        q = [(0, 0, k)]                                                                                                           #Initialize q with upper left cell and number of obstacles I can remove now.
-        visited = {}                                                                                                              #Store the max number of obstacles can remove at each visited cell.
-        step = 0                                                                                                                  #Initialize step.
-        while q:                                                                                                                  #Begin BFS.
-            newq = []                                                                                                             #Initialize newq.
-            for x, y, c in q:                                                                                                     #Traverse q.
-                if x == m - 1 and y == n - 1:                                                                                     #If reaches lower right cell, return current step.
+        m, n = len(grid), len(grid[0])                                                                                                                                      #Get dimensions.
+        q = [(0, 0, k)]                                                                                                                                                     #Initialize q with upper left cell and number of obstacles I can remove now.
+        visited = {(x, y): (m * n + 1, -1) for x, y in product(range(m), range(n))}                                                                                         #Store the step and remaining move can for visiting each cell, initially m * n + 1 and -1 respectively.
+        step = 0                                                                                                                                                            #Initialize step.
+        while q:                                                                                                                                                            #Begin BFS.
+            newq = []                                                                                                                                                       #Initialize newq.
+            for x, y, r in q:                                                                                                                                               #Traverse q.
+                if x == m - 1 and y == n - 1:                                                                                                                               #If reaches lower right cell, return current step.
                     return step
-                for nx, ny in [(x - 1, y), (x, y + 1), (x + 1, y), (x, y - 1)]:                                                   #Traverse 4 neighbors of current cell.
-                    if nx >= 0 and nx < m and ny >= 0 and ny < n and ((nx, ny) not in visited or visited[(nx, ny)] < c):          #Next cell has to be valid and either is not visited or is visited with a smaller number of obstacles can remove.
-                        if grid[nx][ny] == 0:                                                                                     #If next cell is not obstacle, append its coordinate with same number of obstacles to remove to newq and add in visit.
-                            visited[(nx, ny)] = c
-                            newq.append((nx, ny, c))
-                        elif grid[nx][ny] == 1 and c > 0:                                                                         #Else if next cell is obstacle and we can remove it, append its coordinate with number of obstacles minus 1 to remove to newq and add in visit.
-                            visited[(nx, ny)] = c - 1
-                            newq.append((nx, ny, c - 1))
-            q = newq                                                                                                              #Replace q with newq.
-            step += 1                                                                                                             #Increase step.
-        return -1                                                                                                                 #Return -1 if cannot reach lower right cell.
+                for nx, ny in [(x - 1, y), (x, y + 1), (x + 1, y), (x, y - 1)]:                                                                                             #Traverse 4 neighbors of current cell.
+                    if nx < 0 or nx >= m or ny < 0 or ny >= n or r < grid[nx][ny] or (visited[(nx, ny)][0] <= step and visited[(nx, ny)][1] >= r - grid[nx][ny]):           #If next cell is not valid or it's an obstacle and we don't have move remaining or it's visited with lesser step and more moves remaining, we should skip it. 
+                        continue
+                    visited[(nx, ny)] = (step, r - grid[nx][ny])                                                                                                            #Update the step and remaining move for visiting this cell.
+                    newq.append((nx, ny, r - grid[nx][ny]))                                                                                                                 #Append (nx, ny, r - grid[nx][ny]) to newq.
+            q = newq                                                                                                                                                        #Replace q with newq.
+            step += 1                                                                                                                                                       #Increase step.
+        return -1          
