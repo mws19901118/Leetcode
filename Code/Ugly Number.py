378667b8b8772af9189540f83a0dc7360fa59a4e
Junsheng Wang <mws19901118@gmail.com>
2022-11-17 16:16:11 -0800

    Update Ugly Number.py

--- Code/Ugly Number.py
+++ Code/Ugly Number.py
@@ -1,18 +1,11 @@
-class Solution(object):
-    def isUgly(self, num):
-        """
-        :type num: int
-        :rtype: bool
-        """
-        if num == 0:                  #Rule out 0.
+class Solution:
+    def isUgly(self, n: int) -> bool:
+        if n <= 0:                      #Rule out 0 or negative numbers.
             return False
-        while num % 2 == 0:           #Divided it by 2 as many times as it can.
-            num /= 2
-        while num % 3 == 0:           #Divided it by 3 as many times as it can.
-            num /= 3
-        while num % 5 == 0:           #Divided it by 5 as many times as it can.
-            num /= 5
-        if num == 1:                  #If the result is 1, it's an ugly number; otherwise, it's not.
-            return True
-        else:
-            return False
+        while n % 5 == 0:               #Divided it by 5 as many times as it can.
+            n //= 5
+        while n % 3 == 0:               #Divided it by 3 as many times as it can.
+            n //= 3
+        while n % 2 == 0:               #Divided it by 2 as many times as it can.
+            n //= 2
+        return n == 1                   #If the result is 1, it's an ugly number; otherwise, it's not.
