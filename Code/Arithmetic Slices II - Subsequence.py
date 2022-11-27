c96ec60c95aaa3256ec04d5885b2f95f80c7da76
Junsheng Wang <mws19901118@gmail.com>
2022-11-26 18:38:23 -0800

    Update Arithmetic Slices II - Subsequence.py

--- Code/Arithmetic Slices II - Subsequence.py
+++ Code/Arithmetic Slices II - Subsequence.py
@@ -1,15 +1,10 @@
 class Solution:
     def numberOfArithmeticSlices(self, nums: List[int]) -> int:
         subsequences = [defaultdict(int) for _ in range(len(nums))]         #Store the count of arithmetic subsequences ending at current index by common difference.
-        pairs = [defaultdict(int) for _ in range(len(nums))]                #Store the count of number pairs ending at current index by difference.
         count = 0
         for i in range(len(nums)):                                          #Traverse nums.
             for j in range(i):                                              #Traverse nums[i].
                 diff = nums[i] - nums[j]                                    #Get the diff(nums[i] - nums[j])
-                if diff in subsequences[j]:                                 #If diff is in subsequences[j], so all arithmetic subsequences ending at j with common difference diff can extend to i.
-                    subsequences[i][diff] += subsequences[j][diff]
-                if diff in pairs[j]:                                        #If diff in pairs[j], so all number pairs ending at j whose difference is diff can form an arithmetic subsequence with nums[i]. 
-                    subsequences[i][diff] += pairs[j][diff]
-                pairs[i][diff] += 1                                         #Add the pair (nums[j], nums[i]) to pairs[i] by diff.
-            count += sum(subsequences[i].values())                          #Add total values for subsequences[i] to count.
+                count += subsequences[j][diff]                              #Increase count with subsequences[j][diff], because there will be subsequences[j][diff] new aruthmetic slices ending at nums[j] extending to nums[i].
+                subsequences[i][diff] += subsequences[j][diff] + 1          #subsequences[j][diff] + 1 to subsequences[i][diff], because [nums[j], nums[i]] makes a subsequence as well.
         return count                                                        #Return count.
