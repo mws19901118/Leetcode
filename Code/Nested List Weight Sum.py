dd0deed87f3bc12f9bb2c8abfb0fb2d310eb5321
Junsheng Wang <mws19901118@gmail.com>
2022-11-28 15:38:20 -0800

    Update Nested List Weight Sum.py

--- Code/Nested List Weight Sum.py
+++ Code/Nested List Weight Sum.py
@@ -2,11 +2,29 @@
 # This is the interface that allows for creating nested lists.
 # You should not implement it, or speculate about its implementation
 # """
-#class NestedInteger(object):
+#class NestedInteger:
+#    def __init__(self, value=None):
+#        """
+#        If value is not specified, initializes an empty list.
+#        Otherwise initializes a single integer equal to value.
+#        """
+#
 #    def isInteger(self):
 #        """
 #        @return True if this NestedInteger holds a single integer, rather than a nested list.
 #        :rtype bool
+#        """
+#
+#    def add(self, elem):
+#        """
+#        Set this NestedInteger to hold a nested list and adds a nested integer elem to it.
+#        :rtype void
+#        """
+#
+#    def setInteger(self, value):
+#        """
+#        Set this NestedInteger to hold a single integer equal to value.
+#        :rtype void
 #        """
 #
 #    def getInteger(self):
@@ -23,18 +41,9 @@
 #        :rtype List[NestedInteger]
 #        """
 
-class Solution(object):
-    def DFS(self, nestedList, depth):                       #DFS with current depth.
-        result = 0
-        for x in nestedList:                                #Check each entry in nestedList.
-            if x.isInteger():                               #If it's an integer, add its value multiplied by depth to result.
-                result += depth * x.getInteger()
-            else:                                           #Otherwise, add the return value of DFS with next depth.
-                result += self.DFS(x.getList(), depth + 1)
-        return result
-    def depthSum(self, nestedList):
-        """
-        :type nestedList: List[NestedInteger]
-        :rtype: int
-        """
-        return self.DFS(nestedList, 1)
+class Solution:
+    def calculate(self, nestedInteger: NestedInteger, depth: int) -> int:
+        return depth * nestedInteger.getInteger() if nestedInteger.isInteger() else sum(self.calculate(x, depth + 1) for x in nestedInteger.getList())              #Calculate depth sum for a nested integer with given sum. If it's integer, return the value multuply the depth; otherwise, calculate the depth sum for each nested integer in the list recursively.
+
+    def depthSum(self, nestedList: List[NestedInteger]) -> int:
+        return sum(self.calculate(x, 1) for x in nestedList)                                                                                                        #Return the sum of depth sum for given nested integer list.
