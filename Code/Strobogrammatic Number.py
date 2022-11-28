28a32d1d51f6a280bd0778999c5c798737ed8eb2
Junsheng Wang <mws19901118@gmail.com>
2022-11-28 11:02:48 -0800

    Update Strobogrammatic Number.py

--- Code/Strobogrammatic Number.py
+++ Code/Strobogrammatic Number.py
@@ -1,4 +1,4 @@
 class Solution:
     def isStrobogrammatic(self, num: str) -> bool:
         strobogrammatic = {"0": "0", "1": "1", "6": "9", "8": "8", "9": "6"}                                                            #Define the strobogrammatic dict.
-        return all(num[i] in strobogrammatic and num[len(num) - 1 - i] == strobogrammatic[num[i]] for i in range((len(num) + 1) // 2))  #For all first half(including middle) characters, it should be in the dict and the corresponding character in second half should be strobogrammatic with it. 
+        return all(num[i] in strobogrammatic and num[-(i + 1)] == strobogrammatic[num[i]] for i in range((len(num) + 1) // 2))          #For all first half(including middle) characters, it should be in the dict and the corresponding character in second half should be strobogrammatic with it. 
