112493624c633419c03337daa68770fa0cd44b9f
Junsheng Wang <mws19901118@gmail.com>
2022-12-01 18:09:11 -0800

    Update Determine if Two Strings Are Close.py

--- Code/Determine if Two Strings Are Close.py
+++ Code/Determine if Two Strings Are Close.py
@@ -1,9 +1,4 @@
 class Solution:
     def closeStrings(self, word1: str, word2: str) -> bool:
-        counter1, counter2 = Counter(word1), Counter(word2)                                 #Count the letters in each string.
-        if counter1.keys() != counter2.keys():                                              #Because Operation 1 allows us to freely reorder the string, so close strings must consist of same set of letters.
-            return False
-        counter1, counter2 = Counter(counter1.values()), Counter(counter2.values())         #Now, count the count of letters in each string.
-        if counter1 != counter2:                                                            #Because Operation 2 allows us to freely reassign the letters' frequencies, so the count of count must be same.
-            return False
-        return True                                                                         #Return true finally.
+        counter1, counter2 = Counter(word1), Counter(word2)                                                     #Count the letters in each string.
+        return count1.keys() == count2.keys() and Counter(count1.values()) == Counter(count2.values())          #Because Operation 1 allows us to freely reorder the string, so close strings must consist of same set of letters. Because Operation 2 allows us to freely reassign the letters' frequencies, so the count of count must be same.
