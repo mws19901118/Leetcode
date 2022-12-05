bc556f5d27cf0957962bd2b9915c6c7a937e571e
Junsheng Wang <mws19901118@gmail.com>
2022-12-04 16:35:53 -0800

    Update Middle of the Linked List.py

--- Code/Middle of the Linked List.py
+++ Code/Middle of the Linked List.py
@@ -1,15 +1,10 @@
 # Definition for singly-linked list.
 # class ListNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.next = None
-
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
 class Solution:
-    def middleNode(self, head):
-        """
-        :type head: ListNode
-        :rtype: ListNode
-        """
+    def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
         fast, slow = head, head       #Fast and slow pointers.
         while fast and fast.next:     #Fast pointer moves twice while slow pointer moves once.
             fast = fast.next.next     #When fast pointer reaches the end, slow pointer is in the middle.
