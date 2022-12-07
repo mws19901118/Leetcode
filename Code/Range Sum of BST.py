b80a90bea7f0abc1db3094457cd1edc0fbcffb74
Junsheng Wang <mws19901118@gmail.com>
2022-12-06 16:11:31 -0800

    Update Range Sum of BST.py

--- Code/Range Sum of BST.py
+++ Code/Range Sum of BST.py
@@ -6,8 +6,4 @@
 #         self.right = right
 class Solution:
     def rangeSumBST(self, root: TreeNode, low: int, high: int) -> int:
-        if root is None:                                                                              #If root is none, return 0.
-            return 0
-        s = self.rangeSumBST(root.left, low, high) + self.rangeSumBST(root.right, low, high)          #Calculate the range sum in left child and right child.
-        s += root.val if root.val >= low and root.val <= high else 0                                  #If the value of root is in range, add it to sum.
-        return s
+        return 0 if not root else self.rangeSumBST(root.left, low, high) + self.rangeSumBST(root.right, low, high) + (root.val if root.val >= low and root.val <= high else 0)  #If root is none, return 0. Otherwise, calculate the range sum in left child and right child. If the value of root is in range, add it to sum.
