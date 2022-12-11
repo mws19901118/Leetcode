a6a6da86748e70313373f53cb2aa1d4da3a36353
Junsheng Wang <mws19901118@gmail.com>
2022-12-10 21:32:20 -0800

    Update Binary Tree Maximum Path Sum.py

--- Code/Binary Tree Maximum Path Sum.py
+++ Code/Binary Tree Maximum Path Sum.py
@@ -6,27 +6,13 @@
 #         self.right = None
 
 class Solution(object):
-    def find(self, root):                                               #Get the max path sum of current subtree and the max sum along the path from current node to leaf.
-        if root.left is None and root.right is None:                    #If current node is leaf node, both the result is value of current node.
-            return (root.val, root.val)
-        elif root.left is not None and root.right is None:              #If current node only has left child, get the results from its left child.
-            l = self.find(root.left)
-            t = max(root.val, root.val + l[1])                          #The max sum along the path from current node to leaf is the max of current value and the sum of current value and that value of left child.
-            return (max(l[0], t), t)                                    #The max path sum of current node is the max of that of left child and the max sum along the path from current node to leaf.
-        elif root.left is None and root.right is not None:              #If current node only has right child, get the results from its right child.
-            r = self.find(root.right)  
-            t = max(root.val, root.val + r[1])                          #The max sum along the path from current node to leaf is the max of current value and the sum of current value and that value of right child.
-            return (max(r[0], t), t)                                    #The max path sum of current node is the max of that of right child and the max sum along the path from current node to leaf.
-        else:                                                           #If current has both child, get the results from them respectively.                         
-            l = self.find(root.left)
-            r = self.find(root.right)
-            t = max(root.val, root.val + l[1], root.val + r[1])         #The max sum along the path from current node to leaf is the max of current value, the sum of current value and that value of left child and the sum of current value and that value of right child. 
-            return (max(l[0], r[0], t, root.val + l[1] + r[1]), t)      #The max path sum of current node is the max of that of left child, that of right child and the max sum along the path from current node to leaf and the sum of current value and the max sum along the path from left child to leaf and the max sum along the path from right child to leaf.
-    def maxPathSum(self, root):
-        """
-        :type root: TreeNode
-        :rtype: int
-        """
-        if root is None:
-            return 0
-        return self.find(root)[0]
+    def traverse(self, root: Optional[TreeNode]) -> (int, int):
+        if not root:                                                                                                                                                                                                #If root is none, return 0 and 0.
+            return (0, 0)
+        l, r = self.traverse(root.left), self.traverse(root.right)                                                                                                                                                  #Get the result from left child and right child.
+        maxPathSumFromRoot = max(root.val, root.val + l[1], root.val + r[1])                                                                                                                                        #Find the max path sum from root.
+        maxPathSum = max(l[0] if root.left else maxPathSumFromRoot, r[0] if root.right else maxPathSumFromRoot, (root.val + l[1] + r[1]) if root.left and root.right else maxPathSumFromRoot, maxPathSumFromRoot)   #Max path sum is the max of max path sum from root, max path sum of left child if left child is not none, max path sum of right child if right child is not none and root.val plus both max path sum of both children if both children is not none.
+        return maxPathSum, maxPathSumFromRoot                                                                                                                                                                       #Return maxPathSum and maxPathSumFromRoot.
+    
+    def maxPathSum(self, root: Optional[TreeNode]) -> int:
+        return self.traverse(root)[0]
