e99678d567367edb60bda17677a949c89812e054
Junsheng Wang <mws19901118@gmail.com>
2022-12-12 17:08:28 -0800

    Update Minimum Falling Path Sum.py

--- Code/Minimum Falling Path Sum.py
+++ Code/Minimum Falling Path Sum.py
@@ -1,16 +1,8 @@
 class Solution:
-    def minFallingPathSum(self, A: List[List[int]]) -> int:
-        if len(A) == 0 or len(A[0]) == 0:       #Handle empty list.
-            return 0
-        curr = A[-1]                            #Use curr to store the min falling path sum for each element in current level as the starting number. We do dp bottum up, so initially it's the lowest level in A.
-        for i in reversed(range(len(A) - 1)):   #Do DP bottom up.
-            upper = []                          #Store the min falling path sum for each element in upper level as the starting number.
-            for j, x in enumerate(A[i]):        #Traverse through upper level.
-                t = curr[j]                     #The min falling path sum for index j is A[i][j] + min(curr[j - 1], curr[j], curr[j + 1]) if j - 1 and j + 1 are valid index.
-                if j > 0:
-                    t = min(t, curr[j - 1])
-                if j < len(A[i]) - 1:
-                    t = min(t, curr[j + 1])
-                upper.append(t + x)             #Append min falling path sum for index j to upper.
-            curr = upper                        #Replace curr with upper as we finished DP on current level.
-        return min(curr)                        #Return the min value of curr, which is the min falling path sum of top level after DP.
+    def minFallingPathSum(self, matrix: List[List[int]]) -> int:
+        if len(matrix) == 1:                                                                                                                                                            #If matrix only has 1 element, just return it.
+            return matrix[0][0]
+        dp = copy.deepcopy(matrix[0])                                                                                                                                                   #Instensiate dp list with the first row in matrix.
+        for i in range(1, len(matrix)):                                                                                                                                                 #Traverse each row downward.
+            dp = [min(dp[0], dp[1]) + matrix[i][0]] + [min(dp[j - 1], dp[j], dp[j + 1]) + matrix[i][j] for j in range(1, len(matrix) - 1)] + [min(dp[-2], dp[-1]) + matrix[i][-1]]      #The new dp[i] is the min of (dp[i - 1], dp[i], dp[i + 1]) + matrix[i][j]; except for the cell on first and last column, which should removing the invalid dp[i - 1] or dp[i + 1].
+        return min(dp)                                                                                                                                                                  #Return min(dp).
