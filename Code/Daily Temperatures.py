d72de30e026b8fe94fb15ec90a497b7dbc7b1983
Junsheng Wang <mws19901118@gmail.com>
2022-12-17 21:25:59 -0800

    Update Daily Temperatures.py

--- Code/Daily Temperatures.py
+++ Code/Daily Temperatures.py
@@ -1,10 +1,9 @@
 class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
-        stack = []                                              #Initialize a stack to store temperature and date that has no warmer temperature yet.
-        answer = [0] * len(temperatures)                        #Initialize answer.
+        stack, answer = [], [0] * len(temperatures)             #Initialize a stack to store index of temperature that has no warmer temperature yet and initialize answer.
         for i, x in enumerate(temperatures):                    #Traverse temperatures.
-            while stack and stack[-1][0] < x:                   #While stack is not empty and the temperature at top of stack is smaller than current temperature, set answer[stack[-1][1]] to i - stack[-1][1] and pop stack.
-                answer[stack[-1][1]] = i - stack[-1][1]
-                stack.pop()
-            stack.append((x, i))                                #Append current temperature and date to stack.
+            while stack and temperatures[stack[-1]] < x:        #While stack is not empty and the temperature at the index on top of stack is smaller than current temperature, pop stack.
+                index = stack.pop()
+                answer[index] = i - index                       #Set answer[index] to i - index.
+            stack.append(i)                                     #Append current index to stack.
         return answer                                           #Return answer.
