5528f9d96df9d76c694ccff3b8569aa33852fb11
Junsheng Wang <mws19901118@gmail.com>
2022-12-19 16:23:06 -0800

    Update Keys and Rooms.py

--- Code/Keys and Rooms.py
+++ Code/Keys and Rooms.py
@@ -1,11 +1,7 @@
 class Solution:
-    def DFS(self, rooms: List[List[int]], current: int, visited: set) -> bool:      #DFS.
-        visited.add(current)                                                        #Add current room to visited.
-        if len(visited) == len(rooms):                                              #If visited rooms count equals to total number of rooms, return true.
-            return True
-        for x in rooms[current]:                                                    #For all the keys in current room, if it's room is not visited and can visit all room after DFS from there, return true.
-            if x not in visited and self.DFS(rooms, x, visited):
-                return True
-        return False                                                                #Otherwise cannot visited all room yet, return false.
     def canVisitAllRooms(self, rooms: List[List[int]]) -> bool:
-        return self.DFS(rooms, 0, set())                                            #Do DFS.
+        def DFS(current: int, visited: set) -> bool:                                                                            #DFS.
+            visited.add(current)                                                                                                #Add current room to visited.
+            return len(visited) == len(rooms) or any(DFS(x, visited) for x in rooms[current] if x not in visited)               #If visited rooms count equals to total number of rooms, return true; otherwise, for all the keys in current room and its room is not visited, return true if either can visit all room after DFS from there.
+
+        return DFS(0, set())                                                                                                    #Start DFS from the first room.
