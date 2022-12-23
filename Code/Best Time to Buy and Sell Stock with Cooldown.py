a123d623df05d724aaf2e27d3c28f8493afd4458
Junsheng Wang <mws19901118@gmail.com>
2022-12-22 17:55:27 -0800

    Update Best Time to Buy and Sell Stock with Cooldown.py

--- Code/Best Time to Buy and Sell Stock with Cooldown.py
+++ Code/Best Time to Buy and Sell Stock with Cooldown.py
@@ -1,9 +1,7 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        if not prices:                                  #Return 0 if prices is empty.
-            return 0
-        sell, wait, hold = [0], [0], [-prices[0]]       #Initialize the list to store max profit to sell, wait or hold on the ith day.
-        for p in prices[1:]:                            #Traverse prices.
+        sell, wait, hold = [0], [0], [float('-inf')]    #Initialize the list to store max profit to sell, wait or hold on the ith day.
+        for p in prices:                                #Traverse prices.
             s, w, h = sell[-1], wait[-1], hold[-1]      #Get the max profit from selling, waiting or holding on previous day.
             sell.append(h + p)                          #If sell today, the max profit now is h + p.
             wait.append(max(s, w))                      #If wait today, the max profit now is max(s, w).
