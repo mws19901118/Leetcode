165c1f7cc6f4e5000c5926c07242330ee218f9f9
Junsheng Wang <mws19901118@gmail.com>
2022-12-23 18:26:53 -0800

    Update Domino and Tromino Tiling.py

--- Code/Domino and Tromino Tiling.py
+++ Code/Domino and Tromino Tiling.py
@@ -3,9 +3,9 @@ class Solution:
         if n < 3:                                                     #If n < 3, return n as there is one length 1 tiling and two length 2 tilings.
             return n
         division = 10 ** 9 + 7                                        #Division is 10 ** 9 + 7.
-        dp1, dp2, dp3 = 1, 2, 5                                       #Initialize DP value for the last 3 elements; inititally for n = 1, 2, 3.
-        for i in range(3, n):                                         #Compute the DP value, from 4 to n; suppose the DP function is F(), then the dp value is F(x).
-            dp1, dp2, dp3 = dp2, dp3, (dp3 * 2 + dp1) % division)     #For any x > 3. Tiling can be formed by add a vertical domino to each of length x - 1 tiling, or add 2 horizontal dominos to each of length x - 2 tiling.
+        dp1, dp2, dp3 = 1, 1, 2                                       #Initialize DP value for the last 3 elements; inititally for n = 0, 1, 2.
+        for i in range(2, n):                                         #Compute the DP value, from 3 to n; suppose the DP function is F(), then the dp value is F(x).
+            dp1, dp2, dp3 = dp2, dp3, (dp3 * 2 + dp1) % division)     #For any x >= 3. Tiling can be formed by add a vertical domino to each of length x - 1 tiling, or add 2 horizontal dominos to each of length x - 2 tiling.
         return dp3                                                    #We can also use 2 trominos at both end and any number of dominos(including 0) in the middle to form tiling whose length is longer than or equal to 3.
                                                                       #Below are examples, '-' as domino and '*' as tromino.
                                                                       #length of 3:
@@ -21,10 +21,9 @@ class Solution:
                                                                       #  * * - - * *
                                                                       #  * - - - - *
                                                                       #and so on and so forth...
-                                                                      #Because trominos can be flip up and down to make different tilling, we can form 2 new tilings for each of length i tiling, 0 < i <= x - 3.
-                                                                      #And 2 new tilings for length x ifself.
-                                                                      #Thus, F(x) = F(x - 1) + F(x - 2) + (F(1) + ... + F(x - 3) + 1) * 2.
-                                                                      #In same way, F(x + 1) = F(x) + F(x - 1) + (F(1) + ... + F(x - 2) + 1) * 2 = F(x) + (F(x - 1) + F(x - 2) + (F(1) + ... + F(x - 3) + 1) * 2) + F(x - 2) = F(x) * 2 + F(x - 2).
-                                                                      #So, the state transition equation is F(n) = F(n - 1) * 2 + F(n - 3) for n > 3.
+                                                                      #Because trominos can be flip up and down to make different tilling, we can form 2 new tilings for each of length i tiling, 0 <= i <= x - 3.
+                                                                      #Thus, F(x) = F(x - 1) + F(x - 2) + (F(0) + ... + F(x - 3)) * 2.
+                                                                      #In same way, F(x + 1) = F(x) + F(x - 1) + (F(0) + ... + F(x - 2)) * 2 = F(x) + (F(x - 1) + F(x - 2) + (F(0) + ... + F(x - 3)) * 2) + F(x - 2) = F(x) * 2 + F(x - 2).
+                                                                      #So, the state transition equation is F(n) = F(n - 1) * 2 + F(n - 3) for n >= 3.
                                                                       #Compute the DP value and get the modulo.
                                                                       #Return dp3.
