78f22be9b7bab8058f2f449fa49fabc7d54a1f59
Junsheng Wang <junsheng.wang@uipath.com>
2022-12-25 03:55:32 -0500

    Update Longest Subsequence With Limited Sum.py

--- Code/Longest Subsequence With Limited Sum.py
+++ Code/Longest Subsequence With Limited Sum.py
@@ -1,7 +1,7 @@
 class Solution:
     def answerQueries(self, nums: List[int], queries: List[int]) -> List[int]:
         nums.sort()                                                         #Sort nums. Because we only care the length of subsequence for the entire list(not required to end at any index), the order does not matter.
-        prefixSum = [0]                                                     #Initialize prefix sum of sorted nums.
+        prefixSum = [0]                                                     #Calculate prefix sum of sorted nums; prefix sum of at index i is the smallest subsequence sum of length i.
         for x in nums:
             prefixSum.append(x + prefixSum[-1])
-        return [bisect.bisect_right(prefixSum, x) - 1 for x in queries]     #Binary search for the rightmost index to insert each query then minus 1. If query is greater than or equal to a prefix sum, it could take the subsequence with length equals to the index.
+        return [bisect.bisect_right(prefixSum, x) - 1 for x in queries]     #Binary search for the rightmost index to insert each query then minus 1. If query is greater than or equal to a prefix sum at index i, it could contain the subsequence with length i.
