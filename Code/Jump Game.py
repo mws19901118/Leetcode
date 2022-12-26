b15ae89fba858b87495144a80d8c7989f576ced2
Junsheng Wang <mws19901118@gmail.com>
2022-12-25 21:30:01 -0800

    Update Jump Game.py

--- Code/Jump Game.py
+++ Code/Jump Game.py
@@ -1,9 +1,8 @@
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
-        i, reach = 0, 0                         #Initialize a pointer at 0 and the farthest reachable index.
-        while i <= reach:                       #Traverse nums while can reach index i.
+        reach = 0                               #Initialize the farthest reachable index.
+        for i, x in enumerate(nums):            #Traverse nums.
+            if i > reach:                       #If current index is beyond reach, return false.
+                return False
             reach = max(reach, i + nums[i])     #Update reach if we can reach farther from current index.
-            if reach >= len(nums) - 1:          #If we can reach the end, return true.
-                return True
-            i += 1                              #Move i to next.
-        return False                            #Return false cause we cannot reach the end.
+        return True                             #Return true after traverse.
