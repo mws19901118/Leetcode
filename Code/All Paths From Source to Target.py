43787352dc144b502a1819396af56896eb92a72b
Junsheng Wang <mws19901118@gmail.com>
2022-12-29 19:37:42 -0800

    Update All Paths From Source to Target.py

--- Code/All Paths From Source to Target.py
+++ Code/All Paths From Source to Target.py
@@ -1,14 +1,15 @@
-class Solution:
-    def DFS(self, graph: List[List[int]], stack: List[int], result: List[List[int]]):
-        if stack and stack[-1] == len(graph) - 1:                                           #If the last element in stack is target, append the deep copy of stack to result and return.
-            result.append(copy.deepcopy(stack))
-            return
-        for x in graph[stack[-1]]:                                                          #Traverse the neighbors of stack[-1] and keep DFS.
-            stack.append(x)
-            self.DFS(graph, stack, result)
-            stack.pop()
-            
     def allPathsSourceTarget(self, graph: List[List[int]]) -> List[List[int]]:
+        visited = set([])
         result = []
-        self.DFS(graph, [0], result)                                                        #DFS.
+        def DFS(stack: List[int]):
+            if stack[-1] == len(graph) - 1:                                             #If the last element in stack is target, append the deep copy of stack to result and return.
+                result.append(copy.deepcopy(stack))
+                return
+            for x in graph[stack[-1]]:                                                  #Traverse the neighbors of stack[-1] and keep DFS.
+                stack.append(x)
+                visited.add(x)
+                DFS(stack)
+                visited.remove(x)
+                stack.pop()
+        DFS([0])                                                                        #DFS.
         return result
