ee97ee578651340498eddb3ddd01143709dfd0e6
Junsheng Wang <mws19901118@gmail.com>
2022-12-31 17:17:27 -0800

    Update Word Pattern.py

--- Code/Word Pattern.py
+++ Code/Word Pattern.py
@@ -1,24 +1,15 @@
-class Solution(object):
-    def wordPattern(self, pattern, str):
-        """
-        :type pattern: str
-        :type str: str
-        :rtype: bool
-        """
-        words = str.split(' ')                                                                        #Split str.
-        if len(words) != len(pattern):                                                                #If words and pattern have differenct length, return false.
+class Solution:
+    def wordPattern(self, pattern: str, s: str) -> bool:
+        wordToPattern, patternToWord = {}, {}                                                                                   #Store the mapping from word to pattern and mapping from pattern to word.
+        words = s.split(' ')                                                                                                    #Split s by space to get words.
+        if len(words) != len(pattern):                                                                                          #If length of words does not match the length of pattern, return false.
             return False
-        wordtopattern = {}                                                                            #Map word to pattern.
-        patterntoword = {}                                                                            #Map pattern to word.
-        for i in range(len(words)):
-            if words[i] not in wordtopattern and pattern[i] not in patterntoword:                     #If current word and pattern are both new, add the 1 to 1 map relation of word and pattern.
-                wordtopattern[words[i]] = pattern[i]
-                patterntoword[pattern[i]] = words[i]
-            elif words[i] in wordtopattern and pattern[i] in patterntoword:                           #If current word and pattern is a 1 to 1 map relation, continue; otherwise, return false.
-                if pattern[i] == wordtopattern[words[i]] and words[i] == patterntoword[pattern[i]]:
-                    continue
-                else:
-                    return False
-            else:
+        for w, p in zip(words, pattern):                                                                                        #Traverse words and patterns.
+            if w not in wordToPattern and p not in patternToWord:                                                               #If both word and pattern are not seen before, add mappings between each other.
+                wordToPattern[w] = p
+                patternToWord[p] = w
+            elif w in wordToPattern and p in patternToWord and wordToPattern[w] == p and patternToWord[p] == w:                 #If both word and pattern are seen before, add they are mapped to each other, continue.
+                continue
+            else:                                                                                                               #Otherwise, return false.
                 return False
-        return True
+        return True                                                                                                             #Return true.
