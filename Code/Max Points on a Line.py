9706663fd76ac224ec75632ca3c92639c9ed2717
Junsheng Wang <mws19901118@gmail.com>
2023-01-07 18:39:00 -0800

    Update Max Points on a Line.py

--- Code/Max Points on a Line.py
+++ Code/Max Points on a Line.py
@@ -1,33 +1,19 @@
-# Definition for a point
-# class Point:
-#     def __init__(self, a=0, b=0):
-#         self.x = a
-#         self.y = b
-
 class Solution:
-    # @param points, a list of Points
-    # @return an integer
-    def maxPoints(self, points):
-        length=len(points)
-        if length<2:
-            return length
-        else:
-            result=0
-            for i in xrange(length):    #loop for every point
-                line={'inf':0}          #dictionary for line, the key is slope,value is count
-                repeat=1
-                for j in xrange(length):
-                    if i==j:
-                        continue
-                    if points[i].x==points[j].x and points[i].y!=points[j].y:     #vertical situation
-                        line['inf']+=1
-                    elif points[i].x!=points[j].x:
-                        slope=float(points[i].y-points[j].y)/(points[i].x-points[j].x)
-                        if not line.has_key(slope):       #current slope is not in the dictionary
-                            line[slope]=1
-                        else:                             #current slope is in the dictionary
-                            line[slope]+=1
-                    else:
-                        repeat+=1                         #count repeat points
-                result=max(result,max(line.values())+repeat)    #update the max points
-            return result
+    def formLine(self, x1: int, y1: int, x2: int, y2: int) -> (float, float):
+        if x1 == x2:                                                            #If x1 == x2, k is infinity and b is x1.
+            return (float('inf'), x1)
+        else:                                                                   #Otherwise k is (y1 - y2) / (x1 - x2), and b is y1 - k * x1.
+            k = (y1 - y2) / (x1 - x2)
+            b = y1 - k * x1
+            return (k, b)
+
+    def maxPoints(self, points: List[List[int]]) -> int:
+        if len(points) == 1:                                                    #If there is only one point, return 1.
+            return 1
+        pointsOnLine = collections.defaultdict(set)                             #Store the points on a line.
+        for i, (x, y) in enumerate(points):                                     #Traverse points.
+            for u, v in points[i + 1:]:                                         #Traverse points[i + 1:].
+                k, b = self.formLine(x, y, u, v)                                #Calculate the k and b for the link y = kx + b formed by (x, y) and (nx, ny)
+                pointsOnLine[(k, b)].add((x, y))                                #Add (x, y) to the line.
+                pointsOnLine[(k, b)].add((u, v))                                #Add (u, v) to the line.
+        return max(len(p) for p in pointsOnLine.values())                       #Return the max count of points of lines.
