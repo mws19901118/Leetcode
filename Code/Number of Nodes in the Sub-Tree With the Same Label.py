dbb9adec5949b2e59b68e39a6c790cc87d3aca0c
Junsheng Wang <mws19901118@gmail.com>
2023-01-12 20:34:26 -0800

    Update Number of Nodes in the Sub-Tree With the Same Label.py

--- Code/Number of Nodes in the Sub-Tree With the Same Label.py
+++ Code/Number of Nodes in the Sub-Tree With the Same Label.py
@@ -1,25 +1,21 @@
 class Solution:
     def countSubTrees(self, n: int, edges: List[List[int]], labels: str) -> List[int]:
-        adjacentList = collections.defaultdict(list)                                  #Build the adjacent list.
+        adjacentList = collections.defaultdict(list)                                    #Build the adjacent list.
         for x, y in edges:
             adjacentList[x].append(y)
             adjacentList[y].append(x)
-        result = [0] * n                                                              #Initialze the result to be 0 for all nodes.
+        result = [0] * n                                                                #Initialze the result to be 0 for all nodes.
 
-        def DFS(stack: List[int], visited: set[int]) -> collections.Counter:
-            count = collections.Counter()                                             #Initialize a counter.
-            count[labels[stack[-1]]] = 1                                              #The node count of current label is 1.
-            for x in adjacentList[stack[-1]]:                                         #Traverse neighbors.
-                if x in visited:                                                      #If neighbor is visited, skip.
+        def DFS(curr: int, prev: int) -> collections.Counter:
+            count = collections.Counter()                                               #Initialize a counter.
+            count[labels[curr]] = 1                                                     #The node count of current label is 1.
+            for x in adjacentList[curr]:                                                #Traverse neighbors.
+                if x == prev:                                                           #If x is same as prev, it's visited, so skip.
                     continue
-                stack.append(x)                                                       #Append neighbor to stack.
-                visited.add(x)                                                        #Mark neighbor as visited.
-                for k, v in DFS(stack, visited).items():                              #Traverse the DFS result from neighbor.
-                    count[k] += v                                                     #Add all count of node to current counter.
-                visited.remove(x)                                                     #Remove neighbor from visited.
-                stack.pop()                                                           #Pop stack.
-            result[stack[-1]] = count[labels[stack[-1]]]                              #Set the result for current node.
-            return count                                                              #Return counter.
+                for k, v in DFS(x, curr).items():                                       #Traverse the DFS result from neighbor.
+                    count[k] += v                                                       #Add all count of node to current counter.
+            result[curr] = count[labels[curr]]                                          #Set the result for current node.
+            return count                                                                #Return counter.
 
-        DFS([0], set([0]))                                                            #DFS from node 0.
+        DFS(0, -1)                                                                      #DFS from node 0.
         return result
