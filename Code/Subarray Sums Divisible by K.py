289e766949d99dab5be02cf0df15193715f5cb25
Junsheng Wang <mws19901118@gmail.com>
2023-01-18 22:50:29 -0800

    Update Subarray Sums Divisible by K.py

--- Code/Subarray Sums Divisible by K.py
+++ Code/Subarray Sums Divisible by K.py
@@ -1,16 +1,9 @@
-from collections import Counter
-class Solution(object):
-    def subarraysDivByK(self, A, K):
-        """
-        :type A: List[int]
-        :type K: int
-        :rtype: int
-        """
-        s = [0]                                               #Store remainder of prefix sum from A[0] to A[i]. Add 0 to represent that in the beginning, the sum is 0.
-        for x in A:
-            s.append((s[-1] + x) % K)                         #Append the newest remainder to s.
-        count = Counter(s)                                    #Count the occurence of each remainder.
-        return sum(v * (v - 1) / 2 for v in count.values())   #For each pair of indexes, i and j, if s[i] = s[j], then s[i] + (A[i] + ... + A[j]) % K = s[j].
-                                                              #Thus, A[i] to A[j] is a subarray whose sum is divisible by K.
-                                                              #Then, we only need to calculate for each remainder, how many pairs are there.
-                                                              #If remainder s occurs v times, the number of pairs is v * (v - 1) / 2
+class Solution:
+    def subarraysDivByK(self, nums: List[int], k: int) -> int:
+        s = [0]                                                 #Store remainder of prefix sum from nums[0] to nums[i]. Add 0 to represent that in the beginning, the sum is 0.
+        for x in nums:                                          #Append the newest remainder to s.
+            s.append((s[-1] + x) % k)                           #Count the occurence of each remainder.
+        count = collections.Counter(s)                          #For each pair of indexes, i and j, if s[i] = s[j], then s[i] + (nums[i] + ... + nums[j]) % k = s[j].
+        return sum(v * (v - 1) // 2 for v in count.values())    #Thus, nums[i] to nums[j] is a subarray whose sum is divisible by k.
+                                                                #Then, we only need to calculate for each remainder, how many pairs are there.
+                                                                #If remainder s occurs v times, the number of pairs is v * (v - 1) // 2.
