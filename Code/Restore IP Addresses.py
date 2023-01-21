db8c794a4438d230d2b5f93156d97f20a8d4e4a0
Junsheng Wang <mws19901118@gmail.com>
2023-01-20 17:09:27 -0800

    Update Restore IP Addresses.py

--- Code/Restore IP Addresses.py
+++ Code/Restore IP Addresses.py
@@ -1,24 +1,9 @@
 class Solution:
-    # @param s, a string
-    # @return a list of strings
-    def restoreIpAddresses(self, s):
-        n=len(s)
-        result=[]
-        i=1
-        while i<=3:                                         #First delimiter
-            j=i+1
-            while j<=i+3 and j<n:                           #Second delimiter
-                k=j+1
-                while k<=j+3 and k<n:                       #Third delimiter
-                    t1=int(s[:i])                           #Four parts of IP
-                    t2=int(s[i:j])
-                    t3=int(s[j:k])
-                    t4=int(s[k:])
-                    if (t1>=0 and t1<=255) and (t2>=0 and t2<=255) and (t3>=0 and t3<=255) and (t4>=0 and t4<=255):     #Check validity.
-                        ans=str(t1)+'.'+str(t2)+'.'+str(t3)+'.'+str(t4)
-                        if result.count(ans)==0 and len(ans)==n+3:                      #Length should equal original length plus 3 delimiters.
-                            result.append(ans)
-                    k+=1
-                j+=1
-            i+=1
+    def restoreIpAddresses(self, s: str) -> List[str]:
+        result = []
+        for i in range(1, len(s)):                                                                                              #Enumerate the place to insert first delimiter.
+            for j in range(i + 1, len(s)):                                                                                      #Enumerate the place to insert second delimiter.
+                for k in range(j + 1, len(s)):                                                                                  #Enumerate the place to insert third delimiter.
+                    if all(x and (x == "0" or (int(x) < 256 and x[0] != "0")) for x in [s[:i], s[i:j], s[j:k], s[k:]]):         #Check all 4 chunks are valid: not empty, between 0 to 255 and no leading 0.
+                        result.append(".".join([s[:i], s[i:j], s[j:k], s[k:]]))                                                 #Join the chucks together with "." and add to result.
         return result
