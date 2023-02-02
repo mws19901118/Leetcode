0b688802f49b3001d1c2d6eee4953d566d14e9f4
Junsheng Wang <mws19901118@gmail.com>
2023-02-01 16:39:20 -0800

    Update Verifying an Alien Dictionary.py

--- Code/Verifying an Alien Dictionary.py
+++ Code/Verifying an Alien Dictionary.py
@@ -1,14 +1,10 @@
 class Solution:
-    def inOrder(self, a: str, b: str, orderTable: dict) -> bool:                                        #Determine if a is lexicographicaly smalled than or equal to b in orderTable.
-        i = 0
-        while i < len(a) and i < len(b) and a[i] == b[i]:                                               #Find the first difference character between a and b.
-            i += 1
-        if i == len(a):                                                                                 #If it reaches the end of a, meaning a is a prefix of b, then a is smalled than or equal to b.
-            return True
-        if i == len(b):                                                                                 #If it reaches the end of b, meaning b is a prefix ofab, then a is not smalled than or equal to b.
-            return False
-        return orderTable[a[i]] < orderTable[b[i]]                                                      #Return if the order of a[i] is smaller than the order of b[i].
-    
     def isAlienSorted(self, words: List[str], order: str) -> bool:
         orderTable = {x: i for i, x in enumerate(order)}                                                #Convert order to a hash table.
-        return all(self.inOrder(words[i], words[i + 1], orderTable) for i in range(len(words) - 1))     #Return all word is lexicographicaly smalled than or equal to next word.
+        def inOrder(a: str, b: str) -> bool:
+            i = 0
+            while i < len(a) and i < len(b) and a[i] == b[i]:                                           #Find the first difference character between a and b.
+                i += 1
+            return i == len(a) or (i != len(b) and orderTable[a[i]] < orderTable[b[i]])                 #If a < b then either i reaches the end of a or (i hasn't reach the end of b and the order of a[i] is smaller than the order of b[i]).
+
+        return all(inOrder(words[i], words[i + 1]) for i in range(len(words) - 1))                      #Return all word is lexicographicaly smalled than or equal to next word.
