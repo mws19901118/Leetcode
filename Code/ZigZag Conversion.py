c261367830a1156378ada0b6953dd0ea0f4542ac
Junsheng Wang <mws19901118@gmail.com>
2023-02-02 16:42:38 -0800

    Update ZigZag Conversion.py

--- Code/ZigZag Conversion.py
+++ Code/ZigZag Conversion.py
@@ -1,28 +1,10 @@
 class Solution:
-    # @param {string} s
-    # @param {integer} numRows
-    # @return {string}
-    def convert(self, s, numRows):
-        if numRows==1:                            #If there is only one row, we don't have to convert.
+    def convert(self, s: str, numRows: int) -> str:
+        if numRows == 1:                                                                    #If there is only one row, we don't have to convert.
             return s
-        l=len(s)
-        section=(numRows-1)*2                     #The length of each zigzag section is (numRows-1)*2
-        n=l/section                               #Calculate the number of full section.
-        answer=''
-        for i in range(n):                        #Deal with the first row, which has only 1 character in each section.
-            answer+=s[i*section]
-        if n*section<l:                           #Check if there is another character in the remain section.
-            answer+=s[n*section]
-        for j in range(1,numRows-1):
-            for i in range(n):                    #Deal with the mid rows, which has 2 characters in each section.
-                answer+=s[i*section+j]            #Deal with the first character.
-                answer+=s[i*section+section-j]    #Deal with the second character.
-            if n*section+j<l:                     #Check if there is another character in the remain section.
-                answer+=s[n*section+j]
-            if n*section+section-j<l:             #Check if there is another character in the remain section.
-                answer+=s[n*section+section-j]
-        for i in range(n):                        #Deal with the last row, which has only 1 character in each section.
-            answer+=s[i*section+numRows-1]
-        if n*section+numRows-1<l:                 #Check if there is another character in the remain section.
-            answer+=s[n*section+numRows-1]
-        return answer
+        rows = [[] for _ in range(numRows)]                                                 #Initialize characters at each row.
+        segment = (numRows - 1) * 2                                                         #Each segment of zigzag is of (numRows - 1) * 2 length.
+        for i, x in enumerate(s):                                                           #Traverse s.
+            j = i % segment                                                                 #Find the index of s in current segment.
+            rows[j if j < numRows - 1 else (numRows - 1) * 2 - j].append(x)                 #If j < numRows - 1, append s to rows[j], first part of zipzag; otherwise, append s to rows[(numRows - 1) * 2 - j], second part of zigzag.
+        return "".join("".join(r) for r in rows)                                            #Join each row then join all rows together.
