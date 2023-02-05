90f9d8e6da2e4bb295acc87b4a7603abf713fd23
Junsheng Wang <mws19901118@gmail.com>
2023-02-04 18:03:33 -0800

    Update Find All Anagrams in a String.py

--- Code/Find All Anagrams in a String.py
+++ Code/Find All Anagrams in a String.py
@@ -1,16 +1,12 @@
 class Solution:
     def findAnagrams(self, s: str, p: str) -> List[int]:
-        diff = [0] * 26                                               #Maintain the diff of letter count between running window in s and p.
-        for x in p:                                                   #Initialize the diff(letter count of p) and count.
-            diff[ord(x) - ord('a')] += 1
-        count = sum(x != 0 for x in diff)                             #Count the number of letters not match.
-        result = []
-        for i in range(len(s)):
-            diff[ord(s[i]) - ord('a')] -= 1                           #Update diff and count everytime the running window has a new letter.
-            count -= diff[ord(s[i]) - ord('a')] == 0
-            if count == 0:                                            #If no not matched letter between running window and p, an anagram is found, so append the current starting index of running window to result list. 
-                result.append(i - len(p) + 1)                         
-            if i >= len(p) - 1:                                       #Pop current starting index out of running window if its length is len(p) and update diff and count if necessary.
-                count += diff[ord(s[i - len(p) + 1]) - ord('a')] == 0
-                diff[ord(s[i - len(p) + 1]) - ord('a')] += 1
-        return result
+        c1, c2 = collections.Counter(p), collections.Counter(s[:len(p)])        #Count each letter in p and each letter in s[:len(p)].
+        result = []                                                             #Initialize result.
+        if all(c1[x] == c2[x] for x in c1):                                     #If all letters in c1 has same count in c2, s[:len(p)] is an anagrams of p, so add 0 to result.
+            result.append(0)
+        for i in range(1, len(s) - len(p) + 1):                                 #Traverse from 1 to len(s) - len(p).
+            c2[s[i + len(p) - 1]] += 1                                          #Move s[i - 1 + len(p)] out of c2.
+            c2[s[i - 1]] -= 1                                                   #Move s[i - 1] in to c2.
+            if all(c1[x] == c2[x] for x in c1):                                 #If all letters in c1 has same count in c2, s[i:i + len(p)] is an anagrams of p, so add i to result.
+                result.append(i)
+        return result                                                           #Return result.
