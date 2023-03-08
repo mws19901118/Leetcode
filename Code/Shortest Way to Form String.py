b9bbf4ca6bc7a2a765fd1061219a3281a343b730
Junsheng Wang <mws19901118@gmail.com>
2023-03-07 18:29:49 -0800

    Create Shortest Way to Form String.py

---
+++ Code/Shortest Way to Form String.py
@@ -0,0 +1,16 @@
+class Solution:
+    def shortestWay(self, source: str, target: str) -> int:
+        indexes = collections.defaultdict(list)                             #Store the indexes of each letter in source.
+        for i, x in enumerate(source):
+            indexes[x].append(i)
+        count, prevIndex = 0, -1                                            #Initialize the count and prev index in source.
+        for x in target:                                                    #Traverse target.
+            if x not in indexes:                                            #Return -1 if x not in indexes, meaning x not in source as well.
+                return -1
+            index = bisect.bisect_left(indexes[x], prevIndex + 1)           #Find the first index of x which id greater than prevIndex.
+            if index == len(indexes[x]):                                    #If not found, increase count and set prevIndex to first index of x.
+                count += 1
+                prevIndex = indexes[x][0]
+            else:                                                           #Otherwise, just update prevIndex to the found index.
+                prevIndex = indexes[x][index]
+        return count + 1                                                    #Return count + 1.
