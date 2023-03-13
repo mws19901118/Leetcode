4989917acc906f807c466555bb1c8ff6c6852e65
Junsheng Wang <mws19901118@gmail.com>
2023-03-12 17:42:34 -0700

    Update Symmetric Tree.py

--- Code/Symmetric Tree.py
+++ Code/Symmetric Tree.py
@@ -1,35 +1,16 @@
-# Definition for a  binary tree node
+# Definition for a binary tree node.
 # class TreeNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
-
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
 class Solution:
-    # @param root, a tree node
-    # @return a boolean
-    def isSymmetric(self, root):
-        if root==None:                                          #If root is empty, return true.
+    def traverse(self, left: Optional[TreeNode], right: Optional[TreeNode]) -> bool:                                    #Traverse 2 trees simultaneously to determine if they are symmetric to each other.
+        if not left and not right:                                                                                      #If both are none, return true.
             return True
-        leftw=root.left                                         #Use leftw to represent the left wing of root.
-        rightw=root.right                                       #Use rightw to represent the right wing of root.
-        while leftw!=None and rightw!=None:
-            if leftw.val!=rightw.val:                           #If value of left wing node is not equal to value of right wing node, return false.
-                return False
-            elif (leftw.right!=None and rightw.left==None) or (leftw.right==None and rightw.left!=None):      #If one of right child of left wing node and left child of right wing node is empty and the other is not empty, return false.
-                return False
-            elif leftw.right!=None and rightw.left!=None and leftw.right.val!=rightw.left.val:                #If the value of right child of left wing node is not equal to the value of left child of right wing node, return false.
-                return False
-            else:
-                virtualroot=TreeNode(-1)                        #Add a virtual root to right child of left wing node and left child of right wing node.
-                virtualroot.left=leftw.right
-                virtualroot.right=rightw.left
-                if self.isSymmetric(virtualroot)==False:        #If the tree whose root is the virtual root is not symmertric, return false.
-                    return False
-                else:                                           #Otherwise, use the left child of left wing node to replace the left wing node and the right child of right wing node to replace the right wing node.
-                    leftw=leftw.left
-                    rightw=rightw.right
-        if leftw!=None or rightw!=None:                         #If one of left wing node and right wing node is empty and the other is not empty, return false.
+        elif (left and not right) or (not left and right) or left.val != right.val:                                     #If only one of them is none or the values don't match, return false.
             return False
-        else:                                                   #If the tree satisfy all the requirement, return true.
-            return True
+        return self.traverse(left.left, right.right) and self.traverse(left.right, right.left)                          #Return true if both left subtree of left and right subtree of right are symmetric and right subtree of left and left subtree of right are symmetric.
+
+    def isSymmetric(self, root: Optional[TreeNode]) -> bool:
+        return self.traverse(root.left, root.right)                                                                     #Traverse from root recursively.
