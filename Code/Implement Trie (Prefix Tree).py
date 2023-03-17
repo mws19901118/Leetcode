fd98be5d1b1dd56a793002efc35f8f288873abfe
Junsheng Wang <mws19901118@gmail.com>
2023-03-16 17:44:57 -0700

    Update Implement Trie (Prefix Tree).py

--- Code/Implement Trie (Prefix Tree).py
+++ Code/Implement Trie (Prefix Tree).py
@@ -4,39 +4,33 @@ class Trie:
         """
         Initialize your data structure here.
         """
-        self.dict = {}
+        self.dict = defaultdict(lambda:Trie())
         self.hasWord = False
 
     def insert(self, word: str) -> None:
         """
         Inserts a word into the trie.
         """
-        if len(word) == 0:
-            self.hasWord = True                                                         #If the word ends, set the indicator to true.
+        if not word:
+            self.hasWord = True                                                                     #If the word ends, set the indicator to true.
             return
-        if word[0] not in self.dict:                                                    #If current character is not in the dict, add it to the dict.
-            self.dict[word[0]] = Trie()
-        self.dict[word[0]].insert(word[1:])                                             #If the word doesn't end, insert its next character recursively.
+        self.dict[word[0]].insert(word[1:])                                                         #If the word doesn't end, insert its next character recursively.
 
     def search(self, word: str) -> bool:
         """
         Returns if the word is in the trie.
         """
-        if len(word) == 0:                                                              #If the word ends, return the indicator.
+        if not word:                                                                                #If the word ends, return the indicator.
             return self.hasWord
-        if word[0] not in self.dict:                                                    #If current character is not in the list, return false.
-            return False
-        return self.dict[word[0]].search(word[1:])                                      #If the word doesn't end, search its next character recursively.
+        return word[0] in self.letters and self.letters[word[0]].search(word[1:])                   #If the word doesn't end, search its next character recursively.          
 
     def startsWith(self, prefix: str) -> bool:
         """
         Returns if there is any word in the trie that starts with the given prefix.
         """
-        if len(prefix) == 0:                                                            #If the prefix ends, return true.
+        if not prefix:                                                                              #If the prefix ends, return true.
             return True
-        if prefix[0] not in self.dict:                                                  #If current character is not in the list, return false.
-            return False
-        return self.dict[prefix[0]].startsWith(prefix[1:])                              #Otherwise, search its next character recursively.
+        return prefix[0] in self.letters and self.letters[prefix[0]].startsWith(prefix[1:])         #Otherwise, search its next character recursively.
 
 
 # Your Trie object will be instantiated and called as such:
