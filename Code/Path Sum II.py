a2c92876862d71b9d069da0014df3bf19d60dde9
Junsheng Wang <mws19901118@gmail.com>
2023-03-26 17:52:04 -0700

    Update Path Sum II.py

--- Code/Path Sum II.py
+++ Code/Path Sum II.py
@@ -5,18 +5,19 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def DFS(self, root: TreeNode, targetSum: int, stack: List[int], result: List[List[int]]) -> None:
-        if not root:                                                                                        #If root is none, return.
-            return
-        stack.append(root.val)                                                                              #Append root.val to stack.
-        if not root.left and not root.right and targetSum == root.val:                                      #If root is leaf node and targetSum equals root.val, append deepcopy of stack to result.
-            result.append(deepcopy(stack))
-        else:                                                                                               #Otherwise, keep DFS on root.left and root.right for targetSum - root.val.
-            self.DFS(root.left, targetSum - root.val, stack, result)
-            self.DFS(root.right, targetSum - root.val, stack, result)
-        stack.pop()                                                                                         #Pop stack.
-        
     def pathSum(self, root: TreeNode, targetSum: int) -> List[List[int]]:
-        result = []                                                                                         #Initialize result.
-        self.DFS(root, targetSum, [], result)                                                               #DFS.
-        return result
+        paths = []                                                                      #Initialize paths.
+
+        def DFS(root: Optional[TreeNode], stack: List[int], remain: int):               #DFS.
+            if not root:                                                                #If root is none, return.
+                return
+            stack.append(root.val)                                                      #Append root.val to stack.
+            remain -= root.val                                                          #Substract root.val from remain.
+            if not root.left and not root.right and not remain:                         #If root is leaf node and remain is 0, we wound a path sum so append the deepcopy of stack to paths.
+                paths.append(deepcopy(stack))
+            DFS(root.left, stack, remain)                                               #Keep DFS in root.left.
+            DFS(root.right, stack, remain)                                              #Keep DFS is root.right.
+            stack.pop()                                                                 #Pop stack.
+        
+        DFS(root, [], targetSum)                                                        #Start DFS from root.
+        return paths                                                                    #Return paths.
