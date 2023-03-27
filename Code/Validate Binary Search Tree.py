fc3f55810a644cea60af71b09a46da0b75c1efb5
Junsheng Wang <mws19901118@gmail.com>
2023-03-26 18:26:47 -0700

    Update Validate Binary Search Tree.py

--- Code/Validate Binary Search Tree.py
+++ Code/Validate Binary Search Tree.py
@@ -6,10 +6,11 @@
 #         self.right = right
 class Solution:
     def isValid(self, root, minV, maxV) -> bool:
-        if root is None:
+        if not root:
             return True
         if root.val <= minV or root.val >= maxV:                                                            #If value of current node is smaller than min valid value or larger than max valid value, return false.
             return False
         return self.isValid(root.left, minV, root.val) and self.isValid(root.right, root.val, maxV)         #The value of left child should be smaller than current value and the value of right child should be larger than current value; otherwise it is not a valid BST.
+    
     def isValidBST(self, root: TreeNode) -> bool:
         return self.isValid(root, -0xFFFFFFFF, 0xFFFFFFFF)
