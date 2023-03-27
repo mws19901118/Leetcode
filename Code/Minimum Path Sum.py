90bb036f9023247bdea48233b9fd8db758b2caca
Junsheng Wang <mws19901118@gmail.com>
2023-03-26 17:24:17 -0700

    Update Minimum Path Sum.py

--- Code/Minimum Path Sum.py
+++ Code/Minimum Path Sum.py
@@ -1,20 +1,10 @@
 class Solution:
-    # @param grid, a list of lists of integers
-    # @return an integer
-    def minPathSum(self, grid):                                               #Simple dynamic programming
-        m=len(grid)
-        if m==0:
-            return 0
-        n=len(grid[0])
-        if n==0:
-            return 0
-        minSum=[[0 for i in range(n) ] for j in range(m)]
-        minSum[0][0]=grid[0][0]
-        for i in range(1,m):
-            minSum[i][0]=grid[i][0]+minSum[i-1][0]
-        for i in range(1,n):
-            minSum[0][i]=grid[0][i]+minSum[0][i-1]
-        for i in range(1,m):
-            for j in range(1,n):
-                minSum[i][j]=min(minSum[i-1][j],minSum[i][j-1])+grid[i][j]
-        return minSum[m-1][n-1]
+    def minPathSum(self, grid: List[List[int]]) -> int:
+        m, n = len(grid), len(grid[0])                                              #Get the dimensions of grid.
+        dp = [0]                                                                    #Build the path sum for first row.
+        for x in grid[0]:
+            dp.append(dp[-1] + x)
+        dp[0] = float('inf')                                                        #Set dp[0] to a real large number.
+        for i, j in product(range(1, m), range(n)):                                 #Traverse the rest of matrix.
+            dp[j + 1] = min(dp[j], dp[j + 1]) + grid[i][j]                          #Calculate the min path sum at each cell, which is the min from left(dp[j]) and from top(dp[j + 1]) plus value at current cell.
+        return dp[-1]                                                               #Return dp[-1].
