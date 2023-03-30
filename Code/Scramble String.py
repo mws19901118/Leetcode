34e0afbddb20569fff524fb4d75806e20a4a283f
Junsheng Wang <mws19901118@gmail.com>
2023-03-29 23:28:58 -0700

    Update Scramble String.py

--- Code/Scramble String.py
+++ Code/Scramble String.py
@@ -1,20 +1,5 @@
 class Solution:
-    # @return a boolean
-    def isScramble(self, s1, s2):
-        def Scramble(s1,s2,dict):
-            if dict.has_key((s1,s2)):
-                return dict[(s1,s2)]
-            else:
-                if s1==s2:                      #If s1 equals to s2, they are obviously scramble.
-                    dict[(s1,s2)]=True
-                    return True
-                else:
-                    n=len(s1)
-                    for i in range(1,n):        #Enum all the split points.
-                        if (Scramble(s1[:i], s2[:i], dict) and Scramble(s1[i:], s2[i:], dict)) or (Scramble(s1[:i], s2[-i:], dict) and Scramble(s1[i:], s2[:-i], dict)):
-                            dict[(s1,s2)]=True  #The first part is to determine if current string is consist of several scramble substrings, while the second part is to determine if current string is scramble.
-                            return True
-                    dict[(s1,s2)]=False
-                    return False
-        dict={}                                 #Dict to store intermediate results.
-        return Scramble(s1, s2, dict)
+    @cache                                                                                                                                                                                                       #Cache result.
+    def isScramble(self, s1: str, s2: str) -> bool:
+        return s1 == s2 or any((self.isScramble(s1[:i], s2[:i]) and self.isScramble(s1[i:], s2[i:])) or (self.isScramble(s1[:i], s2[-i:]) and self.isScramble(s1[i:], s2[:-i])) for i in range(1, len(s1)))      #For s2 to be scramble of s1, either s1 == s2, or there exist an index i so that s2[:i] is scramble of s1[:i] and s2[i:] is scramble of s1[i:] or there exist an index i so that s2[-i:] is scramble of s1[:i] and s2[:-i] is scramble of s1[i:].
+    
