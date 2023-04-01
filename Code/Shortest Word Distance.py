0fc9843df97ad9a5821fd93894e5714e3745458b
Junsheng Wang <mws19901118@gmail.com>
2023-03-31 17:53:55 -0700

    Update Shortest Word Distance.py

--- Code/Shortest Word Distance.py
+++ Code/Shortest Word Distance.py
@@ -1,30 +1,14 @@
-class Solution(object):
-    def shortestDistance(self, words, word1, word2):
-        """
-        :type words: List[str]
-        :type word1: str
-        :type word2: str
-        :rtype: int
-        """
-        i = 0
-        while words[i] != word1 and words[i] != word2:            #Find the first occurrence of either word1 or word2.
-            i += 1
-        if words[i] == word1:                                     #If flag is true, it indicates that the last occurrence is word1.
-            flag = True
-        elif words[i] == word2:                                   #If flag is false, it indicates that the last occurrence is word2.
-            flag = False
-        result = len(words)
-        last = i                                                  #Record the index of last occurrence of either word1 or word2.
-        while i < len(words):
-            if words[i] == word1:
-                if flag is False:                                 #If current word is word1 and last occurrence is word2, update result and set flag to be false.
-                    result = min(result, i - last)
-                    flag = True
-                last = i                                          #Update last occurrence.
-            elif words[i] == word2:
-                if flag is True:                                  #If current word is word2 and last occurrence is word1, update result and set flag to be true.
-                    result = min(result, i - last)
-                    flag = False
-                last = i                                          #Update last occurrence.
-            i += 1
+class Solution:
+    def shortestDistance(self, wordsDict: List[str], word1: str, word2: str) -> int:
+        word1Prev, word2Prev = -1, -1                               #Store the previous index of word1 and word2 repectively, initially both -1.
+        result = len(wordsDict)
+        for i, x in enumerate(wordsDict):                           #Traverse wordsDict.
+            if x == word1:                                          #Process when x is word1.
+                if word2Prev >= 0:                                  #Update result if we have seen word2.
+                    result = min(result, i - word2Prev)
+                word1Prev = i                                       #Update word1Prev.
+            elif x == word2:                                        #Process when x is word2.
+                if word1Prev >= 0:                                  #Update result if we have seen word1.
+                    result = min(result, i - word1Prev)
+                word2Prev = i                                       #Update word2Prev.
         return result
