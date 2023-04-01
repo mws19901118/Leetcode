f660fb9a4843c64eed870bc634fc2260fbd4ab1f
Junsheng Wang <mws19901118@gmail.com>
2023-03-31 17:45:53 -0700

    Update Shortest Word Distance III.py

--- Code/Shortest Word Distance III.py
+++ Code/Shortest Word Distance III.py
@@ -1,39 +1,23 @@
-class Solution(object):
-    def shortestWordDistance(self, words, word1, word2):
-        """
-        :type words: List[str]
-        :type word1: str
-        :type word2: str
-        :rtype: int
-        """
-        if word1 != word2:                                          #If words1 is not equal to words2, it's the same as Shortest Word Distance I.
-            i = 0
-            while words[i] != word1 and words[i] != word2:
-                i += 1
-            if words[i] == word1:
-                flag = True
-            elif words[i] == word2:
-                flag = False
-            result = len(words)
-            last = i
-            while i < len(words):
-                if words[i] == word1:
-                    if flag is False:
-                        result = min(result, i - last)
-                        flag = True
-                    last = i
-                elif words[i] == word2:
-                    if flag is True:
-                        result = min(result, i - last)
-                        flag = False
-                    last = i
-                i += 1
+class Solution:
+    def shortestWordDistance(self, wordsDict: List[str], word1: str, word2: str) -> int:
+        if word1 == word2:                                                      #If word1 and word2 are same, find the shortest distance by checking every index of word1.
+            prev = -1
+            result = len(wordsDict)
+            for i, x in enumerate(wordsDict):
+                if x == word1:
+                    if prev >= 0:
+                        result = min(result, i - prev)
+                    prev = i
             return result
-        else:                                                       #Otherwise, find the shortest distance by checking every index of word1.
-            result = len(words)
-            last = words.index(word1)
-            for i in range(last + 1, len(words)):
-                if words[i] == word1:
-                    result = min(result, i - last)
-                    last = i
-            return result
+        word1Prev, word2Prev = -1, -1                                           #Otherwise, it's the same as Shortest Word Distance I.
+        result = len(wordsDict)
+        for i, x in enumerate(wordsDict):
+            if x == word1:
+                if word2Prev >= 0:
+                    result = min(result, i - word2Prev)
+                word1Prev = i
+            elif x == word2:
+                if word1Prev >= 0:
+                    result = min(result, i - word1Prev)
+                word2Prev = i
+        return result
