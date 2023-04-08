39d7d1a4a977caac4090e0ee5b3c9dec5df62b4e
Junsheng Wang <mws19901118@gmail.com>
2023-04-07 17:20:52 -0700

    Update Clone Graph.py

--- Code/Clone Graph.py
+++ Code/Clone Graph.py
@@ -8,22 +8,14 @@ class Node:
 
 class Solution:
     def cloneGraph(self, node: 'Node') -> 'Node':
-        if node is None:
-            return None
-        newNode = Node(node.val)                                                        #Initalize the copy of given node.
-        visited = set()                                                                 #Record visited nodes.
-        valToNodeMap = {node.val: newNode}                                              #Use dict to map node by its value.
-        q = [node]                                                                      #Initialize queue for BFS.
-        while q:                                                                        #BFS.
-            newq = []                                                                   #Initialize new queue.
-            for x in q:                                                                 #Traverse through nodes in queue.
-                if x in visited:                                                        #If current node is visited, skip it.
-                    continue
-                for y in x.neighbors:                                                   #Traverse through nodes in the neighbors of current node.
-                    if y.val not in valToNodeMap:                                       #If this neighbor is not in map, initialize a copy and add to map.
-                        valToNodeMap[y.val] = Node(y.val)
-                    valToNodeMap[x.val].neighbors.append(valToNodeMap[y.val])           #Append the copy of neighbor to the neighbors of copy of current node.
-                    newq.append(y)                                                      #Append neighbor to new queue.
-                visited.add(x)                                                          #Add current node to visited.
-            q = newq                                                                    #Replace queue with new queue for next iteration.
-        return newNode
+        clonedNodes = {}                                                            #Store cloned nodes.
+        def cloneNode(node: 'Node') -> 'Node':                                      
+            if not node:                                                            #If node is none, return none.
+                return None
+            if node.val in clonedNodes:                                             #If current node is already cloned, return the cloned node.
+                return clonedNodes[node.val]
+            clone = Node(node.val)                                                  #Cloud current node.
+            clonedNodes[node.val] = clone                                           #Put cloned node into dictionary.
+            clone.neighbors = [cloneNode(x) for x in node.neighbors]                #Recursively clone all neighbors.
+            return clone                                                            #Return cloned node.
+        return cloneNode(node)
