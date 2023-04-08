afa828d2c024e01a46d964d7cb254dc9ccb0dede
Junsheng Wang <mws19901118@gmail.com>
2023-04-07 22:54:38 -0700

    Update Logger Rate Limiter.py

--- Code/Logger Rate Limiter.py
+++ Code/Logger Rate Limiter.py
@@ -1,23 +1,14 @@
 class Logger:
 
     def __init__(self):
-        """
-        Initialize your data structure here.
-        """
-        self.d = {}                                                       #Use a dict to store the timestamp of last print.
+        self.logs = {}                                                                      #Store logs in a dictionary.
 
     def shouldPrintMessage(self, timestamp: int, message: str) -> bool:
-        """
-        Returns true if the message should be printed in the given timestamp, otherwise returns false.
-        If this method returns false, the message will not be printed.
-        The timestamp is in seconds granularity.
-        """
-        if message not in self.d or self.d[message] + 10 <= timestamp:    #If message not in dict or last timestamp is 10 seconds ago, update timestamp and return True.
-            self.d[message] = timestamp
-            return True
-        else:                                                             #Otherwise, return false.
+        if message in self.logs and self.logs[message] + 10 > timestamp:                    #If message is in dictionary and previous timestamp plus 10 is greater than current timestamp, return false.
             return False
-            
+        self.logs[message] = timestamp                                                      #Upsert the timestamp for message.
+        return True                                                                         #Return true.
+
 # Your Logger object will be instantiated and called as such:
 # obj = Logger()
 # param_1 = obj.shouldPrintMessage(timestamp,message)
