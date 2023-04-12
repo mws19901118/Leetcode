2a8fe984bfd9f59f4a5e4127c1fafbe685432b78
Junsheng Wang <mws19901118@gmail.com>
2023-04-11 23:02:54 -0700

    Update Meeting Rooms II.py

--- Code/Meeting Rooms II.py
+++ Code/Meeting Rooms II.py
@@ -1,6 +1,6 @@
 class Solution:
     def minMeetingRooms(self, intervals: List[List[int]]) -> int:
-        points = sorted([(x, 1) for x, y in intervals] + [(y, -1) for x, y in intervals])           #Break intervals to points, 1 for start point and -1 for end point.
+        points = sorted([(x, 1) for x, y in intervals] + [(y, -1) for x, y in intervals])           #Break intervals to points, 1 for start point and -1 for end point and sort in asending order.
         count, result = 0, 0                                                                        #Initialize meeting room count and result.
         for x, y in points:                                                                         #Traverse points.
             count += y                                                                              #Increase or decrease count depending on if it's start or end.
