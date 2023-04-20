5c7232fa93d8387e2d04d5823b6f666e92b2d273
Junsheng Wang <mws19901118@gmail.com>
2023-04-19 23:00:38 -0700

    Update Maximum Width of Binary Tree.py

--- Code/Maximum Width of Binary Tree.py
+++ Code/Maximum Width of Binary Tree.py
@@ -6,16 +6,14 @@
 #         self.right = right
 class Solution:
     def widthOfBinaryTree(self, root: Optional[TreeNode]) -> int:
-        if not root:                                                            #If root is none, return 0.
-            return 0
         level = [(root, 0)]                                                     #Put root and its index(starting from 0) in a queue.
         width = 0
         while level:
             nextLevel = []
             for node, index in level:                                           #Tree level order traversal.
-                if node.left is not None:                                       #If the left child of current node is not none, calculate the index of left child and add left child and its index to queue.
+                if not node.left:                                               #If the left child of current node is not none, calculate the index of left child and add left child and its index to queue.
                     nextLevel.append((node.left, index * 2))
-                if node.right is not None:                                      #If the right child of current node is not none, calculate the index of right child and add right child and its index to queue.
+                if not node.right:                                              #If the right child of current node is not none, calculate the index of right child and add right child and its index to queue.
                     nextLevel.append((node.right, index * 2 + 1))
             width = max(width, level[-1][1] - level[0][1] + 1)                  #Calculate width of current level and update max width.
             level = nextLevel
