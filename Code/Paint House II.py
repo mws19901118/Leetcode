47696b557f09a0ea663401408db573d18e49ec13
Junsheng Wang <mws19901118@gmail.com>
2023-05-17 23:27:03 -0700

    Update Paint House II.py

--- Code/Paint House II.py
+++ Code/Paint House II.py
@@ -1,51 +1,16 @@
-class Solution(object):
-    def minCostII(self, costs):
-        """
-        :type costs: List[List[int]]
-        :rtype: int
-        """
-        m = len(costs)                                                                #Record the number of houses.
-        if m == 0:
-            return 0
-        n = len(costs[0])                                                             #Record the number of colors.
-        if n == 0:
-            return 0
-        minIndex = -1                                                                 #Record the index of last used color.
-        firstMin = 0x7fffffff                                                         #Record the min sum of costs before current house.
-        secondMin = 0x7fffffff                                                        #Record the second min sum of costs before current house.
-        for j in range(n):                                                            #Find minIndex and firstMin for the first house.
-            if costs[0][j] < firstMin:
-                firstMin = costs[0][j]
-                minIndex = j
-        for j in range(n):                                                            #Find secondMin for the first house.
-            if j == minIndex:                                                         #When comes to the firstMin, ignore it.
-                continue
-            if costs[0][j] < secondMin:
-                secondMin = costs[0][j]
-        for i in range(1, m):
-            localMinIndex = -1                                                        #Record the index of min cost color except for the color used by last house.
-            localFirstMin = 0x7fffffff                                                #Record the min cost except for the color used by last house.
-            localSecondMin = 0x7fffffff                                               #Record the second min cost except for the color used by last house.
-            for j in range(n):                                                        #Find localMinIndex and localFirstMin for the current house.
-                if j == minIndex:                                                     #When comes to the last used color, ignore it.
-                    continue
-                if costs[i][j] < localFirstMin:
-                    localFirstMin = costs[i][j]
-                    localMinIndex = j
-            for j in range(n):                                                        #Find localFirstMin for the current house.
-                if j == minIndex or j == localMinIndex:                               #When comes to the last used coloe or localMinIndex, ignore it.
-                    continue
-                if costs[i][j] < localSecondMin:
-                    localSecondMin = costs[i][j]
-            temp = secondMin + costs[i][minIndex]                                     #Recoed the min cost if don't use the last used color on last house but use it on current house.
-            if temp < firstMin + localFirstMin:                                       #If temp is smaller than the min cost do not using last used color on current house, using last used color on current house is a better solution.
-                secondMin = firstMin + localFirstMin                                  #Update secondMin.
-                firstMin = temp                                                       #Update firstMin
-            else:                                                                     #Otherwise, using the min cost color except for the color used by last house is a better solution.
-                if temp < firstMin + localSecondMin:                                  #Determin which value is the new secondMin and update it.
-                    secondMin = temp
-                else:
-                    secondMin = firstMin + localSecondMin
-                firstMin += localFirstMin                                             #Update firstMin.
-                minIndex = localMinIndex                                              #Update minIndex.
-        return firstMin                                                               #Return firstMin.
+class Solution:
+    def minCostII(self, costs: List[List[int]]) -> int:                                                                             #Get n and k.
+        n, k = len(costs), len(costs[0])
+        minCost = [[0 for _ in range(k)]] + [[float('inf') for _ in range(k)] for _ in range(n)]                                    #Initialize a minCost matrix; minCost[i + 1][j] means the min cost so far until painting house i with color j. It has a dummy first row representing a virtual house.
+        firstMinCost, secondMinCost = 0, 0                                                                                          #Initialize the min cost and second min cost of painting previous house.
+        for i in range(1, n + 1):                                                                                                   #Traverse houses.
+            newFirstMinCost, newSecondMinCost = float('inf'), float('inf')                                                          #Initialize the min cost and second min cost of painting current house.
+            for j in range(k):                                                                                                      #Traverse each color.
+                minCost[i][j] = (secondMinCost if minCost[i - 1][j] == firstMinCost else firstMinCost) + costs[i - 1][j]            #If minCost[i - 1][j] == firstMinCost, we cannot paint current house with same color, so use secondMinCost; otherwise use firstMinCost. Then plus costs[i - 1][j] to paint color j. 
+                if minCost[i][j] <= newFirstMinCost:                                                                                #Update newFirstMinCost if necessary.
+                    newSecondMinCost = newFirstMinCost
+                    newFirstMinCost = minCost[i][j]
+                elif minCost[i][j] <= newSecondMinCost:                                                                             #Update newSecondMinCost if necessary.
+                    newSecondMinCost = minCost[i][j]
+            firstMinCost, secondMinCost = newFirstMinCost, newSecondMinCost                                                         #Replace firstMinCost and secondMinCost with newFirstMinCost and newSecondMinCost to go to next house.
+        return firstMinCost                                                                                                         #Return firstMinCost as it is the min cost of painting last house.
