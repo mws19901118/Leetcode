1568dadb26440bbd9803abd361abe1117b5a148f
Junsheng Wang <mws19901118@gmail.com>
2023-05-22 17:58:45 -0700

    Update Kth Largest Element in a Stream.py

--- Code/Kth Largest Element in a Stream.py
+++ Code/Kth Largest Element in a Stream.py
@@ -3,17 +3,14 @@ class KthLargest:
     def __init__(self, k: int, nums: List[int]):
         self.k = k                                #Store k.
         self.heap = []                            #Initialize a min heap.
-        for x in nums:                            #Push all value in nums to heap.
-            self.push(x)
-
-    def add(self, val: int) -> int:
-        self.push(val)                            #Push value to heap.
-        return self.heap[0]                       #Return heap top.
+        for x in nums:                            #Add all value in nums to heap.
+            self.add(x)
     
-    def push(self, val: int):                     #Insert value into heap and maintain heap length.
-        heapq.heappush(self.heap, val)
-        if len(self.heap) > self.k:
+    def add(self, val: int) -> int:
+        heapq.heappush(self.heap, val)            #Return heap top.
+        if len(self.heap) > self.k:               #Insert value into heap and maintain heap length.
             heapq.heappop(self.heap)
+        return self.heap[0]                       #Return heap top.
 
 # Your KthLargest object will be instantiated and called as such:
 # obj = KthLargest(k, nums)
