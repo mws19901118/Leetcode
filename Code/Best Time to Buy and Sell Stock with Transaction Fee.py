1a4f31151a3c7bde6385571a8833866126c32425
Junsheng Wang <mws19901118@gmail.com>
2023-06-21 19:55:22 -0700

    Update Best Time to Buy and Sell Stock with Transaction Fee.py

--- Code/Best Time to Buy and Sell Stock with Transaction Fee.py
+++ Code/Best Time to Buy and Sell Stock with Transaction Fee.py
@@ -1,7 +1,6 @@
 class Solution:
     def maxProfit(self, prices: List[int], fee: int) -> int:
-        sold, hold = 0, -prices[0]                      #Initialize the max cash we have sold stock or we currently hold stock for the first day.
-        for i in range(1, len(prices)):                 #Traverse prices[1:].
-            sold = max(sold, hold + prices[i] - fee)    #If we have sold stock, take the max of cash in 2 situations: we sell today or we sold before.
-            hold = max(hold, sold - prices[i])          #If we hold stock, take the max of cash in 2 situations: we buy stock or we bought before.
-        return sold                                     #At the end, we mush hold no stocks, so return sold.
+        sold, hold = 0, float('-inf')                                        #Initialize the max cash we have sold stock or we currently hold stock before traversing.
+        for x in prices:                                                     #Traverse prices.
+            sold, hold = max(sold, hold + x - fee), max(hold, sold - x)      #If we have sold stock, take the max of cash in 2 situations: we sell today or we sold before. If we hold stock, take the max of cash in 2 situations: we buy stock or we bought before.
+        return sold                                                          #At the end, we mush hold no stocks, so return sold.
