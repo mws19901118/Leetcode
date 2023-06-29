8924b37cdcdfd2a1a7728e4bb5fd59c5e898dcb6
Junsheng Wang <mws19901118@gmail.com>
2023-06-28 23:50:56 -0700

    Update Count Univalue Subtrees.py

--- Code/Count Univalue Subtrees.py
+++ Code/Count Univalue Subtrees.py
@@ -1,39 +1,17 @@
 # Definition for a binary tree node.
-# class TreeNode(object):
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
-
-class Solution(object):
-    def find(self, root):                                                                                     #Returen the number of univalue subtrees of current tree and if the current node is a root of univalue subtree.
-        if root is None:
-            return (0, False)
-        if root.left is None and root.right is None:                                                          #If current node is a leaf node, it has 1 univalue subtree and is the root of that univalue subtree.
-            return (1,True)
-        elif root.left is None and root.right is not None:                                                    #If current node doesn't have left child but has right child, get the result from right child.
-            r = self.find(root.right)
-            if root.val == root.right.val and r[1] is True:                                                   #If current value equals right child's value and right child is root of a univalue subtree, there is a new univalue subtree and current node is its root.
-                return (r[0] + 1, True)
-            else:                                                                                             #Otherwise, there is no new univalue subtree.
-                return (r[0], False)
-        elif root.left is not None and root.right is None:                                                    #If current node doesn't have right child but has left child, get the result from left child.
-            l = self.find(root.left)
-            if root.val == root.left.val and l[1] is True:                                                    #If current value equals left child's value and left child is root of a univalue subtree, there is a new univalue subtree and current node is its root.
-                return (l[0] + 1, True)
-            else:                                                                                             #Otherwise, there is no new univalue subtree.
-                return (l[0], False)
-        else:
-            r = self.find(root.right)                                                                         #If current node has both left child and right child, get results from both of them.
-            l = self.find(root.left)
-            if root.val == root.left.val and root.val == root.right.val and l[1] is True and r[1] is True:    #If current value equals both children's value and both children are root of a univalue subtreem there is a new univalue subtree and current node is its root.
-                return (l[0] + r[0] + 1, True)
-            else:                                                                                             #Otherwise, there is no new univalue subtree.
-                return (l[0] + r[0], False)
-    def countUnivalSubtrees(self, root):
-        """
-        :type root: TreeNode
-        :rtype: int
-        """
-        t = self.find(root)                                                                                 #Recursively find the count of univalue subtrees.
-        return t[0]
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def countUnivalSubtrees(self, root: Optional[TreeNode]) -> int:
+        def traverse(root: Optional[TreeNode]) -> (int, bool, int):                                                                            #Traverse tree, return the total univalue subtree count under root, if current substree is univalue and root value(could be none) as a tuple.
+            if not root:                                                                                                                       #If root is none, return (0, True, None) as we treat it as univalue but don't count it.
+                return 0, True, None
+            leftCount, leftIsUnivalue, leftValue = traverse(root.left)                                                                         #Traverse left child.
+            rightCount, rightIsUnivalue, rightValue = traverse(root.right)                                                                     #Traverse right child.
+            rootIsUnivalue = leftIsUnivalue and rightIsUnivalue and leftValue in [None, root.val] and rightValue in [None, root.val]           #Current substree is univalue if both left subtree and right subtree is univalue and their value is eother none or same as value of root.
+            return leftCount + rightCount + int(rootIsUnivalue), rootIsUnivalue, root.val                                                      #Return the sum of univalue subtree and if current substree is univalue and root value. 
+        
+        return traverse(root)[0]                                                                                                               #Return the total univalue subtree count from the result of traverse(root).
