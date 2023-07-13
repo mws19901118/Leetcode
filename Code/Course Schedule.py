04bfa088861877eaab8e847e6048f40b7603ac14
Junsheng Wang <mws19901118@gmail.com>
2023-07-12 18:52:22 -0700

    Update Course Schedule.py

--- Code/Course Schedule.py
+++ Code/Course Schedule.py
@@ -3,10 +3,9 @@ class Solution:
         adjacentList = defaultdict(list)
         indegree = [0 for x in range(numCourses)]
         for x, y in prerequisites:                                                  #Traverse prerequisites.
-            inDegree[y] += 1                                                        #Calculate the in-degree.
+            indegree[y] += 1                                                        #Calculate the in-degree.
             adjacentList[x].append(y)                                               #If x is a prerequisite of y, generate an edge pointing to x from y.
         q = [i for i in range(numCourses) if not indegree[i]]                       #Find all classes with no prerequisite.
-        count = 0                                                                   #Count classes that have finished all prerequisites.
         while q:                                                                    #BFS.
             newq = []
             for x in q:                                                             #For each class in queue, update the in-degree of its neighbors after traverse.
@@ -14,6 +13,5 @@ class Solution:
                     indegree[y] -= 1
                     if not indegree[y]:                                             #If neighbor has 0 in-degree, all prerequisites are finished, add it to newq.
                         newq.append(y)
-            count += len(q)                                                         #Add len(q) to count.
             q = newq                                                                #Replace q with newq.
-        return count == numCourses                                                  #Return if all classes have finished all prerequisites.
+        return all(not indegree[i] for i in range(numCourses))                      #Return if all classes have finished all prerequisites.
