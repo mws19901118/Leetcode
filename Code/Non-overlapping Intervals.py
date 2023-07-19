af0c87eaf71ccd961782160d27f0fc94dc6a5be9
Junsheng Wang <mws19901118@gmail.com>
2023-07-18 23:09:47 -0700

    Update Non-overlapping Intervals.py

--- Code/Non-overlapping Intervals.py
+++ Code/Non-overlapping Intervals.py
@@ -1,14 +1,11 @@
 class Solution:
     def eraseOverlapIntervals(self, intervals: List[List[int]]) -> int:
         intervals.sort()                                          #Sort intervals by beginnings.
-        count, i, j = 0, 0, 1                                     #Initalize count and 2 pointers.
-        while j < len(intervals):                                 #Traverse intervals until interval j reaches the end.
-            if intervals[j][1] <= intervals[i][1]:                #If interval j is fully covered by interval i, remove interval i and move interval i to interval j. 
+        count, end = 0, float('-inf')                             #Initialize count and current interval end.
+        for x, y in intervals:                                    #Traverse intervals.
+            if x < end:                                           #If x < end, there is a overlap, so increase count.
                 count += 1
-                i = j
-            elif intervals[j][0] < intervals[i][1]:               #Else if interval j's beginning is smaller than interval i's end, remove interval j.
-                count += 1
-            else:                                                 #Otherwise, move interval i to interval j.
-                i = j
-            j += 1                                                #Interval j goes to the next.
-        return count                                              #Return count.
+                end = min(end, y)                                 #And always take the interval with smaller end to reduce the chance of overlapping in the future.
+            else:                                                 #Otherwise just update end to y.
+                end = y
+        return count
