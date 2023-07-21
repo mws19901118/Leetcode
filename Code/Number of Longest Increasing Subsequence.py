d2e1182794f0e1fb9e730587e5cfd8c1fb565bc2
Junsheng Wang <mws19901118@gmail.com>
2023-07-20 22:34:28 -0700

    Update Number of Longest Increasing Subsequence.py

--- Code/Number of Longest Increasing Subsequence.py
+++ Code/Number of Longest Increasing Subsequence.py
@@ -1,15 +1,15 @@
 class Solution:
     def findNumberOfLIS(self, nums: List[int]) -> int:
-        if not nums:                                                                          #If nums is empty, return 0.
-            return 0
-        length, count = [1] * len(nums), [0] * len(nums)                                      #Initialize 2 lists for dynamic programming. One for longest increasing subsequence, the other one for its count.
-        for i, x in enumerate(nums):                                                          #Traverse through nums.
-            for j, y in enumerate(nums[:i]):                                                  #Calculete the longest increasing subsequence ending at i.
-                if y < x:                                                                     #If y < x, length[i] = max(length[i], length[j] + 1).
-                    length[i] = max(length[i], length[j] + 1)
-            for j, y in enumerate(nums[:i]):                                                  #Calculate the count of longest increasing subsequence ending at i.
-                if y < x and length[j] == length[i] - 1:                                      #If y < x and length[j] == length[i] - 1, there is a longest increasing subsequence ending at i which passes j.
-                    count[i] += count[j]                                                      #So add count[j] to count[i].
-            count[i] = max(count[i], 1)                                                       #count[i] should be at lease 1.
-        maxLength = max(length)                                                               #Find the longest increasing subsequence of entire nums.
-        return sum(count[i] for i in range(len(nums)) if length[i] == maxLength)              #Sum all the count of such longest increasing subsequences.
+        dp = [(0, 0)] * len(nums)                                    #Initialize dp list.
+        maxLength = 0                                                #Initialize max length.
+        for i, x in enumerate(nums):                                 #Traverse nums.
+            length, count = 1, 1                                     #Initialize the max length ending at current number and its count.
+            for j, y in enumerate(nums[:i]):                         #Traverse nums[:i].
+                if y < x:                                            #If y < x, we find at least one new increasing subsequence.
+                    if dp[j][0] + 1 > length:                        #If dp[j][0] + 1 > length, the new increaseing subsequence is longer than current max length.
+                        length, count = dp[j][0] + 1, dp[j][1]       #Update length and count.
+                    elif dp[j][0] + 1 == length:                     #If d[j][0] + 1 == length, directly add dp[j][1] to count.
+                        count += dp[j][1]
+            maxLength = max(length, maxLength)                       #Update maxLength.
+            dp[i] = (length, count)                                  #Set dp[i[ to (length, count).
+        return sum(c for l, c in dp if l == maxLength)               #Return the sum of count of subsequences whose length is maxLength.
