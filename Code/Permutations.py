d6b9dd6f019cba1e766a2caf93412f7446cace0d
Junsheng Wang <mws19901118@gmail.com>
2023-08-01 18:08:51 -0700

    Update Permutations.py

--- Code/Permutations.py
+++ Code/Permutations.py
@@ -1,26 +1,17 @@
 class Solution:
-    def backtrack(self, list, nums, flag, tlen, length, ans):
-        if tlen==length:
-            temp=[]
-            for i in list:
-                temp.append(i)                                              #If temporary length equals length of nums, copy list to temp.
-            ans.append(temp)
-        else:
-            l=len(nums)
-            for i in range(l):
-                if flag[i]==False:
-                    list.append(nums[i])
-                    tlen+=1
-                    flag[i]=True
-                    self.backtrack(list, nums, flag, tlen, length, ans)
-                    list.pop()
-                    tlen-=1
-                    flag[i]=False
-    # @param {integer[]} nums
-    # @return {integer[][]}
-    def permute(self, nums):
-        flag=[False]*len(nums)                                              #Record if current element is already taken by permulation.
-        list=[]
-        ans=[]                                                              #Record the possible combination.
-        self.backtrack(list, nums, flag, 0, len(nums), ans)                 #Backtrack.
-        return ans
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        stack, result = [], []                                                #Initialize stack and result.
+        masks = {i : 1 << (i + 1) for i in range(len(nums))}                  #Use bit masks to indicate if each number is already used.
+        def backtrack(state: int):                                            #Backtracking with a bit mask of all numbers used.
+            if len(stack) == len(nums):                                       #If length of stack is same as length of nums, we found a permutation, so append the copy of stack to result and return.
+                result.append(stack.copy())
+                return
+            for i, x in enumerate(nums):                                      #Traverse nums.
+                if state & masks[i]:                                          #If current number is already used, skip.
+                    continue
+                stack.append(x)                                               #Append x to stack.
+                backtrack(state | masks[i])                                   #Keep backtracking with updated state.
+                stack.pop()                                                   #Pop stack.
+        
+        backtrack(0)                                                          #Backtrack from no number used.
+        return result
