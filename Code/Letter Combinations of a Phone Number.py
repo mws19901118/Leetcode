e841284284f3d1222a63a2efbb7637a86cb57860
Junsheng Wang <mws19901118@gmail.com>
2023-08-02 17:28:04 -0700

    Update Letter Combinations of a Phone Number.py

--- Code/Letter Combinations of a Phone Number.py
+++ Code/Letter Combinations of a Phone Number.py
@@ -1,16 +1,15 @@
 class Solution:
-    def dfs(self, digitToLetter: dict, digits: str, result: List[str], stack: List[str]) -> None:
-        if not digits:                                                                                                          #If reaches the end of digits, join stack and append to result if stack is not empty, then return.
-            if stack:
-                result.append("".join(stack))
-            return
-        for x in digitToLetter[digits[0]]:                                                                                      #Traverse the corresponding letters of current digit.
-            stack.append(x)                                                                                                     #Append the letter to the stack.
-            self.dfs(digitToLetter, digits[1:], result, stack)                                                                  #Keep DFS next digit.
-            stack.pop()                                                                                                         #Pop stack.
-            
     def letterCombinations(self, digits: str) -> List[str]:
-        digitToLetter = {'2': 'abc', '3': 'def', '4': 'ghi', '5': 'jkl', '6': 'mno', '7': 'pqrs', '8': 'tuv', '9': 'wxyz'}      #Map digits to characters.
-        result=[]
-        self.dfs(digitToLetter, digits, result, [])                                                                             #Begin backtracking with index=h0.
-        return result                                                                                                           #Return result.
+        letterMap = {"2": "abc", "3": "def", "4": "ghi", "5": "jkl", "6": "mno", "7": "pqrs", "8": "tuv", "9": "wxyz"}        #Initialize mapping.
+        result, stack = [], []                                                                                                #Initialize result and stack.
+        def backtrack() -> None:                                                                                              #Backtracking.
+            if len(stack) == len(digits):                                                                                     #If all digits are mapped, backtrack comes to end. 
+                if len(digits) > 0:                                                                                           #If digits is not empty, join stack and append it to result.
+                    result.append("".join(stack))
+                return
+            for x in letterMap[digits[len(stack)]]:                                                                           #Traverse the mapping letters of current digit.
+                stack.append(x)                                                                                               #Append x to stack.
+                backtrack()                                                                                                   #Keep backtracking.
+                stack.pop()                                                                                                   #Pop stack.
+        backtrack()                                                                                                           #Start backtracking.
+        return result
