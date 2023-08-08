ff19a3222cc6cd5fb5b2d158e0806518271d6502
Junsheng Wang <mws19901118@gmail.com>
2023-08-07 19:57:51 -0700

    Update Encode and Decode Strings.py

--- Code/Encode and Decode Strings.py
+++ Code/Encode and Decode Strings.py
@@ -1,37 +1,23 @@
 class Codec:
-
-    def encode(self, strs):
+    def encode(self, strs: List[str]) -> str:
         """Encodes a list of strings to a single string.
-        
-        :type strs: List[str]
-        :rtype: str
         """
-        result = ""
-        for s in strs:
-            l = len(s)
-            result += str(l)                                #Put the length of each string with an additional space in front of each string as the delimiter.
-            result += " "
-            result += s
-        return result
-        
-    def decode(self, s):
+        return "".join(str(len(x)) + "#" + x for x in strs)            #Append length of each string and a delimiter '#' infront of each string as prefix and join them together.
+
+    def decode(self, s: str) -> List[str]:
         """Decodes a single string to a list of strings.
-        
-        :type s: str
-        :rtype: List[str]
         """
-        result = []
+        result = []                                                    #Initialize result.
         i = 0
-        l = len(s)
-        while i < l:
+        while i < len(s):                                              #Use 2 pointers to traverse s.
             j = i
-            while j < l and s[j] != ' ':                    #Get the length of each string.
+            while j < len(s) and s[j].isnumeric():
                 j += 1
-            t = int(s[i:j])
-            j += 1                                          #Jump over the additional space.
-            result.append(s[j:j + t])                       #Append the target string to result list according to its length.
-            i = j + t
-        return result
+            length = int(s[i:j])                                       #Get the length of current string.
+            result.append(s[j + 1:j + 1 + length])                     #Extract the word and append it to result.
+            i = j + 1 + length                                         #Move i to next string prefix.
+        return result                                                  #Return result.
+
 
 # Your Codec object will be instantiated and called as such:
 # codec = Codec()
