9999d53c71f30982f41deecd945275497c87e96c
Junsheng Wang <mws19901118@gmail.com>
2023-08-07 21:20:30 -0700

    Update Search in Rotated Sorted Array.py

--- Code/Search in Rotated Sorted Array.py
+++ Code/Search in Rotated Sorted Array.py
@@ -3,21 +3,10 @@ class Solution:
         start, end = 0, len(nums) - 1
         while start <= end:
             mid = (start + end) // 2
-            if nums[mid] == target:
+            if nums[mid] == target:                                                                                        #If found target, return mid.
                 return mid
-            if nums[mid] >= nums[start] and nums[mid] > nums[end]:          #If nums[mid] >= nums[start] and nums[mid] > nums[end], the rotate pivot is the right side of mid.
-                if nums[mid] > target and target >= nums[start]:            #If nums[start] <= target < nums[mid], target is in the left side of mid; otherwise, target is in the right side of mid.
-                    end = mid - 1
-                else:
-                    start = mid + 1
-            elif nums[mid] < nums[start] and nums[mid] <= nums[end]:        #If nums[mid] < nums[start] and nums[mid] <= nums[end], the rotate pivot is the left side of mid.
-                if nums[mid] < target and target <= nums[end]:              #If nums[mid] < target <= nums[end], target is in the right side of mid; otherwise, target is in the left side of mid.
-                    start = mid + 1
-                else:
-                    end = mid - 1
-            elif nums[mid] >= nums[start] and nums[mid] <= nums[end]:       #If nums[start] <= nums[mid] <= nums[end], it's a normal binary search.
-                if nums[mid] < target:
-                    start = mid + 1
-                else:
-                    end = mid - 1
-        return -1                                                           #If can not find target, return -1.
+            if nums[0] <= nums[mid] < target or nums[mid] < target <= nums[-1] or target < nums[0] <= nums[mid]:           #1. If nums[mid] is smaller than target and both nums[mid] and target is on same slope, target is in the second half(between mid and end).
+                start = mid + 1                                                                                            #2. If nums[mid] is greter than target and nums[mid] is before pivot and target is after pivot, target is in the second half(between mid and end).
+            else:                                                                                                          #Otherwise, target is in the first half(between start and mid).
+                end = mid - 1
+        return -1                                                                                                          #If can not find target, return -1.
