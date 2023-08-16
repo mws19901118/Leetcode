c07081ec7e4f25945e9ae695370a7ca163c33cf8
Junsheng Wang <mws19901118@gmail.com>
2023-08-15 19:42:17 -0700

    Update Sliding Window Maximum.py

--- Code/Sliding Window Maximum.py
+++ Code/Sliding Window Maximum.py
@@ -1,13 +1,13 @@
 class Solution:
     def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
         result = []
-        dq = deque()                                            #Initalize a deque, which contains all possible numbers to be sliding window max and is sorted in non-ascending order.
-        for i, x in enumerate(nums):                            #Traverse through nums.
-            if i >= k and nums[i - k] == dq[0]:                 #If i >= k and nums[i - k] eqauls the current max, pop it from left.
+        dq = deque()                                            #Initalize a deque, which contains indexes of all possible numbers to be sliding window max and is sorted in non-ascending order.
+        for i, x in enumerate(nums):                            #Traverse nums.
+            if dq and dq[0] <= i - k:                           #If dq is not empty and the first index is out of sliding window, pop it from left.
                 dq.popleft()
-            while dq and dq[-1] < x:                            #While dq is not empty and the tail of dq is smaller than current new number, pop tail of dq.
+            while dq and nums[dq[-1]] <= x:                     #While dq is not empty and the number on last index of dq is smaller than current new number, pop tail of dq.
                 dq.pop()
-            dq.append(x)                                        #Append current new number to dq.
-            if i >= k - 1:                                      #If i >= k - 1, append current max to result.
-                result.append(dq[0])
+            dq.append(i)                                        #Append current index to dq.
+            if i >= k - 1:                                      #If i >= k - 1, append current max, which is the number on first index, to result.
+                result.append(nums[dq[0]])
         return result
