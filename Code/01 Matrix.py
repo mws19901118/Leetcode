5b7c6489b929c395e70a7120e3a6d8f47c0832fb
Junsheng Wang <mws19901118@gmail.com>
2023-08-16 18:00:58 -0700

    Update 01 Matrix.py

--- Code/01 Matrix.py
+++ Code/01 Matrix.py
@@ -3,15 +3,14 @@ class Solution:
         m, n = len(mat), len(mat[0])                                                                                                          #Get demensions of mat.
         distance = [[0 for _ in range(n)] for _ in range(m)]                                                                                  #Create distance matrix with same dimensions.
         q = []                                                                                                                                #Initialize queue.
-        for i in range(m):                                                                                                                    #Traverse mat.
-            for j in range(n):
-                if mat[i][j] == 0:
-                    continue
-                if any(0 <= x < m and 0 <= y < n and mat[x][y] == 0 for x, y in [(i + 1, j), (i, j + 1), (i - 1, j), (i, j - 1)]):            #If current cell is 1 and has neighbor who is 0, add current cell to q and set its distance to be 1.
-                    q.append((i, j))
-                    distance[i][j] = 1
-                else:                                                                                                                         #If current cell is 1 and has no neighbors who is 0, set current cell to be -1.
-                    distance[i][j] = -1
+        for i, j in product(range(m), range(n)):                                                                                              #Traverse mat.
+            if mat[i][j] == 0:
+                continue
+            if any(0 <= x < m and 0 <= y < n and mat[x][y] == 0 for x, y in [(i + 1, j), (i, j + 1), (i - 1, j), (i, j - 1)]):                #If current cell is 1 and has neighbor who is 0, add current cell to q and set its distance to be 1.
+                q.append((i, j))
+                distance[i][j] = 1
+            else:                                                                                                                             #If current cell is 1 and has no neighbors who is 0, set current cell to be -1.
+                distance[i][j] = -1
         while q:                                                                                                                              #BFS.
             newq = []                                                                                                                         #Initialize new queue.
             for i, j in q:                                                                                                                    #For each cell in q, traverse the 4 neighbors.
