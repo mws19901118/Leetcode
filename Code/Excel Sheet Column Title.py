ec6fdbb063e6e20f69687d63666f3900cd06526e
Junsheng Wang <mws19901118@gmail.com>
2023-08-22 00:47:30 -0700

    Update Excel Sheet Column Title.py

--- Code/Excel Sheet Column Title.py
+++ Code/Excel Sheet Column Title.py
@@ -1,20 +1,8 @@
 class Solution:
-    # @return a string
-    def convertToTitle(self, num):
-        ans=''
-        a=[0]
-        for i in range(14):
-            a.append((a[-1]+1)*26)                      #Generate the form of amount of column titles of which length is no longer than the value of corresponding index.  
-        num=num-1                                       #Begin with 0, not 1.
-        i=0
-        while num>=a[i]:                                #Find the length of column title of current number.
-            i=i+1
-        i=i-1
-        temp=0
-        while i>=1:                                     #Deal with the situation when length is larger than 1(prevent index out of bounds).
-            temp=(num-a[i])/(a[i]-a[i-1])               #Calculate the correspoding digit of character, which should be the answer of num divided by the amount of column titles of which length is exactly current length minus 1.
-            ans=ans+chr(ord('A')+temp)                  #Transform between digit and ascii.
-            num=num-(temp+1)*(a[i]-a[i-1])              #Iterate, let num be the remaining number.
-            i=i-1                                       #Shorten to calculate the next character.
-        ans=ans+chr(ord('A')+num)                       #Deal with the situation when length equals 1.
-        return ans
+    def convertToTitle(self, columnNumber: int) -> str:
+        title = ''                                                #Initialize title.
+        while columnNumber:                                       #Iterate while columnNumber is greater than 0.
+            columnNumber -= 1                                     #Decrease column number to handle carry; because for example from 'Z' to 'AA', it increase 1 but looks like 2(analogy to '9' and '11').
+            title += chr((columnNumber) % 26 + ord('A'))          #Convert like converting base 10 number to base 26 number.
+            columnNumber //= 26
+        return title[::-1]                                        #Return the reverse of title.
