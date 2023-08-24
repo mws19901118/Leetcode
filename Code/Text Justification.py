7dea845ee5e5b6a17e88d73d419c6b59d54a2de9
Junsheng Wang <mws19901118@gmail.com>
2023-08-23 20:45:40 -0700

    Update Text Justification.py

--- Code/Text Justification.py
+++ Code/Text Justification.py
@@ -1,49 +1,24 @@
 class Solution:
-    # @param words, a list of strings
-    # @param L, an integer
-    # @return a list of strings
-    def fullJustify(self, words, L):
-        result=[]
-        if words==[] or L==0:                                             #Deal with this special situation.
-            result.append("")
-            return result
-        length=len(words)
-        i=0
-        j=0
-        while i+j<length:
-            clength=0                                                     #Record the length of current line.
-            cline=[]                                                      #Record the current line.
-            while i+j<length and clength+len(words[i+j])<=L:
-                cline.append(words[i+j])                                  #Traverse forward to add suitable words for current line and update clength.
-                clength+=len(words[i+j])+1
-                j+=1
-            no=len(cline)                                                 #Calculate the number of words in current line.
-            clength-=no                                                   #Calculate the number of charactors without spaces.
-            str=""
-            if i+j==length:                                               #If current line is the last line, add one space between every 2 adjacent words, also add spaces at the end to make up a string whose length is L.
-                for k in range(i,length-1):
-                    str+=words[k]+" "
-                str+=words[-1]
-                while len(str)<L:
-                    str+=" "
-                result.append(str)
+    def fullJustify(self, words: List[str], maxWidth: int) -> List[str]:
+        result = []                                                                        #Initialize result.
+        i  = 0
+        while i < len(words):                                                              #Traverse words with 2 pointers.
+            j = i
+            length = 0                                                                     #Initialize sum of words length on current line.
+            while j < len(words) and length + j - i + len(words[j]) <= maxWidth:           #While words[j] can be added to current line with minimum white spaces, add words[j] to current line.
+                length += len(words[j])
+                j += 1
+            if j == len(words):                                                            #If j reaches the end of words, it is the last line.
+                line = " ".join(words[i:])                                                 #Join words[i:] by white space.
+                result.append(line + " " * (maxWidth - len(line)))                         #Add extra white spaces at the end to make it maxWidth then append line to result.
+            elif j == i + 1:                                                               #If current line only has one word, add extra white spaces after words[i] to make it maxWidth then append line to result.
+                result.append(words[i] + " " * (maxWidth - len(words[i])))
             else:
-                if no==1:                                                 #If there is only one word in current line, add spaces at the end to make up a string whose length is L.
-                    str+=cline[0]
-                    for k in range(clength,L):
-                        str+=" "
-                    result.append(str)
-                else:                                                     #If current line is not the last line and contains more than one word, do the followings.
-                    a=(L-clength)/(no-1)                                  #Caculate the min spaces between 2 words.
-                    b=(L-clength)%(no-1)                                  #The first b intevals should contain a+1 spaces.
-                    for k in range(no-1):
-                        str+=cline[k]
-                        for t in range(a):
-                            str+=" "
-                        if k<b:
-                            str+=" "
-                    str+=cline[-1]
-                    result.append(str)
-            i+=j                                                          #Clear j.
-            j=0
+                gap, cou t = divmod(maxWidth - length, j - i - 1)                          #Calulate the min length of gap and how many gaps will have one extra white space to distribute white spaces as evenly as possible.
+                line = ""
+                for k in range(i, j - 1):                                                  #Traverse words[i:j - 1].
+                    line += words[k]                                                       #Append words[k] to line.
+                    line += " " * (gap + (1 if k < i + count else 0))                      #Append needed white spaces(at least gap, and for the first count slots, add one more extra white space).
+                result.append(line + words[j - 1])                                         #Append words[j - 1] then append line to result.
+            i = j
         return result
