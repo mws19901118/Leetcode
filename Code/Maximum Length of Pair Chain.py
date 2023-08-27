267ff83363faa43b7dd00a403ccd4619ef019e93
Junsheng Wang <mws19901118@gmail.com>
2023-08-26 18:56:07 -0700

    Update Maximum Length of Pair Chain.py

--- Code/Maximum Length of Pair Chain.py
+++ Code/Maximum Length of Pair Chain.py
@@ -1,11 +1,11 @@
 class Solution:
     def findLongestChain(self, pairs: List[List[int]]) -> int:
         pairs.sort(key = lambda x: x[1])      #Sort by end value in asending order.
-        curr = float('-inf')                  #Initialize current ending.
+        curr = float('-inf')                  #Initialize current max ending.
         result = 0
 
         for a, b in pairs:                    #Traverse pairs.
-            if a > curr:                      #If starting is greater than current ending, we can add current pair to chain.
+            if a > curr:                      #If starting is greater than current ending, because current ending is smaller than any ending after it, adding current pair to chain is optimal.
                 result += 1                   #Increase result.
-                curr = b                      #Update current ending.
+                curr = b                      #Update current max ending.
         return result
