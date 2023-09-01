752ac00bea40be1e3794d80aa7b2be58c0a548f6
Junsheng Wang <mws19901118@gmail.com>
2023-08-31 20:37:22 -0700

    Update Counting Bits.py

--- Code/Counting Bits.py
+++ Code/Counting Bits.py
@@ -1,13 +1,9 @@
-class Solution(object):
-    def countBits(self, num):
-        """
-        :type num: int
-        :rtype: List[int]
-        """
-        result = [0]                                  #Initially, there is no '1' in 0.
-        i = 1                                         #Iterate every power of 2, starting from 1.
-        while i <= num:
-            for j in range(i, min(2 * i, num + 1)):   #For every number j from i to min(2 * i, num + 1), result[j] = result[j - i] + 1, becase of the '1' on the most significant bit.
-                result.append(result[j - i] + 1)
-            i *= 2                                    #Go to the next power of 2.
-        return result
+class Solution:
+    def countBits(self, n: int) -> List[int]:
+        ans = [0] * (n + 1)                            #Initialize ans with n + 1 zeros.
+        mask = 0                                       #Use this mask to take all bits except the mostsignificant bit; initially it's 0.
+        for i in range(1, n + 1):                      #Traverse from 1 to n.
+            ans[i] = ans[i & mask] + 1                 #The most significant bit of i is 1, so add the count of 1s in the rest then plus 1.
+            if i & mask == mask:                       #If the rest is same as mask, it's time to move to a longer binary.
+                mask = mask << 1 | 1                   #So we need to update mask as well, shift left 1 bit and or witth 1.
+        return ans
