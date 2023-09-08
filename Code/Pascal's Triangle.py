80d2ad2fadc73f90b76e6c7832e8280f2b59e754
Junsheng Wang <mws19901118@gmail.com>
2023-09-07 17:55:22 -0700

    Update Pascal's Triangle.py

--- Code/Pascal's Triangle.py
+++ Code/Pascal's Triangle.py
@@ -1,9 +1,6 @@
 class Solution:
     def generate(self, numRows: int) -> List[List[int]]:
-        p = [[1]]                                               #Initialize first row.
-        for i in range(1, numRows):                             #Generate remaining row.
-            p.append([1])                                       #Append the leftmost 1.
-            for j in range(1, i):                               #Generate numbers in the middle.
-                p[-1].append(p[i - 1][j - 1] + p[i - 1][j])
-            p[-1].append(1)                                     #Append the rightmost 1.
-        return p
+        result = [[1]]                                                                                                #Initialize result.
+        for _ in range(numRows - 1):                                                                                  #Iterate numRows - 1 times.
+            result.append([1] + [result[-1][i] + result[-1][i + 1] for i in range(len(result[-1]) - 1)] + [1])        #Generate next row and append it to result.
+        return result
