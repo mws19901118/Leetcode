e32f8331055f31a59b140c8cce25eb70e0546e5d
Junsheng Wang <mws19901118@gmail.com>
2023-09-17 18:57:35 -0700

    Update The K Weakest Rows in a Matrix.py

--- Code/The K Weakest Rows in a Matrix.py
+++ Code/The K Weakest Rows in a Matrix.py
@@ -1,3 +1,3 @@
 class Solution:
     def kWeakestRows(self, mat: List[List[int]], k: int) -> List[int]:
-        return [sorted([(row.count(1), i) for i, row in enumerate(mat)])[i][1] for i in range(k)]   #Sort the matrix row indexes by the number of soldiers and then row indexes, then get the first K indexes.
+        return [index for count, index in sorted([(row.count(1), i) for i, row in enumerate(mat)])[:k]]   #Sort the matrix row indexes by the number of soldiers and then row indexes, then get the first K indexes.
