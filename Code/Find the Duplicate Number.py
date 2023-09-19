cddb890313d4f0e1d14c02d30e747f73fb68bfaf
Junsheng Wang <mws19901118@gmail.com>
2023-09-19 09:13:34 -0700

    Update Find the Duplicate Number.py

--- Code/Find the Duplicate Number.py
+++ Code/Find the Duplicate Number.py
@@ -1,11 +1,15 @@
 class Solution:
     def findDuplicate(self, nums: List[int]) -> int:
-        start, end = 1, len(nums) - 1                           #The numbers ranges from 1 to n.
-        while start < end:                                      #Binary search for the answer in 1 to n.
-            mid = (start + end) // 2
-            count = sum(x <= mid for x in nums)                 #Count how many numbers in nums are smaller than or equal to mid.
-            if count <= mid:                                    #If the count is no larger than mid, there can not be any duplicates number from 1 to mid, binary search in mid+1 to n.
-                start = mid + 1
-            else:                                               #Otherwise, the duplicates number must be between 1 and mid, binary search in 1 to mid.
-                end = mid
-        return start
+        fast = slow = nums[0]            #https://en.wikipedia.org/wiki/Cycle_detection#Tortoise_and_hare
+        while True:
+            slow = nums[slow]
+            fast = nums[nums[fast]]
+            if fast == slow:
+                break
+        
+        slow = nums[0]
+        while slow != fast:
+            slow = nums[slow]
+            fast = nums[fast]
+        
+        return fast
