8824d014e5657ca6d89b324856aff7c8394d7c84
Junsheng Wang <mws19901118@gmail.com>
2023-09-19 23:31:15 -0700

    Update Minimum Operations to Reduce X to Zero.py

--- Code/Minimum Operations to Reduce X to Zero.py
+++ Code/Minimum Operations to Reduce X to Zero.py
@@ -1,17 +1,10 @@
 class Solution:
     def minOperations(self, nums: List[int], x: int) -> int:
-        positions = {0: 0}                                                    #The nums list can be divided to three consecutive parts, A, B and C(A, B and C can be empty).
-        cumulativeSum = 0                                                     #Finding minimum operations equals to find the min(len(A) + len(C)) which meets the condition that sum(A) + sum(C) = x.
-        for i, n in enumerate(nums):
-            cumulativeSum += n                                                #Calculate cumulative sum.
-            positions[cumulativeSum] = i + 1                                  #Store cumulative sum in dict as key and value is index of position starting at 1.
-        operations = len(nums) + 1                                            #Initialize minimum operations to be len(nums) + 1.
-        s, i = 0, 0                                                           #Initialize s to be cumulative sum and i to be the index of position from behind.
-        while s <= x and i <= len(nums):                                      #Start traverse nums from behind until s > x or i > len(nums).
-            gap = x - s                                                       #Get the gap between x and s.
-            if gap in positions and positions[gap] - 1 <= len(nums) - i:      #If gap in positions and position[gap] hasn't reach i, i.e. A and C has no intersection, it's a valid case.
-                operations = min(operations, positions[gap] + i)              #Update operations.
-            i += 1                                                            #Increase i.
-            s += nums[-min(i, len(nums))]                                     #Increase s if i is valid.
-        operations = -1 if operations == len(nums) + 1 else operations        #Check if x can be reduced to 0; if not, set operations to -1.
-        return operations                                                     #Return operations.
+        prefixSumMap = {0: -1}                                                                #Store prefix sum and its index, initially it is 0 at -1.
+        result, prefixSum, target = len(nums) + 1, 0, sum(nums) - x                           #Initialize result and prefixSum as well as target. Since we want to find the minimum x + y so that nums[:x] + nums[-y:] == x, it is same as find the longest subarray which sums up at sum(nums) - x.
+        for i, x in enumerate(nums):                                                          #Traverse nums.
+            prefixSum += x                                                                    #Updte prefixSum.
+            prefixSumMap[prefixSum] = i                                                       #Set the index of current prefixSum in map.
+            if prefixSum - target in prefixSumMap:                                            #If prefixSum - target is in map, we found a subarray that satisfies the requirement and its length is i - prefixSumMap[prefixSum - target].
+                result = min(result, len(nums) - i + prefixSumMap[prefixSum - target])        #Then update result if len(nums) - (i - prefixSumMap[prefixSum - target]) is smaller.
+        return result if result < len(nums) + 1 else -1                                       #Return -1 if result is still len(nums) + 1 as we cannot find such subarray; otherwise, return result.
