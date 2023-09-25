9e2f9ef1ea2c07f964f4e6944fa4d92238f6bc75
Junsheng Wang <mws19901118@gmail.com>
2023-09-24 19:18:54 -0700

    Update Find the Difference.py

--- Code/Find the Difference.py
+++ Code/Find the Difference.py
@@ -1,6 +1,4 @@
 class Solution:
     def findTheDifference(self, s: str, t: str) -> str:
         counts, countt = Counter(s), Counter(t)                 #Count letters in s and t.
-        for c in countt:                                        #If any letter is only in countt or its count in s is smaller than its count in t, return that letter.
-            if c not in counts or countt[c] > counts[c]:
-                return c
+        return [x for x in countT if countT[x] - countS[x]][0]  #Return the only letter that has fewer count in countS than in countT.
