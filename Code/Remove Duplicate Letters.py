5e59b39d850e5237cbb0c7c99e7969481c251bc0
Junsheng Wang <mws19901118@gmail.com>
2023-09-25 23:36:43 -0700

    Update Remove Duplicate Letters.py

--- Code/Remove Duplicate Letters.py
+++ Code/Remove Duplicate Letters.py
@@ -7,7 +7,7 @@ class Solution:
             count[c] -= 1                                                   #Decrease the count by 1.
             if c in result:                                                 #If current letter is already in the set, ignore it.
                 continue
-            while stack != [] and stack[-1] > c and count[stack[-1]] > 0:   #If current letter is smaller than the top of stack(stack is not empty) and there is stack top letter behind current letter, pop stack and remove it from result set.
+            while stack and stack[-1] > c and count[stack[-1]] > 0:         #If current letter is smaller than the top of stack(stack is not empty) and there is stack top letter behind current letter, pop stack and remove it from result set.
                 result.remove(stack.pop())
             result.add(c)                                                   #Add current letter to result.
             stack.append(c)                                                 #Push current letter to stack.
