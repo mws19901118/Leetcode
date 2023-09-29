92d83329733179c8067d43e9236bda04c9b7d30c
Junsheng Wang <mws19901118@gmail.com>
2023-09-28 17:03:46 -0700

    Update Monotonic Array.py

--- Code/Monotonic Array.py
+++ Code/Monotonic Array.py
@@ -1,13 +1,3 @@
 class Solution:
-    def isMonotonic(self, A):
-        """
-        :type A: List[int]
-        :rtype: bool
-        """
-        increase, decrease = True, True   #Initialy, it's both monotone increasing and monotone decreasing.
-        for i in range(1, len(A)):
-            if A[i] > A[i - 1]:           #If current number is larger than the previous one, it's no longer monotone decreasing.
-                decrease = False
-            elif A[i] < A[i - 1]:         #If current number is smaller than the previous one, it's no longer monotone increasing.
-                increase = False
-        return increase or decrease
+    def isMonotonic(self, nums: List[int]) -> bool:
+        return all(nums[i] <= nums[i + 1] for i in range(len(nums) - 1)) or all(nums[i] >= nums[i + 1] for i in range(len(nums) - 1))        #Return if array is either monotone incresing or monotone decresing.
