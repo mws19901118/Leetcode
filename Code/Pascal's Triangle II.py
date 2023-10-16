622c18b19e34fd18529761cb5741b09d00659e00
Junsheng Wang <mws19901118@gmail.com>
2023-10-15 17:08:31 -0700

    Update Pascal's Triangle II.py

--- Code/Pascal's Triangle II.py
+++ Code/Pascal's Triangle II.py
@@ -1,8 +1,6 @@
 class Solution:
     def getRow(self, rowIndex: int) -> List[int]:
         result = [1]                                                            #Initially, the left end is 1.
-        for i in range(rowIndex // 2):
-            result.append(result[-1] * (rowIndex - i) // (i + 1))               #Generate the binomial coefficient from left to middle.
-        for i in range((rowIndex + 1) // 2):
-            result.append(result[rowIndex // 2 - i - (rowIndex + 1) % 2])       #Cope the left half reversely to the right half.
+        or i in range(rowIndex):                                                #Generate the binomial coefficient from left to right.
+            result.append(result[-1] * (rowIndex - i) // (i + 1))
         return result
