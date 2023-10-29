7c004dc758a25b4bb23c0591680ad691104fb0c2
Junsheng Wang <mws19901118@gmail.com>
2023-10-28 23:37:59 -0700

    Update Poor Pigs.py

--- Code/Poor Pigs.py
+++ Code/Poor Pigs.py
@@ -1,3 +1,3 @@
 class Solution:
     def poorPigs(self, buckets: int, minutesToDie: int, minutesToTest: int) -> int:
-        return ceil(math.log(buckets, minutesToTest // minutesToDie + 1)) #Each pig can take T tests and have (T + 1) states. So, calculate minimum x such that (T+1) ^ x >= buckets. T = minutesToTest // minutesToDie
+        return ceil(math.log(buckets) / math.log(minutesToTest // minutesToDie + 1))  #Each pig can take T tests and have (T + 1) states. So, calculate minimum x such that (T+1) ^ x >= buckets. T = minutesToTest // minutesToDie
