ae0108a6aed0a399f3733e2ea5e8b02893763568
Junsheng Wang <mws19901118@gmail.com>
2023-11-11 18:19:04 -0800

    Update Bus Routes.py

--- Code/Bus Routes.py
+++ Code/Bus Routes.py
@@ -1,30 +1,28 @@
 class Solution:
-    def buildGraph(self, routes):                                                         #Build graph.
-        stopSetByRoutes = [set(r) for r in routes]                                        #Convert routes from list of list to list of set.
-        graph = [set() for i in range(len(routes))]                                       #Initialize graph to be a list of set.
-        for i in range(len(routes)):                                                      #For each pair of routes, if their stop set has intersection, meaning the 2 routes are connected, so, add the index of route to each other's graph set.
-            for j in range(i + 1, len(routes)):
-                if stopSetByRoutes[i] & stopSetByRoutes[j]:
-                    graph[i].add(j)
-                    graph[j].add(i)
-        return stopSetByRoutes, graph
-    
-    def numBusesToDestination(self, routes: List[List[int]], S: int, T: int) -> int:
-        if S == T:                                                                        #If S and T is same, return 0.
+    def numBusesToDestination(self, routes: List[List[int]], source: int, target: int) -> int:
+        if source == target:                                                  #If source and target is same, no need to take any bus.
             return 0
-        stopSetByRoutes, graph = self.buildGraph(routes)                                  #Build graph.
-        q = set([i for i, stopSet in enumerate(stopSetByRoutes) if S in stopSet])         #Initialize q to be the set of routes which has S.
-        visited = set()                                                                   #Initialize visied to be an empty set.
-        count = 1                                                                         #Initialize count to be 1.
-        while q:                                                                          #While we have routes to take, do iteration.
-            visited |= q                                                                  #Add all routes in this iteration to visited.
-            nextq = set()                                                                 #Initialize routes to take for next iteration.
-            for route in q:                                                               #For each route, if T in its stop set, return count.
-                if T in stopSetByRoutes[route]:
+        stops, adjacentList = defaultdict(set), defaultdict(list)             #Initialize the routes of each stop, and the adjacent list of routes.
+        for i, r in enumerate(routes):                                        #Traverse routes.
+            for x in r:                                                       #Traverse stops in current route.
+                for j in stops[x]:                                            #For all routes already exist for current stop, build an edge between it and current route.
+                    adjacentList[i].append(j)
+                    adjacentList[j].append(i)
+                stops[x].add(i)                                               #Add i to stops[x].
+        if source not in stops or target not in stops:                        #If either source and target not in stops, return -1.
+            return -1
+        count = 1                                                             #Initialize count.
+        q = stops[source]                                                     #Get the initial routes at source.
+        visited = set(q)                                                      #Initialize a set for visited routes.
+        while q:                                                              #BFS.
+            newq = []                                                         #Initialize new queue.
+            for x in q:                                                       #Traverse queue.
+                if x in stops[target]:                                        #If current route can reach target, return count.
                     return count
-                for r in graph[route]:                                                    #Add all connected routes to nextq if not in visited.
-                    if r not in visited:
-                        nextq.add(r)
-            q = nextq                                                                     #Update q to be nextq.
-            count += 1                                                                    #Increase count by 1.
-        return -1                                                                         #Return -1 if we cannot reach T.
+                for y in adjacentList[x]:                                     #Traverse the adjacent list of current route.
+                    if y not in visited:                                      #If y is not vistied, add y to visited and append y to newq.
+                        visited.add(y)
+                        newq.append(y)
+            q = newq                                                          #Replace q with newq.
+            count += 1                                                        #Increase count.
+        return -1                                                             #Return -1 if cannot reach target.
