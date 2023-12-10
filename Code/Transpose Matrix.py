b1b119af0b1f33a9b5aedde9bd0b5fa4c7b43bb3
Junsheng Wang <mws19901118@gmail.com>
2023-12-09 16:31:16 -0800

    Update Transpose Matrix.py

--- Code/Transpose Matrix.py
+++ Code/Transpose Matrix.py
@@ -1,3 +1,3 @@
 class Solution:
     def transpose(self, matrix: List[List[int]]) -> List[List[int]]:
-        return [[matrix[i][j] for i in range(len(matrix))] for j in range(len(matrix[0]))]      #Transpose matrix.
+        return list(map(list, zip(*matrix)))      #Flatten matrix to each row and traverse each row simultaneously to convert each column to row and then return as a list of list.
