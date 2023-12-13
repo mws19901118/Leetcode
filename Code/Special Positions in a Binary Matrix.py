66906fea2ca3e3480276376cfc9abc3e360ee9f9
Junsheng Wang <mws19901118@gmail.com>
2023-12-12 21:09:38 -0800

    Create Special Positions in a Binary Matrix.py

---
+++ Code/Special Positions in a Binary Matrix.py
@@ -0,0 +1,4 @@
+class Solution:
+    def numSpecial(self, mat: List[List[int]]) -> int:
+        row, col = [sum(x) for x in mat], [sum(x) for x in zip(*mat)]                                                                  #Count the 1 of each row and column.
+        return sum(int(mat[i][j] == 1 and row[i] == 1 and col[j] == 1) for i, j in product(range(len(row)), range(len(col))))          #Traverse each cell, return the number of cells which is 1 and corresponding row and column both only has one 1.
