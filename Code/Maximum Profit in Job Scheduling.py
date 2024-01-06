c247f92430a1aee5901a5c1c6aafdab3ca59e93d
Junsheng Wang <mws19901118@gmail.com>
2024-01-06 01:07:08 -0800

    Update Maximum Profit in Job Scheduling.py

--- Code/Maximum Profit in Job Scheduling.py
+++ Code/Maximum Profit in Job Scheduling.py
@@ -1,9 +1,8 @@
 class Solution:
     def jobScheduling(self, startTime: List[int], endTime: List[int], profit: List[int]) -> int:
-        sortedJobs = sorted([(s, e, p) for s, e, p in zip(startTime, endTime, profit)])     #Sort jobs by starting time.
-        sortedStartTime = [x[0] for x in sortedJobs]                                        #Separate out the list of sorted start time for binary search.
+        jobs = sorted([(s, e, p) for s, e, p in zip(startTime, endTime, profit)])           #Sort jobs by starting time.
         dp = [0] * (len(sortedJobs) + 1)                                                    #Initialize dp array, dp[i] means the max profit of scheduling jobs in sortedJobs[i:].
-        for i in reversed(range(len(sortedJobs))):                                          #Traverse sortedJobs backward.
-            index = bisect_left(sortedStartTime, sortedJobs[i][1])                          #Find the index of first job whose start time is larger than or equal to the end time of current job. 
-            dp[i] = max(sortedJobs[i][2] + dp[index], dp[i + 1])                            #dp[i] is the max of dp[i + 1] and dp[index] plus profit of current job.
+        for i, (s, e, p) in enumerate(reversed(jobs)):                                      #Traverse sortedJobs backward.
+            index = bisect_left(jobs, (e, -1, -1))                                          #Find the index of first job whose start time is larger than or equal to the end time of current job. 
+            dp[-(i + 2)] = max(p + dp[index], dp[-(i + 1)])                                 #dp[-(i + 2)] is the max of dp[-(i + 1)] and dp[index] plus profit of current job.
         return dp[0]                                                                        #Return dp[0]
