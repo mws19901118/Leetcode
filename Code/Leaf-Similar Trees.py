2c5f00350e22978195d2cedc14289e168fce05c9
Junsheng Wang <mws19901118@gmail.com>
2024-01-08 16:37:28 -0800

    Update Leaf-Similar Trees.py

--- Code/Leaf-Similar Trees.py
+++ Code/Leaf-Similar Trees.py
@@ -5,17 +5,12 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def traverse(self, root: Optional[TreeNode], leaves: List[int]) -> None:                        #Store leaf values in sequence in an array by pre-order traversal recursively.
-        if not root:
-            return
-        if not root.left and not root.right:
-            leaves.append(root.val)
-            return
-        self.traverse(root.left, leaves)
-        self.traverse(root.right, leaves)                                                           
-
     def leafSimilar(self, root1: Optional[TreeNode], root2: Optional[TreeNode]) -> bool:
-        leaves1, leaves2 = [], []
-        self.traverse(root1, leaves1)                                                               #Find the leaf values of root1.
-        self.traverse(root2, leaves2)                                                               #Find the leaf values of root2.
-        return leaves1 == leaves2                                                                   #Compare.
+        def getLeaf(root: Optional[TreeNode]) -> None:              #Traverse tree to get all leaves.
+            if not root:                                            #If root is none, return.
+                return
+            if not root.left and not root.right:                    #If root is leaf, return its value in generator.
+                yield root.val
+            yield from getLeaf(root.left)                           #Return the leaves in left subtree in generator.
+            yield from getLeaf(root.right)                          #Return the leaves in right subtree in generator.
+        return list(getLeaf(root1)) == list(getLeaf(root2))         #Judging if the leaf sequence in 2 trees are same.
