51a3e554ac13fff2fa47bc2d196d9137413dc235
Junsheng Wang <mws19901118@gmail.com>
2024-01-15 16:13:24 -0800

    Update Insert Delete GetRandom O(1).py

--- Code/Insert Delete GetRandom O(1).py
+++ Code/Insert Delete GetRandom O(1).py
@@ -1,28 +1,28 @@
 class RandomizedSet:
 
     def __init__(self):
-        self.array = []                                           #Use an array to store the values.
-        self.map = {}                                             #Use a map to store the index in array of each element.
+        self.values = []                                                              #Use a list to store the values.
+        self.indexes = {}                                                             #Use a dictionary to store the index of each element in list.
 
     def insert(self, val: int) -> bool:
-        if val in self.map:                                       #If val is already in map, return false.
+        if val in self.indexes:                                                       #If val is already in dictionary, return false.
             return False
-        self.array.append(val)                                    #Append val to array.
-        self.map[val] = len(self.array) - 1                       #Add the index of val to map.
+        self.values.append(val)                                                       #Append val to list.
+        self.indexes[val] = len(self.values) - 1                                      #Add the index of val to dictionary.
         return True
 
     def remove(self, val: int) -> bool:
-        if val not in self.map:                                   #If val is not in map, return false.
+        if val not in self.indexes:                                                   #If val is not in dictionary, return false.
             return False
-        self.map[self.array[-1]] = self.map[val]                  #Update the index of the last element to be index of val.
-        self.array[self.map[val]] = self.array[-1]                #Also move the last element to the index of val.
-        self.array.pop()                                          #Remove the last element.
-        del self.map[val]                                         #Delete the index of val from map.
+        self.indexes[self.values[-1]] = self.indexes[val]                             #Update the index of the last element to be index of val.
+        self.values[self.indexes[val]] = self.values[-1]                              #Also move the last element to the index of val.
+        self.indexes.pop(val)                                                         #Delete the index of val from dictionary.
+        self.values.pop()                                                             #Pop the last element.
         return True
 
     def getRandom(self) -> int:
-        return random.choice(self.array)                          #Return a random element in array.
-
+        return random.choice(self.values)                                             #Return a random element in list.
+        
 # Your RandomizedSet object will be instantiated and called as such:
 # obj = RandomizedSet()
 # param_1 = obj.insert(val)
