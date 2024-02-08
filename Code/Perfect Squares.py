18b6fe1bb42ecf792655ce2619f0b6b10b6dadc4
Junsheng Wang <mws19901118@gmail.com>
2024-02-07 16:35:46 -0800

    Update Perfect Squares.py

--- Code/Perfect Squares.py
+++ Code/Perfect Squares.py
@@ -1,18 +1,17 @@
 class Solution:
     def numSquares(self, n: int) -> int:
-        q = [n]                                     #Store numbers in current level.
-        s = set()                                   #Store all the appeared number.
-        step = 1                                    #Initialize steps.
-        while q:                                    #BFS.
+        q = [n]                                                     #Store numbers in current level.
+        s = set()                                                   #Store all the appeared number.
+        step = 1                                                    #Initialize steps.
+        while q:                                                    #BFS.
             newq = []
             for x in q:
-                t = int(math.sqrt(x))               #Calculate the floor of root of x.
-                for i in reversed(range(t + 1)):    #Check from t to 1.
+                for i in reversed(range(int(math.sqrt(x)) + 1)):    #Check from the floor of root of x to 1.
                     curr = x - i * i
-                    if curr == 0:                   #If reach 0, return step.
+                    if not curr:                                    #If reach 0, return step.
                         return step
-                    if curr not in s:               #If it's not is s, add it to s and aopend it to newq.
+                    if curr not in s:                               #If it's not is s, add it to s and aopend it to newq.
                         newq.append(curr)
                         s.add(curr)
-            q = newq                                #Replace q with newq.
-            step += 1                               #Increase step by 1.
+            q = newq                                                #Replace q with newq.
+            step += 1                                               #Increase step by 1.
