82b580786f638d9601a5e27920d80ab035540da2
Junsheng Wang <mws19901118@gmail.com>
2024-02-18 16:06:06 -0800

    Update Power of Two.py

--- Code/Power of Two.py
+++ Code/Power of Two.py
@@ -1,7 +1,5 @@
 class Solution:
-    # @param {integer} n
-    # @return {boolean}
-    def isPowerOfTwo(self, n):
-        return (n>0) and (n&n-1)==0     #If n is power of 2, in binary form, the most significant bit is 1, the others are 0.
-                                        #However, in binary form, all the bits are 1 ans the length is 1 bit smaller than that of n. So n%n-1 is 0.
-                                        #Be sure that n is greater than 0.
+    def isPowerOfTwo(self, n: int) -> bool:
+        return n and not n & (n - 1)        #If n is power of 2, in binary form, the most significant bit is 1, the others are 0.
+                                            #And for n - 1, in binary form, all the bits are 1 and the length is 1 bit smaller than that of n. So n & (n - 1) == 0.
+                                            #Be sure that n is greater than 0.
