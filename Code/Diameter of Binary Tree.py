ffade784cd53755e244ebf5aca15c67ad710cd47
Junsheng Wang <mws19901118@gmail.com>
2024-02-26 17:19:07 -0800

    Update Diameter of Binary Tree.py

--- Code/Diameter of Binary Tree.py
+++ Code/Diameter of Binary Tree.py
@@ -6,11 +6,11 @@
 #         self.right = None
 
 class Solution:
-    def traverse(self, root: TreeNode) -> tuple:                                            #For each node, find the height of node plus one and the diameter of the subtree.
-        if root is None:
-            return (0, 0)
-        left = self.traverse(root.left)                                                     #Traverse through left subtree.
-        right = self.traverse(root.right)                                                   #Traverse through right subtree.
-        return (max(left[0], right[0]) + 1, max(left[1], right[1], left[0] + right[0]))     #Calculete the height and diameter. Diameter should be the max of left subtree diameter, right subtree diameter and the sum of left child height plus one and right child hight plus one.
     def diameterOfBinaryTree(self, root: TreeNode) -> int:
-        return self.traverse(root)[1]                                                       #Return the diameter.
+        def traverse(root: Optional[TreeNode]) -> (int, int):                                                #For each node, find the height of node plus one and the diameter of the subtree.
+            if not root:
+                return 0, 0
+            leftHeight, leftMax = traverse(root.left)                                                        #Traverse through left subtree.
+            rightHeight, rightMax = traverse(root.right)                                                     #Traverse through right subtree.
+            return max(leftHeight, rightHeight) + 1, max(leftMax, rightMax, leftHeight + rightHeight + 1)    #Calculete the height and diameter. Diameter should be the max of left subtree diameter, right subtree diameter and the sum of left child height plus one and right child hight plus one.
+        return traverse(root)[1] - 1                                                                         #Return the diameter.
