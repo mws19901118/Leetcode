413703f0d67261f1553ff521a37c8cf5c21928c2
Junsheng Wang <mws19901118@gmail.com>
2024-03-01 18:48:33 -0800

    Update Squares of a Sorted Array.py

--- Code/Squares of a Sorted Array.py
+++ Code/Squares of a Sorted Array.py
@@ -1,18 +1,12 @@
 class Solution:
     def sortedSquares(self, nums: List[int]) -> List[int]:
-        minAbs = 10001
-        minAbsIndex = -1
-        for i, x in enumerate(nums):                                                            #Find the element in A with min abs value and its index.
-            if abs(x) < minAbs:
-                minAbs = abs(x)
-                minAbsIndex = i
-        left, right = minAbsIndex - 1, minAbsIndex + 1                                          #Left pointer, traverse to the left of index and right pointer, traverse to the right of index.
-        squares = [minAbs * minAbs]                                                             #Add the square of minAbs to the squares list.
-        while left >= 0 or right < len(nums):
-            if left >= 0 and (right >= len(nums) or abs(nums[left]) <= abs(nums[right])):       #If right pointer reaches the right end or the abs value of right pointer is not smaller than that of left pointer, add the square of value of left pointer to the squares list.
-                squares.append(nums[left] * nums[left])
-                left -= 1
-            else:                                                                               #Otherwise, add the square of value of right pointer to the squares list.
-                squares.append(nums[right] * nums[right])
-                right += 1
-        return squares
+        i, j = 0, len(nums) - 1                                #Initialize 2 pointers at the beginning and the end of nums.
+        result = []
+        while i <= j:                                          #Iterate while 2 pointers not meet.
+            if abs(nums[i]) >= abs(nums[j]):                   #If abs(nums[i]) is not smaller than abs(nums[j]), append nums[i] * nums[i] and move i forward.
+                result.append(nums[i] * nums[i])
+                i += 1
+            else:                                              #Otherwise, append nums[j] * nums[j] and move j backward.
+                result.append(nums[j] * nums[j])
+                j -= 1
+        return result[::-1]                                    #Reverse the result and return.
