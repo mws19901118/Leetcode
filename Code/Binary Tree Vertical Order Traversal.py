ce75dff88e9c4fb632215a671b802f6881e353fb
Junsheng Wang <mws19901118@gmail.com>
2024-03-02 23:23:16 -0800

    Update Binary Tree Vertical Order Traversal.py

--- Code/Binary Tree Vertical Order Traversal.py
+++ Code/Binary Tree Vertical Order Traversal.py
@@ -1,32 +1,22 @@
-class Solution(object):
-    def traverse(self, root, row, column, dict):                    #Traverse through the tree recursively.
-        if root is None:
-            return
-        if column not in dict:                                      #If current column hasn't occurred before, add a new dict corresponding to current column.
-            dict[column] = {}
-        if row not in dict[column]:                                 #If current row hasn't occurred in current column, add a new list corresponding to current column and current row.
-            dict[column][row] = []
-        dict[column][row].append(root.val)                          #Append the value of root according to column and row.
-        self.traverse(root.left, row + 1, column - 1, dict)         #Recursively traverse the left child of current node; its row is row + 1; its column is column - 1.
-        self.traverse(root.right, row + 1, column + 1, dict)        #Recursively traverse the right child of current node; its row is row + 1; its column is column + 1.
-        
-    def verticalOrder(self, root):
-        """
-        :type root: TreeNode
-        :rtype: List[List[int]]
-        """
-        if root is None:
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def verticalOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        if not root:                                                                                    #If root is none, return empty list.
             return []
-        dict = {}                                                   #Store value according to column and row.
-        self.traverse(root, 0, 0, dict)                             #Traverse starting from root as column 0, row 0.
-        result = []
-        startc = min(dict.keys())                                   #Get the min value of column
-        endc = max(dict.keys())                                     #Get the max value of column
-        for i in range(startc, endc + 1):                           #Check every column.
-            result.append([])
-            startr = min(dict[i].keys())                            #Get the min value of row in current column.
-            endr = max(dict[i].keys())                              #Get the max value of row in current column.
-            for j in range(startr, endr + 1):
-                if j in dict[i]:
-                    result[i - startc].extend(dict[i][j])           #Add the list of value of the certain column and row to the end of result of current column.
-        return result
+        q = [(root, 0)]                                                                                 #Initialize a queue with root and current column coordinate 0.
+        cols = defaultdict(list)                                                                        #Store numbers in each column from top to bottom.
+        while q:                                                                                        #BFS.
+            newq = []
+            for x, i in q:                                                                              #Traverse each node and its column coordinate.
+                cols[i].append(x.val)                                                                   #Append the value of current node to its column.
+                if x.left:                                                                              #If x.left is not none, append it and its column coordinate to newq.
+                    newq.append((x.left, i - 1))
+                if x.right:                                                                             #If x.right is not none, append it and its column coordinate to newq.
+                    newq.append((x.right, i + 1))
+            q = newq                                                                                    #Replace q with newq.
+        return [cols[i] for i in range(min(cols.keys()), max(cols.keys()) + 1)]                         #Return each column from the min column coordinate to max column coordinate. It's guaranteed to be consecutive, because 2 nodes cannot be connected in 2 columns whose gap is greater than 1.
