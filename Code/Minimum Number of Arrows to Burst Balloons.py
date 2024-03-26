5ae3f82f6138ae08f13f01641e799365a8dd2a06
Junsheng Wang <mws19901118@gmail.com>
2024-03-25 21:24:51 -0700

    Update Minimum Number of Arrows to Burst Balloons.py

--- Code/Minimum Number of Arrows to Burst Balloons.py
+++ Code/Minimum Number of Arrows to Burst Balloons.py
@@ -1,7 +1,7 @@
 class Solution:
     def findMinArrowShots(self, points: List[List[int]]) -> int:
         points.sort(key = lambda x: x[1])                           #Sort points by ending point in ascending order.
-        end, count = float('-inf'), 0                               #Initialize the earilist end of ballons bursted by current arrow and arrows count.
+        end, count = -inf, 0                                        #Initialize the earilist end of ballons bursted by current arrow and arrows count.
         for l, r in points:                                         #Traverse points.
             if l > end:                                             #If the start of current ballon is greater than end, a new arrow is needed and reset end.
                 count += 1
