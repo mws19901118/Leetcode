bf9ccd171a9624ff7a0e0eee7dae2a0447692855
Junsheng Wang <mws19901118@gmail.com>
2024-03-26 17:36:17 -0700

    Update Subarray Product Less Than K.py

--- Code/Subarray Product Less Than K.py
+++ Code/Subarray Product Less Than K.py
@@ -1,12 +1,10 @@
 class Solution:
     def numSubarrayProductLessThanK(self, nums: List[int], k: int) -> int:
-        end, product, count = 0, 1, 0                                   #Maintain a sliding window starting from start index whose product is less than k.
-        for start in range(len(nums)):
-            while end < len(nums) and product * nums[end] < k:          #While the product is smaller than k, extend the right end of window towards right.
-                product *= nums[end]
-                end += 1
-            if end > start:                                             #If window size is larger than 0, add window length to count and move the number on start out of window.
-                count += end - start
-                product //= nums[start]
-            end = max(start + 1, end)                                   #End should be at least not smaller than start.
+        p, start, count = 1, 0, 0                                        #Maintain a sliding window starting from start index whose product is less than k.
+        for i, x in enumerate(nums):                                     #Traverse nums.
+            p *= x                                                       #Multiply x to product.
+            while p >= k and start <= i:                                 #While product is greater than or equal to k and start is not greater than i, move forward the start of window.
+                p //= nums[start]                                        #Also update product.
+                start += 1
+            count += i - start + 1                                       #Add the window size(i - start + 1) to count.
         return count
