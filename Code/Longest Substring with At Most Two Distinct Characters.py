7317cb472b3ed5cdcc8b3f5236d38cf82d8a27f6
Junsheng Wang <mws19901118@gmail.com>
2024-03-31 18:12:13 -0700

    Update Longest Substring with At Most Two Distinct Characters.py

--- Code/Longest Substring with At Most Two Distinct Characters.py
+++ Code/Longest Substring with At Most Two Distinct Characters.py
@@ -1,26 +1,11 @@
-class Solution(object):
-    def lengthOfLongestSubstringTwoDistinct(self, s):
-        """
-        :type s: str
-        :rtype: int
-        """
-        dict = {}                                   #Use a dict to store the last appeareance index of a character.
-        l = len(s)
-        maxl = 0                                    #Record the max length.
-        i = 0                                       #Record the starting index of current substring.
-        j = 0                                       #Record the ending index of current substring.
-        while j < l:
-            if s[j] in dict or len(dict) < 2:       #If the length of dict is smaller than 2 or current character is already in dict, add it and the index to dict.
-                dict[s[j]] = j
-                if j - i + 1 > maxl:                #Update maxl if necessary.
-                    maxl = j - i + 1
-            else:                                   #Maintain a dict whose length is 2 when encountering a new character.
-                lastappearance = l
-                for k in dict.keys():               #Find the smaller index of the last appearance of the 2 characters. The characters on and before that index no longer belong to current substring.
-                    if dict[k] < lastappearance:
-                        lastappearance = dict[k]
-                del dict[s[lastappearance]]         #Remove that character from dict.
-                i = lastappearance + 1              #Update the starting index.
-                dict[s[j]] = j                      #Add the new character and its index to dict.
-            j += 1
-        return maxl
+class Solution:
+    def lengthOfLongestSubstringTwoDistinct(self, s: str) -> int:
+        indexes = {}                                    #Store the indexes of last occurrence of each character.
+        left, result = -1, 0                            #Initialize left bound of sliding window(not inclusive) and result.
+        for i, x in enumerate(s):                       #Traverse s.
+            indexes[x] = i                              #Set indexes[x] to i.
+            if len(indexes) > 2:                        #If the length of indexes is greater than 2, pop the least index in indexes.values() and move left to it.
+                left = min(indexes.values())
+                indexes.pop(s[left])
+            result = max(result, i - left)              #Update result if i - left is larger.
+        return result
