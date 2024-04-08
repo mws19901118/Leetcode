178502c4a5ce23e98a555c1fccb5b99a1c95d687
Junsheng Wang <mws19901118@gmail.com>
2024-04-07 20:39:55 -0700

    Update Verify Preorder Sequence in Binary Search Tree.py

--- Code/Verify Preorder Sequence in Binary Search Tree.py
+++ Code/Verify Preorder Sequence in Binary Search Tree.py
@@ -1,16 +1,11 @@
-class Solution(object):
-    def verifyPreorder(self, preorder):
-        """
-        :type preorder: List[int]
-        :rtype: bool
-        """
-        l = len(preorder)
-        stack = []                                            #Use stack to store the descending sequence we have before current number.
-        lowerbound = -0x80000000                              #Record the lower bound of current number.
-        for i in range(l):
-            while stack != [] and stack[-1] <= preorder[i]:   #For each number, find its possible parent, i.e. the first element(if any) in stack which is larger than current element.
-                lowerbound = stack.pop()                      #To maintain a BST, current number should be larger than all the popped numbers, so update lower bound every time.
-            if preorder[i] <= lowerbound:                     #Judge if it's valid.
+class Solution:
+    def verifyPreorder(self, preorder: List[int]) -> bool:
+        stack = []                                                      #Use stack to store the descending sequence we have before current number.
+        lowerbound = 1                                                  #Record the lower bound of current number.
+        for x in preorder:
+            if x < lowerbound:                                          #Judge if it's valid.
                 return False
-            stack.append(preorder[i])                         #Append current number to stack.
-        return True                                           #If no conflicts found, return true.
+            while stack and stack[-1] < x:                              #For each number, find its possible parent, i.e. the first element(if any) in stack which is larger than current element.
+                lowerbound = max(lowerbound, stack.pop())               #To maintain a BST, current number should be larger than all the popped numbers, so update lower bound every time.
+            stack.append(x)                                             #Append current number to stack.
+        return True                                                     #If no conflicts found, return true.
