231522dc36f56b0bd19443404e8114272a655c91
Junsheng Wang <mws19901118@gmail.com>
2024-04-13 19:30:50 -0700

    Update Make Array Non-decreasing or Non-increasing.py

--- Code/Make Array Non-decreasing or Non-increasing.py
+++ Code/Make Array Non-decreasing or Non-increasing.py
@@ -4,10 +4,10 @@ class Solution:
         heap = []                                                     #Use a min heap to store all numbers visited.
         for x in nums:                                                #Traverse nums.
             if heap and x > heap[0]:                                  #If current number is larger than the top of head, we have to do operations.
-                non_increasing += x - heapq.heappop(heap)             #Pop the heap, let's say it is y, then make x - y opeartions. Then the adjusted value of x, y count be any number between [y, x].
+                non_increasing += x - heapq.heappop(heap)             #Pop the heap, let's say it is y, then make x - y opeartions. Then the adjusted value of x, y can be any number between [y, x].
                 heapq.heappush(heap, x)                               #After adjust y to x, push x back to heap.
             heapq.heappush(heap, x)                                   #Push x to heap.
-                                                                      #Suppose there is an incoming number z. If z is smaller than y, x and y stay (y, y). if z is greater than the previous smallest number, p. z can be adjusted as well and falls in [y, x].
+                                                                      #Suppose there is an incoming number z. If z is smaller than y, x and y stay (y, y). If z is greater than the current smallest number, z and current smallest number also need to be adjusted.
         non_decresing = 0                                             #Do the same thing to calulate the cost of making arrary non-decreasing.
         heap = []
         for n in nums:
