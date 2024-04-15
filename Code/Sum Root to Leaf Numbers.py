38c99fb12cf9918b33f9361818abc9043f1452c6
Junsheng Wang <mws19901118@gmail.com>
2024-04-14 17:13:53 -0700

    Update Sum Root to Leaf Numbers.py

--- Code/Sum Root to Leaf Numbers.py
+++ Code/Sum Root to Leaf Numbers.py
@@ -5,13 +5,12 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def traverse(self, root: Optional[TreeNode], number: int) -> int:                       #Traverse tree with the number from root to the parent of current node.
-        if not root:                                                                        #If current node is none, return 0.
-            return 0
-        number = number * 10 + root.val                                                     #Update number to include current node.
-        if not root.left and not root.right:                                                #If current node is leaf, return number.
-            return number
-        return self.traverse(root.left, number) + self.traverse(root.right, number)         #Return the sum of result of traversing left child and right child.
-    
     def sumNumbers(self, root: Optional[TreeNode]) -> int:
-        return self.traverse(root, 0)                                                       #Start traversing from root with 0.
+        def traverse(root: Optional[TreeNode], number: int) -> int:                   #Traverse tree with the number from root to the parent of current node.
+            if not root:                                                              #If current node is none, return 0.
+                return 0
+            number = number * 10 + root.val                                           #Update number to include current node.
+            if not root.left and not root.right:                                      #If current node is leaf, return number.
+                return number
+            return traverse(root.left, number) + traverse(root.right, number)         #Return the sum of result of traversing left child and right child.
+        return traverse(root, 0)                                                      #Return thr result of traversing from root with 0.
