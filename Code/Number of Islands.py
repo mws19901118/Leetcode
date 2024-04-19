9f25007af7fde50a56ac9d60a913905fdff77a02
Junsheng Wang <mws19901118@gmail.com>
2024-04-18 17:13:24 -0700

    Update Number of Islands.py

--- Code/Number of Islands.py
+++ Code/Number of Islands.py
@@ -1,21 +1,15 @@
 class Solution:
-    def BFS(self, grid: List[List[str]], i: int, j:int):
-        grid[i][j] == '2'                                                                                               #Set current coordinate as visited.
-        q = [(i, j)]                                                                                                    #Initialize queue.
-        while q:                                                                                                        #BFS while q is not empty.
-            nextq = []                                                                                                  #Initialize nextq.
-            for x, y in q:                                                                                              #Traverse each coordinate in queue.
-                for nx, ny in [(x - 1, y), (x, y + 1), (x + 1, y), (x, y - 1)]:                                         #Traverse each neighbor.
-                    if nx >= 0 and nx < len(grid) and ny >= 0 and ny < len(grid[0]) and grid[nx][ny] == '1':            #If the neighbor is valid and unvisited land.
-                        grid[nx][ny] = '2'                                                                              #Mark neighbor as visited.
-                        nextq.append((nx, ny))                                                                          #Append the coordinate of neighbor to nextq.
-            q = nextq                                                                                                   #Replace q with nextq.
-            
     def numIslands(self, grid: List[List[str]]) -> int:
-        count = 0                                                                                                       #Initialize count.
-        m, n = len(grid), len(grid[0])                                                                                  #Get the dimensions.
-        for i, j in product(range(m), range(n)):                                                                        #Traverse grid.
-            if grid[i][j] == '1':                                                                                       #If grid is '1', increase count and do BFS to mark the island as visited.
-                count += 1
-                self.BFS(grid, i, j)
+        m, n, count = len(grid), len(grid[0]), 0                                                #Get dimensions and initialize count.
+        for i, j in product(range(m), range(n)):                                                #Traverse each cell in grid.
+            if grid[i][j] == "1":                                                               #If gird[i][j] is '1', we found an island.
+                q = deque([(i, j)])                                                             #Put (i, j) in a deque.
+                grid[i][j] = "2"                                                                #Set grid[i][j] to '2' to mark it as visited.
+                while q:                                                                        #BFS while q is not empty.
+                    x, y = q.popleft()                                                          #Popleft q.
+                    for u, v in [(x - 1, y), (x, y + 1), (x + 1, y), (x, y - 1)]:               #Traverse all 4 neighbors.
+                        if 0 <= u < m and 0 <= v < n and grid[u][v] == "1":                     #If the neighbor is '1', it is part of the island.
+                            grid[u][v] = "2"                                                    #Mark neighbor as visited.
+                            q.append((u, v))                                                    #Append it to q.
+                count += 1                                                                      #Increase count.
         return count
