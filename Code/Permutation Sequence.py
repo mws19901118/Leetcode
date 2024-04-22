fcb6e55bf9b7b2a45e9ccd4faa9838af924ba998
Junsheng Wang <mws19901118@gmail.com>
2024-04-21 20:54:09 -0700

    Update Permutation Sequence.py

--- Code/Permutation Sequence.py
+++ Code/Permutation Sequence.py
@@ -1,19 +1,14 @@
 class Solution:
     # @return a string
     def getPermutation(self, n, k):
-        k-=1                                #k minus 1, because the index of a list begins with 0.
-        seq=[]
-        result=""
-        for i in range(1,n+1):
-            seq.append(i)                   #Store all the digits between 1 and n in a list.
-        amount=1
-        for i in range(1,n):
-            amount*=i                       #Calculate (n-1)!, which is the amount of permutations of n starting with a certain digit.
-        for i in range(1,n):
-            index=k/amount                  #Calculate the starting digit, which is the index-th element in seq.
-            k=k%amount                      #Calculate the remaining k to calculate the next digit.
-            result+=str(seq[index])
-            seq.remove(seq[index])          #Remove this digit from seq.
-            amount/=(n-i)                   #Divide amount by n-1, in order to calculate the next digit.
-        result+=str(seq[0])                 #Add the only element left in seq to result. It's the last digit.
+        k -= 1                                                        #k minus 1, because the index of a list begins with 0.
+        numbers = [i for i in range(1, n + 1)]                        #Store all available numbers in an array and keep them sorted in asending order.
+        result = ""
+        amount = factorial(n)                                         #Initialize the amount of permutations of n starting with a certain digit.
+        while numbers:                                                #Iterate while numbers is not empty.
+            fold //= len(numbers)                                     #Calculate the amount of permutations with a certain digit for current length of remaining numbers.
+            index = k // fold                                         #Find the index of first digit in remaining numbers.
+            result += str(numbers[index])                             #Append it to result.
+            numbers = numbers[:index] + numbers[index + 1:]           #Remove it from remaining numbers and update numbers.
+            k %= fold                                                 #Update k for next iteration.
         return result
