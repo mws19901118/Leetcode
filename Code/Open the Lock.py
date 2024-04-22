3369dd3bc67ab1a96941edc487e9f0e815abc307
Junsheng Wang <mws19901118@gmail.com>
2024-04-21 17:36:42 -0700

    Update Open the Lock.py

--- Code/Open the Lock.py
+++ Code/Open the Lock.py
@@ -1,24 +1,21 @@
 class Solution:
     def openLock(self, deadends: List[str], target: str) -> int:
         deadEndsSet = set(deadends)                                                   #Convert dean ends list to set.
-        q = set(["0000"])                                                             #Initialize queue with "0000".
-        visited = set()                                                               #Use a set to store all visited state.
-        turns = 0                                                                     #Initialize number of turns.
-        while q:                                                                      #NFS while q is not empty.
-            newq = set()                                                              #Initialize a new queue.
-            for x in q:                                                               #Traverse all states in q.
-                if x == target:                                                       #If reaches target, return turns directly.
-                    return turns
-                if x in deadEndsSet:                                                  #If is dead end, skip current state.
+        q = ["0000"]                                                                  #Initialize queue with "0000".
+        visited = set(q)                                                              #Use a set to store all visited state.
+        result = 0
+        while q:                                                                      #BFS while q is not empty.
+            newq = []                                                                 #Initialize a new queue.
+            for s in q:                                                               #Traverse all states in q.
+                if s in deadEndsSet:                                                  #If is dead end, skip current state.
                     continue
-                for i in range(4):                                                    #Try all 4 wheels.
-                    plusOne = x[:i] + str((int(x[i]) + 1) % 10) + x[i + 1:]           #Turn forward, and add the state to newq if it's not visited.
-                    if plusOne not in visited:
-                        newq.add(plusOne)
-                    minusOne = x[:i] + str((int(x[i]) - 1) % 10) + x[i + 1:]          #Turn backward, and add the state to newq if it's not visited.
-                    if minusOne not in visited:
-                        newq.add(minusOne)
-            visited |= q                                                              #Update visited.
+                if s == target:                                                       #If reaches target, return turns directly.
+                    return result
+                for i, turn in product(range(4), [-1, 1]):                            #Try all 4 wheels and 2 directions.
+                    afterTurn = s[:i] + str((int(s[i]) + turn) % 10) + s[i + 1:]      #Get the state after turn.
+                    if afterTurn not in visited:                                      #If it is not visited, add it to visited and newq.
+                        visited.add(afterTurn)
+                        newq.append(afterTurn)
             q = newq                                                                  #Replace q with newq.
-            turns += 1                                                                #Increase turns.
+            result += 1
         return -1                                                                     #Return -1 if cannot reach target.
