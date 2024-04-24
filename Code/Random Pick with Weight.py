e4464abb6b00a1aaa8d93f4f296e0d6652675035
Junsheng Wang <mws19901118@gmail.com>
2024-04-23 23:33:36 -0700

    Update Random Pick with Weight.py

--- Code/Random Pick with Weight.py
+++ Code/Random Pick with Weight.py
@@ -1,4 +1,3 @@
-import random
 class Solution:
 
     def __init__(self, w: List[int]):
@@ -8,16 +7,7 @@ class Solution:
 
     def pickIndex(self) -> int:
         r = random.randint(1, self.weightSum[-1])                             #Random an int in range between 1 and the max cumulative sum.
-        start, end = 0, len(self.weightSum)
-        while start <= end:                                                   #Do binary search in cumulative sum to find the largest index whose cumulative sum is smaller than the random int.
-            mid = (start + end) // 2
-            if self.weightSum[mid - 1] < r and self.weightSum[mid] >= r:
-                return mid - 1                                                #Return index - 1.
-            elif self.weightSum[mid] < r:
-                start = mid + 1
-            else:
-                end = mid - 1
-
+        return bisect_right(self.weightSum, r) - 1                            #Do binary search in cumulative sum to find the largest index whose cumulative sum is smaller than the random int. and return the index - 1.
 
 # Your Solution object will be instantiated and called as such:
 # obj = Solution(w)
