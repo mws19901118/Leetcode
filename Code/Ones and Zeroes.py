85e10a01158d5100dd2e857b9345849ca8618011
Junsheng Wang <mws19901118@gmail.com>
2024-04-28 12:24:36 -0700

    Update Ones and Zeroes.py

--- Code/Ones and Zeroes.py
+++ Code/Ones and Zeroes.py
@@ -1,15 +1,9 @@
 class Solution:
-    def find(self, strs: List[str], cache: defaultdict, m: int, n: int, i: int, zero: int, one: int) -> int:
-        if i >= len(strs) or (zero <= 0 and one <= 0):                                                                  #If i reaches the end of strs or zero and one are both not greater than 0, return 0 cause we cannot form subset further.
-            return 0
-        if (i, zero, one) in cache:                                                                                     #If the combination of (i, zero, one) is already in cache, return the value in cache.
-            return cache[(i, zero, one)]
-        else:
-            x, y = strs[i].count('0'), strs[i].count('1')                                                               #Count '0' and '1' in strs[i].
-            a = 1 + self.find(strs, cache, m, n, i + 1, zero - x, one - y) if zero - x >= 0 and one - y >= 0 else 0     #Calculate the max size of subset we can form beyond if we include str[i].
-            b = self.find(strs, cache, m, n, i + 1, zero, one)                                                          #Calculate the max size of subset we can form beyond if we exclude str[i].
-            cache[(i, zero, one)] = max(a, b)                                                                           #Store the max of a and b in cache for the combination in cache.
-            return cache[(i, zero, one)]                                                                                #Return the value.
     def findMaxForm(self, strs: List[str], m: int, n: int) -> int:
-        cache = defaultdict(int)                                                                                        #Store the max size of subset we can form with strs[i:], zero as the limit of 0 and one as the limit as 1.
-        return self.find(strs, cache, m, n, 0, m, n)                                                                    #Find the max size of subset from the beginning of strs with memorization.
+        zeros, ones = [sum([1 for y in x if y == "0"]) for x in strs], [sum([1 for y in x if y == "1"]) for x in strs]                                        #Calculate the number of 0 and 1 respectively in each string.
+        @cache                                                                                                                                                #Cache result.
+        def dp(index: int, x: int, y: int):                                                                                                                   #DP to find the max subset in strs[index:] with x zeros and y ones allowed.                                                                          
+            if index == len(strs):                                                                                                                            #If index reaches the end, return 0.
+                return 0
+            return max(dp(index + 1, x, y), (dp(index + 1, x - zeros[index], y - ones[index]) + 1) if x >= zeros[index] and y >= ones[index] else 0)          #Return the max of dp(index + 1, x, y) and dp(index + 1, x - zeros[index], y - ones[index]) + 1 if current string does not have more than x zeros or y ones.
+        return dp(0, m, n)                                                                                                                                    #Return the result of dp(0, m, n).
