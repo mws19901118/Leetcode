8b701d7c273cc9f91a9ec2aeeb6fbc3af6fd1026
Junsheng Wang <mws19901118@gmail.com>
2024-04-27 20:31:16 -0700

    Update Sum of Distances in Tree.py

--- Code/Sum of Distances in Tree.py
+++ Code/Sum of Distances in Tree.py
@@ -1,32 +1,29 @@
 class Solution:
-    def sumDistanceAndNode(self, n: int, p: int, adjacentList: List[List[int]], distanceAndNodeSum: List[tuple]) -> None:
-        distanceSum, nodeSum = 0, 0                                                                                                                                               #Initialize total distance to current root and node count.
-        for x in adjacentList[n]:                                                                                                                                                 #Traverse adjacentList[n].
-            if x == p:                                                                                                                                                            #If x is the previous node of n, skip.
-                continue
-            self.sumDistanceAndNode(x, n, adjacentList, distanceAndNodeSum)                                                                                                       #Populate distanceAndNodeSum for x.
-            distanceSum += distanceAndNodeSum[x][0]                                                                                                                               #Increate distanceSum by distanceAndNodeSum[0].
-            nodeSum += distanceAndNodeSum[x][1]                                                                                                                                   #Increate nodeSum by 1.
-        distanceSum += nodeSum                                                                                                                                                    #Now distanceSum is the sum of distances in its lower level, all nodes are 1 edge away from connecting to n. So, add nodeSum to distanceSum.
-        nodeSum += 1                                                                                                                                                              #Increase 1 to nodeSum because of n itself.
-        distanceAndNodeSum[n] = (distanceSum, nodeSum)                                                                                                                            #Update distanceAndNodeSum[n].
-
-    def calculateResult(self, n: int, distanceFromParent: int, nodesFromParent, result: List[int], adjacentList: List[List[int]], distanceAndNodeSum: List[tuple]) -> None:
-        result[n] = distanceFromParent + distanceAndNodeSum[n][0]                                                                                                                 #Overall, result[n] equals distances carried over from parent plus the sum of distance from all the nodes in subtree to current root.
-        for x in adjacentList[n]:                                                                                                                                                 #Traverse adjacentList[n]. 
-            if result[x] != 0:                                                                                                                                                    #If result[x] is already set, x is traversed so it's the parent of n and we need to skip x.
-                continue
-            carryOverNodes = nodesFromParent + distanceAndNodeSum[n][1] - distanceAndNodeSum[x][1]                                                                                #Calculate the carry over nodes count, which equals nodesFromParent plus all nodes under n except nodes under x
-            carryOverDistances = result[n] - distanceAndNodeSum[x][0] - distanceAndNodeSum[x][1] + carryOverNodes                                                                 #Calculate the carry over to x. Extract distances from x to n. Then add carryOverNodes, because they all need one more edge away from connecting to x.
-            self.calculateResult(x, carryOverDistances, carryOverNodes, result, adjacentList, distanceAndNodeSum)                                                                 #Keep calculating result at x.
-    
     def sumOfDistancesInTree(self, n: int, edges: List[List[int]]) -> List[int]:
-        adjacentList = [[] for _ in range(n)]                                                                                                                                     #Initialize adjacent list.
-        for a, b in edges:                                                                                                                                                        #Build adjacent list.
-            adjacentList[a].append(b)
-            adjacentList[b].append(a)
-        distanceAndNodeSum = [None] * n                                                                                                                                           #Asuming the whole tree root is at 0, store the sum of distance from all the nodes in subtree to current root and the node count of the subtree.    
-        result = [0] * n                                                                                                                                                          #Initialize result of each node.
-        self.sumDistanceAndNode(0, None, adjacentList, distanceAndNodeSum)                                                                                                        #Populate distanceAndNodeSum for each node, starting from node 0.
-        self.calculateResult(0, 0, 0, result, adjacentList, distanceAndNodeSum)                                                                                                   #Populate result for each node, starting from node 0.
-        return result                                                                                                                                                             #Return result.
+        adjacentList = [[] for _ in range(n)]                                                                                            #Build adjacent list.
+        for x, y in edges:
+            adjacentList[x].append(y)
+            adjacentList[y].append(x)
+        
+        result, count, distance = [0] * n, [0] * n, [0] * n                                                                              #Initialize result, also node count for each subtree and total distance of all nodes in subtree to current root for each subtree.
+        def findCountAndDistanceInSubtree(curr: int, prev: int):                                                                         #Populate count and distance.
+            count[curr], distance[curr] = 1, 0                                                                                           #Initialize count[curr] and distance[curr].
+            for x in adjacentList[curr]:                                                                                                 #Traverse adjacentList[curr].
+                if x == prev:                                                                                                            #If x is prev, skip because it's parent of curr.
+                    continue
+                findCountAndDistanceInSubtree(x, curr)                                                                                   #Recursively populate count and distance for the subtree whose root is x.
+                count[curr] += count[x]                                                                                                  #Add count[x] to count[curr].
+                distance[curr] += distance[x] + count[x]                                                                                 #Add distance[x] + count[x] to distance[curr].
+
+        def updateResult(curr: int, prev: int, countFromAbove: int, distanceFromAbove: int):                                             #Update result with count and distance passed from parent and siblings.
+            result[curr] = distance[curr] + distanceFromAbove                                                                            #Sum of distance to curr is the sum of distance passed from parent and siblings plus total distance of all nodes in subtree to current root.
+            for x in adjacentList[curr]:                                                                                                 #Traverse adjacentList[curr].
+                if x == prev:                                                                                                            #If x is prev, skip because it's parent of curr.
+                    continue
+                nextCountFromAbove = countFromAbove + count[curr] - count[x]                                                             #Count to be passed to x is countFromAbove plus count of nodes in the subtree whose root is curr but not in the subtree whose root is x.
+                nextDistanceFromAbove = distanceFromAbove + nextCountFromAbove + distance[curr] - distance[x] - count[x]                 #Distance to be passed to x is distanceFromAbove + (distance[curr] - distance[x] - count[x])(distance in subtree whose root is curr except those from x), then plus nextCountFromAbove.
+                updateResult(x, curr, nextCountFromAbove, nextDistanceFromAbove)                                                         #Recursively update result with updated count from above and distance from above.
+        
+        findCountAndDistanceInSubtree(0, -1)                                                                                             #Populate count and distance from node 0.
+        updateResult(0, -1, 0, 0)                                                                                                        #Update result from node 0, with no count and distance from above.
+        return result
