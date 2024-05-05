429c73a948b5d1e981036f9d65e98f54dab66256
Junsheng Wang <mws19901118@gmail.com>
2024-05-05 12:05:09 -0700

    Update Max Consecutive Ones III.py

--- Code/Max Consecutive Ones III.py
+++ Code/Max Consecutive Ones III.py
@@ -1,13 +1,10 @@
 class Solution:
     def longestOnes(self, nums: List[int], k: int) -> int:
-        indexes = deque()                             #Store the indexes of 0.
-        count, start, result = 0, -1, 0               #Initialize count of 0, start of sliding window(not inclusive) and result.
-        for i, x in enumerate(nums):                  #Traverse nums.
-            if x == 0:                                #If x is 0, add its idnex to indexes and increase count by 1.
-                indexes.append(i)
-                count += 1
-                if count > k:                         #If there are more than k 0, decrease count by 1 and set start to be the first index in indexes, thus we are maintain a sliding window with at most k 0.
-                    count -= 1
-                    start = indexes.popleft()
-            result = max(result, i - start)           #Update result.
-        return result
+        start, count, length = 0, 0, 0                    #Initialize the start of sliding window, count of 0 and max length.
+        for i, x in enumerate(nums):                      #Traverse nums.
+            count += 1 - x                                #Increase count of zero if necessary.
+            while count > k:                              #While count of zero is greater than k, move start forward.
+                count -= 1 - nums[start]
+                start += 1
+            length = max(length, i - start + 1)           #Now, we can flip all zeros in window and make the window consecutive ones. Update length if necessary.
+        return length
