d54abc3e1977803582acb51d8c53f3c33b7fb22a
Junsheng Wang <mws19901118@gmail.com>
2024-05-04 19:08:22 -0700

    Update Move Zeroes.py

--- Code/Move Zeroes.py
+++ Code/Move Zeroes.py
@@ -1,18 +1,12 @@
-class Solution(object):
-    def moveZeroes(self, nums):
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
         """
-        :type nums: List[int]
-        :rtype: void Do not return anything, modify nums in-place instead.
+        Do not return anything, modify nums in-place instead.
         """
-        l = len(nums)
-        fast = 0                                        #Use two pointers.
-        slow = 0
-        while fast < l:
-            if nums[slow] == 0 and nums[fast] == 0:     #If nums[slow] is 0 and nums[fast] is 0, increasefast by 1.
-                fast += 1
-            elif nums[slow] == 0 and nums[fast] != 0:   #If nums[slow] is 0 and nums[fast] is not 0, swap the 2 number.
-                nums[slow] = nums[fast]
-                nums[fast] = 0
-            else:                                       #Otherwise, increase both fast and slow by 1.
-                fast += 1
-                slow += 1
+        index = 0                                    #Initialize index to put current number.
+        for x in nums:                               #Traverse nums.
+            if x != 0:                               #If current number is not 0, put it on index and move forward index.
+                nums[index] = x
+                index += 1
+        for i in range(index, len(nums)):            #Set all number in nums[index:] to 0.
+            nums[i] = 0
