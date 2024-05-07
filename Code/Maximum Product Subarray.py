fb4e1b55c8fa0d2bc49990c74d4cfececacfd21b
Junsheng Wang <mws19901118@gmail.com>
2024-05-07 09:46:34 -0700

    Update Maximum Product Subarray.py

--- Code/Maximum Product Subarray.py
+++ Code/Maximum Product Subarray.py
@@ -1,6 +1,7 @@
 class Solution:
     def maxProduct(self, nums: List[int]) -> int:
-        minp, maxp, result = 1, 1, -math.inf                                        #Initialize the min product and max product at each index and result as well.
+        minp, maxp, result = 1, 1, -inf                                             #Initialize the min product and max product at each index and result as well.
         for x in nums:                                                              #Traverse nums.
             minp, maxp = min(minp * x, maxp * x, x), max(minp * x, maxp * x, x)     #Update the min product and max product at each index.
             result = max(maxp, result)                                              #Update result.
+        return result
