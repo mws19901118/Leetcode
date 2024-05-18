643f4f8137db2aafdd5431a51c17fef87899c69b
Junsheng Wang <mws19901118@gmail.com>
2024-05-17 18:29:56 -0700

    Update Distribute Coins in Binary Tree.py

--- Code/Distribute Coins in Binary Tree.py
+++ Code/Distribute Coins in Binary Tree.py
@@ -1,23 +1,18 @@
 # Definition for a binary tree node.
-# class TreeNode(object):
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
-
-class Solution(object):
-    def dfs(self, root):                                                            #DFS under current node. Return the difference between coins and nodes and the number of necessary moves.
-        if root == None:                                                            #If current node is none, return both 0.
-            return (0, 0)
-        l = self.dfs(root.left)                                                     #DFS left child.
-        r = self.dfs(root.right)                                                    #DFS right child.
-        return (l[0] + r[0] + root.val - 1, l[1] + r[1] + abs(l[0]) + abs(r[0]))    #The difference between coins and nodes in the tree equals that of left child(l[0]) plus that of right child{r[0] plus that of current node(root.val - 1). 
-                                                                                    #No matter if left child lacks l[1] coins or needs to give out l[1] coins, there must be l[1] moves between left child and current node.
-                                                                                    #The same goes to right child.
-                                                                                    #So, the necessary moves is l[1] + r[1] + abs(l[0]) + abs(r[0]).
-    def distributeCoins(self, root):
-        """
-        :type root: TreeNode
-        :rtype: int
-        """
-        return self.dfs(root)[1]                                                    #DFS from root node and return result.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def distributeCoins(self, root: Optional[TreeNode]) -> int:
+        def distribute(root: Optional[TreeNode]):                                                                         #DFS under current node and return the difference between coins and nodes and the number of necessary moves.
+            if not root:                                                                                                  #If current node is none, return both 0.
+                return 0, 0
+            leftDiff, leftMoves = distribute(root.left)                                                                   #DFS left child.
+            rightDiff, rightMmoves = distribute(root.right)                                                               #DFS right child.
+            return leftDiff + rightDiff + root.val - 1, leftMoves + rightMmoves + abs(leftDiff) + abs(rightDiff)          #The difference between coins and nodes in the tree equals that of leftDiff + rightDiff + root.val - 1. 
+                                                                                                                          #No matter if left child lacks leftMoves coins or needs to give out leftMoves coins, there must be leftMoves moves between left child and current node.
+                                                                                                                          #The same goes to right child.
+                                                                                                                          #So, the necessary moves is leftMoves + rightMmoves + abs(leftDiff) + abs(rightDiff).
+        return distribute(root)[1]                                                                                        #DFS from root node and return result.
