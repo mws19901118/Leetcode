dd07d8affa4662b90cd84d2c6372d5468f675a18
Junsheng Wang <mws19901118@gmail.com>
2024-05-25 00:20:15 -0700

    Update Word Break II.py

--- Code/Word Break II.py
+++ Code/Word Break II.py
@@ -1,40 +1,16 @@
 class Solution:
-    # @param s, a string
-    # @param dict, a set of string
-    # @return a list of strings
-    def wordBreak(self, s, dict):
-        n = len(s)
-        A = [None] * n
-        i = n-1
-        while i >= 0:
-            if s[i:n] in dict:
-                A[i] = [n]  # A[i] contains "n" means s[i..n-1] is a word
-            else:
-                A[i] = []
-            # Check al possible j break
-            for j in xrange(i+1, n):
-                if A[j] and s[i:j] in dict:
-                    A[i].append(j)
-            i -= 1
- 
-        # Step 2: find all possible sequences of breaks,
-        # which equals to find all paths from A[0] and stop when the break is "n".
-        # So it converts to BFS on a graph, with at most n steps.
-        res = []  # possible sentences with break
-        path_list = [[0]]  # initially, there is only one path containing the source node
-        while path_list:
-            new_list = []
-            # For each path,
-            # 1) If the path ends with break "n", then segment the string with breaks of the path
-            # 2) otherwise, expand it with all possible breaks
-            for path in path_list:
-                if path[-1] == n:  # segment!
-                    # Get words according to the breaks
-                    temp = [ s[path[i]:path[i+1]] for i in xrange(len(path)-1) ]
-                    # join words together
-                    res.append(" ".join(temp))
-                else:  # expand the path
-                    for j in A[path[-1]]:
-                        new_list.append(path+[j])
-            path_list = new_list
-        return res
+    def wordBreak(self, s: str, wordDict: List[str]) -> List[str]:
+        @cache                                                                    #Cache result.
+        def dp(index: int) -> List[str]:                                          #Find all the sentences for s[index:].
+            if index == len(s):                                                   #If reaches the end of s, return a list with only one empty string.
+                return [""]
+            result = []
+            for i in range(index + 1, len(s) + 1):                                #Traverse all possible places to break strubg,
+                if s[index:i] in wordSet:                                         #If s[index:i] is a valid word, we can break at i.
+                    nextResult = dp(i)                                            #Get all the sentences for s[i:].
+                    for x in nextResult:                                          #Concatenate each sentence after s[index:i] with correct delimiter and append it to result.
+                        split = "" if x == "" else " "
+                        result.append(s[index:i] + split + x)
+            return result
+        wordSet = set(wordDict)                                                   #Store valid words in set.
+        return dp(0)                                                              #Return the result of dp[0].
