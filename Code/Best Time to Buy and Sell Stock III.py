19eb7ab5f925e9c049566800e3eb90dd2c0808b3
Junsheng Wang <mws19901118@gmail.com>
2024-05-26 12:30:20 -0700

    Update Best Time to Buy and Sell Stock III.py

--- Code/Best Time to Buy and Sell Stock III.py
+++ Code/Best Time to Buy and Sell Stock III.py
@@ -1,20 +1,17 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        maxprofit, minprice = 0, 0x80000000
+        maxprofit, minprice = 0, 100001
         p1 = [0] * len(prices)
         for i in range(len(prices)):                                    #Calculate the maxprofit for 1 transition from beginning to prices[i].
             minprice = min(minprice, prices[i])      
             maxprofit = max(prices[i] - minprice, maxprofit)                 
             p1[i] = maxprofit
             
-        maxprofit, maxprice = 0, -1                                     #Calculate the maxprofit for 1 transition from prices[i] to end.
+        maxprofit, maxprice = 0, -1
         p2 = [0] * len(prices)
-        for i in reversed(range(len(prices))):
-            maxprice = max(maxprice, prices[i])      
+        for i in reversed(range(len(prices))):                          #Calculate the maxprofit for 1 transition from prices[i] to end.
+            maxprice = max(maxprice, prices[i])
             maxprofit = max(maxprice - prices[i], maxprofit)                 
             p2[i] = maxprofit
         
-        maxp = 0
-        for i in range(len(prices)):                                    #Calculate the maxprofit for 2 transitions, seperated at prices[i].
-            maxp = max(p1[i] + p2[i], maxp)
-        return maxp
+        return max(p1[i] + p2[i] for i in range(len(prices)))           #Calculate the maxprofit for 2 transitions, seperated at prices[i].
