071264b71f4a4c463db0e20af6cac40f421d4e55
Junsheng Wang <mws19901118@gmail.com>
2024-05-26 15:12:28 -0700

    Update 132 Pattern.py

--- Code/132 Pattern.py
+++ Code/132 Pattern.py
@@ -4,7 +4,7 @@ class Solution:
         for x in nums[1:]:                                                 #Populate nums.
             minValue.append(min(minValue[-1], x))
         for j in reversed(range(len(nums))):                               #Traverse nums backwards.
-            if nums[j] > minValue[j]:                                      #If nums[j] > minValue[j], we need to find if there is a number in nums[j + 1:] that is greater than minValue[j],
+            if nums[j] > minValue[j]:                                      #If nums[j] > minValue[j], we need to find if there is a number in nums[j + 1:] that is greater than minValue[j]. For the nums[j] <= minValue[j], it will be also smaller than any minValue[k] 0 <= k < j, as minValue[k] >= minValue[j].
                 while stack and stack[-1] <= minValue[j]:                  #While stack id not empty and top of stack is smaller than or equal to minValue[j], pop stack.
                     stack.pop()
                 if stack and stack[-1] < nums[j]:                          #If stack is not empty and top of stack is smaller than nums[j], since stack is mono decreasing so the top of stack is smallest, we found a 132 triplet, return true.
