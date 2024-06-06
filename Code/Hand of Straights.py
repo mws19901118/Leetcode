1de2b610233a51e83b0315ee351815131cb17cfa
Junsheng Wang <mws19901118@gmail.com>
2024-06-05 17:15:05 -0700

    Update Hand of Straights.py

--- Code/Hand of Straights.py
+++ Code/Hand of Straights.py
@@ -1,18 +1,13 @@
 class Solution:
-    def isNStraightHand(self, hand: List[int], W: int) -> bool:
-        if len(hand) % W:                                             #If the number of cards is not a multiple of W, return false.
-            return False
-        d = collections.Counter(hand)                                 #Count each card.
-        sortedCards = sorted(d.keys())                                #Sort unique card.
-        sortedCards = collections.deque(sortedCards)
-        while len(d) > 0:                                             #While there are cards remanining.
-            minCard = sortedCards[0]                                  #Find the smallest card.
-            temp = d[minCard]                                         #Use a temp value to store current count of smallest card.
-            for i in range(W):                                        #For W consecutive number starting from the smallest card.
-                if minCard + i not in d or d[minCard + i] < temp:     #If it's not in hand or its count is smaller than the smallest card, the cards cannot form satisfied group for all smallest card, then return false.
+    def isNStraightHand(self, hand: List[int], groupSize: int) -> bool:
+        count = Counter(hand)                                #Count each card.
+        cardValue = sorted(count.keys())                     #Sort card value in asending order.
+        for x in cardValue:                                  #Traverse card value.
+            if not count[x]:                                 #If current card is processed, skip.
+                continue
+            currentCount = count[x]                          #Store current count.
+            for i in range(groupSize):                       #Traverse the card value that form a group with current value as start.
+                if count[x + i] < currentCount:              #If any value has fewer count than current count, return false.
                     return False
-                d[minCard + i] -= temp                                #Subtracting the count of smallest card from each card to get the remain card.
-                if d[minCard + i] == 0:                               #If no card remain, delete it from d and pop left of sorted cards(it is guarenteed to be the current smallest card).
-                    del d[minCard + i]
-                    sortedCards.pop(0)
-        return True
+                count[x + i] -= currentCount                 #Deduct currentCount from the count of current card.
+        return True                                          #Return true at the end.
