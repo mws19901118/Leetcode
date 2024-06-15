d9613c454458f2b34c4881305e1de771bca66761
Junsheng Wang <mws19901118@gmail.com>
2024-06-15 15:26:14 -0700

    Create Put Boxes Into the Warehouse I.py

---
+++ Code/Put Boxes Into the Warehouse I.py
@@ -0,0 +1,14 @@
+class Solution:
+    def maxBoxesInWarehouse(self, boxes: List[int], warehouse: List[int]) -> int:
+        effectiveHeight = [0] * len(warehouse)                                #Initialize the effective height at each room, which is the max possible box to put here from left.
+        minHeight = inf                                                       #Initialize the min height from left.
+        for i in range(len(warehouse)):                                       #Traverse warehouse from left to right.
+            minHeight = min(warehouse[i], minHeight)                          #Update min height.
+            effectiveHeight[i] = minHeight                                    #Set effectiveHeight[i] to minHeight.
+        effectiveHeight.sort()                                                #Sort effectiveHeight so it is guaranteed that effective[i] can be reach even if all effective[:i] are filled.
+        boxes.sort()                                                          #Sort boxes.
+        index = 0                                                             #Initialize an index to traverse boxes so boxes[:index] can all be put in warehouse.
+        for x in effectiveHeight:                                             #Traverse effectiveHeight.
+            if index < len(boxes) and boxes[index] <= x:                      #If index is valid and boxes[index] can be put here, put it here and move forward index.
+                index += 1
+        return index                                                          #Return index.
