a2c279336f6a64790bad8ecac31baa5d2bf85852
Junsheng Wang <mws19901118@gmail.com>
2024-07-01 17:10:14 -0700

    Update Intersection of Two Arrays II.py

--- Code/Intersection of Two Arrays II.py
+++ Code/Intersection of Two Arrays II.py
@@ -1,7 +1,7 @@
 class Solution:
     def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        count1, count2 = Counter(nums1), Counter(nums2)         #Count nums1 and nums2.
+        count = Counter(nums1) & Counter(nums2)                 #Count nums1 and nums2 and get intersection.
         result = []
-        for x in count1:                                        #For each number in count1, find the min value of its count in nums1 and nums2.
-            result.extend([x] * min(count1[x], count2[x]))      #Append the number to result for times of min value above.
+        for x in count:                                         #For each number in count, append count[x] times to result.
+            result.extend([x] * count[x])
         return result
