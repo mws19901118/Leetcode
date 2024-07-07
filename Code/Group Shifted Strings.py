ac0a8f59e5cdc4620b119c8680dc9c47ef670b2d
Junsheng Wang <mws19901118@gmail.com>
2024-07-06 18:46:02 -0700

    Update Group Shifted Strings.py

--- Code/Group Shifted Strings.py
+++ Code/Group Shifted Strings.py
@@ -1,26 +1,9 @@
-class Solution(object):
-    def groupStrings(self, strings):
-        """
-        :type strings: List[str]
-        :rtype: List[List[str]]
-        """
-        dict = {}                                                                                 #Use a dict to group all the shifted strings.
-        for s in strings:
-            flag = False                                                                          #Indicates if current string can be shifted to strings already in dict.
-            for key in dict.keys():                                                               #Compare current string with each key in dict.
-                if len(s) == len(key):                                                            #If one string can be shifted to another string, they should have the same length.
-                    i = 0
-                    while i < len(s):
-                        if (ord(s[i]) - ord(key[i])) % 26 != (ord(s[0]) - ord(key[0])) % 26:      #If find a place where the distance between 2 characters is different with the distance of the first characters of 2 string, they can't be grouped together.
-                            break
-                        i += 1
-                    if i == len(s):                                                               #If current string can be grouped to a key string, append it to the corresponding list, set flag to be true and break.
-                        dict[key].append(s)
-                        flag = True
-                        break
-            if flag is False:                                                                     #If current string can't be grouped to any key strings, set it as a new key.
-                dict[s] = [s]
-        result = dict.values()
-        for l in result:
-            l.sort()                                                                              #Sort every list.
-        return result
+class Solution:
+    def groupStrings(self, strings: List[str]) -> List[List[str]]:
+        def shift(s: str) -> str:                                                                    #Shift string to align start of string at 'a'.
+            delta = ord(s[0]) - ord('a')                                                             #Get the delta from 'a' to the start of string.
+            return "".join(chr(ord('a') + (ord(x) - ord('a') - delta) % 26) for x in s)              #Shift each character delta characters.
+        groups = defaultdict(list)                                                                   #Group strings by the shifted string.
+        for x in strings:
+            groups[shift(x)].append(x)
+        return groups.values()                                                                       #Return the groups.
