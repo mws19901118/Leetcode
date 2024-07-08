d615aaa1b240972553915c1d75d68f2615b314ae
Junsheng Wang <mws19901118@gmail.com>
2024-07-07 17:03:29 -0700

    Update Custom Sort String.py

--- Code/Custom Sort String.py
+++ Code/Custom Sort String.py
@@ -1,11 +1,11 @@
 class Solution:
-    def customSortString(self, order: str, str: str) -> str:
-        count = Counter(str)                        #Count each letter in str.
-        sortedStr = ""                              #Initialize sorted str.
-        for x in order:                             #Traverse order.
-            if x in count:                          #If x is in str, add all its occurence to sortedStr and delete it from count.
-                sortedStr += x * count[x]
-                del count[x]
-        for x in count:                             #Traverse remaining count.
-            sortedStr += x * count[x]               #Add all its occurence to sortedStr.
-        return sortedStr                            #Return sorted str.
+    def customSortString(self, order: str, s: str) -> str:
+        orderSet = set(x for x in order)            #Store characters in order in a set.
+        count = Counter(s)                          #Count each character in s.
+        result = ""
+        for x in order:                             #Append each character(all occurances) in s that is in order.
+            result += x * count[x]
+        for x in count:                             #Append the charcters(all occurances) in s that is not in order
+            if x not in orderSet:
+                result += x * count[x]
+        return result
