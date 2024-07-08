444792690cf45e41266a6e2a5233833fb521bd5a
Junsheng Wang <mws19901118@gmail.com>
2024-07-08 00:35:36 -0700

    Update Insert into a Sorted Circular Linked List.py

--- Code/Insert into a Sorted Circular Linked List.py
+++ Code/Insert into a Sorted Circular Linked List.py
@@ -1,18 +1,18 @@
 class Solution:
     def insert(self, head: 'Node', insertVal: int) -> 'Node':
-        if not head:                                                                             #If head is none, creat a new single circular linked list and return.
+        if not head:                                                                                       #If head is none, creat a new single circular linked list and return.
             newNode = Node(insertVal, None)
             newNode.next = newNode
             return newNode
  
-        prev, curr = head, head.next                                                             #Initialize prev and curr to be head and head.next.
-        while True:                                                                              #Traverse the circular linked list.
-            if prev.val <= insertVal <= curr.val or \                                            #Scenario 1: insertVal is between prev.val and curr.val, then just insert it after prev.
-            (prev.val > curr.val and (insertVal >= prev.val or insertVal <= curr.val)):          #Scenario 2: prev.val > curr.val, which means prev.val is the largest and curr.val is the smallest. If insertVal is not smaller to prev.val or not larger than curr.val, insertVal should also be inserted after prev.
-                prev.next = Node(insertVal, curr)
-                return head                                                                      #Directly return after insertion.
-            prev, curr = prev.next, curr.next
-            if prev == head:                                                                     #If prev == head, we have traversed the whole circular linked list and not insert the insertVal yet, so break. And it means all nodes have same value.
+        curr = head                                                                                        #Initialize and curr to be head.
+        while True:                                                                                        #Traverse the circular linked list.
+            if curr.val <= insertVal <= curr.next.val or \                                                 #Scenario 1: insertVal is between curr.val and curr.next.val, then just insert it after curr.
+            (curr.val > curr.next.val and (insertVal >= curr.val or insertVal <= curr.next.val)):          #Scenario 2: curr.val > curr.next.val, which means curr.val is the largest and curr.next.val is the smallest. If insertVal is not smaller to curr.val or not larger than curr.next.val, insertVal should also be inserted after curr.
+                curr.next = Node(insertVal, curr.next)
+                return head                                                                                #Directly return after insertion.
+            curr = curr.next
+            if curr == head:                                                                               #If curr == head, we have traversed the whole circular linked list and not insert the insertVal yet, so break. And it means all nodes have same value.
                 break
-        prev.next = Node(insertVal, curr)                                                        #Then we can insert the insertVal at any place, could be just after prev.
-        return head                                                                              #Return head.
+        curr.next = Node(insertVal, curr.next)                                                             #Then we can insert the insertVal at any place, could be just after curr.
+        return head                                                                                        #Return head.
