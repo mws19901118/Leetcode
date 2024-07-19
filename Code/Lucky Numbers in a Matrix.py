68efcb3df0f88ea26467300a7fab529a2a74bbf7
Junsheng Wang <mws19901118@gmail.com>
2024-07-18 17:14:28 -0700

    Create Lucky Numbers in a Matrix.py

---
+++ Code/Lucky Numbers in a Matrix.py
@@ -0,0 +1,6 @@
+class Solution:
+    def luckyNumbers (self, matrix: List[List[int]]) -> List[int]:
+        m, n = len(matrix), len(matrix[0])                                                                          #Get dimensions.
+        rowMin = [min(x) for x in matrix]                                                                           #Get the min value of each row.
+        colMax = [max(matrix[j][i] for j in range(m)) for i in range(n)]                                            #Get the max value of each column.
+        return [matrix[i][j] for i, j in product(range(m), range(n)) if matrix[i][j] == rowMin[i] == colMax[j]]     #Traverse matrix and return all lucky numbers.
