dd35fa9a4a04cf1404a1d46164f3766b0d4c10e8
Junsheng Wang <mws19901118@gmail.com>
2024-08-12 22:50:27 -0700

    Update Combination Sum II.py

--- Code/Combination Sum II.py
+++ Code/Combination Sum II.py
@@ -1,18 +1,19 @@
-import copy
 class Solution:
-    def backtracking(self, candidates, trace, currentSum, target, result):
-        if currentSum == target:
-            result.append(copy.deepcopy(trace))                                                             #Deepcopy trace to result.
-        elif currentSum < target:
-            for i in range(len(candidates)):
-                if i != 0 and candidates[i] == candidates[i - 1]:                                           #Eliminate duplication.
-                    continue
-                trace.append(candidates[i])
-                self.backtracking(candidates[i + 1:], trace, currentSum + candidates[i], target, result)    #Because elements in combination should be non-descending and only appear once, use candidates[i + 1:] in the next step.
-                trace.pop()
-            
     def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
         candidates.sort()                                                                                   #Sort the candidates.
         result = []
-        self.backtracking(candidates, [], 0, target, result)                                                #Backtracking
+        def backtracking(stack: List[int], index: int, remain: int) -> None:
+            if not remain:                                                                                  #If reaches target. deepcopy stack to result.
+                result.append(stack.copy())
+                return
+            for i in range(index, len(candidates)):                                                         #Traverse candidates[index:].
+                if candidates[i] > remain:                                                                  #If candidates[i] > remain, the combination sum will exceeds target, so stop loop here.
+                    break
+                if i > index and candidates[i] == candidates[i - 1]:                                        #Skip duplicate numbers because we don't need to add all duplicate numbers in one iteration. Let them be handled by later iterations. Otherwise. there will be duplications.
+                    continue
+                stack.append(candidates[i])                                                                 #Append candidates[i] to stack.
+                backtracking(stack, i + 1, remain - candidates[i])                                          #Keep backtracking.
+                stack.pop()                                                                                 #Pop stack.
+
+        backtracking([], 0, target)                                                                         #Start backtracking
         return result
