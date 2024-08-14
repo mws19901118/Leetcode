c329bceb0dd7b6f18f3d1ef7f4dd96b6f0680ee5
Junsheng Wang <mws19901118@gmail.com>
2024-08-13 18:51:11 -0700

    Update Find K-th Smallest Pair Distance.py

--- Code/Find K-th Smallest Pair Distance.py
+++ Code/Find K-th Smallest Pair Distance.py
@@ -1,20 +1,21 @@
 class Solution:
-    def possible(self, nums, guess, k):
-        count = left = 0                            #Count the pairs whose distance is not larger than guess.
-        for right, x in enumerate(nums):            #Traverse through nums.
-            while x - nums[left] > guess:           #For each index right, maintain left, the smallest value such that nums[right] - nums[left] <= guess.
-                left += 1                           #So for all the pairs whose right boundary is right, and left bondary is between left and right, the distance is not larger than guess.
-            count += right - left                   #Update count for each right.
-        return count >= k                           #Return if count is equal to or larger than k.
-    
     def smallestDistancePair(self, nums: List[int], k: int) -> int:
-        nums.sort()                                 #Sort nums.
-        start, end = 0, nums[-1] - nums[0]          #Binary search for anwser, starting from 0 and ending at the max distance in nums.
-        while start < end:
-            mid = (start + end) >> 1
-            if self.possible(nums, mid, k):         #Determine if mid is possible to be equal to or larger than the k-th smallest pair distance.
-                end = mid                           #If so, binary search from start to mid to find a smaller solution.
-            else:                                   #Othwise, binary search from mid + 1 to end.
-                start = mid + 1
+        @cache                                                                  #Cache result.
+        def countPair(threshold: int) -> int:                                   #Cound pairs smaller than or equal to threshold.
+            right, count = 0, 0                                                 #Initialize the right pointer of sliding window and count.
+            for i, x in enumerate(nums):                                        #Traverse nums.
+                while right < len(nums) and nums[right] <= x + threshold:       #While right is not reaching the end and nums[right] <= x + threshold, move forward right.
+                    right += 1
+                count += right - i - 1                                          #All the pair which starts at i and ends in nums[i + 1:right] is a pair that is smaller than or equal to threshold.
+            return count                                                        #Return count.
 
-        return start                                #Return start after binary search.
+        nums.sort()                                                             #Sort nums.
+        start, end = 0, nums[-1] - nums[0]                                      #Initialize start and end.
+        while start <= end:                                                     #Binary search.
+            mid = (start + end) // 2                                            #Calculate mid.
+            if countPair(mid) >= k and countPair(mid - 1) < k:                  #If there are at least k pairs that are not greater than mid and fewer than k pairs that are not greater than mid - 1, mid is the k-th smallest pair.
+                return mid
+            elif countPair(mid - 1) >= k:                                       #If there are at least k pairs that are not greater than mid - 1, move end to mid - 1.
+                end = mid - 1
+            else:                                                               #Otherwise, move start to mid + 1.
+                start = mid + 1
