045e02d240465a04a2991255f64ad87a9cf900b9
Junsheng Wang <mws19901118@gmail.com>
2024-08-19 22:25:04 -0700

    Update Stone Game II.py

--- Code/Stone Game II.py
+++ Code/Stone Game II.py
@@ -1,10 +1,10 @@
 class Solution:
     def stoneGameII(self, piles: List[int]) -> int:
         @cache                                                                              #Cache result.
-        def dp(index: int, m: int, isAlice: bool):                                          #Use dp to calculate the max stones Alice can get for piles[index + 1:] and m and if the player is Alice.
+        def dp(index: int, m: int, isAlice: bool) -> int:                                   #Use dp to calculate the max stones Alice can get for piles[index + 1:] and m and if the player is Alice.
             if index >= len(piles) - 1:                                                     #If index >= len(piles) - 1, it reaches the end, so return 0.
                 return 0
-            result = 0 if isAlice else float('inf')                                         #Initialize result, if player is alice, we will return current max value; otherwise, return current min value.
+            result = 0 if isAlice else inf                                                  #Initialize result, if player is alice, we will return current max value; otherwise, return current min value.
             s = 0                                                                           #Initialize the sum of stones Alice takes in this turn.
             for x in range(1, min(2 * m + 1, len(piles) - index)):                          #Traverse from 1 to min(2 * m, len(piles) - index - 1).
                 s += piles[index + x]                                                       #Take current pile.
