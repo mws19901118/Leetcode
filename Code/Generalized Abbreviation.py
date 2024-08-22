fd1ace526c447bca6af3dec4f3a7c78fb81b7e05
Junsheng Wang <mws19901118@gmail.com>
2024-08-21 19:36:13 -0700

    Update Generalized Abbreviation.py

--- Code/Generalized Abbreviation.py
+++ Code/Generalized Abbreviation.py
@@ -1,26 +1,17 @@
-class Solution(object):
-    def generateAbbreviations(self, word):
-        """
-        :type word: str
-        :rtype: List[str]
-        """
-        l = len(word)                           #Get the length of word.
-        n = 2 ** l                              #Each character has 2 state: indicated by itself or indicated by digit. So total number of state is 2^l.
-        result = []
-        for i in range(n):
-            abbr = ""
-            temp = i
-            count = 0                           #Count consecutive characters indicated by digit.
-            for j in range(l):
-                if temp % 2 == 0:               #If temp is even, it's indicated by digit.
-                    count += 1
-                else:                           #Otherwise it's indicated by itself.
-                    if count != 0:              #If count is not 0, add count to abbreviation and reset count.
-                        abbr += str(count)
-                        count = 0               #Add it self to abbareviation.
-                    abbr += word[j]
-                temp = temp >> 1                #Shift temp.
-            if count != 0:                      #If count is not 0, add count to abbreviation and reset count.
-                abbr += str(count)
-            result.append(abbr)
-        return result
+class Solution:
+    def generateAbbreviations(self, word: str) -> List[str]:
+        result = set()                                                        #Initialize result set.
+        def backtracking(index: int, stack: List[str]) -> None:               #Backtracking for word[index:].
+            if index == len(word):                                            #If index reaches the end, join stack and add to result.
+                result.add("".join(stack))
+            for i in range(index, len(word)):                                 #Traverse word[index:].
+                if not (stack and stack[-1].isdigit()):                       #If stack is empty or the last in stack is not digit, we can abbreviate word[index:i + 1].
+                    stack.append(str(i - index + 1))                          #Append str(i - index + 1) to stack.
+                    backtracking(i + 1, stack)                                #Keep backtracking.
+                    stack.pop()                                               #Pop stack.
+                stack.append(word[index:i + 1])                               #Also handle not abbreviate by adding word[index:i + 1] to stack.
+                backtracking(i + 1, stack)                                    #Keep backtracking.
+                stack.pop()                                                   #Pop stack.
+        
+        backtracking(0, [])                                                   #Start backtracking from 0 and empty stack.
+        return list(result)                                                   #Convert result to list and return.
