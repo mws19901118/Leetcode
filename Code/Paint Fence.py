d5fc0aa34de8447f85e7c9db3c68da18b3f5d61e
Junsheng Wang <mws19901118@gmail.com>
2024-09-04 23:47:48 -0700

    Update Paint Fence.py

--- Code/Paint Fence.py
+++ Code/Paint Fence.py
@@ -1,6 +1,6 @@
 class Solution:
     def numWays(self, n: int, k: int) -> int:
-        result = [k, k * k]                                     #The first 2 number are k and k^2.
+        result = [k, k * k]                                     #The first 2 number are k and k ** 2.
         for i in range(2, n):
-            result.append((result[-1] + result[-2]) * (k - 1))  #Current color should be different from either the previous color or the color before the previous color.
+            result.append((result[-1] + result[-2]) * (k - 1))  #Current color should be different from either the previous color: (k - 1) * result[-1] or same with previous color but different with the color before the previous color (k - 1) * result[-2].
         return result[n - 1]
