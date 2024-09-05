fa4ba294157bc78030fdcacd2489b5c60f506dfa
Junsheng Wang <mws19901118@gmail.com>
2024-09-04 21:01:55 -0700

    Update Reverse Integer.py

--- Code/Reverse Integer.py
+++ Code/Reverse Integer.py
@@ -1,26 +1,13 @@
 class Solution:
-    # @param {integer} x
-    # @return {integer}
-    def reverse(self, x):
-        flag=False
-        if x<0:                                         #Deal with negative integer.
-            x=abs(x)
-            flag=True
-        l=0
-        d=1
-        while x>=d:                                     #Get the length of integer.
-            l+=1
-            d*=10
-        d/=10
-        answer=0
-        m=1
-        for i in range(l):                              #Reverse integer. Use d(10^(l-1-i)) and m(10^i) to accelerate.
-            answer+=(x/d)*m
-            x%=d
-            d/=10
-            m*=10
-        if flag:
-            answer=-answer
-        if answer>2147483647 or answer<-2147483648:     #Deal with overflow.
-            return 0
-        return answer
+    def reverse(self, x: int) -> int:
+        sign = 1 if x >= 0 else -1                            #Get the sign of x.
+        x = abs(x)                                            #Take the absolute value of x.
+        result = 0                                            #Initialize result.
+        while x:                                              #Iterate while x > 0.
+            result *= 10                                      #Left shift(as base 10) result a digit.
+            result += x % 10                                  #Add the last digit of x to result.
+            x //= 10                                          #Right shift(as base 10) x a digit.
+        result *= sign                                        #Restore the sign.
+        if result < -(2 ** 31) or result > 2 ** 31 - 1:       #Make sure result is valid 32 bits integer, otherwise return 0.
+            result = 0
+        return result
