205a817006bb3a960c3ea70ca5619cc0e00a5c0e
Junsheng Wang <mws19901118@gmail.com>
2024-09-06 22:13:18 -0700

    Update Linked List in Binary Tree.py

--- Code/Linked List in Binary Tree.py
+++ Code/Linked List in Binary Tree.py
@@ -1,34 +1,28 @@
 # Definition for singly-linked list.
 # class ListNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.next = None
-
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
 # Definition for a binary tree node.
 # class TreeNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
-
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
 class Solution:
-    def isSubPathHelper(self, node, root, isLinkedListHead):                                                                                        #Helper function to carry the information whether current linked list node is the head of original linked list during recursion.
-        if node is None:                                                                                                                            #If linked list head is none, we have finished recursion for the linked list and found a subpath in tree, return true.
-            return True
-        if root is None:                                                                                                                            #If linked list head is not none but tree root is none, we can't do recursion anymore, so return false.
-            return False
-        if isLinkedListHead:                                                                                                                        #If current linked list node is linked list head, we check if linked list node value equals tree root value.
-            subPathStartsHere = False
-            if node.val == root.val:                                                                                                                #If so, do recursion for the next node in linked list and left child and right child of tree root respectively, and get the OR result of them.
-                subPathStartsHere = self.isSubPathHelper(node.next, root.left, False) or self.isSubPathHelper(node.next, root.right, False)
-            if subPathStartsHere is True:                                                                                                           #If result is true, subpath starts here so return true.
-                return True
-            else:                                                                                                                                   #Otherwise, do recursion for the current node in linked list and left child and right child of tree root respectively, and return the OR result of them. 
-                return self.isSubPathHelper(node, root.left, True) or self.isSubPathHelper(node, root.right, True)
-        else:
-            if node.val != root.val:                                                                                                                #If current linked list node is not linked list head, check if linked list node value equals tree root value. If not equal, return false directly. 
-                return False
-            return self.isSubPathHelper(node.next, root.left, False) or self.isSubPathHelper(node.next, root.right, False)                          #Otherwise, do recursion for the next node in linked list and left child and right child of tree root respectively, and return the OR result of them.
-
     def isSubPath(self, head: ListNode, root: TreeNode) -> bool:
-        return self.isSubPathHelper(head, root, True)
+        def traverse(node, root, isLinkedListHead):                                                                                 #Traverse binary tree and linked list simultaneously and carry the information whether current linked list node is the head of original linked list during recursion.
+            if not node:                                                                                                            #If linked list head is none, we have finished recursion for the linked list and found a subpath in tree, return true.
+                return True
+            if not root:                                                                                                            #If linked list head is not none but tree root is none, we can't do recursion anymore, so return false.
+                return False
+            if isLinkedListHead:                                                                                                    #If current linked list node is linked list head, we check if linked list node value equals tree root value.
+                if node.val == root.val and (traverse(node.next, root.left, False) or traverse(node.next, root.right, False)):      #If so, do recursion for the next node in linked list and left child and right child of tree root respectively, and get the OR result of them.
+                    return True                                                                                                     #If result is true, subpath starts here so return true.
+                else:                                                                                                               #Otherwise, do recursion for the current node in linked list and left child and right child of tree root respectively, and return the OR result of them. 
+                    return traverse(node, root.left, True) or traverse(node, root.right, True)
+            else:
+                if node.val != root.val:                                                                                            #If current linked list node is not linked list head, check if linked list node value equals tree root value. If not equal, return false directly. 
+                    return False
+                return traverse(node.next, root.left, False) or traverse(node.next, root.right, False)                              #Otherwise, do recursion for the next node in linked list and left child and right child of tree root respectively, and return the OR result of them.
+        return traverse(head, root, True)
