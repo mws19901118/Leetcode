21dfbf84551dc2d29817de7ece1e6e9a0b7f221f
Junsheng Wang <mws19901118@gmail.com>
2024-09-12 23:44:48 -0700

    Update Edit Distance.py

--- Code/Edit Distance.py
+++ Code/Edit Distance.py
@@ -1,10 +1,15 @@
 class Solution:
     def minDistance(self, word1: str, word2: str) -> int:
-        dp = [[0 for _ in range(len(word2) + 1)] for _ in range(len(word1) + 1)]                                                    #Initialize 2D dp array for edit distance.
-        for i in range(len(word1)):                                                                                                 #Initialize the edit distance from empty string to word1.
-            dp[i + 1][0] = i + 1
-        for i in range(len(word2)):                                                                                                 #Initialize the edit distance from empty string to word2.
-            dp[0][i + 1] = i + 1
-        for i, j in itertools.product(range(len(word1)), range(len(word2))):                                                        #Traverse each pair of characters in word1 and word2.
-            dp[i + 1][j + 1] = dp[i][j] if word1[i] == word2[j] else min(dp[i][j + 1], dp[i][j], dp[i + 1][j]) + 1                  #If word1[i] == word2[j], dp[i + 1][j + 1] = dp[i][j]; otherwise, dp[i + 1][j + 1] = min(dp[i][j + 1], dp[i][j], dp[i + 1][j]) + 1.
-        return dp[-1][-1]                                                                                                           #Return dp[-1][-1] as the edit distance of word1 and word2.
+        m, n = len(word1), len(word2)                                        #Get the dimensions.
+        dp = [i for i in range(n + 1)]                                       #Initialize dp: dp[j] is the min edit distance for word2[:j] and word1[:i] for each iteration i. Since initially i is 0, dp[j] is just the edit distance between word2[:j] and an empty string.
+        for i in range(m):                                                   #Traverse each index in word1.
+            prev = dp[0]                                                     #Set prev to dp[0], which is used to represent the min edit distance between word1[:i] and word2[:j]. Since initiatlly j is 0, then prev is dp[0].
+            dp[0] = i + 1                                                    #Update dp[0] to be the edit distance between word1[:i + 1] and empty string(j is 0).
+            for j in range(n):                                               #Traverse each index in word2.
+                temp = dp[j + 1]                                             #Temporarily store dp[j + 1].
+                if word1[i] == word2[j]:                                     #If word1[i] == word2[j], the edit distance between word1[:i + 1] and word2[:j + 1] is same as the edit distance between word1[:i] and word2[:j], i.e. prev.
+                    dp[j + 1] = prev
+                else:                                                        #Otherwise, the edit distance between word1[:i + 1] and word2[:j + 1] is the minimum of edit distance between word[:i + 1] and word2[:j], i.e. dp[j], edit distance between word[:i] and word2[:j + 1], i.e. dp[j + 1], and edit distance between word[:i] and word2[:j], i.e. prev, then plus 1.
+                    dp[j + 1] = min(dp[j], dp[j + 1], prev) + 1
+                prev = temp                                                  #Update prev to be temp.
+        return dp[-1]                                                        #Return dp[-1].
