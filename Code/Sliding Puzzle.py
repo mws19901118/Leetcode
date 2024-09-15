00fb07d7c36297aa4ebbe58fc76eef0946dc369e
Junsheng Wang <mws19901118@gmail.com>
2024-09-15 15:20:39 -0700

    Update Sliding Puzzle.py

--- Code/Sliding Puzzle.py
+++ Code/Sliding Puzzle.py
@@ -1,39 +1,37 @@
-import copy
 class Solution:
-    def finished(self, board):                                                                              #Given the tuple representing the board, determine if game reaches the end.
-        return all(board[i] == i + 1 for i in range(len(board) - 1))                                        #The ith element should be equal to i + 1, except the last element.
-    
-    def convertBoardToTuple(self, board):                                                                   #Covert the board from list of list to tuple.
-        r = []
-        for x in board:                                                                                     #Flatten the list of list.
-            r.extend(x)
-        return tuple(r)                                                                                     #Convert to tuple.
-    
     def slidingPuzzle(self, board: List[List[int]]) -> int:
-        trace = set()                                                                                       #Use set to record trace.
-        count = 0                                                                                           #Count number of moves.
-        p = None
-        for i in range(len(board)):                                                                         #Find the inital position of empty square.
-            for j in range(len(board[i])):
-                if board[i][j] == 0:
-                    p = (i, j)
-        q = [(self.convertBoardToTuple(board), p)]                                                          #Store the current pattern of board, as a tuple of board tuple and white square position tuple.
-        while q:                                                                                            #BFS.
-            nextq = []                                                                                      #Next level.
-            for x in q:
-                b, c = x[0], x[1]                                                                           #Get board and white square postion.
-                if self.finished(b):                                                                        #If board is in finished pattern, return count.
-                    return count
-                if b in trace:                                                                              #If board has already been passed, continue.
-                    continue
-                for d in [(0, -1), (1, 0), (0, 1), (-1, 0)]:                                                #Enumerate 4 directions.
-                    newc = (c[0] + d[0], c[1] + d[1])                                                       #Calculate new position.
-                    if newc[0] >= 0 and newc[0] < len(board) and newc[1] >= 0 and newc[1] < len(board[0]):  #If new positon is valid.
-                        t = list(b)                                                                         #Convert board to a list.
-                        t[c[0] * len(board[0]) + c[1]] = b[newc[0] * len(board[0]) + newc[1]]               #Swap the square of current position and new position.
-                        t[newc[0] * len(board[0]) + newc[1]] = 0
-                        nextq.append((tuple(t), newc))                                                      #Append current pattern of board, as a tuple of board tuple and white square position tuple, to nextq.
-                trace.add(b)
-            count += 1                                                                                      #Increase count by 1.
-            q = nextq                                                                                       #Replace q with nextq.
-        return -1                                                                                           #If cannot finish, return -1.
+        def serialize(matrix: List[List[str]]) -> str:                                                #Serialize board to string.
+            return "".join([str(x) for x in matrix[0]] + [str(x) for x in matrix[1]])
+
+        def deserialize(s: str) -> List[List[int]]:                                                   #Deserialize board from string.
+            matrix = [[0, 0, 0], [0, 0, 0]]
+            for i, j in product(range(2), range(3)):
+                matrix[i][j] = int(s[i * 3 + j])
+            return matrix
+
+        x, y = -1, -1                                                                                 #Find the initial position of 0.
+        for i, j in product(range(2), range(3)):
+            if not board[i][j]:
+                x, y = i, j
+                break
+        q = [(x, y, serialize(board))]
+        visited = set([q[0][2]])
+        moves = 0
+        while q:                                                                                      #BFS.
+            newq = []
+            for x, y, s in q:                                                                         #Traverse q,
+                if s == "123450":                                                                     #If s is the target state, return moves.
+                    return moves
+                for u, v in [(x - 1, y), (x, y + 1), (x + 1, y), (x, y - 1)]:                         #Traverse 4 neighbors.
+                    if u < 0 or u >= 2 or v < 0 or v >= 3:                                            #If neighbor is invalid, continue. 
+                        continue
+                    currBoard = deserialize(s)                                                        #Deserialize board.
+                    currBoard[u][v], currBoard[x][y] = currBoard[x][y], currBoard[u][v]               #Perform swap with neighbor.
+                    new_s = serialize(currBoard)
+                    if new_s in visited:                                                              #If the new board is visited, continue.
+                        continue
+                    visited.add(new_s)                                                                #Mark new board as visited.
+                    newq.append((u, v, new_s))                                                        #Append neighbor and new board to newq.
+            q = newq
+            moves += 1
+        return -1                                                                                     #Return -1 if cannot reach target state.
