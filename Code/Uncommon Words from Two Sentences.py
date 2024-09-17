336fa7a59a9902a03fa5e8077c89f1743518c6be
Junsheng Wang <mws19901118@gmail.com>
2024-09-16 17:29:10 -0700

    Update Uncommon Words from Two Sentences.py

--- Code/Uncommon Words from Two Sentences.py
+++ Code/Uncommon Words from Two Sentences.py
@@ -1,24 +1,4 @@
-from collections import defaultdict
 class Solution:
-    def uncommonFromSentences(self, A, B):
-        """
-        :type A: str
-        :type B: str
-        :rtype: List[str]
-        """
-        wordsA = A.split(" ")                       #Split A by space.
-        wordsB = B.split(" ")                       #Split B by space.
-        dictA = defaultdict(int)
-        dictB = defaultdict(int)
-        for a in wordsA:                            #Count each word in A.
-            dictA[a] += 1
-        for b in wordsB:                            #Count each word in B.
-            dictB[b] += 1
-        result = []
-        for a in dictA:
-            if a not in dictB and dictA[a] == 1:    #If a word only appears only once in A and not in B, add to result. 
-                result.append(a)
-        for b in dictB:
-            if b not in dictA and dictB[b] == 1:    #If a word only appears only once in B and not in A, add to result.
-                result.append(b)
-        return result
+    def uncommonFromSentences(self, s1: str, s2: str) -> List[str]:
+        words1, words2 = Counter(s1.split(" ")), Counter(s2.split(" "))                                                                            #Count words in s1 and words in s2.
+        return (set(x for x in words1 if words1[x] == 1) - words2.keys()) | (set(x for x in words2 if words2[x] == 1) - words1.keys())             #Find uncommon words.
