7eb1aa21203e7d6cc49f44f4285996dbc5387421
Junsheng Wang <mws19901118@gmail.com>
2024-09-16 21:50:03 -0700

    Update IP to CIDR.py

--- Code/IP to CIDR.py
+++ Code/IP to CIDR.py
@@ -1,29 +1,23 @@
 class Solution:
     def ipToCIDR(self, ip: str, n: int) -> List[str]:
+        def intToCIDR(x: int, length: int) -> str:                                                #Convert ip in int to CIDR string with given length.
+            chunks = [0, 0, 0, 0]
+            for i in range(4):                                                                    #Calculate the 4 chunks of ip.
+                chunks[i], x = divmod(x, 1 << 8 * (3 - i))
+            return ".".join([str(x) for x in chunks]) + "/" + str(length)                         #Convert each chunk to string and join by "," then add "/" and length as string.
+
+        ipInt = sum(int(x) * 1 << 8 * (3 - i) for i, x in enumerate(ip.split(".")))               #Convert ip string to int.
+        masks = {i: 1 << (32 - i) for i in range(33)}                                             #Create bit mask for each length of CIDR from 0 to 32 inclusive.
+        limit = ipInt + n                                                                         #Calculate the limit of ips.
         result = []
-        power = [2 ** 24, 2 ** 16, 2 ** 8, 2 ** 0]                #Record the power of 2 for each IP segment.
-        ipSegments = list(map(lambda x:int(x), ip.split(".")))    #Convert IP from string to a list of integers; each integer is a segment of IP, ordering from high to low.
-        ipInt = 0
-        for i in range(4):                                        #Convert IP to int.
-            ipInt += ipSegments[i] * power[i]
-        while n > 0:                                              #While there are remaining IPs in range.
-            p = 1
-            while ipInt % p == 0 and n >= p:                      #Find the largest power of 2 that is not large than remaining IPs and is a divider of current IP integer.
-                p *= 2                                            #Such power of 2 can form the largest possible CIDR block.
-            p /= 2
-            
-            t = ipInt                                             #Covert IP integer to CIDR.
-            CIDR = ""
-            for i in range(4):
-                CIDR += str(int(t / power[i]))
-                t %= power[i]
-                if i != 3:
-                    CIDR += "."
-                else:
-                    CIDR += "/"
-            CIDR += str(32 - int(math.log(p, 2)))
-            result.append(CIDR)
-            
-            n -= p                                                #Update IP integer and remaining IPs.
-            ipInt += p
+        while ipInt < limit:                                                                      #Iterate while ipInt is smaller than limit.
+            i = 0
+            while i <= 32 and ipInt & (masks[i] - 1) != 0:                                        #Find if current ip can be the start of CIDR at any length(from longer to shorter).
+                i += 1                                                                            #Basically for CIDR length x, check if the last 32 - i bits are all 0.
+            coverage = masks[i]                                                                   #For the CIDR length x, it can cover 1 << (32 - i) number of ip.
+            while ipInt + coverage - 1 >= limit:                                                  #While the whole coverage reaches ip limit, shrink the length and coverage until not reaching ip limit.
+                coverage >>= 1
+                i += 1
+            result.append(intToCIDR(ipInt, i))                                                    #Covert current ip to CIDR with length i and add CIDR to result.
+            ipInt += coverage                                                                     #Move ipInt forward to the first ip that cannot cover by this CIDR.
         return result
