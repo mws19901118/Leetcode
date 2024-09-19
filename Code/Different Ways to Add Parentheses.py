fd744e0c40c5c0cd646d667596dfd0f5ae30f1d3
Junsheng Wang <mws19901118@gmail.com>
2024-09-18 18:27:58 -0700

    Update Different Ways to Add Parentheses.py

--- Code/Different Ways to Add Parentheses.py
+++ Code/Different Ways to Add Parentheses.py
@@ -1,12 +1,8 @@
 class Solution:
-    # @param {string} input
-    # @return {integer[]}
-    def diffWaysToCompute(self, input):
-        result=[a+b if c == '+' else a-b if c == '-' else a*b     #Divede and conquer at each operator.
-            for i, c in enumerate(input) if c in '+-*'            #Use 'enumerate' to get every the index and character.
-            for a in self.diffWaysToCompute(input[:i])            #Get the result of expression before c.
-            for b in self.diffWaysToCompute(input[i+1:])]         #Get the result of expression after c.
-        if result==[]:                                            #If result is none, the expression only has a integer, return it in list.
-            return [int(input)]
-        else:
-            return result
+    @cache
+    def diffWaysToCompute(self, expression: str) -> List[int]:
+        result = [a + b if c == '+' else a - b if c == '-' else a * b     #Divede and conquer at each operator.
+            for i, c in enumerate(expression) if c in '+-*'               #Use 'enumerate' to get every the index and character.
+            for a in self.diffWaysToCompute(expression[:i])               #Get the result of expression before c.
+            for b in self.diffWaysToCompute(expression[i + 1:])]          #Get the result of expression after c.
+        return [int(expression)] if not result else result                #If result is empty, expression is just a number so return it in a list; otherwise, return result.
