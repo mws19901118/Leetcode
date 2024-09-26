c78432b707abecfb4c654d84f81c9da4f1530e99
Junsheng Wang <mws19901118@gmail.com>
2024-09-25 18:01:17 -0700

    Update My Calendar I.py

--- Code/My Calendar I.py
+++ Code/My Calendar I.py
@@ -1,31 +1,12 @@
-class Node:                                             #Binary search tree node for interval.
-    def __init__(self, start, end):
-        self.start, self.end = start, end               #Initialize node with interval start and end.
-        self.left, self.right = None, None              #Initially, left child and right child is none.
-
-    def insert(self, node) -> bool:                     #Insert node to binary search tree.
-        if node.start >= self.end:                      #If node's start is not smaller than current end, set node to be the right child of current node and return true if right child is none.
-            if not self.right:
-                self.right = node
-                return True
-            return self.right.insert(node)              #If right child is not none, insert it to right subtree recursively.
-        elif node.end <= self.start:                    #If node's end is not smaller than current start, set node to be the left child of current node and return true if left child is none.
-            if not self.left:
-                self.left = node
-                return True
-            return self.left.insert(node)               #If left child is not none, insert it to left subtree recursively.
-        else:                                           #Else, there is overlap, so return false.
-            return False
+from sortedcontainers import SortedList
 
 class MyCalendar:
-
     def __init__(self):
-        self.root = Node(-1, -1)                        #Initialize binary search tree with a dummyhead.
+        self.calendar = SortedList()                                                                                                        #Store events in a sorted list.
 
     def book(self, start: int, end: int) -> bool:
-        return self.root.insert(Node(start, end))       #Return the result of insert new interval node.
-
-
-# Your MyCalendar object will be instantiated and called as such:
-# obj = MyCalendar()
-# param_1 = obj.book(start,end)
+        index = self.calendar.bisect_right((start, end))                                                                                    #Binary search for the rightmost index to insert current event.
+        if (index > 0 and self.calendar[index - 1][1] > start) or (index < len(self.calendar) and self.calendar[index][0] < end):           #If it conflicts with event before or after, return false.
+            return False
+        self.calendar.add((start, end))                                                                                                     #Otherwise, insert current event and return true.
+        return True
