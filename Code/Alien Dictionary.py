efd85d93892e66ed68fdb051afa9e9ff76879ab3
Junsheng Wang <mws19901118@gmail.com>
2024-09-29 22:12:06 -0700

    Update Alien Dictionary.py

--- Code/Alien Dictionary.py
+++ Code/Alien Dictionary.py
@@ -1,39 +1,26 @@
-class Solution(object):
-    def alienOrder(self, words):
-        """
-        :type words: List[str]
-        :rtype: str
-        """
-        matrix = [[0 for i in range(26)] for j in range(26)]            #This is adjacent matrix.
-        indegree = {}                                                   #Record the indegree of each character.
-        alphabet = set()                                                #Record the set of characters
-        for i in range(len(words)):
-            flag = False
-            for j in range(len(words[i])):
-                alphabet.add(words[i][j])                               #Add character to alphabet.
-                                                                        #The first different between current word and next word will generate a topological order of 2 characters.
-                if flag == False and i < len(words) - 1 and j < len(words[i + 1]) and words[i][j] != words[i + 1][j]:
-                    matrix[ord(words[i][j]) - ord('a')][ord(words[i + 1][j]) - ord('a')] = 1
-                    flag = True
-                    
-        for i in alphabet:                                              #Calculate the indegree of each character.
-            count = 0
-            for j in range(26):
-                if matrix[j][ord(i) - ord('a')] == 1:
-                    count += 1
-            indegree[i] = count
-        result = []
-        while len(alphabet) != 0:                                       #Topological sort.
-            c = '0'
-            for i in alphabet:                                          #Find the character with 0 indgree.
-                if indegree[i] == 0:
-                    c = i
-                    break
-            if c == '0':                                                #If can not find, there are cycles, thus no order is valid.
+class Solution:
+    def alienOrder(self, words: List[str]) -> str:
+        adjacentList, indegree, letters = defaultdict(list), Counter(), set()                            #Initialize adjacent list, indegree and letters.
+        for x in words:                                                                                  #Traverse each word and add each leeter to letters.
+            for y in x:
+                letters.add(y)
+        for i in range(len(words) - 1):                                                                  #Traverse each adjacent pair of words.
+            j, k = 0, 0
+            while j < len(words[i]) and k < len(words[i + 1]) and words[i][j] == words[i + 1][k]:        #Traverse 2 words simultonously until they diverge.
+                j += 1
+                k += 1
+            if j < len(words[i]) and k == len(words[i + 1]):                                             #If k reaches end first, it is invalid, so return "" directly.
                 return ""
-            for j in range(26):                                         #Update indgree of the characters which have direct link from character.
-               if matrix[ord(c) - ord('a')][j] == 1:
-                   indegree[chr(j + ord('a'))] -= 1
-            result.append(c)                                            #Append current character to result list.
-            alphabet.remove(c)                                          #Remove current character from alphabet.
-        return ''.join(result)
+            if j < len(words[i]) and k < len(words[i + 1]):                                              #Add words[i + 1][k] to the adjacent list of words[i][j] also increase indegree of words[i + 1][k].
+                adjacentList[words[i][j]].append(words[i + 1][k])
+                indegree[words[i + 1][k]] += 1
+        result = []
+        dq = deque([x for x in letters if not indegree[x]])                                              #Find the starting letters whose indegree is 0.
+        while dq:                                                                                        #Topological sort in BFS.
+            x = dq.popleft()
+            result.append(x)
+            for y in adjacentList[x]:
+                indegree[y] -= 1
+                if not indegree[y]:
+                    dq.append(y)
+        return "" if len(result) < len(letters) else "".join(result)                                     #If not all letters are in result, there are cycles, so return ""; otherwise, join result and return.
