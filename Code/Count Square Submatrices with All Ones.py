e90c20db724a1f084459d0a70c044ce678da66f1
Junsheng Wang <mws19901118@gmail.com>
2024-10-26 23:23:06 -0700

    Update Count Square Submatrices with All Ones.py

--- Code/Count Square Submatrices with All Ones.py
+++ Code/Count Square Submatrices with All Ones.py
@@ -2,13 +2,9 @@ class Solution:
     def countSquares(self, matrix: List[List[int]]) -> int:
         m, n = len(matrix), len(matrix[0])                                                                                  #Get the dimensions.
         result = 0                                                                                                          #Count squares.
-        sideLength = [[0 for j in range(n)] for i in range(m)]                                                              #Use a matrix with same dimension to store the max square side length of each coordinate.
-        for i in range(m):
-            for j in range(n):
-                if matrix[i][j] == 1:
-                    if i > 0 and j > 0:                                                                                     #If current coordinate is not left side or top side, its max square side length is the min square side length of left coordinate, top coordiante, left top coordinate plus one.
-                        sideLength[i][j] = min(sideLength[i - 1][j], sideLength[i][j - 1], sideLength[i - 1][j - 1]) + 1
-                    else:                                                                                                   #Otherwise, it's just one.
-                        sideLength[i][j] = 1
-                    result += sideLength[i][j]                                                                              #The number of squares whose bottom right corner is current coordinate equals the max square side length.
+        sideLength = [[0 for j in range(n + 1)] for i in range(m + 1)]                                                      #Use a matrix with same dimension to store the max square side length of each coordinate.
+        for i, j in product(range(m), range(n)):                                                                            #Traverse matrix.
+            if matrix[i][j]:                                                                                                #If current coordinate is 1, its max square side length is the min square side length of left coordinate, top coordiante, left top coordinate plus one.                                                                             #If current coordinate is not left side or top side, 
+                sideLength[i][j] = min(sideLength[i - 1][j], sideLength[i][j - 1], sideLength[i - 1][j - 1]) + 1
+                result += sideLength[i][j]                                                                                  #The number of squares whose bottom right corner is current coordinate equals the max square side length.
         return result
