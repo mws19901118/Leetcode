c4d74d9ffe607b5b681b7e018ed0bc0df99902ca
Junsheng Wang <mws19901118@gmail.com>
2025-01-07 14:50:28 -0800

    Update Perform String Shifts.py

--- Code/Perform String Shifts.py
+++ Code/Perform String Shifts.py
@@ -1,10 +1,4 @@
 class Solution:
     def stringShift(self, s: str, shift: List[List[int]]) -> str:
-        count = 0
-        for x in shift:                   #Count the total distance shift right.
-            if x[0] == 1:
-                count += x[1]
-            else:
-                count -= x[1]
-        count %= len(s)                   #Get the modulo because total distance shift should be smaller than length of string. 
-        return s[-count:] + s[:-count]    #Perform shift.
+        count = sum((-y if not x else y) for x, y in shift) % len(s) #Count the total distance shift right and get the modulo because total distance shift should be smaller than length of string. 
+        return s[-count:] + s[:-count]                               #Perform shift.
