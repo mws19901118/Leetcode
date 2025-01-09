4fdeaf5e6b0b5c54b7854ef6af25692601c8fc21
Junsheng Wang <mws19901118@gmail.com>
2025-01-08 16:24:36 -0800

    Update Count Prefix and Suffix Pairs I.py

--- Code/Count Prefix and Suffix Pairs I.py
+++ Code/Count Prefix and Suffix Pairs I.py
@@ -1,3 +1,3 @@
 class Solution:
     def countPrefixSuffixPairs(self, words: List[str]) -> int:
-        return sum(int(len(x) <= len(y) and y[:len(x)] == y[-len(x):] == x) for i, x in enumerate(words) for _, y in enumerate(words[i + 1:]))    #Enumerate each pair, and return the count of pair x and y that x is both prefix and suffix of y.
+        return sum(int(y.startswith(x) and y.endswith(x)) for i, x in enumerate(words) for _, y in enumerate(words[i + 1:]))    #Enumerate each pair, and return the count of pair x and y that x is both prefix and suffix of y.
