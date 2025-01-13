e056f8eadfdc0887d99c4a9a36b49fe1d18d5350
Junsheng Wang <mws19901118@gmail.com>
2025-01-12 20:42:10 -0800

    Create Minimum Length of String After Operations.py

---
+++ Code/Minimum Length of String After Operations.py
@@ -0,0 +1,3 @@
+class Solution:
+    def minimumLength(self, s: str) -> int:
+        return sum((x + 1) % 2 + 1 for x in Counter(s).values())    #Count each character in s, and for each count x, reduce it by 2 until smaller than 3. SUm up the result and return.
