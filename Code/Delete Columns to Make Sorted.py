8620edf2603dc9aa8719ff686b25a645c8b08d64
Junsheng Wang <mws19901118@gmail.com>
2025-01-22 16:29:42 -0800

    Update Delete Columns to Make Sorted.py

--- Code/Delete Columns to Make Sorted.py
+++ Code/Delete Columns to Make Sorted.py
@@ -1,4 +1,4 @@
 class Solution:
     def minDeletionSize(self, strs: List[str]) -> int:
-        m, n = len(strs), len(strs[0])                                                                      #Get dimensions.
+        m, n = len(strs), len(strs[0])                                                                      #Get the dimensions.
         return sum(any(strs[j][i] > strs[j + 1][i] for j in range(m - 1)) for i in range(n))                #Return the count of columns that are not sorted.
