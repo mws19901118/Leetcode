1586ad62f8a5a0bc18849f1ad55a094ef7f56c5a
Junsheng Wang <mws19901118@gmail.com>
2025-03-19 22:46:13 -0700

    Update Number of Islands II.py

--- Code/Number of Islands II.py
+++ Code/Number of Islands II.py
@@ -17,10 +17,10 @@ class UnionFind:                                                                
         
 class Solution:
     def numIslands2(self, m: int, n: int, positions: List[List[int]]) -> List[int]:
-        count, result, ufs = 0, [], {}                                                                                      #Initialize count, result and union finds.
+        count, result, ufs = 0, [], {}                                                                                      #Initialize count, result and Union-Finds.
         for i, j in positions:                                                                                              #Traverse positions.
             if (i, j) not in ufs:                                                                                           #If (i, j) is not in ufs, it's a new island.
-                ufs[(i, j)] = UnionFind((i, j))                                                                             #Create a new UnionFind for (i, j).
+                ufs[(i, j)] = UnionFind((i, j))                                                                             #Create a new Union-Find for (i, j).
                 count += 1                                                                                                  #Increase count.
                 for x, y in [(i - 1, j), (i, j + 1), (i + 1, j), (i, j - 1)]:                                               #Traverse all neighbors.
                     if (x, y) in ufs and not ufs[(i, j)].union(ufs[(x, y)]):                                                #If neighbor is valid and is island and can be unioned with current island, decrease count.
