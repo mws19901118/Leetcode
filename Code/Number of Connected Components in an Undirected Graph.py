339ac055b2e1aad000ad5fa365f4af26fba3228a
Junsheng Wang <mws19901118@gmail.com>
2025-03-19 22:51:10 -0700

    Update Number of Connected Components in an Undirected Graph.py

--- Code/Number of Connected Components in an Undirected Graph.py
+++ Code/Number of Connected Components in an Undirected Graph.py
@@ -1,21 +1,23 @@
-class UnionFind:                                                                                #Union find.
-    def __init__(self, x: int) -> None:
-        self.label = x
+class UnionFind:                                                                               #Union-Find class.
+    def __init__(self, x: int):
         self.parent = None
+        self.label = x
 
-    def find(self):
+    def find(self) -> 'UnionFind':
         if not self.parent:
             return self
         self.parent = self.parent.find()
         return self.parent
 
-    def union(self, x) -> None:
-        if self.find() is not x.find():
-            self.find().parent = x.find()
+    def union(self, uf: 'UnionFind') -> bool:
+        if self.find().label == uf.find().label:
+            return True
+        self.find().parent = uf.find()
+        return False
 
 class Solution:
     def countComponents(self, n: int, edges: List[List[int]]) -> int:
-        uf = [UnionFind(x) for x in range(n)]                                                   #Insteniate a list of n UnionFind.
-        for a, b in edges:                                                                      #Union the UnionFind of the 2 nodes of each edge.
+        uf = [UnionFind(x) for x in range(n)]                                                   #Create a Union-Find for each node..
+        for a, b in edges:                                                                      #Union the Union-Finds of the 2 nodes of each edge.
             uf[a].union(uf[b])
-        return len(set([x.find().label for x in uf]))                                           #Find size of distinct label of the parent of each UnionFind. 
+        return len(set([x.find().label for x in uf]))                                           #Find size of distinct label of the parent of each Union-Find. 
