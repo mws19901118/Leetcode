8a485829d947daa17f45a6337a474606963edd08
Junsheng Wang <mws19901118@gmail.com>
2025-03-19 23:10:28 -0700

    Update The Earliest Moment When Everyone Become Friend.py

--- Code/The Earliest Moment When Everyone Become Friend.py
+++ Code/The Earliest Moment When Everyone Become Friend.py
@@ -1,7 +1,7 @@
-class UnionFind:                                                                          #Union find class.
-    def __init__(self, label: int):
-        self.label = label
+class UnionFind:                                                                          #Union-Find class.
+    def __init__(self, x: int):
         self.parent = None
+        self.label = x
 
     def find(self) -> 'UnionFind':
         if not self.parent:
@@ -9,7 +9,7 @@ class UnionFind:                                                                
         self.parent = self.parent.find()
         return self.parent
 
-    def union(self, uf: 'UnionFind') -> bool:                                             #Union self and uf then return if union is required. 
+    def union(self, uf: 'UnionFind') -> bool:
         if self.find().label == uf.find().label:
             return False
         self.find().parent = uf.find()
@@ -17,7 +17,7 @@ class UnionFind:                                                                
 
 class Solution:
     def earliestAcq(self, logs: List[List[int]], n: int) -> int:
-        ufs = [UnionFind(i) for i in range(n)]                                            #Instentiate a UnionFind for each person.
+        ufs = [UnionFind(i) for i in range(n)]                                            #Instentiate a Union-Find for each person.
         for t, x, y in sorted(logs):                                                      #Sort logs by timestamp in asending order and traverse.
             if ufs[x].union(ufs[y]):                                                      #If ufs[x] and ufs[y] needs union, they become friends at current time, decrease n by 1.
                 n -= 1
