8f8277e215cd9d0d02a67deecde437beb8943c40
Junsheng Wang <mws19901118@gmail.com>
2025-03-19 23:03:29 -0700

    Update Redundant Connection.py

--- Code/Redundant Connection.py
+++ Code/Redundant Connection.py
@@ -11,15 +11,15 @@ class UnionFind:                                                                
 
     def union(self, uf: 'UnionFind') -> bool:
         if self.find().label == uf.find().label:
-            return True
+            return False
         self.find().parent = uf.find()
-        return False
+        return True
         
 class Solution:
     def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
         ufs = [UnionFind(i + 1) for i in range(len(edges))]                         #Create a Union-Find for each node.
         result = []                                                                 #Initialize result.
         for x, y in edges:                                                          #Traverse edges.
-            if ufs[x - 1].union(ufs[y - 1]):                                        #If union ufs[x - 1] and ufs[y - 1] and update result to [x, y] if they already are in same component.
+            if not ufs[x - 1].union(ufs[y - 1]):                                    #If union ufs[x - 1] and ufs[y - 1] and update result to [x, y] if they already are in same component.
                 result = [x, y]
         return result
