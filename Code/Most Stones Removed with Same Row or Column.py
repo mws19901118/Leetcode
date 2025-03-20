2965a976e54618506b2be1639ed63fbc9d3c9617
Junsheng Wang <mws19901118@gmail.com>
2025-03-19 22:49:06 -0700

    Update Most Stones Removed with Same Row or Column.py

--- Code/Most Stones Removed with Same Row or Column.py
+++ Code/Most Stones Removed with Same Row or Column.py
@@ -1,7 +1,7 @@
-class UnionFind:                                                                    #UnionFind class.
-    def __init__(self, label):
+class UnionFind:                                                                     #Union-Find class.
+    def __init__(self, x: int):
         self.parent = None
-        self.label = label
+        self.label = x
 
     def find(self) -> 'UnionFind':
         if not self.parent:
@@ -9,14 +9,16 @@ class UnionFind:                                                                
         self.parent = self.parent.find()
         return self.parent
 
-    def union(self, uf: 'UnionFind'):
-        if self.find().label != uf.find().label:
-            self.find().parent = uf.find()
+    def union(self, uf: 'UnionFind') -> bool:
+        if self.find().label == uf.find().label:
+            return True
+        self.find().parent = uf.find()
+        return False
 
 class Solution:
     def removeStones(self, stones: List[List[int]]) -> int:
         rows, columns = {}, {}                                                      #Store the index of first stone in each row and column.
-        ufs = [UnionFind((x, y)) for x, y in stones]                                #Initialize a union find for each stone.
+        ufs = [UnionFind((x, y)) for x, y in stones]                                #Initialize a Union-Find for each stone.
         for i, (x, y) in enumerate(stones):                                         #Traverse stones.
             if x not in rows:                                                       #If x not in rows, set rows[x] to i.
                 rows[x] = i
