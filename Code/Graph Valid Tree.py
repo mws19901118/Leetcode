0634674992d60faa02a8fd9fcae6503d79b6e8b2
Junsheng Wang <mws19901118@gmail.com>
2025-03-19 23:08:11 -0700

    Update Graph Valid Tree.py

--- Code/Graph Valid Tree.py
+++ Code/Graph Valid Tree.py
@@ -11,11 +11,11 @@ class UnionFind:                                                                
 
     def union(self, uf: 'UnionFind') -> bool:
         if self.find().label == uf.find().label:
-            return True
+            return False
         self.find().parent = uf.find()
-        return False
+        return True
 
 class Solution:
     def validTree(self, n: int, edges: List[List[int]]) -> bool:
         ufs = [UnionFind(i) for i in range(n)]                                             #Create a Union-Find for each node.
-        return len(edges) == n - 1 and not any(ufs[x].union(ufs[y]) for x, y in edges)     #The number of edges should be exactly n - 1; also there shouldn't be any edge connecting 2 nodes that are already in same component.
+        return len(edges) == n - 1 and not any(not ufs[x].union(ufs[y]) for x, y in edges) #The number of edges should be exactly n - 1; also there shouldn't be any edge connecting 2 nodes that are already in same component.
