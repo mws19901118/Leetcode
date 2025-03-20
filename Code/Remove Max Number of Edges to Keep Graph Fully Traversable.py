7a73fabe4104301dbdf38f55c0d7d126a05f5aee
Junsheng Wang <mws19901118@gmail.com>
2025-03-19 22:59:35 -0700

    Update Remove Max Number of Edges to Keep Graph Fully Traversable.py

--- Code/Remove Max Number of Edges to Keep Graph Fully Traversable.py
+++ Code/Remove Max Number of Edges to Keep Graph Fully Traversable.py
@@ -1,7 +1,7 @@
-class UnionFind:                                                                                                                                                                            #Union find.
+class UnionFind:                                                                                                                                                                            #Union-Find class.
     def __init__(self, x: int):
-        self.label = x
         self.parent = None
+        self.label = x
 
     def find(self) -> 'UnionFind':
         if not self.parent:
@@ -9,16 +9,16 @@ class UnionFind:                                                                
         self.parent = self.parent.find()
         return self.parent
 
-    def union(self, uf: 'UnionFind') -> bool:                                                                                                                                                 #If 2 union finds are not unioned already, union them and return true; otherwise, return false.
-        if not self.find().label == uf.find().label:
-            self.find().parent = uf.find()
-            return True
-        return False
+    def union(self, uf: 'UnionFind') -> bool:
+        if self.find().label == uf.find().label:
+            return False
+        self.find().parent = uf.find()
+        return True
 
 class Solution:
     def maxNumEdgesToRemove(self, n: int, edges: List[List[int]]) -> int:
-        ufs1, ufs2 = [UnionFind(i) for i in range(n)], [UnionFind(i) for i in range(n)]                                                                                                     #Create union find objects for alice and bob separately for each node.
+        ufs1, ufs2 = [UnionFind(i) for i in range(n)], [UnionFind(i) for i in range(n)]                                                                                                     #Create Fnion-Find objects for alice and bob separately for each node.
         type1, type2, type3 = [(e[1], e[2]) for e in edges if e[0] == 1], [(e[1], e[2]) for e in edges if e[0] == 2], [(e[1], e[2]) for e in edges if e[0] == 3]                            #Group edges by type.
-        result = sum(int(int(ufs1[x - 1].union(ufs1[y - 1])) + int(ufs2[x - 1].union(ufs2[y - 1])) == 0) for x, y in type3)                                                                 #Traverse type3 edges first, try yo union nodes for both alice and bob; if both alice nodes and bob nodes don't need union, increase count as current edge is not needed.
+        result = sum(int(int(ufs1[x - 1].union(ufs1[y - 1])) + int(ufs2[x - 1].union(ufs2[y - 1])) == 0) for x, y in type3)                                                                 #Traverse type3 edges first, try to union nodes for both alice and bob; if both alice nodes and bob nodes don't need union, increase count as current edge is not needed.
         result += sum(int(not ufs1[x - 1].union(ufs1[y - 1])) for x, y in type1) + sum(int(not ufs2[x - 1].union(ufs2[y - 1])) for x, y in type2)                                           #Traverse type1 edges and type2 edges do the same process for alice nodes and bob nodes respectively and update result.
         return -1 if (len(set([x.find().label for x in ufs1])) > 1 or len(set([x.find().label for x in ufs2])) > 1) else result                                                             #If either alice nodes or bob nodes has more than one connected component, return -1 because graph cannot be fully traversed by both; otherwise return result. 
