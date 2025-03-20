d501ac9d85c5bbad102fefb421f043ed580c2153
Junsheng Wang <mws19901118@gmail.com>
2025-03-19 22:53:33 -0700

    Update Redundant Connection.py

--- Code/Redundant Connection.py
+++ Code/Redundant Connection.py
@@ -1,15 +1,15 @@
-class UnionFind:                                                                    #Union Find class.
-    def __init__(self, x: int):                                                     #Initialize.
-        self.label = x
+class UnionFind:                                                                    #Union-Find class.
+    def __init__(self, x: int):
         self.parent = None
+        self.label = x
 
-    def find(self) -> 'UnionFind':                                                  #Find and update parent.
+    def find(self) -> 'UnionFind':
         if not self.parent:
             return self
         self.parent = self.parent.find()
         return self.parent
 
-    def union(self, uf: 'UnionFind') -> bool:                                       #Union with another union find and return if they are in different compoment before union.
+    def union(self, uf: 'UnionFind') -> bool:
         if self.find().label == uf.find().label:
             return True
         self.find().parent = uf.find()
@@ -17,7 +17,7 @@ class UnionFind:                                                                
         
 class Solution:
     def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
-        ufs = [UnionFind(i + 1) for i in range(len(edges))]                         #Create a union find for each node.
+        ufs = [UnionFind(i + 1) for i in range(len(edges))]                         #Create a Union-Find for each node.
         result = []                                                                 #Initialize result.
         for x, y in edges:                                                          #Traverse edges.
             if ufs[x - 1].union(ufs[y - 1]):                                        #If union ufs[x - 1] and ufs[y - 1] and update result to [x, y] if they already are in same component.
