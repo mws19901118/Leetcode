c34d28b866f8e062db248ab469a514842264219b
Junsheng Wang <mws19901118@gmail.com>
2025-03-19 22:42:06 -0700

    Update Number of Islands II.py

--- Code/Number of Islands II.py
+++ Code/Number of Islands II.py
@@ -1,18 +1,18 @@
-class UnionFind:                                                                                                            #Union find.
-    def __init__(self, label: int) -> None:
-        self.label = label
+class UnionFind:                                                                                                            #Union-Find class.
+    def __init__(self, x: int):
         self.parent = None
-    
-    def find(self) -> 'UnionFind':                                                                                          #Find the parent of current node. 
+        self.label = x
+
+    def find(self) -> 'UnionFind':
         if not self.parent:
             return self
         self.parent = self.parent.find()
         return self.parent
 
-    def union(self, uf: 'UnionFind') -> bool:                                                                               #Union current node with another node, and return whether the union is needed.
-        if self.find().label != uf.find().label:
-            self.find().parent = uf.find()
+    def union(self, uf: 'UnionFind') -> bool:
+        if self.find().label == uf.find().label:
             return True
+        self.find().parent = uf.find()
         return False
         
 class Solution:
@@ -23,7 +23,7 @@ class Solution:
                 ufs[(i, j)] = UnionFind((i, j))                                                                             #Create a new UnionFind for (i, j).
                 count += 1                                                                                                  #Increase count.
                 for x, y in [(i - 1, j), (i, j + 1), (i + 1, j), (i, j - 1)]:                                               #Traverse all neighbors.
-                    if (x, y) in ufs and ufs[(i, j)].union(ufs[(x, y)]):                                                    #If neighbor is valid and is island and can be unioned with current island, decrease count.
+                    if (x, y) in ufs and not ufs[(i, j)].union(ufs[(x, y)]):                                                #If neighbor is valid and is island and can be unioned with current island, decrease count.
                         count -= 1
             result.append(count)                                                                                            #Append count to result.
         return result
