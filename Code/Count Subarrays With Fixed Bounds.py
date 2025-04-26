bbb005aafaa725e80c24a5ce3f04dd90b34c8f16
Junsheng Wang <mws19901118@gmail.com>
2025-04-25 20:05:06 -0700

    Update Count Subarrays With Fixed Bounds.py

--- Code/Count Subarrays With Fixed Bounds.py
+++ Code/Count Subarrays With Fixed Bounds.py
@@ -1,25 +1,21 @@
 class Solution:
     def countSubarrays(self, nums: List[int], minK: int, maxK: int) -> int:
-        count = 0                                                                       #Initialize count.
-        i = 0                                                                           #Initialize the left boundary of sliding window.
-        while i < len(nums):                                                            #Traverse nums.
-            if nums[i] < minK or nums[i] > maxK:                                        #If nums[i] is not within bound, move to next.
-                i += 1
-                continue
-            j = i                                                                       #Initialize the right boundary.
-            minK_q, maxK_q = deque(), deque()                                           #Initialize the indexes of minK and maxK within current window.
-            while j < len(nums) and minK <= nums[j] <= maxK:                            #Move the right boundary as far as possible.
-                if nums[j] == minK:                                                     #Push j to minK_q if nums[j] == minK.
-                    minK_q.append(j)
-                if nums[j] == maxK:                                                     #Push j to maxK_q if nums[j] == maxK.
-                    maxK_q.append(j)
+        def calculate(start: int, end: int) -> int:                    #Calculate the number of fixed-bound subarrays in nums[start:end + 1] assuming all numbers are within bounds.
+            count, last_min, last_max = 0, -1, -1                      #Initialize count and last index of minK and last index of maxK.  
+            for i in range(start, end + 1):                            #Traverse nums[start:end + 1].
+                if nums[i] == minK:                                    #Update last_min.
+                    last_min = i
+                if nums[i] == maxK:                                    #Update last_max.
+                    last_max = i
+                if last_min != -1 and last_max != -1:                  #If both minK and maxK is visited, all subarrays starting in nums[start:min(last_min, last_max) + 1] and ending at i are fixed-bound.
+                    count += min(last_min, last_max) - start + 1
+            return count
+
+        result, i = 0, 0                                                #Initialize result.
+        while i < len(nums):                                            #Traverse nums to find the segment that all numbers are within bounds.
+            j = i
+            while j < len(nums) and minK <= nums[j] <= maxK:
                 j += 1
-            while i < j and minK_q and maxK_q:                                          #Move left boundary while nums[i:j] is valid subarray with fixed bound.
-                count += j - max(minK_q[0], maxK_q[0])                                  #For each k in max(minK_q[0], maxK_q[0] to j - 1. nums[i:k + 1] is valid subarray with fixed bound.
-                if i == minK_q[0]:                                                      #If i == minK_q[0], pop left minK_q[0].
-                    minK_q.popleft()
-                if i == maxK_q[0]:                                                      #If i == maxK_q[0], pop left maxK_q[0].
-                    maxK_q.popleft()
-                i += 1
-            i = j                                                                       #Set i to j.
-        return count                                                                    #Return count.
+            result += calculate(i, j - 1)                               #Add the total fixed-bound subarrays for current segment to result.
+            i = max(j, i + 1)
+        return result
