8a381c1c124792beb66be74782e2ac13d4b4907d
Junsheng Wang <mws19901118@gmail.com>
2025-04-28 19:49:58 -0700

    Update Count Subarrays Where Max Element Appears at Least K Times.py

--- Code/Count Subarrays Where Max Element Appears at Least K Times.py
+++ Code/Count Subarrays Where Max Element Appears at Least K Times.py
@@ -1,13 +1,10 @@
 class Solution:
     def countSubarrays(self, nums: List[int], k: int) -> int:
-        max_element = max(nums)                        #Find the max element in nums.
-        count, start, result = 0, 0, 0                 #Initialize max element count in window, start of window and total result.
-        for i, x in enumerate(nums):                   #Traverse nums.
-            if x == max_element:                       #If x is max_element, increase count.
-                count += 1
-            while count == k:                          #Move forward start while count equals k to maintain a window which has less than k max elements.
-                if nums[start] == max_element:         #If nums[start] is max_element, decrease count.
-                    count -= 1
+        result, start, count, max_v = 0, 0, 0, max(nums)    #Initialize result, start of sliding window, count of max value also find the max value.
+        for i, x in enumerate(nums):                        #Traverse nums.
+            count += x == max_v                             #Update count.
+            while count >= k:                               #While count is greater than or equal to k, move forward start and update count.
+                count -= nums[start] == max_v
                 start += 1
-            result += start                            #For subarrays ending at i, the start of subarray has to be outside the window, so the number of valid subarraies is start.
+            result += start                                 #Now, for all subarrays starting in nums[:start] and ending at x will have at least k max value.
         return result
