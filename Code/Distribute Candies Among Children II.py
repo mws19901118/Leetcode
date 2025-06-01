5ff4a7a24f82a234734d65e19c188720c1f405ac
Junsheng Wang <mws19901118@gmail.com>
2025-06-01 14:05:01 -0700

    Update Distribute Candies Among Children II.py

--- Code/Distribute Candies Among Children II.py
+++ Code/Distribute Candies Among Children II.py
@@ -1,6 +1,6 @@
 class Solution:
     def distributeCandies(self, n: int, limit: int) -> int:
-        result = comb(n + 2, 2)                                  #Initialize result to be the number of ways to distribute without limit.
+        result = comb(n + 2, 2)                                  #Initialize result to be the number of ways to distribute without limit. Suppose there are x candies, to distribute them to 3 children, the number of ways is (x + 1) * (x + 2) // 2. Because there are x - 1 gaps initially(including 2 edges), we can place a board in any gap, now there are x + 2 gaps. Place another board in any gap now, but divide by 2 because 2 boards are equal.
         if n - (limit + 1) + 2 >= 0:                             #Deduct the number of ways to distribute with at least one child over limit.
             result -= 3 * comb(n - (limit + 1) + 2, 2)           #To calculate that, assign limit + 1 to one child, then distribute the rest then multiply by 3(3 choices to choose over limit children).
         if n - (limit + 1) * 2 + 2 >= 0:                         #Add back the number of ways to distribute with at least two children over limit, because they are deducted twice.
