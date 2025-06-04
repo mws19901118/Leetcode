4263f4b5e5e36ab28bb0a4d05e4da8abec5c1760
Junsheng Wang <mws19901118@gmail.com>
2025-06-04 16:59:58 -0700

    Update Find the Lexicographically Largest String From the Box I.py

--- Code/Find the Lexicographically Largest String From the Box I.py
+++ Code/Find the Lexicographically Largest String From the Box I.py
@@ -7,9 +7,9 @@ class Solution:
                 while j + k < n and word[i + k] == word[j + k]:         #Find the smallest k such that word[j + k] is valid and does not equal to word[i + k].
                     k += 1
                 if j + k < n and word[i + k] < word[j + k]:             #If such k exists and word[i + k] < word[j + k], suffix word[j:] will be larger than suffix word[i:].
-                    i, j = j, max(j + 1, i + k + 1)                     #So, move i to j and move j to the max of j + 1 and i + k + 1.
+                    i, j = j, max(j + 1, i + k + 1)                     #So, move i to j and move j to the max of j + 1 and i + k + 1. Because for any index x between i and i + k + 1(exclusive), word[x] won't be greater than word[i] and word[x:i + k + 1] won't be greater than the substring with same length and starting at j. But, i + k + 1 might be smaller than j + 1, so we take the greater.
                 else:                                                   #Otherwise, move j to j + k + 1.
-                    j = j + k + 1                                       #Because for any index x before that, word[x:j + k + 1] won't be greater than the substring with same length and starting at i.
+                    j = j + k + 1                                       #Because for any index x before that, word[x] won't be greater than word[i] and word[x:j + k + 1] won't be greater than the substring with same length and starting at i.
             return word[i:]                                             #Return word[i:].
 
         if numFriends == 1:                                             #If numFriends is 1, word cannot be split, so return word itself.
