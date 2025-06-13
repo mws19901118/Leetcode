cce2f4099d351fe54b312272ab9e7ab7d076a5c9
Junsheng Wang <mws19901118@gmail.com>
2025-06-12 20:15:38 -0700

    Update Minimize the Maximum Difference of Pairs.py

--- Code/Minimize the Maximum Difference of Pairs.py
+++ Code/Minimize the Maximum Difference of Pairs.py
@@ -1,7 +1,9 @@
 class Solution:
     def minimizeMax(self, nums: List[int], p: int) -> int:
-        @cache                                                                #Binary searcg
-        def countPairs(threshold: int):                                       #Suppose nums is sorted ascendingly, count the max distinct adjacent pairs whose difference is smaller than or equal to given threshold.
+        @cache                                                                #Cache result.
+        def countPairs(threshold: int) -> bool:                               #Suppose nums is sorted ascendingly, count the max distinct adjacent pairs whose difference is smaller than or equal to given threshold.
+            if threshold < 0:
+                return False
             count, i = 0, 0
             while i < len(nums) - 1:                                          #Traverse nums.
                 if nums[i + 1] - nums[i] <= threshold:                        #If nums[i + 1] - nums[i] <= threshold, increase count.
@@ -9,17 +11,15 @@ class Solution:
                     i += 2                                                    #Because if not taking (i, i + 1), nums[i + 1:] should produces more pairs than nums[i:] but nums[i + 1:] is shorter so it is impossible.
                 else:
                     i += 1
-            return count
+            return count >= p
 
-        if not p:                                                             #If p is 0, directly return 0.
-            return 0
         nums.sort()                                                           #Sort nums.
         start, end = 0, nums[-1] - nums[0]
         while start <= end:                                                   #Binary search the min threshold and return when found.
             mid = (start + end) // 2
-            if countPairs(mid) >= p and countPairs(mid - 1) < p:
+            if countPairs(mid) and not countPairs(mid - 1):
                 return mid
-            elif countPairs(mid) < p:
+            elif not countPairs(mid):
                 start = mid + 1
             else:
                 end = mid - 1
