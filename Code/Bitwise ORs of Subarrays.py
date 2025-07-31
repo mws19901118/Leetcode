8c8f49155c5b659ca02bcccde2cecf99d5320705
Junsheng Wang <mws19901118@gmail.com>
2025-07-31 09:15:34 -0700

    Update Bitwise ORs of Subarrays.py

--- Code/Bitwise ORs of Subarrays.py
+++ Code/Bitwise ORs of Subarrays.py
@@ -3,6 +3,6 @@ class Solution:
         result = set()                                        #Store all bitwise OR results.
         current_or = {0}                                      #Store the bitwise OR results of subarrays ending at current index; initially it only has 0.
         for x in arr:                                         #Traverse arr.
-            current_or = {x | y for y in current_or} | {x}    #Update current_or by applying bitwise OR operations on each number in current_or and x. Also add x to it if it is not already there.
+            current_or = {x | y for y in current_or} | {x}    #Update current_or by applying bitwise OR operations on each number in current_or and x. Also add x to it if it is not already there. Because bitwise OR operation only increase the number by adding more 1s to the binary so the max size of current_or is 32.
             result |= current_or                              #Union result with current_or.
         return len(result)                                    #Return the length of result
