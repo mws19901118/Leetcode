1755958494e0a976f64f9790e78021a43c458ead
Junsheng Wang <mws19901118@gmail.com>
2025-08-14 17:50:53 -0700

    Update Power of Four.py

--- Code/Power of Four.py
+++ Code/Power of Four.py
@@ -1,4 +1,3 @@
 class Solution:
-    def isPowerOfFour(self, num: int) -> bool:
-        binary = format(num, 'b')                                                                                   #Convert to binary.
-        return len(binary) % 2 == 1 and binary[0] == '1' and all(x == '0' for x in binary[1:])                      #Check the length of binary is odd and most signification bit is 1 and the rest of binary is all 0.
+    def isPowerOfFour(self, n: int) -> bool:
+        return n > 0 and not n & (n - 1) and len(format(n, 'b')) & 1 == 1        #Check if n is power of 2 and its binary has odd bits.
