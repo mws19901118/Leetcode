607cf629e1b65ba543083d5d7d7eec0e44a83e4b
Junsheng Wang <mws19901118@gmail.com>
2025-09-13 19:04:39 -0700

    Update Coin Change.py

--- Code/Coin Change.py
+++ Code/Coin Change.py
@@ -1,9 +1,7 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        dp = [-1] * (amount + 1)                                                                    #Store the least coins needed to get this value(from 0 to amount), initially -1.
-        dp[0] = 0                                                                                   #To get 0, you don't need any coins.
-        for c in coins:                                                                             #Traverse the coins list.
-            for i in range(c, amount + 1):                                                          #Then traverse the value from c to amount.
-                if dp[i - c] != -1:                                                                 #If dp[i - c] == -1, current value cannot be formed from i - c, so continue.
-                    dp[i] = dp[i - c] + 1 if dp[i] == -1 else min(dp[i], dp[i - c] + 1)             #Update dp[i] tp take the minimum of dp[i] and dp[i - c] + 1 if dp[i] != -1; otherwise directly set dp[i] to dp[i - c] + 1.
-        return dp[amount]                                                                           #Return the number of coins of amount.
+        dp = [0] + [inf] * amount                                #Initialize dp, starting with 0 at 0 and inf for the rest.
+        for x in coins:                                          #Traverse coins.
+            for i in range(amount + 1 - x):                      #Then traverse the value from 0 to amount + 1 - x.
+                dp[i + x] = min(dp[i + x], dp[i] + 1)            #Update dp[i + x] to take the minimum of dp[i + x] and dp[i] + 1.
+        return dp[amount] if dp[amount] < inf else -1            #Return dp[amount] if it is not inf; otherwise, return -1.
