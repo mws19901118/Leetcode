560e78f54d7e7afd52027f115bacb4bf911935d7
Junsheng Wang <mws19901118@gmail.com>
2025-09-23 20:18:54 -0700

    Update Fraction to Recurring Decimal.py

--- Code/Fraction to Recurring Decimal.py
+++ Code/Fraction to Recurring Decimal.py
@@ -1,26 +1,20 @@
 class Solution:
-    # @return a string
-    def fractionToDecimal(self, numerator, denominator):
-        ans=''
-        if numerator*denominator<0:                                 #Deal with negative numbers.
-            numerator=abs(numerator)
-            denominator=abs(denominator)
-            ans='-'
-        if numerator%denominator==0:                                #Numerator can be divided with no remainder.
-            ans=ans+str(numerator/denominator)    
-        else:
-            ans=ans+str(numerator/denominator)   
-            remainder=numerator%denominator
-            r={}                                                    #This is the dictionary, of which the key is already existing remainder and the value is its index.
-            n=[]                                                    #This is the list recording the new numerator in sequence.
-            index=0
-            while remainder!=0 and r.has_key(remainder)==False:     #If current numerator can be divided with no remainder or we detect the recurring period, break.
-                r[remainder]=index
-                n.append(str(remainder*10/denominator))             #Long division method
-                remainder=remainder*10%denominator
-                index=index+1
-            if remainder==0:
-                ans=ans+'.'+''.join(n)                              #Join the list.
-            elif r.has_key(remainder):
-                ans=ans+'.'+''.join(n[:r[remainder]])+'('+''.join(n[r[remainder]:])+')'     #Join the list in two parts.
-        return ans
+    def fractionToDecimal(self, numerator: int, denominator: int) -> str:
+        flag = "-" if numerator / denominator < 0 else ""                                               #If the result is negative, add a minus flag.
+        numerator, denominator = abs(numerator), abs(denominator)                                       #Take the absolute value of numerator and denominator.
+        quotient, remain = divmod(numerator, denominator)                                               #Calculate the initial integer quotient and remain.
+        if not remain:                                                                                  #If no remain, just return the integer quotient as string with flag.
+            return flag + str(quotient)
+        stack = []                                                                                      #Store decimal digits in a list.
+        visited = defaultdict(int)                                                                      #Store the corresponding digits index in stack for each remain.
+        while remain and remain not in visited:                                                         #Iterate while remain is not 0 and not visited.
+            digit, newRemain = divmod(remain * 10, denominator)                                         #Calculate the current digit for quotient and new remain after divide remain * 10 by denominator.
+            visited[remain] = len(stack)                                                                #Set the index in visited for remain.
+            stack.append(str(digit))                                                                    #Append digit as string to stack.
+            remain = newRemain                                                                          #Update remain with new remain.
+        decimal = ""                                                                                    #Initailize decimal string.
+        if not remain:                                                                                  #If no remain, just join the stack.
+            decimal = "".join(stack)
+        else:                                                                                           #Otherwise, join the stack before the index of remain, add left parentheses, join the rest of stack and add right parentheses.
+            decimal = "".join(stack[:visited[remain]]) + "(" + "".join(stack[visited[remain]:]) + ")"
+        return flag + str(quotient) + "." + decimal                                                     #Return the concatenation of flag, quotient as string, dot and decimal.
