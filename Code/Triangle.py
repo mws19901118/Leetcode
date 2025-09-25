c37205cb64bfcc4ff94e51a7543d847b0996112a
Junsheng Wang <mws19901118@gmail.com>
2025-09-24 19:34:37 -0700

    Update Triangle.py

--- Code/Triangle.py
+++ Code/Triangle.py
@@ -1,7 +1,6 @@
 class Solution:
     def minimumTotal(self, triangle: List[List[int]]) -> int:
-        level = triangle[0]                                                                                                                                                             #Record the minimum path sum at current row, initially it's the first row of triangle.
-        for i in range(1, len(triangle)):                                                                                                                                               #Traverse triangle by level.
-            nextLevel = [level[0] + triangle[i][0]] + [triangle[i][j] + min(level[j - 1], level[j]) for j in range(1, len(triangle[i]) - 1)] + [level[-1] + triangle[i][-1]]            #Updatet the minimal path sum at next level.
-            level = newLevel                                                                                                                                                            #Replace level with nextLevel.
-        return min(level)                                                                                                                                                               #Return minumum number in level.
+        dp = triangle[0]                                                                                                     #Store the minimum path sum at current row, initially it's the first row of triangle.
+        for r in triangle[1:]:                                                                                               #Traverse triangle by level starting from 2nd level.
+            dp = [dp[0] + r[0]] + [r[i] + min(dp[i - 1], dp[i]) for i in range(1, len(r) - 1)] + [dp[-1] + r[-1]]            #Calculate the minimal path sum at current level and updae dp.
+        return min(dp)                                                                                                       #Return minimum number in dp.
