f95a8eca4ebafa8307d522969de6ffe665eaf445
Junsheng Wang <mws19901118@gmail.com>
2025-09-26 22:27:40 -0700

    Update Largest Triangle Area.py

--- Code/Largest Triangle Area.py
+++ Code/Largest Triangle Area.py
@@ -1,12 +1,5 @@
-import itertools                                                                                        #Import itertools to get combinations from a list.
-class Solution(object):
-    def calculateArea(self, p0, p1, p2):                                                                #Calculete triangle area based on coordinates of each point.
-        area = 0.5 * abs(p0[0] * (p1[1] - p2[1]) + p1[0] * (p2[1] - p0[1]) + p2[0] * (p0[1] - p1[1]))
-        return area
-    
-    def largestTriangleArea(self, points):
-        """
-        :type points: List[List[int]]
-        :rtype: float
-        """
-        return max(self.calculateArea(*triangle) for triangle in itertools.combinations(points, 3))     #Enumerate every triagnle and return the max.
+class Solution:
+    def largestTriangleArea(self, points: List[List[int]]) -> float:
+        def calculateArea(p0: List[int], p1: List[int], p2: List[int]) -> float:                                    #Calculete triangle area based on coordinates of each point.
+            return 0.5 * abs(p0[0] * (p1[1] - p2[1]) + p1[0] * (p2[1] - p0[1]) + p2[0] * (p0[1] - p1[1]))
+        return max(calculateArea(*triangle) for triangle in itertools.combinations(points, 3))                      #Enumerate every triagnle and return the max.
