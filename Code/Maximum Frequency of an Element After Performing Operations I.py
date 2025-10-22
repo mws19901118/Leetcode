46e3993d2888e49e5261bfcf8cbb063510ba0c1c
Junsheng Wang <mws19901118@gmail.com>
2025-10-22 00:10:56 -0700

    Update Maximum Frequency of an Element After Performing Operations I.py

--- Code/Maximum Frequency of an Element After Performing Operations I.py
+++ Code/Maximum Frequency of an Element After Performing Operations I.py
@@ -11,5 +11,5 @@ class Solution:
         for i in range(n + 1):                                                  #Traverse from 0 to n.
             left = prefixSum[i] - prefixSum[max(0, i - k)]                      #Find the total numbers in the range max(0, i - k) to i - 1.
             right = prefixSum[min(n + 1, i + k + 1)] - prefixSum[i + 1]         #Find the total numbers in the range of i + 1 to min(i + k, n).
-            result = max(result, count[i] + min(numOperations, left + right))   #Since the operation has a limit, we can update total min(numOperations, left + right) numbers to i. Thus update result if count[i] + min(numOperations, left + right) is larger.
+            result = max(result, count[i] + min(numOperations, left + right))   #Since the operation has a limit, we can update at most min(numOperations, left + right) numbers to i. Thus update result if count[i] + min(numOperations, left + right) is larger.
         return result
