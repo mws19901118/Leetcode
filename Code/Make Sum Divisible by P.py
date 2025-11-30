d7968cbae49c2a40cd438d6509e23968e3346d8e
Junsheng Wang <mws19901118@gmail.com>
2025-11-29 22:09:47 -0800

    Update Make Sum Divisible by P.py

--- Code/Make Sum Divisible by P.py
+++ Code/Make Sum Divisible by P.py
@@ -1,15 +1,13 @@
 class Solution:
     def minSubarray(self, nums: List[int], p: int) -> int:
-        prefixRemainder = [0]                                            #Store the prefix sum remainder by p.
-        for i, x in enumerate(nums):
-            prefixRemainder.append((x + prefixRemainder[-1]) % p)
-        if not prefixRemainder[-1]:                                      #If the last prefix sum remainder is 0, then the sum of nums is divisible by p and no need to remove any subarray.
+        prefixSum, result, remainder = 0, len(nums), sum(nums) % p      #Initialize prefix sum, result and calculate the remainder of overall sum divided by p.
+        if not remainder:                                               #If no remainder, directly return 0.
             return 0
-        rightmostIndex = defaultdict(lambda: -1)                         #Store the rightmost index of each prefix sum remainder.
-        result = len(nums)                                               #Initialize the result to be length of nums.
-        for i, x in enumerate(prefixRemainder):                          #Traverse prefix sum remainders.
-            rightmostIndex[x] = i                                        #Update the rightmost index of current prefix sum remainder.
-            start = (x - prefixRemainder[-1]) % p                        #Calculate the start so that the sum of nums[rightmostIndex[start]:i] divide by p has remainder equals prefixRemainder[-1].
-            if rightmostIndex[start] != -1:                              #If rightmostIndex[start] is not -1, update result if i - rightmostIndex[start] is smaller.
-                result = min(result, i - rightmostIndex[start])
-        return -1 if result == len(nums) else result                     #Return -1 if result is still length of nums; otherwise, return result.
+        lastIndex = {0: -1}                                             #Initialize the last index i of remaidner x such that sum(nums[:i + 1]) % p == x; initially the lastIndex of 0 is -1.
+        for i, x in enumerate(nums):                                    #Traverse nums.
+            prefixSum += x                                              #Update prefix sum.
+            target = (prefixSum % p - remainder) % p                    #Calculate the target remainder, while is (prefixSum % p - remainder) % p.
+            if target in lastIndex:                                     #If it exists, let's say the index is j, then the sum of the rest of array, sum(nums) - (sum(nums[:i + 1]) - sum(nums[:j + 1]), can be divided by p.
+                result = min(result, i - lastIndex[target])             #So, we need the largest j to minimize i - j.
+            lastIndex[prefixSum % p] = i                                #Update the last index if prefixSum % p to i.
+        return result if result < len(nums) else -1                     #If result is smaller than len(nums), return it; otherwise return -1 because cannot remove the entire array,
