cb1a9c5cbcc324d923695dee90804ca1d316342b
Junsheng Wang <mws19901118@gmail.com>
2026-01-01 17:21:52 -0800

    Update N-Repeated Element in Size 2N Array.py

--- Code/N-Repeated Element in Size 2N Array.py
+++ Code/N-Repeated Element in Size 2N Array.py
@@ -1,11 +1,6 @@
-from collections import Counter
 class Solution:
-    def repeatedNTimes(self, A):
-        """
-        :type A: List[int]
-        :rtype: int
-        """
-        count = Counter(A)                      #Count each element.
-        for k in count:
-            if count[k] == int(len(A) / 2):     #Find the element repated N times.
-                return k
+    def repeatedNTimes(self, nums: List[int]) -> int:
+        count = Counter(nums)                        #Count each element.
+        for x in count:                              #Traverse count.
+            if count[x] == len(nums) // 2:           #Find the N-repeated element and return.
+                return x
