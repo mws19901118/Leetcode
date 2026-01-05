889013619f3f99fc87d28328a58bd6a161c2d090
Junsheng Wang <mws19901118@gmail.com>
2026-01-04 18:34:43 -0800

    Update Maximum Matrix Sum.py

--- Code/Maximum Matrix Sum.py
+++ Code/Maximum Matrix Sum.py
@@ -1,10 +1,10 @@
 class Solution:
     def maxMatrixSum(self, matrix: List[List[int]]) -> int:
-        m, n = len(matrix), len(matrix[0])                                  #Get the dimensions.
-        non_positive_count, abs_min, abs_sum = 0, inf, 0                    #Initialize the non-positive number count, minimum of abs of each number and sum of abs of each number.
-        for i, j in product(range(m), range(n)):                            #Traverse matrix.
-            non_positive_count += int(matrix[i][j] <= 0)                    #Update non_positive_count.
-            abs_min = min(abs_min, abs(matrix[i][j]))                       #Update abs_min.
-            abs_sum += abs(matrix[i][j])                                    #Update abs_sum.
-        return abs_sum - (abs_min if non_positive_count & 1 else 0) * 2     #Since we can move the non-positive sign inside matrix without modifying the count of non-negative numbers. So, just move all non-positive signs into adjacent areas.
-                                                                            #Next flip them: if the count of non-positive sign is even, we can flip all of them to non-negative and the result is abs_sum; otherwise, there is one left and desinate it to be the number of abs_min, so the result is abs_sum - abs_min * 2.
+        n = len(matrix)                                                 #Get the dimension.
+        nonPositiveCount, absMin, absSum = 0, inf, 0                    #Initialize the non-positive number count, minimum of abs of each number and sum of abs of each number.
+        for i, j in product(range(n), range(n)):                        #Traverse matrix.
+            nonPositiveCount += int(matrix[i][j] <= 0)                  #Update nonPositiveCount.
+            absMin = min(absMin, abs(matrix[i][j]))                     #Update absMin.
+            absSum += abs(matrix[i][j])                                 #Update absSum.
+        return absSum - (absMin if nonPositiveCount & 1 else 0) * 2     #Since we can move the non-positive sign inside matrix without modifying the count of non-negative numbers. So, just move all non-positive signs into adjacent areas.
+                                                                        #Next flip them: if the count of non-positive sign is even, we can flip all of them to non-negative and the result is absSum; otherwise, there is one left and desinate it to be the number of absMin, so the result is absSum - absMin * 2.
