fdcef35e9b8ec0236c8119f1f4492e6c6454faf0
Junsheng Wang <mws19901118@gmail.com>
2026-01-09 22:17:34 -0800

    Update Minimum ASCII Delete Sum for Two Strings.py

--- Code/Minimum ASCII Delete Sum for Two Strings.py
+++ Code/Minimum ASCII Delete Sum for Two Strings.py
@@ -5,12 +5,12 @@ class Solution:
         for i, j in product(range(m + 1), range(n + 1)):                                           #Traverse s1 and s2.
             if not i and not j:                                                                    #If i == 0 and j == 0, s1[:i] and s2[:j] are 2 empty strings, so no action needed.
                 continue
-            elif not i and j:                                                                      #If i == 0 and j > 0, s1[:i] is empty, to make them equal, delete all character in s2[:j].
-                dp[0][j] = dp[0][j - 1] + ord(s2[j - 1])
-            elif i and not j:                                                                      #If i > 0 and j == 0, s3[:j] is empty, to make them equal, delete all character in s1[:i].
-                dp[i][0] = dp[i - 1][0] + ord(s1[i - 1])
+            elif not i:                                                                            #If i == 0 and j > 0, s1[:i] is empty, to make them equal, delete all character in s2[:j].
+                dp[i][j] = dp[i][j - 1] + ord(s2[j - 1])
+            elif not j:                                                                            #If i > 0 and j == 0, s3[:j] is empty, to make them equal, delete all character in s1[:i].
+                dp[i][j] = dp[i - 1][j] + ord(s1[i - 1])
             elif s1[i - 1] != s2[j - 1]:                                                           #If s1[i - 1] != s2[j - 1], we have to pick the min sum of deleting either s1[i - 1] or s2[i - 2].
                 dp[i][j] = min(dp[i - 1][j] + ord(s1[i - 1]),  dp[i][j - 1] + ord(s2[j - 1]))
             else:                                                                                  #If s1[i - 1] == s2[j - 1], we don't need to delete, dp[i][j] is same as dp[i - 1][j - 1].
                 dp[i][j] = dp[i - 1][j - 1]
-        return dp[m][n]                                                                            #Return dp[m][n].
+        return dp[-1][-1]                                                                          #Return dp[-1][-1].
