410740565a4e5a62c4fc540bd1a3489b70585781
Junsheng Wang <mws19901118@gmail.com>
2026-01-25 16:16:38 -0800

    Update Minimum Absolute Difference.py

--- Code/Minimum Absolute Difference.py
+++ Code/Minimum Absolute Difference.py
@@ -1,12 +1,5 @@
 class Solution:
     def minimumAbsDifference(self, arr: List[int]) -> List[List[int]]:
-        arr.sort()                                        #Sort arr.
-        result, minAbs = [], 200001                       #Initialize result and minimum absolute difference.
-        for i in range(len(arr) - 1):                     #Traverse arr.
-            diff = arr[i + 1] - arr[i]                    #Compute the absolute difference between current pair.
-            if diff < minAbs:                             #If diff < minAbs, update minAbs and clear result.
-                minAbs = diff
-                result.clear()
-            if diff == minAbs:                            #If diff == minAbs, append current pair to result.
-                result.append([arr[i], arr[i + 1]])
-        return result
+        arr.sort()                                                                                            #Sort arr.
+        minDifference = min(arr[i + 1] - arr[i] for i in range(len(arr) - 1))                                 #Calculate the min difference.
+        return [[arr[i], arr[i + 1]] for i in range(len(arr) - 1) if arr[i + 1] - arr[i] == minDifference]    #Return all pairs with min difference.
