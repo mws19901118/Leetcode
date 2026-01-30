14c563e6242f710f13ed2fa78a021fc1c493b77f
Junsheng Wang <mws19901118@gmail.com>
2026-01-29 20:10:38 -0800

    Update Minimum Cost to Convert String I.py

--- Code/Minimum Cost to Convert String I.py
+++ Code/Minimum Cost to Convert String I.py
@@ -6,7 +6,6 @@ class Solution:
         for x, y, c in zip(original, changed, cost):
             matrix[ord(x) - ord('a')][ord(y) - ord('a')] = min(c, matrix[ord(x) - ord('a')][ord(y) - ord('a')])           #Since there could be multiple costs for same pair, take the smallest one.
         for k, i, j in product(range(26), range(26), range(26)):                                                          #Use floyd-warshall algorithm to precompute the cost from one character to another.
-            if matrix[i][j] > matrix[i][k] + matrix[k][j]:
-                matrix[i][j] = matrix[i][k] + matrix[k][j]
+            matrix[i][j] = min(matrix[i][j], matrix[i][k] + matrix[k][j])
         result = sum(matrix[ord(x) - ord('a')][ord(y) - ord('a')] for x, y in zip(source, target))                        #Sum up cost.
         return result if result < inf else -1                                                                             #Return result if it is not inf; otherwise, return -1.
