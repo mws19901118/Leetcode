e55fa8f03bbec1cfe977cb4507d650e630d92fa5
Junsheng Wang <mws19901118@gmail.com>
2026-03-05 07:39:09 -0800

    Update Minimum Changes To Make Alternating Binary String.py

--- Code/Minimum Changes To Make Alternating Binary String.py
+++ Code/Minimum Changes To Make Alternating Binary String.py
@@ -1,10 +1,3 @@
 class Solution:
     def minOperations(self, s: str) -> int:
-        result = len(s)                        #Initialize result.
-        for initial in [0, 1]:                 #Check the case of starting with 0 and starting with 1.
-            curr, count = initial, 0           #Store initial value and count.
-            for x in s:                        #Traverse s.
-                count += (int(x) == curr)      #Increase count if int(x) is not curr.
-                curr ^= 1                      #XOR curr with 1.
-            result = min(result, count)        #Update result if necessary.
-        return result
+        return min(sum(int(x) ^ b ^ (i & 1) for i, x in enumerate(s)) for b in [0, 1])    #Try both starting with 0 and 1 then take the smaller result.
