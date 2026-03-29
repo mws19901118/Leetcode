13247cf9622ed206549e9908a7e6e3666189e152
Junsheng Wang <mws19901118@gmail.com>
2026-03-28 22:52:26 -0700

    Create Find the String with LCP.py

---
+++ Code/Find the String with LCP.py
@@ -0,0 +1,24 @@
+class Solution:
+    def findTheString(self, lcp: List[List[int]]) -> str:
+        n = len(lcp)                                                                                                  #Get the dimension.
+        if any(lcp[i][i] != n - i for i in range(n)):                                                                 #Validate the lcp on the diagonal, since it is the lcp of 2 identical string so the value should be the length of rest string.
+            return ""
+        if any(any(lcp[i][j] != lcp[j][i] or lcp[i][j] > n - j for j in range(i + 1, n)) for i in range(n - 1)):      #Traverse the half above diagonal and validate the value should equal to the value at diagonally symmetrical position and not greater than the length of rest string.
+            return ""
+        s = []                                                                                                        #Initialize result list.
+        for i in range(n):                                                                                            #Traverse n to construct s.
+            used = [False] * 26                                                                                       #Store if any letter is used that current position cannot use.
+            for j in range(i):                                                                                        #Traverse from 0 to i - 1.
+                if not lcp[j][i]:                                                                                     #If lcp[j][i] is 0, s[j] is used.
+                    used[ord(s[j]) - ord('a')] = True
+            if False not in used:                                                                                     #If all letters are used, return "" as not letter can be assigned to current position.
+                return ""
+            s.append(chr(ord('a') + used.index(False)))                                                               #Find the smallest unused character and append it to s.
+        for i in range(n - 1):
+            for j in range(i + 1, n):
+                for k in range(lcp[i][j]):
+                    if s[j + k] != s[i + k]:
+                        return ""
+                if j + lcp[i][j] < n and s[j + lcp[i][j]] == s[i + lcp[i][j]]:
+                    return ""
+        return "".join(s)                                                                                             #Join s and return.
