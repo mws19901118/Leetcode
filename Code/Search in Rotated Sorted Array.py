bd733515dce00c4d5e28477a0bbc9cd3d884795b
Junsheng Wang <mws19901118@gmail.com>
2026-05-22 00:25:25 -0700

    Update Search in Rotated Sorted Array.py

--- Code/Search in Rotated Sorted Array.py
+++ Code/Search in Rotated Sorted Array.py
@@ -3,10 +3,10 @@ class Solution:
         start, end = 0, len(nums) - 1
         while start <= end:
             mid = (start + end) // 2
-            if nums[mid] == target:                                                                                        #If found target, return mid.
+            if nums[mid] == target:                                                                                                 #If found target, return mid.
                 return mid
-            if nums[0] <= nums[mid] < target or nums[mid] < target <= nums[-1] or target < nums[0] <= nums[mid]:           #1. If nums[mid] is smaller than target and both nums[mid] and target is on same slope, target is in the second half(between mid and end).
-                start = mid + 1                                                                                            #2. If nums[mid] is greter than target and nums[mid] is before pivot and target is after pivot, target is in the second half(between mid and end).
-            else:                                                                                                          #Otherwise, target is in the first half(between start and mid).
+            if nums[start] <= nums[mid] < target or nums[mid] < target <= nums[end] or target < nums[start] <= nums[mid]:           #1. If nums[mid] is smaller than target and both nums[mid] and target is on same slope, target is in the second half(between mid and end).
+                start = mid + 1                                                                                                     #2. If nums[mid] is greter than target and nums[mid] is before pivot and target is after pivot, target is in the second half(between mid and end).
+            else:                                                                                                                   #Otherwise, target is in the first half(between start and mid).
                 end = mid - 1
-        return -1                                                                                                          #If can not find target, return -1.
+        return -1                                                                                                                   #If can not find target, return -1.
