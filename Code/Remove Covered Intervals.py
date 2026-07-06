dd521f9c1333c88735f3012f273e1b558f77df40
Junsheng Wang <mws19901118@gmail.com>
2026-07-05 23:17:19 -0700

    Update Remove Covered Intervals.py

--- Code/Remove Covered Intervals.py
+++ Code/Remove Covered Intervals.py
@@ -1,12 +1,6 @@
 class Solution:
     def removeCoveredIntervals(self, intervals: List[List[int]]) -> int:
-        def cmp(a: List[int], b: List[int]) -> int:                                                 #Sort intervals by start asendingly then by end desendingly.
-            if a[0] != b[0]:
-                return a[0] - b[0]
-            else:
-                return b[1] - a[1]
-
-        intervals.sort(key = cmp_to_key(cmp))
+        intervals.sort(key = lambda x: (x[0], -x[1]))                                               #Sort intervals by start in ascending order then by end in descending order.
         last, count = 0, len(intervals)                                                             #Store the index of last uncovered interval, initially 0. Intialize count as the length of intervals.
         for i in range(1, len(intervals)):                                                          #Traverse starting from 1.
             if intervals[i][0] >= intervals[last][0] and intervals[i][1] <= intervals[last][1]:     #If current interval is covered by last uncovered interval, subtract count by 1.
