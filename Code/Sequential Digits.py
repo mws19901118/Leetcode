e5879e4c42e3e92b46fa038d873ecdae7143a7e2
Junsheng Wang <mws19901118@gmail.com>
2026-07-12 18:08:55 -0700

    Update Sequential Digits.py

--- Code/Sequential Digits.py
+++ Code/Sequential Digits.py
@@ -1,13 +1,11 @@
 class Solution:
     def sequentialDigits(self, low: int, high: int) -> List[int]:
-        result, digits = [], [1,2,3,4,5,6,7,8]                      #Initialize result and starting digits.
-        while digits and digits[0] <= high:                         #BFS until digits is empty or the smallest number in digits is greater than high.
-            newDigits = []                                          #Initialize new digits.
-            for x in digits:                                        #Traverse digits.
-                if low <= x <= high:                                #If x is within range, append it to result.
-                    result.append(x)
-                lastDigit = x % 10                                  #Get the last digit of x.
-                if lastDigit != 9:                                  #If x is not 9, append the next sequential digit to new digits.
-                    newDigits.append(x * 10 + lastDigit + 1)
-            digits = newDigits                                      #Replace digits with newDigits.
+        result = []
+        for length in range(len(str(low)), len(str(high)) + 1):    #Enumerate the possible length from low to high.
+            for i in range(1, 11 - length):                        #Enumerate the possible starting digit for each length.
+                num = i
+                for j in range(i + 1, i + length):                 #Generate number based on the length and starting digit.
+                    num = num * 10 + j
+                if low <= num <= high:                             #If it is within the range, append it to result.
+                    result.append(num)
         return result
